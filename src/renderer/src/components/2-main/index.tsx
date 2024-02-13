4ecf4b4be0254cfa1f801fa3d6150537c437073c
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-12 18:35:49 -0800

    WIP. web dev run. fix ui components location

--- src/renderer/src/components/2-main/index.tsx
+++ src/renderer/src/components/2-main/index.tsx
@@ -1,17 +1,17 @@
 import { Versions } from '../versions-list';
 import electronLogo from '../../assets/electron.svg';
+import { Button } from '@/ui';
 
 export function SectionMain() {
     const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
     return (
         <div>
-            Main
-
-            <div className="action">
+            <Button onClick={ipcHandle}>Send IPC</Button>
+            {/* <div className="action">
                 <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
                     Send IPC
                 </a>
-            </div>
+            </div> */}
 
             <Versions />
             <img alt="logo" className="logo" src={electronLogo} />
