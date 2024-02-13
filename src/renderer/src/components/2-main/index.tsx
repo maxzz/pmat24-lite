d0b942b74b1a54acf2941b32c272b69206617a25
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-12 19:56:29 -0800

    WIP. web dev run. update main section layout

--- src/renderer/src/components/2-main/index.tsx
+++ src/renderer/src/components/2-main/index.tsx
@@ -1,32 +1,19 @@
-import { useAtomValue } from 'jotai';
-import { filesContentAtom } from '@/store';
 import { Button } from '@/ui';
-import { Versions } from '../versions-list';
 import { DroppedFilesView } from './1-dropped-files-view';
-
-// function DroppedFiles() {
-//     const filesContent = useAtomValue(filesContentAtom);
-//     return (
-//         <div className="">
-//             Dropped files
-
-//             {filesContent.map((fileContent, idx) => (
-//                 <Card fileContent={fileContent} key={`${idx}=${fileContent.name}`} />
-//             ))}
-
-//         </div>
-//     );
-// }
+import { Versions } from '../versions-list';
 
 export function SectionMain() {
     const ipcHandle = (): void => window.electron.ipcRenderer.send('ping');
     return (
-        <div className="p-4">
+        <div className="p-4 flex flex-col">
+            <div className="flex-1">
+                <DroppedFilesView />
+            </div>
+
             <div className="flex items-center gap-2">
                 <Button variant="outline" onClick={ipcHandle}>Send IPC</Button>
                 <Versions />
             </div>
-            <DroppedFilesView />
         </div>
     );
 }
