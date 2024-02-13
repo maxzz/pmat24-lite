a92b52e55edcd682f0adb8e0b5f5d45a919e34c5
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-13 15:38:01 -0800

    WIP. refresh button and theme-switch

--- src/renderer/src/components/1-header/index.tsx
+++ src/renderer/src/components/1-header/index.tsx
@@ -2,7 +2,7 @@ import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
 
 export function SectionHeader() {
     return (
-        <div className="p-4">
+        <div className="p-4 flex items-center justify-between">
             SectionHeader
             <ThemeSwitch />
         </div>
