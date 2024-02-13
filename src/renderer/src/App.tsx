06aeb4d0ead23814be4cd64d72a87e723e562f5d
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-12 19:23:37 -0800

    WIP. web dev run. connect drag-n-drop for web

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,13 +1,20 @@
 import { SectionHeader } from "./components/1-header";
 import { SectionMain } from "./components/2-main";
 import { SectionFooter } from "./components/3-footer";
+import { UISymbolDefs } from "@ui/icons";
+import { DropItDoc } from "./ui";
+import { doDroppedFilesAtom } from "./store";
 
 export function App(): JSX.Element {
     return (<>
+        <UISymbolDefs />
+
         <div className="dark h-screen text-sm text-foreground bg-background grid grid-rows-[auto,1fr,auto]">
             <SectionHeader />
             <SectionMain />
             <SectionFooter />
         </div>
+
+        <DropItDoc doDroppedFilesAtom={doDroppedFilesAtom} />
     </>);
 }
--- src/renderer/src/ui/shadcn/index.tsx
+++ src/renderer/src/ui/shadcn/index.tsx
@@ -10,3 +10,5 @@ export * from "./switch";
 export * from "./textarea";
 export * from "./textarea-auto-grow";
 export * from "./tooltip";
+
+export * from "./ui-drop-it-doc";
