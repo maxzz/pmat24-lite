f8be8407d493c0c5e48bb377f24e930b56db284d
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-26 17:34:43 -0800

    WIP. add our preloader API

--- src/electron/preload/index.ts
+++ src/electron/preload/index.ts
@@ -1,8 +1,24 @@
-import { contextBridge } from 'electron';
+import { IpcRendererEvent, contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 
 // Custom APIs for renderer
-const api = {};
+const api: TmApi = {
+    callMain: (data: any): void => {
+        const channel: PreloadChannelNames = 'call-main';
+        ipcRenderer.send(channel, data);
+    },
+
+    invokeMain: (data: any): any => {
+        const channel: PreloadChannelNames = 'invoke-main';
+        return ipcRenderer.invoke(channel, data);
+    },
+
+    setCbCallFromMain: (callback: (event: IpcRendererEvent, data: any) => void) => {
+        const channel: PreloadChannelNames = 'send-to-renderer';
+        ipcRenderer.removeAllListeners(channel);
+        ipcRenderer.on(channel, callback);
+    },
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to renderer only if context isolation is enabled,
 // otherwise just add to the DOM global.
@@ -10,7 +26,7 @@ const api = {};
 if (process.contextIsolated) {
     try {
         contextBridge.exposeInMainWorld('electron', electronAPI);
-        contextBridge.exposeInMainWorld('api', api);
+        contextBridge.exposeInMainWorld('tmApi', api);
     } catch (error) {
         console.error(error);
     }
@@ -18,5 +34,5 @@ if (process.contextIsolated) {
     // @ts-ignore (define in dts)
     window.electron = electronAPI;
     // @ts-ignore (define in dts)
-    window.api = api;
+    window.tmApi = api;
 }
