66e0c9fc83b3fb15efad543aa62689ee8aee0346
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-26 18:13:16 -0800

    WIP. menu. add menubar, but orginal was broken

--- src/renderer/src/components/1-header/index.tsx
+++ src/renderer/src/components/1-header/index.tsx
@@ -1,9 +1,11 @@
 import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
+import { MenubarDemo } from "./1-menu";
 
 export function SectionHeader() {
     return (
         <div className="p-4 flex items-center justify-between">
             SectionHeader
+            <MenubarDemo />
             <ThemeSwitch />
         </div>
     );
