43c619791fd357948417d32b68e53ef4fdf0581f
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-26 18:46:20 -0800

    WIP. menu. menubar back to original w/ fix

--- src/renderer/src/ui/shadcn/menubar.tsx
+++ src/renderer/src/ui/shadcn/menubar.tsx
@@ -89,30 +89,6 @@ const MenubarSubContent = forwardRef<ElementRef<typeof Prim.SubContent>, Compone
 );
 MenubarSubContent.displayName = Prim.SubContent.displayName;
 
-const MenubarContentClasses2 = "\
-p-1 \
-min-w-[12rem] \
-z-50 \
-rounded-md \
-border \
-bg-popover \
-text-popover-foreground \
-shadow-md \
-\
-data-[state=open]:animate-in \
-data-[state=open]:fade-in-0 \
-data-[state=open]:zoom-in-95 \
-\
-data-[state=closed]:fade-out-0 \
-data-[state=closed]:zoom-out-95 \
-\
-data-[side=bottom]:slide-in-from-top-2 \
-data-[side=left]:slide-in-from-right-2 \
-data-[side=right]:slide-in-from-left-2 \
-data-[side=top]:slide-in-from-bottom-2 \
-\
-overflow-hidden \
-";
 const MenubarContentClasses = "\
 p-1 min-w-[12rem] z-50 \
 \
@@ -142,7 +118,7 @@ const MenubarContent = forwardRef<ElementRef<typeof Prim.Content>, ComponentProp
                 align={align}
                 alignOffset={alignOffset}
                 sideOffset={sideOffset}
-                className={cn(MenubarContentClasses2, className )}
+                className={cn(MenubarContentClasses, className)}
                 {...rest}
             />
         </Prim.Portal>
@@ -150,32 +126,39 @@ const MenubarContent = forwardRef<ElementRef<typeof Prim.Content>, ComponentProp
 );
 MenubarContent.displayName = Prim.Content.displayName;
 
+const MenubarItemClasses = "\
+relative px-2 py-1.5 text-sm \
+\
+focus:text-accent-foreground \
+focus:bg-accent \
+\
+data-[disabled]:opacity-50 \
+data-[disabled]:pointer-events-none \
+\
+rounded-sm select-none outline-none cursor-default \
+flex items-center";
 const MenubarItem = forwardRef<ElementRef<typeof Prim.Item>, ComponentPropsWithoutRef<typeof Prim.Item> & { inset?: boolean; }>(
     ({ className, inset, ...rest }, ref) => (
-        <Prim.Item
-            ref={ref}
-            className={cn(
-                "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-                inset && "pl-8",
-                className
-            )}
-            {...rest}
-        />
+        <Prim.Item ref={ref} className={cn(MenubarItemClasses, inset && "pl-8", className)} {...rest} />
     )
 );
 MenubarItem.displayName = Prim.Item.displayName;
 
+const MenubarCheckboxRadioItemClasses = "\
+relative pl-8 pr-2 py-1.5 text-sm \
+\
+focus:text-accent-foreground \
+focus:bg-accent \
+\
+data-[disabled]:opacity-50 \
+data-[disabled]:pointer-events-none \
+\
+rounded-sm outline-none select-none cursor-default \
+flex items-center";
+
 const MenubarCheckboxItem = forwardRef<ElementRef<typeof Prim.CheckboxItem>, ComponentPropsWithoutRef<typeof Prim.CheckboxItem>>(
     ({ className, children, checked, ...rest }, ref) => (
-        <Prim.CheckboxItem
-            ref={ref}
-            className={cn(
-                "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-                className
-            )}
-            checked={checked}
-            {...rest}
-        >
+        <Prim.CheckboxItem ref={ref} className={cn(MenubarCheckboxRadioItemClasses, className)} checked={checked} {...rest}>
             <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
                 <Prim.ItemIndicator>
                     <CheckIcon className="h-4 w-4" />
@@ -189,14 +172,7 @@ MenubarCheckboxItem.displayName = Prim.CheckboxItem.displayName;
 
 const MenubarRadioItem = forwardRef<ElementRef<typeof Prim.RadioItem>, ComponentPropsWithoutRef<typeof Prim.RadioItem>>(
     ({ className, children, ...rest }, ref) => (
-        <Prim.RadioItem
-            ref={ref}
-            className={cn(
-                "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-                className
-            )}
-            {...rest}
-        >
+        <Prim.RadioItem ref={ref} className={cn(MenubarCheckboxRadioItemClasses, className)} {...rest}>
             <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
                 <Prim.ItemIndicator>
                     <DotFilledIcon className="h-4 w-4 fill-current" />
@@ -210,32 +186,21 @@ MenubarRadioItem.displayName = Prim.RadioItem.displayName;
 
 const MenubarLabel = forwardRef<ElementRef<typeof Prim.Label>, ComponentPropsWithoutRef<typeof Prim.Label> & { inset?: boolean; }>(
     ({ className, inset, ...rest }, ref) => (
-        <Prim.Label
-            ref={ref}
-            className={cn(
-                "px-2 py-1.5 text-sm font-semibold",
-                inset && "pl-8",
-                className
-            )}
-            {...rest}
-        />
+        <Prim.Label ref={ref} className={cn("px-2 py-1.5 text-sm font-semibold", inset && "pl-8", className)} {...rest} />
     )
 );
 MenubarLabel.displayName = Prim.Label.displayName;
 
-const MenubarSeparator = forwardRef<ElementRef<typeof Prim.Separator>, ComponentPropsWithoutRef<typeof Prim.Separator>>(({ className, ...rest }, ref) => (
-    <Prim.Separator
-        ref={ref}
-        className={cn("-mx-1 my-1 h-px bg-muted", className)}
-        {...rest}
-    />
-)
+const MenubarSeparator = forwardRef<ElementRef<typeof Prim.Separator>, ComponentPropsWithoutRef<typeof Prim.Separator>>(
+    ({ className, ...rest }, ref) => (
+        <Prim.Separator ref={ref} className={cn("-mx-1 my-1 h-px bg-muted", className)} {...rest} />
+    )
 );
 MenubarSeparator.displayName = Prim.Separator.displayName;
 
 const MenubarShortcut = ({ className, ...rest }: HTMLAttributes<HTMLSpanElement>) => {
     return (
-        <span className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)}            {...rest} />
+        <span className={cn("ml-auto text-xs tracking-widest text-muted-foreground", className)} {...rest} />
     );
 };
 MenubarShortcut.displayname = "MenubarShortcut";
