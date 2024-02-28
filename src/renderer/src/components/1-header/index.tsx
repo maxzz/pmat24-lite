8bb9d4b7a6254a4aae0965d91e7507d1308e4c0a
Max Zakharzhevskiy <maxzz@msn.com>
2024-02-27 18:53:32 -0800

    WIP. menu. add dropdown menu

--- src/renderer/src/components/1-header/index.tsx
+++ src/renderer/src/components/1-header/index.tsx
@@ -1,10 +1,12 @@
 import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
 import { MenubarDemo } from "./1-menu";
+import { DropdownMenuDemo } from "./1-dropdown-menu";
 
 export function SectionHeader() {
     return (
         <div className="p-4 flex items-center justify-between">
             SectionHeader
+            <DropdownMenuDemo />
             <MenubarDemo />
             <ThemeSwitch />
         </div>
