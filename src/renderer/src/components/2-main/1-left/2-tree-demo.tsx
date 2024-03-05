b6e7da84ba7b68c86a74a971ec68526a3f1b123e
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-05 15:19:03 -0800

    WIP. tree. add tree demo data

---
+++ src/renderer/src/components/2-main/1-left/2-tree-demo.tsx
@@ -0,0 +1,78 @@
+import { useMemo, useState } from "react";
+import { proxy, useSnapshot } from "valtio";
+import { Tree, DataItemWState, DataItem, duplicateTree, findTreeItemById, walkItems } from "@ui/shadcn/tree";
+import { data } from "./1-tree-data";
+import { AppWindow as IconFile, Folder as IconFolder } from "lucide-react"; // Workflow as IconFile, File as IconFile
+import { inputFocusClasses } from "@ui/shared-styles";
+import { classNames } from "@/utils";
+// import { SimpleSplitPane } from "@ui/shadcn/split-pane";
+import { Checkbox } from "@ui/shadcn";
+import { appSettings } from "@/store";
+
+const initialItemId = "6.1.2";
+
+function addStateToTreeItems(data: DataItem[]): DataItemWState[] {
+    const newTree = duplicateTree(data);
+    walkItems(newTree, (item) => (item as DataItemWState).state = proxy({ selected: false }));
+    return newTree as DataItemWState[];
+}
+
+const dataWithState = addStateToTreeItems(data);
+
+export function DemoTreeWithOptions() {
+    const [content, setContent] = useState(() => {
+        const initialItem = findTreeItemById(dataWithState, initialItemId);
+        return initialItem?.name || "No content selected";
+    });
+
+    // const { arrowFirst: snapArrowFirst, hideFolderIcon: snapHideFolderIcon } = useSnapshot(appSettings.treeState);
+    const snapArrowFirst = false;
+    const snapHideFolderIcon = false;
+
+    const TreeMemo = useMemo(
+        () => {
+            return <Tree
+                data={dataWithState}
+                className={`w-full h-full border rounded-l-md ${inputFocusClasses}`}
+                initialSelectedItemId={initialItemId}
+                onSelectChange={(item) => setContent(item?.name ?? "")}
+                IconForFolder={IconFolder}
+                IconForItem={IconFile}
+                arrowFirst={snapArrowFirst}
+                hideFolderIcon={snapHideFolderIcon}
+            />;
+        }, [snapArrowFirst, snapHideFolderIcon]
+    );
+
+    return (
+        <div className="mr-12 p-0.5 w-full h-full">
+
+            {/* <SimpleSplitPane vertical={false} className="splitpane h-full"> */}
+                <div className="w-full">
+                    {TreeMemo}
+                </div>
+
+                {/* <div className={classNames("flex-1 w-full h-full min-w-0 border border-l rounded-r-md z-10", inputFocusClasses)} tabIndex={0}>
+                    <div className="min-w-0 overflow-hidden">
+                        <div className="px-2 py-1">
+                            {content}
+                        </div>
+                    </div>
+                </div>
+            </SimpleSplitPane> */}
+
+            {/* <div className="mt-2 text-sm text-muted-foreground">
+                <label className="flex items-center">
+                    <Checkbox className="mr-2" checked={snapArrowFirst} onCheckedChange={() => appSettings.treeState.arrowFirst = !appSettings.treeState.arrowFirst } />
+                    Icons first
+                </label>
+
+                <label className="flex items-center">
+                    <Checkbox className="mr-2" checked={snapHideFolderIcon} onCheckedChange={() => appSettings.treeState.hideFolderIcon = !appSettings.treeState.hideFolderIcon} />
+                    Hide folder icons
+                </label>
+            </div> */}
+
+        </div>
+    );
+}
--- src/renderer/src/components/2-main/1-left/index.tsx
+++ src/renderer/src/components/2-main/1-left/index.tsx
@@ -1,10 +1,12 @@
 import { LongPanel } from "../2-right/LongPanel";
+import { DemoTreeWithOptions } from "./2-tree-demo";
 
 export function PanelA() {
     return (
         <div className="m-1 h-full bg-muted rounded-md">
-            <span className="font-semibold">One</span>
+            {/* <span className="font-semibold">One</span> */}
             {/* <LongPanel /> */}
+            <DemoTreeWithOptions />
         </div>
     );
 }
