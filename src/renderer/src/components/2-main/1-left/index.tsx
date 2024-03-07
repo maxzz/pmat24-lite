7fe612b2482de62387d4698aece3ea9e7d0a2d0a
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-07 14:59:06 -0800

    WIP. panels. switch to themes border

--- src/renderer/src/components/2-main/1-left/index.tsx
+++ src/renderer/src/components/2-main/1-left/index.tsx
@@ -4,8 +4,8 @@ import { DemoTreeWithOptions } from "./2-tree-demo";
 
 export function PanelA() {
     return (
-        <div className="p-1 pr-0.5 h-full text-xs">
-            <div className={`h-full ring-border ring-1 rounded-l overflow-hidden ${inputFocusWithinClasses} focus-within:ring-offset-0`}>
+        <div className="p-1 pr-0 h-full text-xs">
+            <div className={`h-full ring-border/70 ring-1 rounded-l overflow-hidden ${inputFocusWithinClasses} focus-within:ring-offset-0`}>
                 <div className={`h-full bg-background flex flex-col`}>
                     <PanelHeader />
                     
--- src/renderer/src/components/2-main/2-right/index.tsx
+++ src/renderer/src/components/2-main/2-right/index.tsx
@@ -3,18 +3,16 @@ import { LongPanel } from "./LongPanel";
 import { PanelHeader } from "./0-header";
 
 export const inputFocusWithinClasses = "\
-focus-within::ring-primary-600 \
-dark:focus-within:ring-primary-400 \
-focus-within:ring-offset-primary-200 \
-dark:focus-within:ring-offset-primary-800 \
+focus-within:ring-border \
+focus-within:ring-offset-background \
 focus-within:ring-1 \
 focus-within:ring-offset-1 \
 focus-within:outline-none";
 
 export function PanelB() {
     return (
-        <div className={`p-1 pl-0.5 h-full text-xs`}>
-            <div className={`h-full ring-border ring-1 rounded-r overflow-hidden ${inputFocusWithinClasses} focus-within:ring-offset-0`}>
+        <div className={`p-1 pl-0 h-full text-xs`}>
+            <div className={`h-full ring-border/70 ring-1 rounded-r overflow-hidden ${inputFocusWithinClasses} focus-within:ring-offset-0`}>
                 <div className={`h-full bg-background flex flex-col`}>
                     <PanelHeader />
                     <div className="outline-none overflow-auto" tabIndex={0}>
