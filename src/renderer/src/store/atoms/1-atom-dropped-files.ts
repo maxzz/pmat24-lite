d269fd377d28907c35ca0ad99f472d38be230feb
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-09 17:43:12 -0800

    WIP. tree data. notes on file content incoming sources. 4. final version

--- src/renderer/src/store/atoms/1-atom-dropped-files.ts
+++ src/renderer/src/store/atoms/1-atom-dropped-files.ts
@@ -5,74 +5,28 @@ import { pmAllowedToOpenExt, type FileContent } from "@shared/ipc-types";
 
 /**
  * File content is coming from different sources:
- *   * a. without electron (web process only): dropped files
- *     - just files from a single folder (no folders or subfolders)
- *   * b. with electron: dropped file names from renderer
- *     - just files from a single folder (no folders or subfolders)
- *   * c. with electron (main process): dialog open folder
- *     - from a single chosen folder all files and folders recursively
- *   * d. with electron (main process): dialog open files
- *     - just files from a single folder (no folders or subfolders)
- * ```
- * ┌─────────────────────────────────────┬──────────────────────────────────────┐
- * │ without electron                    │ with electron                        │
- * │ (web process only):                 │                                      │
- * ├─────────────────────────────────────┼──────────────────────────────────────┤
- * │ dropped files                       │ dropped filenames from renderer      │
- * │ just files from a single folder     │ just files from a single folder      │
- * │     (no folders or subfolders)      │     (no folders or subfolders)       │
- * │                                     ├──────────────────────────────────────┤
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     ├──────────────────────────────────────┤
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     ├──────────────────────────────────────┤
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     │                                      │
- * │                                     │                                      │
- * └─────────────────────────────────────┴──────────────────────────────────────┘
- * ```
- * 
-  with electron, without electron
-  dropped filenames from renderer, (web process only): dropped files
-  just files from a single folder, with electron: dropped file names from renderer
-      (no folders or subfolders) , just files from a single folder (no folders or subfolders)
-
-  with electron (main process): dialog open folder,
-  from a single chosen folder all files and folders recursively,
-  with electron (main process): dialog open files,
-  just files from a single folder (no folders or subfolders),
 ```
- */
-
-
-/**
+┌───────────────────────────────────────────┬─────────────────────────────────────┐
+│ with electron                             │ without electron                    │
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from renderer                             │ from web process only               │
+│     dropped filenames                     │     dropped files                   │
+│         just files from a single folder   │ just files from a single folder     │
+│         (no folders or subfolders)        │ (no folders or subfolders)          │
+│                                           │                                     │ 
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from main process                         │ from web process only               │
+│     dialog open files                     │     dialog open files               │
+│         just files from a single folder   │     just files from a single folder │
+│         (no folders or subfolders)        │     (no folders or subfolders)      │
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from main process                         │                                     │
+│     dialog open folder                    │                                     │
+│         from a single chosen folder       │                                     │
+│         all files and folders recursively │                                     │
+└───────────────────────────────────────────┴─────────────────────────────────────┘
 ```
-┌───────────────────────────────────────────┬─────────────────────────────────┐
-│ with electron                             │ without electron                │
-├───────────────────────────────────────────┼─────────────────────────────────┤
-│ from renderer                             │ from web process only           │
-│     dropped filenames                     │     dropped files               │
-│         just files from a single folder   │ just files from a single folder │
-│         (no folders or subfolders)        │ (no folders or subfolders)      │
-│                                           │                                 │ 
-├───────────────────────────────────────────┼─────────────────────────────────┤
-│ from main process                         │                                 │
-│     dialog open folder                    │                                 │
-│         from a single chosen folder       │                                 │
-│         all files and folders recursively │                                 │
-├───────────────────────────────────────────┼─────────────────────────────────┤
-│ from main process                         │                                 │
-│     dialog open files                     │                                 │
-│         just files from a single folder   │                                 │
-│         (no folders or subfolders)        │                                 │
-└───────────────────────────────────────────┴─────────────────────────────────┘
-```
-      */
+*/
 export const filesContentAtom = atom<FileContent[]>([]);
 
 // handle files drop for web and electron environments
