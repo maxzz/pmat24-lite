522a88c7237a3541a690f6aa93fd5ed63cac1a01
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-09 19:25:27 -0800

    WIP. tree data. notes on file content incoming sources. 5. corrections and add commonPath()

--- src/renderer/src/components/1-header/index.tsx
+++ src/renderer/src/components/1-header/index.tsx
@@ -1,5 +1,8 @@
 import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
 import { DropdownMenuDemo } from "./1-dropdown-menu";
+import { test } from "@/utils/common-path";
+
+test();
 
 export function SectionHeader() {
     return (
--- src/renderer/src/store/atoms/1-atom-dropped-files.ts
+++ src/renderer/src/store/atoms/1-atom-dropped-files.ts
@@ -10,17 +10,20 @@ import { pmAllowedToOpenExt, type FileContent } from "@shared/ipc-types";
 │ with electron                             │ without electron                    │
 ├───────────────────────────────────────────┼─────────────────────────────────────┤
 │ from renderer                             │ from web process only               │
+│ ─────────────                             │ ─────────────────────               │
 │     dropped filenames                     │     dropped files                   │
 │         just files from a single folder   │ just files from a single folder     │
-│         (no folders or subfolders)        │ (no folders or subfolders)          │
-│                                           │                                     │ 
+│         (no folders or subfolders)        │ or if dropped folder then           │
+│                                           │ files and folders w/ relative path  │ 
 ├───────────────────────────────────────────┼─────────────────────────────────────┤
 │ from main process                         │ from web process only               │
+│ ─────────────────                         │ ─────────────────────               │
 │     dialog open files                     │     dialog open files               │
 │         just files from a single folder   │     just files from a single folder │
 │         (no folders or subfolders)        │     (no folders or subfolders)      │
 ├───────────────────────────────────────────┼─────────────────────────────────────┤
 │ from main process                         │                                     │
+│ ─────────────────                         │                                     │
 │     dialog open folder                    │                                     │
 │         from a single chosen folder       │                                     │
 │         all files and folders recursively │                                     │
