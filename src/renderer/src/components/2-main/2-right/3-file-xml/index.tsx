283e9111f75465fee7ef0417ddc37e2c1cd6b8c9
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-12 18:02:19 -0700

    WIP. xml view. fix theme support

--- src/renderer/src/components/2-main/2-right/3-file-xml/index.tsx
+++ src/renderer/src/components/2-main/2-right/3-file-xml/index.tsx
@@ -5,7 +5,7 @@ import { ScrollArea } from '@/ui';
 export function Body_Xml({ text, ...rest }: { text: string; } & HTMLAttributes<HTMLDivElement>) {
     return (<>
         <ScrollArea>
-            <SyntaxHighlighterXml>
+            <SyntaxHighlighterXml className="p-1 dark:opacity-60">
                 {text}
             </SyntaxHighlighterXml>
         </ScrollArea>
