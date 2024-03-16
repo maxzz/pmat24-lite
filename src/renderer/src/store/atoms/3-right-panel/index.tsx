a3fd7c85c19fb950a18a3c33a593d65c2699e51a
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-15 19:00:13 -0700

    WIP. atoms. update right panel to fileUs type

--- src/renderer/src/store/atoms/3-right-panel/index.tsx
+++ src/renderer/src/store/atoms/3-right-panel/index.tsx
@@ -1,3 +1,13 @@
+import { FileUsAtomType } from "@/store/store-types";
 import { atom } from "jotai";
+import { proxy } from "valtio";
 
-export const xmlTextAtom = atom('');
+// export const xmlTextAtom = atom('');
+
+export type RightPanel = {
+    selected: FileUsAtomType | null; // File list selected atom
+};
+
+export const rightPanel = proxy<RightPanel>({
+    selected: null,
+});
