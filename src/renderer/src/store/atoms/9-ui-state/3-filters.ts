5bcd693a8a5bce4bd1372019fdfab0837f73baef
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-15 18:12:04 -0700

    WIP. atoms. switch showManiAtoms and totalManiAtoms to valtio

--- src/renderer/src/store/atoms/9-ui-state/3-filters.ts
+++ src/renderer/src/store/atoms/9-ui-state/3-filters.ts
@@ -1,15 +1,27 @@
-import { atom } from "jotai";
+import { proxy } from "valtio";
 
 // Filters state
 
-export const showManiAtoms = {
-    normalAtom: atom(true),
-    manualAtom: atom(true),
-    emptyAtom: atom(true),
-};
+// export const showManiAtoms = {
+//     normalAtom: atom(true),
+//     manualAtom: atom(true),
+//     emptyAtom: atom(true),
+// };
 
-export const totalManiAtoms = {
-    manualAtom: atom(0),
-    normalAtom: atom(0),
-    emptyAtom: atom(0),
-};
+// export const totalManiAtoms = {
+//     manualAtom: atom(0),
+//     normalAtom: atom(0),
+//     emptyAtom: atom(0),
+// };
+
+export const shownManis = proxy({ // currently shown manifests
+    normal: true,
+    manual: true,
+    empty: true,
+});
+
+export const totalManis = proxy({ // total manifests
+    manual: 0,
+    normal: 0,
+    empty: 0,
+});
