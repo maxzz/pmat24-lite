af3fa968aef2a366db6b83781df896a8b350b2c8
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-17 17:47:43 -0700

    WIP. atoms. remove deliveredAtom. code clean up

--- src/renderer/src/store/atoms/1-files/1-do-set-files.ts
+++ src/renderer/src/store/atoms/1-files/1-do-set-files.ts
@@ -6,8 +6,38 @@ import { FileContent } from '@shared/ipc-types';
 import { isEmpty, isManual } from '@/store/store-utils';
 import { busyIndicator, totalManis } from '../9-ui-state';
 import { treeFilesAtom } from '../2-tree-files';
-import { deliveredToFileUs } from './1-delivered-to-file-us';
+import { deliveredToFileUs } from './2-delivered-to-file-us';
 
+/**
+ * File content is populated from web or electron environment:
+```
+┌───────────────────────────────────────────┬─────────────────────────────────────┐
+│ with electron                             │ without electron                    │
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from renderer                             │ from web process only               │
+│ ─────────────                             │ ─────────────────────               │
+│     dropped filenames                     │     dropped files                   │
+│         just files from a single folder   │ just files from a single folder     │
+│         (no folders or subfolders)        │ or if dropped folder then           │
+│                                           │ files and folders w/ relative path  │ 
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from main process                         │ from web process only               │
+│ ─────────────────                         │ ─────────────────────               │
+│     dialog open files                     │     dialog open files               │
+│         just files from a single folder   │     just files from a single folder │
+│         (no folders or subfolders)        │     (no folders or subfolders)      │
+├───────────────────────────────────────────┼─────────────────────────────────────┤
+│ from main process                         │                                     │
+│ ─────────────────                         │                                     │
+│     dialog open folder                    │                                     │
+│         from a single chosen folder       │                                     │
+│         all files and folders recursively │                                     │
+└───────────────────────────────────────────┴─────────────────────────────────────┘
+```
+- deliveredAtom - files content populated from web or electron environments
+- filesAtom     - files with reactive content and IDs
+- filteredAtom  - files to show in the tree
+*/
 export const doSetDeliveredFilesAtom = atom(
     null,
     (get, set, deliveredContent: FileContent[]) => {
@@ -52,80 +82,3 @@ export const doSetDeliveredFilesAtom = atom(
         //set(rightPanelData.panelAtom, undefined);
     }
 );
-
-// export const doSetFilesAtom = atom(
-//     null,
-//     (get, set, deliveredContent: FileContent[]) => {
-//         busyIndicator.msg = 'Parsing...';
-//         totalManis.normal = 0;
-//         totalManis.manual = 0;
-//         totalManis.empty = 0;
-
-//         const fileUsItems =
-//             deliveredContent
-//                 .filter((file) => file.size)
-//                 .map((deliveredFile) => {
-//                     const newFileUs = deliveredToFileUs(deliveredFile);
-
-//                     if (isEmpty(newFileUs)) {
-//                         totalManis.empty++;
-//                     } else if (isManual(newFileUs)) {
-//                         totalManis.manual++;
-//                     } else {
-//                         totalManis.normal++;
-//                     }
-
-//                     return newFileUs;
-//                 })
-//                 .filter((fileUs) => {
-//                     if (fileUs.failed) {
-//                         console.error(fileUs.raw);
-//                     }
-//                     return !fileUs.failed;
-//                 });
-
-//         const fileUsAtoms = fileUsItems.map((fileUs) => atom(fileUs));
-
-
-//         console.log('set files2Atom', fileUsAtoms);
-//         //set(_foldAllCardsAtom, -1);
-//         set(files2Atom, fileUsAtoms);
-//         set(treeFilesAtom);
-
-//         busyIndicator.msg = '';
-//         //set(doUpdateCacheAtom);
-//         //set(rightPanelData.panelAtom, undefined);
-//     }
-// );
-
-// export const doSetFilesAtom = atom(
-//     null,
-//     (get, set, accepterFiles: File[]) => {
-//         const droppedIn: FileUsAtomType[] = accepterFiles
-//             .filter((file) => file.size)
-//             .map(
-//                 (fileHandle, idx) => {
-//                     const newFileUs: FileUs = {
-//                         id: uuid(),
-//                         idx,
-//                         fname: fileHandle.name,
-//                         fpath: pathWoFilename(fileHandle),
-//                         fmodi: fileHandle.lastModified || 0,
-//                         size: fileHandle.size,
-//                         file: fileHandle,
-//                         state: {
-//                             isGroupAtom: atom<boolean>(false),
-//                             isCurrentAtom: atom<boolean>(false),
-//                         },
-//                         stats: {} as FileUsStats, // the real one will be assigned after caching content
-//                     };
-//                     return atom<FileUs>(newFileUs);
-//                 }
-//             );
-
-//         //set(_foldAllCardsAtom, -1);
-//         set(files2Atom, droppedIn);
-//         set(doUpdateCacheAtom);
-//         //set(rightPanelData.panelAtom, undefined);
-//     }
-// );
--- src/renderer/src/store/atoms/index.ts
+++ src/renderer/src/store/atoms/index.ts
@@ -2,6 +2,5 @@ export * from "./1-files";
 export * from "./2-tree-files";
 export * from "./3-right-panel";
 
-export * from "./7-delivered";
 export * from "./7-do-web-deliver";
 export * from "./9-ui-state";
