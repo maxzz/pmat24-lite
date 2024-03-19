18db257fd0500d5b17d3c5672da90d7485aa9dbc
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-19 16:53:22 -0700

    WIP. atoms. add filteredAtom

---
+++ src/renderer/src/store/atoms/1-files/3-tree-files/4-filtered.ts
@@ -0,0 +1,77 @@
+import { atom } from 'jotai';
+import { filesAtom } from '../0-files-atom';
+import { FileUsAtomType, Order, SortBy } from "@/store/store-types";
+import { useFileUsByFilter } from '@/store/store-utils';
+import { createRegexByFilter } from "@/store/store-utils/4-filters";
+import { isAnyCap, isAnyCls, isAnyWeb, isAnyWhy, isEmpty, isManual } from "@/store/store-utils/3-mani-utils";
+import { searchFilterData } from '../../9-ui-state/4-filters-search';
+//import { busyAtom, orderAtom, searchFilterData, showManiAtoms, sortByAtom } from '../../9-ui-state';
+
+export const filteredAtom = atom<FileUsAtomType[]>(
+    (get) => {
+        const { regex, winOnly, webOnly, whyOnly, capOnly, clsOnly } = createRegexByFilter(get(searchFilterData.textAtom), get(searchFilterData.caseSensitiveAtom));
+
+        const showNormal = get(showManiAtoms.normalAtom);
+        const showManual = get(showManiAtoms.manualAtom);
+        const showEmpty = get(showManiAtoms.emptyAtom);
+
+        const files = get(filesAtom);
+
+        const isFilesLoading = !!get(busyAtom);
+        if (isFilesLoading) {
+            return files;
+        }
+
+        let result = files.filter((fileAtom: FileUsAtomType) => {
+            const fileUs = get(fileAtom);
+
+            if (capOnly) {
+                return isAnyCap(fileUs, regex);
+            }
+
+            if (clsOnly) {
+                return isAnyCls(fileUs, regex);
+            }
+
+            const isWeb = isAnyWeb(fileUs);
+            if ((winOnly && isWeb) || (webOnly && !isWeb) || (whyOnly && !isAnyWhy(fileUs))) {
+                return false;
+            }
+
+            let useItNow = isEmpty(fileUs) ? showEmpty : isManual(fileUs) ? showManual : showNormal;
+            if (useItNow && regex) {
+                useItNow = useFileUsByFilter(fileUs, regex);
+            }
+            return useItNow;
+        });
+
+        const sortBy = get(sortByAtom);
+        const order = get(orderAtom);
+
+        if (sortBy === SortBy.index) {
+            if (order === Order.highToLow) {
+                result.sort((atomA: FileUsAtomType, atomB: FileUsAtomType) => {
+                    const fileUsA = get(atomA);
+                    const fileUsB = get(atomB);
+                    const a = fileUsA.idx;
+                    const b = fileUsB.idx;
+                    return a < b ? 1 : a > b ? -1 : 0;
+                });
+            }
+        } else if (sortBy === SortBy.url) {
+            result.sort((atomA: FileUsAtomType, atomB: FileUsAtomType) => {
+                const fileUsA = get(atomA);
+                const fileUsB = get(atomB);
+                const a = fileUsA?.stats?.domain || 'zz';
+                const b = fileUsB?.stats?.domain || 'zz';
+                if (order === Order.lowToHigh) {
+                    return a < b ? -1 : a > b ? 1 : 0;
+                } else {
+                    return a < b ? 1 : a > b ? -1 : 0;
+                }
+            });
+        }
+
+        return result;
+    }
+);
--- src/renderer/src/store/atoms/9-ui-state/7-ui-file-items-size.ts
+++ src/renderer/src/store/atoms/9-ui-state/7-ui-file-items-size.ts
@@ -1,8 +1,20 @@
-import { atomWithCallback } from "@/hooks/atomsX";
 import { UISize } from "@/store/store-types";
-import { LocalStorageSave, } from "@/store/store-localstorage-save";
-import { LocalStorage } from "@/store/store-localstorage-load";
+import { proxy } from "valtio";
+
+// import { atomWithCallback } from "@/hooks/atomsX";
+// import { UISize } from "@/store/store-types";
+// import { LocalStorageSave, } from "@/store/store-localstorage-save";
+// import { LocalStorage } from "@/store/store-localstorage-load";
+// export const uiSizeAtom = atomWithCallback<UISize>(LocalStorage.initialData.uiSize, LocalStorageSave.save);
 
 // UI files list items size
 
-export const uiSizeAtom = atomWithCallback<UISize>(LocalStorage.initialData.uiSize, LocalStorageSave.save);
+export type FileListItemsState = {
+    itemSize: UISize;
+};
+
+export const defaultFileListItemsState: FileListItemsState = {
+    itemSize: UISize.normal,
+};
+
+export const fileListItemsState = proxy<FileListItemsState>(defaultFileListItemsState);
