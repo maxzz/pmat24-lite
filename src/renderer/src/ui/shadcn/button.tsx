375a7ae4e03e0abc7fe0a4f370ef15136190233b
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-19 17:56:59 -0700

    WIP. ui. update button defaults

--- src/renderer/src/ui/shadcn/button.tsx
+++ src/renderer/src/ui/shadcn/button.tsx
@@ -36,8 +36,8 @@ const buttonVariantsClasses = cva(defaultVariantClasses, {
         },
     },
     defaultVariants: {
-        variant: "default",
-        size: "default",
+        variant: "outline",
+        size: "xs",
     },
 });
 
