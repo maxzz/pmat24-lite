a4f9a9071173252c9148decb784e6c54d7c79087
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-21 17:46:58 -0700

    WIP. icons. optimizing warning classes

--- src/renderer/src/store/store-utils/7-mani-app-icon.tsx
+++ src/renderer/src/store/store-utils/7-mani-app-icon.tsx
@@ -1,6 +1,7 @@
 import { TreenIconType } from "@/ui/shadcn/tree";
 import { SymbolAppWebChrome, SymbolAppWin } from "@/ui/icons";
 import { classNames } from "@/utils";
+import { AppWindow } from "lucide-react";
 
 export const enum AppIconType {
     web,
@@ -9,20 +10,28 @@ export const enum AppIconType {
     winWarning,
 }
 
+// function AppIcon({ TreenIcon, className, ...rest }: SVGIconTypeProps & { TreenIcon: TreenIconType }) {
+//     return <TreenIcon {...rest} />;
+// }
+
+const aa = ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWebChrome className={classNames("", className)} {...rest} />
+
 export function appIcon(iconType: AppIconType): TreenIconType {
     switch (iconType) {
         case AppIconType.web:
         case AppIconType.webWarning: {
             const warning = iconType === AppIconType.webWarning;
             const warningClasses = warning ? "text-red-500" : "text-muted-foreground";
-            return ({ className }: SVGIconTypeProps) => <SymbolAppWebChrome className={classNames(warningClasses, className)} />;
+            return aa;
+            // return ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWebChrome className={classNames(warningClasses, className)} {...rest} />;
+            // return AppIcon({ TreenIcon: SymbolAppWebChrome, className: warningClasses });
         }
         case AppIconType.win:
             // return AppWindow;
             return SymbolAppWin;
         case AppIconType.winWarning: {
             const warningClasses = "text-red-500";
-            return ({ className }: SVGIconTypeProps) => <SymbolAppWin className={classNames(warningClasses, className)} />;
+            return ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWin className={classNames(warningClasses, className)} {...rest} />;
         }
         default: {
             const _exhaustiveCheck: never = iconType;
