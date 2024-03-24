b045c18a2a8ca86a6d0b87a0a36aee9b11be508f
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-24 14:51:05 -0700

    WIP. tree-item. new tooltip location over icon and text

--- src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/0-tree-icon-and-text.tsx
+++ src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/0-tree-icon-and-text.tsx
@@ -1,7 +1,57 @@
 import { useAtomValue } from "jotai";
-import { TreeIconAndTextProps } from "@ui/shadcn/tree";
+import { TreeIconAndTextProps, TreenIconType } from "@ui/shadcn/tree";
 import { treeItemToFileUs } from "../1-files-tree";
 import { FileIndexAttention } from "./2-file-index-attention";
+import { FileUs } from "@/store/store-types";
+import { isAnyWhy } from "@/store/store-utils";
+import { CardTitleFileIndex } from "./1-file-index";
+import { RealTooltip } from "./3-real-tooltip";
+import { TooltipBody } from "./3-tooltip-body";
+
+// export function FileNameAttention({ fileUs }: { fileUs: FileUs; }) {
+//     const hasBailOut = isAnyWhy(fileUs);
+//     const fileIndex = fileUs.idx + 1;
+//     if (!hasBailOut) {
+//         return <CardTitleFileIndex idx={fileIndex} />;
+//     }
+//     return (
+//         <RealTooltip
+//             trigger={
+//                 <CardTitleFileIndex idx={fileIndex} errors={true} />
+//             }
+//             body={
+//                 <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
+//             }
+//         />
+//     );
+// }
+
+export function FileNameAttention({ IconToRender, name, fileUs, iconClasses }: { fileUs: FileUs; IconToRender: false | TreenIconType | undefined; name: string; iconClasses: string; }) {
+    const hasBailOut = isAnyWhy(fileUs);
+    const fileIndex = fileUs.idx + 1;
+
+    const Body = (<>
+        {IconToRender && <IconToRender className={iconClasses} aria-hidden="true" />}
+
+        <span className="flex-grow truncate">
+            {name}
+        </span>
+    </>);
+
+    if (!hasBailOut) {
+        return Body;
+    }
+    return (
+        <RealTooltip
+            trigger={
+                Body
+            }
+            body={
+                <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
+            }
+        />
+    );
+}
 
 export function TreeIconAndText({ item, Icon, iconClasses, hideFolderIcon }: TreeIconAndTextProps) {
     const fileUsItem = treeItemToFileUs(item);
@@ -11,10 +61,12 @@ export function TreeIconAndText({ item, Icon, iconClasses, hideFolderIcon }: Tre
     return (<>
         <FileIndexAttention fileUs={fileUs} />
 
-        {IconToRender && <IconToRender className={iconClasses} aria-hidden="true" />}
+        <FileNameAttention fileUs={fileUs} IconToRender={IconToRender} name={item.name} iconClasses={iconClasses} />
+
+        {/* {IconToRender && <IconToRender className={iconClasses} aria-hidden="true" />}
 
         <span className="flex-grow truncate">
             {item.name}
-        </span>
+        </span> */}
     </>);
 }
