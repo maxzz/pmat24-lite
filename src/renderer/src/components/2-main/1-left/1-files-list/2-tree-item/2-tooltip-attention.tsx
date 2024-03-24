ee3f9e6a85bc9b32c81ea0fe175b79666d598769
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-24 14:12:58 -0700

    WIP. tree-item. experiment w/ props. 2

--- src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/2-tooltip-attention.tsx
+++ src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/2-tooltip-attention.tsx
@@ -6,33 +6,72 @@ import { TooltipArrow } from "@radix-ui/react-tooltip";
 import { CardTitleFileIndex } from "./1-file-index";
 import { TooltipBody } from "./3-tooltip-body";
 
-export function TooltipAttention({ Children: Children, fileUs }: { fileUs: FileUs; Children: FunctionComponent<{ idx: number } | { idx: number; errors?: boolean }>}) {
-    const hasBailOut = isAnyWhy(fileUs);
-    const fileIndex = fileUs.idx + 1;
+// export function TooltipAttention({ Children: Children, fileUs }: { fileUs: FileUs; Children: FunctionComponent<{ idx: number } | { idx: number; errors?: boolean }>}) {
+//     const hasBailOut = isAnyWhy(fileUs);
+//     const fileIndex = fileUs.idx + 1;
 
-    Children = CardTitleFileIndex;
+//     Children = CardTitleFileIndex;
 
-    if (!hasBailOut) {
-        return <Children idx={fileIndex} />;
-        // return <CardTitleFileIndex idx={fileIndex} />;
-    }
+//     if (!hasBailOut) {
+//         return <Children idx={fileIndex} />;
+//         // return <CardTitleFileIndex idx={fileIndex} />;
+//     }
 
+//     return (
+//         <TooltipProvider>
+//             <Tooltip /** / open={true}/**/>
+//                 <TooltipTrigger asChild>
+//                     <div>
+//                         <CardTitleFileIndex idx={fileIndex} errors={true} />
+//                     </div>
+//                 </TooltipTrigger>
+
+//                 <TooltipPortal>
+//                     <TooltipContent className="p-0 max-w-72 text-xs border-border border shadow">
+//                         <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
+//                         <TooltipArrow className="fill-primary" />
+//                     </TooltipContent>
+//                 </TooltipPortal>
+//             </Tooltip>
+//         </TooltipProvider>
+//     );
+// }
+
+export function TooltipAttention2({ trigger, children }: { trigger: ReactNode; children: ReactNode; }) {
     return (
         <TooltipProvider>
             <Tooltip /** / open={true}/**/>
                 <TooltipTrigger asChild>
                     <div>
-                        <CardTitleFileIndex idx={fileIndex} errors={true} />
+                        {trigger}
                     </div>
                 </TooltipTrigger>
 
                 <TooltipPortal>
                     <TooltipContent className="p-0 max-w-72 text-xs border-border border shadow">
-                        <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
+                        {children}
                         <TooltipArrow className="fill-primary" />
                     </TooltipContent>
                 </TooltipPortal>
             </Tooltip>
         </TooltipProvider>
+    );
+}
+
+export function TooltipAttention({ fileUs }: { fileUs: FileUs; }) {
+    const hasBailOut = isAnyWhy(fileUs);
+    const fileIndex = fileUs.idx + 1;
+    if (!hasBailOut) {
+        return <CardTitleFileIndex idx={fileIndex} />;
+    }
+    return (
+        <TooltipAttention2
+            trigger={
+                <CardTitleFileIndex idx={fileIndex} errors={true} />
+            }
+            children={
+                <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
+            }
+        />
     );
 }
