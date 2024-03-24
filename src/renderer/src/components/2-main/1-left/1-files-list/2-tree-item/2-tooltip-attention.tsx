bedbb90d2d1f66d43e847fab958e563796d75101
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-24 14:17:29 -0700

    WIP. tree-item. experiment w/ props. 3. done

--- src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/2-tooltip-attention.tsx
+++ src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/2-tooltip-attention.tsx
@@ -1,62 +1,8 @@
-import { FunctionComponent, ReactNode } from "react";
 import { FileUs } from "@/store/store-types";
 import { isAnyWhy } from "@/store/store-utils";
-import { Button, Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger } from "@/ui";
-import { TooltipArrow } from "@radix-ui/react-tooltip";
 import { CardTitleFileIndex } from "./1-file-index";
 import { TooltipBody } from "./3-tooltip-body";
-
-// export function TooltipAttention({ Children: Children, fileUs }: { fileUs: FileUs; Children: FunctionComponent<{ idx: number } | { idx: number; errors?: boolean }>}) {
-//     const hasBailOut = isAnyWhy(fileUs);
-//     const fileIndex = fileUs.idx + 1;
-
-//     Children = CardTitleFileIndex;
-
-//     if (!hasBailOut) {
-//         return <Children idx={fileIndex} />;
-//         // return <CardTitleFileIndex idx={fileIndex} />;
-//     }
-
-//     return (
-//         <TooltipProvider>
-//             <Tooltip /** / open={true}/**/>
-//                 <TooltipTrigger asChild>
-//                     <div>
-//                         <CardTitleFileIndex idx={fileIndex} errors={true} />
-//                     </div>
-//                 </TooltipTrigger>
-
-//                 <TooltipPortal>
-//                     <TooltipContent className="p-0 max-w-72 text-xs border-border border shadow">
-//                         <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
-//                         <TooltipArrow className="fill-primary" />
-//                     </TooltipContent>
-//                 </TooltipPortal>
-//             </Tooltip>
-//         </TooltipProvider>
-//     );
-// }
-
-export function TooltipAttention2({ trigger, children }: { trigger: ReactNode; children: ReactNode; }) {
-    return (
-        <TooltipProvider>
-            <Tooltip /** / open={true}/**/>
-                <TooltipTrigger asChild>
-                    <div>
-                        {trigger}
-                    </div>
-                </TooltipTrigger>
-
-                <TooltipPortal>
-                    <TooltipContent className="p-0 max-w-72 text-xs border-border border shadow">
-                        {children}
-                        <TooltipArrow className="fill-primary" />
-                    </TooltipContent>
-                </TooltipPortal>
-            </Tooltip>
-        </TooltipProvider>
-    );
-}
+import { RealTooltip } from "./3-real-tooltip";
 
 export function TooltipAttention({ fileUs }: { fileUs: FileUs; }) {
     const hasBailOut = isAnyWhy(fileUs);
@@ -65,11 +11,11 @@ export function TooltipAttention({ fileUs }: { fileUs: FileUs; }) {
         return <CardTitleFileIndex idx={fileIndex} />;
     }
     return (
-        <TooltipAttention2
+        <RealTooltip
             trigger={
                 <CardTitleFileIndex idx={fileIndex} errors={true} />
             }
-            children={
+            body={
                 <TooltipBody fileUs={fileUs} fileIndex={fileIndex} />
             }
         />
---
+++ src/renderer/src/components/2-main/1-left/1-files-list/2-tree-item/3-real-tooltip.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger } from "@/ui";
+import { TooltipArrow } from "@radix-ui/react-tooltip";
+
+export function RealTooltip({ trigger, body }: { trigger: ReactNode; body: ReactNode; }) {
+    return (
+        <TooltipProvider>
+            <Tooltip /** / open={true}/**/>
+                <TooltipTrigger asChild>
+                    <div>
+                        {trigger}
+                    </div>
+                </TooltipTrigger>
+
+                <TooltipPortal>
+                    <TooltipContent className="p-0 max-w-72 text-xs border-border border shadow">
+                        {body}
+                        <TooltipArrow className="fill-primary" />
+                    </TooltipContent>
+                </TooltipPortal>
+            </Tooltip>
+        </TooltipProvider>
+    );
+}
