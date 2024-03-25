e3c5a871fe614b511283ee19909db05a00808b14
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-25 14:21:26 -0700

    WIP. create mani dialog. add more logic. 2. single app selection

--- src/renderer/src/components/4-dialogs/2-create/1-body.tsx
+++ src/renderer/src/components/4-dialogs/2-create/1-body.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
+import { PrimitiveAtom, atom, useAtom } from "jotai";
 import { Button, Checkbox, Label, ScrollArea } from "@/ui";
 import * as D from "@/ui/shadcn/dialog";
-import { detectedWindows } from "./2-test-detected-windows";
+import { DetectedWindow, detectedWindows } from "./2-test-detected-windows";
 
 export function DialogCreateManiBody({ setIsOpen }: { setIsOpen: (v: boolean) => void; }) {
+    const selectedIdxAtom = useState(() => atom(-1))[0];
     return (
         <div className="min-h-56 text-xs">
 
@@ -22,16 +25,12 @@ export function DialogCreateManiBody({ setIsOpen }: { setIsOpen: (v: boolean) =>
 
                 <div className="border-border border rounded">
                     <ScrollArea className="h-[25vh]">
-                        <div className="px-3 py-2 flex flex-col gap-2">
-                            {detectedWindows.length === 0 && <div className="text-muted-foreground">No windows detected</div>}
-                            {detectedWindows.length !== 0 && detectedWindows.map((window) => (
-                                <Label key={window.id} className="text-xs font-normal flex items-center gap-1">
-                                    <Checkbox className="size-4" />
-                                    <div>{window.name}</div>
-                                </Label>
-                            ))}
-                        </div>
+                        <WindowsList windows={detectedWindows} selectedIdxAtom={selectedIdxAtom} />
                     </ScrollArea>
+                </div>
+
+                <div className="my-4">
+                    No login fields detected
                 </div>
 
                 <div className="mt-4 flex items-center justify-end">
@@ -40,6 +39,21 @@ export function DialogCreateManiBody({ setIsOpen }: { setIsOpen: (v: boolean) =>
 
             </div>
 
+        </div>
+    );
+}
+
+function WindowsList({ windows, selectedIdxAtom }: { windows: DetectedWindow[]; selectedIdxAtom: PrimitiveAtom<number>; }) {
+    const [selectedIdx, setSelectedIdx] = useAtom(selectedIdxAtom);
+    return (
+        <div className="px-3 py-2 flex flex-col gap-2">
+            {detectedWindows.length === 0 && <div className="text-muted-foreground">No windows detected</div>}
+            {detectedWindows.length !== 0 && detectedWindows.map((window, idx) => (
+                <Label key={window.id} className="text-xs font-normal cursor-pointer flex items-center gap-1">
+                    <Checkbox className="size-4" checked={idx === selectedIdx} onCheckedChange={(v) => setSelectedIdx(!!v ? idx : -1)} />
+                    <div>{window.name}</div>
+                </Label>
+            ))}
         </div>
     );
 }
