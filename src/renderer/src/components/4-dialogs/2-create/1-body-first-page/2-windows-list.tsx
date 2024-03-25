1dca718c21028175377a28b860386a92cab3d9e1
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-25 14:51:09 -0700

    WIP. create mani dialog. add more logic. 6. add selected item selection

--- src/renderer/src/components/4-dialogs/2-create/1-body-first-page/2-windows-list.tsx
+++ src/renderer/src/components/4-dialogs/2-create/1-body-first-page/2-windows-list.tsx
@@ -1,24 +1,31 @@
 import { PrimitiveAtom, useAtom } from "jotai";
 import { Checkbox, Label } from "@/ui";
 import { DetectedWindow, detectedWindows } from "./4-test-detected-windows";
+import { classNames } from "@/utils";
 
 export function WindowsList({ selectedIdxAtom }: { windows: DetectedWindow[]; selectedIdxAtom: PrimitiveAtom<number>; }) {
     const [selectedIdx, setSelectedIdx] = useAtom(selectedIdxAtom);
     return (
-        <div className="px-3 py-2 flex flex-col gap-2">
+        <div className="py-2 flex flex-col">
             {detectedWindows.length === 0 && (
-                <div className="text-muted-foreground">
+                <div className="px-3 text-muted-foreground">
                     No windows detected
                 </div>
             )}
 
             {detectedWindows.length !== 0 && detectedWindows.map(
-                (window, idx) => (
-                    <Label key={window.id} className="text-xs font-normal cursor-pointer flex items-center gap-1">
-                        <Checkbox className="size-4" checked={idx === selectedIdx} onCheckedChange={(v) => setSelectedIdx(!!v ? idx : -1)} />
-                        <div>{window.name}</div>
-                    </Label>
-                ))
+                (window, idx) => {
+                    const checked = idx === selectedIdx;
+                    return (
+                        <Label
+                            className={classNames("px-3 py-1.5 text-xs font-normal cursor-pointer flex items-center gap-1", checked && "bg-accent")}
+                            key={window.id}
+                        >
+                            <Checkbox className="size-4" checked={checked} onCheckedChange={(v) => setSelectedIdx(!!v ? idx : -1)} />
+                            <div>{window.name}</div>
+                        </Label>
+                    );
+                })
             }
         </div>
     );
