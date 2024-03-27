413073dfe8395561a85028106e55e80ed5331a0e
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-27 15:34:54 -0700

    WIP. right panel. add simple react-spring transition

--- src/renderer/src/components/2-main/2-right/0-header/1-title/index.tsx
+++ src/renderer/src/components/2-main/2-right/0-header/1-title/index.tsx
@@ -1,6 +1,8 @@
 import { useAtomValue } from "jotai";
 import { rightPanelSelectedContentAtom } from "@/store";
+import { a, useTransition } from "@react-spring/web";
 
+/** /
 export function RightTitle() {
     const rightPanel = useAtomValue(rightPanelSelectedContentAtom);
     if (!rightPanel) {
@@ -9,4 +11,27 @@ export function RightTitle() {
     return (
         <div className="">Name</div>
     );
+}
+/**/
+
+export function RightTitle() {
+    const rightPanel = useAtomValue(rightPanelSelectedContentAtom);
+
+    const transition = useTransition(rightPanel, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+        config: { duration: 200 },
+    });
+
+    // if (!rightPanel) {
+    //     return <div className="">No File</div>;
+    // }
+    return (<>
+        <div className="">
+            {transition((style, item) => item && (
+                <a.div style={style} className="">Name {item ? "1" : "2"}</a.div>
+            ))}
+        </div>
+    </>);
 }
