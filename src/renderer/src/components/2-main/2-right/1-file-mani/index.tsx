b1502da8ee26f7a4b1d22a56008a32319c115d5a
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-30 16:43:27 -0700

    WIP. file mani. before simplify FormBody

--- src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
@@ -5,18 +5,30 @@ import { rightPanelSelectedContentAtom } from "@/store";
 import { FileUs } from "@/store/store-types";
 import { FormEditor } from "./1-form-editor";
 
+// export function FormBody2({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
+//     const { ref, width, height } = useResizeObserver();
+//     return (
+//         <div className="h-full w-full overflow-hidden" ref={ref}>
+//             <ScrollArea style={{ width, height }} horizontal>
+//                 <FormEditor fileUs={fileUs} formIdx={formIdx} />
+//             </ScrollArea>
+//         </div>
+//     );
+// }
+
 export function FormBody({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
     const { ref, width, height } = useResizeObserver();
     return (
-        <div className="h-full w-full overflow-hidden" ref={ref}>
-            <ScrollArea style={{ width, height }} horizontal>
+        // <div className="h-full w-full overflow-hidden" ref={ref}>
+        //     <ScrollArea style={{ width, height }} horizontal>
                 <FormEditor fileUs={fileUs} formIdx={formIdx} />
-            </ScrollArea>
-        </div>
+        //     </ScrollArea>
+        // </div>
     );
 }
 
 export function Body_Mani() {
+    const { ref, width, height } = useResizeObserver();
     const fileUs = useAtomValue(rightPanelSelectedContentAtom);
     if (!fileUs) {
         return null;
@@ -28,13 +40,43 @@ export function Body_Mani() {
                 <TabsTrigger value="switch2" className="text-xs">Password change</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="switch1" className="flex-1 min-h-0 mt-1 p-2 pr-1 bg-muted rounded border-muted-foreground/50 border">
-                <FormBody fileUs={fileUs} formIdx={0} />
-            </TabsContent>
+            <div className="flex-1 min-h-0 mt-1 p-2 pr-1 bg-muted rounded border-muted-foreground/50 border">
+                <div className="h-full w-full overflow-hidden" ref={ref}>
+                    <ScrollArea style={{ width, height }} horizontal>
+                        <TabsContent value="switch1">
+                            <FormBody fileUs={fileUs} formIdx={0} />
+                        </TabsContent>
 
-            <TabsContent value="switch2" className="flex-1 min-h-0 mt-1 p-2 pr-1 bg-muted rounded border-muted-foreground/50 border">
-                <FormBody fileUs={fileUs} formIdx={1} />
-            </TabsContent>
+                        <TabsContent value="switch2">
+                            <FormBody fileUs={fileUs} formIdx={1} />
+                        </TabsContent>
+                    </ScrollArea>
+                </div>
+            </div>
+
         </Tabs>
     );
 }
+
+// export function Body_Mani2() {
+//     const fileUs = useAtomValue(rightPanelSelectedContentAtom);
+//     if (!fileUs) {
+//         return null;
+//     }
+//     return (
+//         <Tabs defaultValue="switch1" className="p-1 h-full flex flex-col">
+//             <TabsList className="self-center">
+//                 <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
+//                 <TabsTrigger value="switch2" className="text-xs">Password change</TabsTrigger>
+//             </TabsList>
+
+//             <TabsContent value="switch1" className="flex-1 min-h-0 mt-1 p-2 pr-1 bg-muted rounded border-muted-foreground/50 border">
+//                 <FormBody fileUs={fileUs} formIdx={0} />
+//             </TabsContent>
+
+//             <TabsContent value="switch2" className="flex-1 min-h-0 mt-1 p-2 pr-1 bg-muted rounded border-muted-foreground/50 border">
+//                 <FormBody fileUs={fileUs} formIdx={1} />
+//             </TabsContent>
+//         </Tabs>
+//     );
+// }
