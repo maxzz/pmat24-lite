ca6821256b8e295e8ef7fd0e12753bfe44afe140
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-29 19:42:25 -0700

    WIP. file mani. move scroll to form view

--- src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
@@ -1,18 +1,31 @@
 import { HTMLAttributes } from "react";
-import useResizeObserver from "use-resize-observer";
-import { FileUs } from "@/store/store-types";
-import { ScrollArea, Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui";
-import { classNames } from "@/utils";
 import { useAtomValue } from "jotai";
+import useResizeObserver from "use-resize-observer";
+import { ScrollArea, Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui";
 import { rightPanelSelectedContentAtom } from "@/store";
+import { FileUs } from "@/store/store-types";
+import { classNames } from "@/utils";
 import { LongPanel } from "../9-nun/LongPanel";
 
-export function FormBody({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
+export function FormBody_nun({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
     const title = formIdx === 0 ? 'Login' : 'Password change';
     return (
         <div className="">
             Form {title}
             {/* {fileUs.meta?.[formIdx]?.disp} */}
+        </div>
+    );
+}
+
+export function FormBody({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
+    const { ref, width, height } = useResizeObserver();
+    const title = formIdx === 0 ? 'Login' : 'Password change';
+    return (
+        <div className={classNames("h-full w-full")} ref={ref}>
+            <ScrollArea style={{ width, height }} horizontal>
+                Form {title}
+                <LongPanel />
+            </ScrollArea>
         </div>
     );
 }
@@ -25,16 +38,15 @@ export function FormsSwitch() {
     return (
         <Tabs defaultValue="switch1" className="mt-2 h-full">
             <TabsList>
-                <TabsTrigger value="switch1">Login</TabsTrigger>
-                <TabsTrigger value="switch2">Password change</TabsTrigger>
+                <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
+                <TabsTrigger value="switch2" className="text-xs">Password change</TabsTrigger>
             </TabsList>
 
             <TabsContent value="switch1" className="h-full bg-red-500">
                 <FormBody fileUs={fileUs} formIdx={0} />
-                <LongPanel />
             </TabsContent>
 
-            <TabsContent value="switch2">
+            <TabsContent value="switch2" className="h-full bg-green-500/40">
                 <FormBody fileUs={fileUs} formIdx={1} />
             </TabsContent>
         </Tabs>
@@ -51,9 +63,18 @@ export function Body_Mani({ className, ...rest }: HTMLAttributes<HTMLDivElement>
     const { ref, width, height } = useResizeObserver();
     return (<>
         <div className={classNames("h-full w-full", className)} ref={ref} {...rest}>
-            <ScrollArea style={{ width, height }} horizontal>
-                <MainEditor />
-            </ScrollArea>
+            <FormsSwitch />
         </div>
     </>);
 }
+
+// export function Body_Mani_nun({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+//     const { ref, width, height } = useResizeObserver();
+//     return (<>
+//         <div className={classNames("h-full w-full", className)} ref={ref} {...rest}>
+//             <ScrollArea style={{ width, height }} horizontal>
+//                 <MainEditor />
+//             </ScrollArea>
+//         </div>
+//     </>);
+// }
