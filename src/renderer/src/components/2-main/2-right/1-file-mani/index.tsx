9c17fe9d28990f6588dbe18b9bb22f707eccd919
Max Zakharzhevskiy <maxzz@msn.com>
2024-03-29 19:31:30 -0700

    WIP. file mani. add scroll on mani view

--- src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/index.tsx
@@ -1,22 +1,41 @@
 import { HTMLAttributes } from "react";
 import useResizeObserver from "use-resize-observer";
+import { FileUs } from "@/store/store-types";
 import { ScrollArea, Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui";
 import { classNames } from "@/utils";
+import { useAtomValue } from "jotai";
+import { rightPanelSelectedContentAtom } from "@/store";
+import { LongPanel } from "../9-nun/LongPanel";
+
+export function FormBody({ fileUs, formIdx }: { fileUs: FileUs; formIdx: number; }) {
+    const title = formIdx === 0 ? 'Login' : 'Password change';
+    return (
+        <div className="">
+            Form {title}
+            {/* {fileUs.meta?.[formIdx]?.disp} */}
+        </div>
+    );
+}
 
 export function FormsSwitch() {
+    const fileUs = useAtomValue(rightPanelSelectedContentAtom);
+    if (!fileUs) {
+        return null;
+    }
     return (
-        <Tabs defaultValue="switch1">
+        <Tabs defaultValue="switch1" className="mt-2 h-full">
             <TabsList>
                 <TabsTrigger value="switch1">Login</TabsTrigger>
                 <TabsTrigger value="switch2">Password change</TabsTrigger>
             </TabsList>
 
-            <TabsContent value="switch1">
-                <div className="">Form1</div>
+            <TabsContent value="switch1" className="h-full bg-red-500">
+                <FormBody fileUs={fileUs} formIdx={0} />
+                <LongPanel />
             </TabsContent>
 
             <TabsContent value="switch2">
-                <div className="">Form2</div>
+                <FormBody fileUs={fileUs} formIdx={1} />
             </TabsContent>
         </Tabs>
     );
@@ -24,15 +43,7 @@ export function FormsSwitch() {
 
 function MainEditor() {
     return (
-        <div className="flex flex-col h-full w-full">
-            <div className="flex-1 h-0">
-                <FormsSwitch />
-                
-                <div className="">
-                    Mani
-                </div>
-            </div>
-        </div>
+        <FormsSwitch />
     );
 }
 
