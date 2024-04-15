76c36f03d5ff0ae8fb53f42671d676d2f51443dd
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-15 15:09:15 -0700

    WIP. main menu. add LoadedCounter component

--- src/renderer/src/components/1-header/0-all/index.tsx
+++ src/renderer/src/components/1-header/0-all/index.tsx
@@ -5,32 +5,38 @@ import { MainDropdownMenu } from "../1-main-menu";
 import { Button } from "@/ui";
 import { doOpenCreateDialogAtom, doOpenDrawerAtom } from "@/store/atoms/4-dialogs";
 
-export function SectionHeader() {
+function LoadedCounter() {
     const treeFiles = useAtomValue(treeFilesAtom);
+    if (!treeFiles?.length) {
+        return null;
+    }
+    return (
+        <div className="text-[.65rem] text-muted-foreground">
+            loaded{' '}
+            <span className="text-text/60">{treeFiles.length}</span>
+            {` file${treeFiles.length > 1 ? 's' : ''}`}
+        </div>
+    );
+}
 
+export function SectionHeader() {
     const doOpenCreateDialog = useSetAtom(doOpenCreateDialogAtom);
     const doOpenDrawer = useSetAtom(doOpenDrawerAtom);
-    
+
     return (
         <div className="px-4 py-2 bg-muted/20 border-border/50 border-b flex items-center justify-between">
             <div className="flex items-center gap-2">
                 <MainDropdownMenu />
 
-                {treeFiles?.length > 0 && (
-                    <div className="text-[.65rem] text-muted-foreground">
-                        loaded{' '}
-                        <span className="text-text/60">{treeFiles.length}</span>
-                        {` file${treeFiles.length > 1 ? 's' : ''}`}
-                    </div>
-                )}
-
-                <Button variant="outline" className="text-[.65rem]" onClick={()=> doOpenCreateDialog(true)}>
+                <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenCreateDialog(true)}>
                     Create mani
                 </Button>
 
-                <Button variant="outline" className="text-[.65rem]" onClick={()=> doOpenDrawer(true)}>
+                <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenDrawer(true)}>
                     Create
                 </Button>
+
+                <LoadedCounter />
             </div>
 
             <ThemeSwitch />
