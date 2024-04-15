7080f5643705b05a4b183405891298a1cd810619
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-15 14:29:14 -0700

    WIP. right panel. update headeer layout

--- src/renderer/src/components/2-main/2-right/0-header/1-title/index.tsx
+++ src/renderer/src/components/2-main/2-right/0-header/1-title/index.tsx
@@ -1,7 +1,7 @@
 import { useAtomValue } from "jotai";
 import { rightPanelSelectedContentAtom } from "@/store";
-import { TitleWithFileUs } from "./2-with-file-us";
-import { TitleNoFile } from "./1-no-file";
+import { TitleWithFileUs } from "./2-title-with-file-us";
+import { TitleNoFile } from "./1-title-no-file";
 
 export function RightTitle() {
     const fileUs = useAtomValue(rightPanelSelectedContentAtom);
