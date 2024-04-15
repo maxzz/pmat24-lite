e8932e35d763cb4f1f56866ac802905306c0585d
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-15 14:34:41 -0700

    WIP. right panel. update headeer layout. 2

--- src/renderer/src/components/2-main/2-right/0-header/0-all/index.tsx
+++ src/renderer/src/components/2-main/2-right/0-header/0-all/index.tsx
@@ -1,14 +1,25 @@
 import { HTMLAttributes } from "react";
+import { useAtomValue } from "jotai";
 import { R_PanelMenu } from "../2-menu";
 import { panelHeaderClasses } from "../../../1-left/0-header/0-all";
-import { classNames } from "@/utils";
-import { RightTitle } from "../1-title";
 import { XmlSwitch } from "../3-xml-switch";
+import { rightPanelSelectedContentAtom } from "@/store";
+import { TitleNoFile } from "./1-title-no-file";
+import { TitleWithFileUs } from "./2-title-with-file-us";
+import { classNames } from "@/utils";
 
 export function R_PanelHeader({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
+    const fileUs = useAtomValue(rightPanelSelectedContentAtom);
+    if (!fileUs) {
+        return (
+            <div className={classNames(panelHeaderClasses, "h-10 flex items-start justify-between", className)} {...rest}>
+                <TitleNoFile />
+            </div>
+        );
+    }
     return (
         <div className={classNames(panelHeaderClasses, "flex items-start justify-between", className)} {...rest}>
-            <RightTitle />
+            <TitleWithFileUs fileUs={fileUs} />
 
             <div className="flex items-center gap-4">
                 <XmlSwitch />
