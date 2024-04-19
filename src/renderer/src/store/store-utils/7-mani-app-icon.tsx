7de3c9e5d18fce36e31b7b091b8327ca04212e24
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-18 18:42:45 -0700

    WIP. icons. warning icons code refactoring. 2

--- src/renderer/src/store/store-utils/7-mani-app-icon.tsx
+++ src/renderer/src/store/store-utils/7-mani-app-icon.tsx
@@ -8,11 +8,12 @@ export const enum AppIconType {
     web,
     win,
     man,            // manual mode
-    // webWarning,     // web app warning
-    // winWarning,     // win app warning
-    // manWarning,     // manual mode warning
     cat,            // field catalog
 }
+
+// webWarning,     // web app warning
+// winWarning,     // win app warning
+// manWarning,     // manual mode warning
 
 const Web = ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWebChrome className={classNames("text-muted-foreground", className)} {...rest} />;
 
@@ -28,30 +29,43 @@ const ManWarning = ({ className, ...rest }: SVGIconTypeProps) => <SymbolManualMo
 
 const Catalog = ({ className, ...rest }: SVGIconTypeProps) => <SymbolCatalog className={classNames("text-muted-foreground", className)} {...rest} />;
 
+const components = {
+    [AppIconType.web]: { icon: SymbolAppWebChrome, /**/ normalClasses: "text-muted-foreground", warningClasses: "text-red-500 fill-red-300 opacity-75" },
+    [AppIconType.win]: { icon: SymbolAppWin,       /**/ normalClasses: "text-muted-foreground", warningClasses: "text-red-500 fill-red-300 opacity-75" },
+    [AppIconType.man]: { icon: SymbolManualMode,   /**/ normalClasses: "text-muted-foreground", warningClasses: "text-red-500 fill-red-300 opacity-75" },
+    [AppIconType.cat]: { icon: SymbolCatalog,      /**/ normalClasses: "text-muted-foreground", warningClasses: "" },
+};
+
+export function appIcon(iconType: AppIconType, warning?: boolean): TreenIconType {
+    const { icon, normalClasses, warningClasses } = components[iconType];
+    
+    const fn = ({ className, ...rest }: SVGIconTypeProps) => icon({ className: classNames(normalClasses, warning && warningClasses, className), ...rest });
+    return fn;
+}
 
 // const WebIe = ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWebIE className={classNames("text-muted-foreground size-3.5 stroke-1", className)} {...rest} />;
 const WebIe = ({ className, ...rest }: SVGIconTypeProps) => <SymbolAppWebIEText className={classNames("text-muted-foreground size-3.5 stroke-1", className)} {...rest} />;
 
 
-export function appIcon(iconType: AppIconType, warning?: boolean): TreenIconType {
-    switch (iconType) {
-        case AppIconType.web:
-            return Web;
-        // case AppIconType.webWarning:
-        //     return WebWarning;
-        case AppIconType.win:
-            return Win; //return SymbolAppWin; // return AppWindow;
-        // case AppIconType.winWarning:
-        //     return WinWarning;
-        case AppIconType.man:
-            return Man;
-        // case AppIconType.manWarning:
-        //     return ManWarning;
-        case AppIconType.cat:
-            return Catalog;
-        default: {
-            const _exhaustiveCheck: never = iconType;
-            return _exhaustiveCheck;
-        }
-    }
-}
+// export function appIcon(iconType: AppIconType, warning?: boolean): TreenIconType {
+//     switch (iconType) {
+//         case AppIconType.web:
+//             return Web;
+//         // case AppIconType.webWarning:
+//         //     return WebWarning;
+//         case AppIconType.win:
+//             return Win; //return SymbolAppWin; // return AppWindow;
+//         // case AppIconType.winWarning:
+//         //     return WinWarning;
+//         case AppIconType.man:
+//             return Man;
+//         // case AppIconType.manWarning:
+//         //     return ManWarning;
+//         case AppIconType.cat:
+//             return Catalog;
+//         default: {
+//             const _exhaustiveCheck: never = iconType;
+//             return _exhaustiveCheck;
+//         }
+//     }
+// }
