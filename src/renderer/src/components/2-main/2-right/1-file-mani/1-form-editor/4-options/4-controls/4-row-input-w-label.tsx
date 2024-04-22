3ffc522fb9967a7068a1dfb668fa31f54b71bbf4
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-22 15:26:48 -0700

    WIP. mani-editor. add Tooltip to RowInputWLabel

--- src/renderer/src/components/2-main/2-right/1-file-mani/1-form-editor/4-options/4-controls/4-row-input-w-label.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/1-form-editor/4-options/4-controls/4-row-input-w-label.tsx
@@ -1,16 +1,29 @@
 import { PrimitiveAtom } from "jotai";
-import { Label } from "@/ui";
+import { Label, Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger } from "@/ui";
 import { RowInputWAtom } from "./2-row-input-w-atom";
 
-export function RowInputWLabel({ valueAtom, label }: { valueAtom: PrimitiveAtom<string>; label: string; }) {
+export function RowInputWLabel({ label, valueAtom }: { label: string; valueAtom: PrimitiveAtom<string>; }) {
     return (<>
-        <Label className="grid grid-cols-subgrid col-span-2 items-center text-xs font-light">
-            <div className="">
-                {label}
-            </div>
-            <RowInputWAtom valueAtom={valueAtom} />
-        </Label>
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Label className="grid grid-cols-subgrid col-span-2 items-center text-xs font-light">
+                        <div className="">
+                            {label}
+                        </div>
+                        <RowInputWAtom valueAtom={valueAtom} />
+                    </Label>
+                </TooltipTrigger>
+
+                <TooltipPortal>
+                    <TooltipContent>
+                        1111111
+                    </TooltipContent>
+                </TooltipPortal>
+            </Tooltip>
+        </TooltipProvider>
+
     </>);
 }
 
-//TODO: validate value, show error tooltip, and sub-grid
+//TODO: validate value, show error tooltip
