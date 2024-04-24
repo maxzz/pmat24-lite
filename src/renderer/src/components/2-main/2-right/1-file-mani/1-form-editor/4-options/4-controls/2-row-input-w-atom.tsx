26469e56d857629b627324cfe3b574e044c82baa
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-23 19:18:42 -0700

    WIP. mani-editor. tooltip, 7. code clean up

--- src/renderer/src/components/2-main/2-right/1-file-mani/1-form-editor/4-options/4-controls/2-row-input-w-atom.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/1-form-editor/4-options/4-controls/2-row-input-w-atom.tsx
@@ -1,8 +1,7 @@
 import { InputHTMLAttributes, useEffect, useState } from 'react';
 import { PrimitiveAtom, useAtom } from 'jotai';
 import { classNames } from '@/utils';
-import { Popover, PopoverArrorWoBottom, PopoverContent, PopoverTrigger, Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger, inputRingClasses } from '@/ui';
-import { PopoverAnchor } from '@radix-ui/react-popover';
+import { Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger, inputRingClasses } from '@/ui';
 
 const rowInputClasses = "\
 px-2 py-1 h-6 \
@@ -65,55 +64,5 @@ export function RowInputWAtom({ valueAtom, className, ...rest }: { valueAtom: Pr
 
             </Tooltip>
         </TooltipProvider>
-    );
-}
-
-export function RowInputWAtom2({ valueAtom, className, ...rest }: { valueAtom: PrimitiveAtom<string>; } & InputHTMLAttributes<HTMLInputElement>) {
-    const [value, setValue] = useAtom(valueAtom);
-    const [openTooltip, setOpenTooltip] = useState(false);
-    return (
-        <Popover open={openTooltip} onOpenChange={setOpenTooltip}>
-            {/* <PopoverTrigger asChild>
-                <input
-                    className={classNames(rowInputClasses, inputRingClasses, className)}
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    // onClick={() => setOpenTooltip(v => !v)}
-                    onClick={() => {}}
-                    {...rest}
-                />
-            </PopoverTrigger> */}
-
-            <PopoverAnchor asChild>
-                <input
-                    className={classNames(rowInputClasses, inputRingClasses, className)}
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    // onClick={() => setOpenTooltip(v => !v)}
-                    onClick={() => { }}
-                    onBlur={() => {
-                        const isValid = value.trim().length > 0;
-                        if (!isValid) {
-                            setOpenTooltip(true);
-                        } else {
-                            setOpenTooltip(false);
-                        }
-                    }}
-                    {...rest}
-                />
-            </PopoverAnchor>
-
-            <PopoverContent className="px-4 py-2 w-auto max-w-64 text-xs border-popover-foreground text-red-500" sideOffset={-12} align="center" arrowPadding={20}>
-                This field cannot be empty <br />
-                This field cannot be empty <br />
-                This field cannot be empty <br />
-                This field cannot be empty
-                This field cannot be empty
-                This field cannot be empty
-
-                <PopoverArrorWoBottom />
-            </PopoverContent>
-
-        </Popover>
     );
 }
