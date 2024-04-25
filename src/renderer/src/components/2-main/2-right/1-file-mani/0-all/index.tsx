e494a814174f407e5c1d21e6d8ab8c1178fc0ad2
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-25 15:59:22 -0700

    WIP. mani-editor. tabs header. 1

--- src/renderer/src/components/2-main/2-right/1-file-mani/0-all/index.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/0-all/index.tsx
@@ -6,6 +6,15 @@ import { FormEditor } from "../1-form-editor";
 import { useMemo, useState } from "react";
 import { classNames } from "@/utils";
 
+function ManiTabList({ hasCpass }: { hasCpass: boolean; }) {
+    return (
+        <TabsList className="self-start">
+            <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
+            <TabsTrigger value="switch2" className="text-xs">{`${hasCpass ? 'Password change' : 'No password change'}`}</TabsTrigger>
+        </TabsList>
+    );
+}
+
 export function Body_Mani_() {
     const { ref, width, height } = useResizeObserver();
 
@@ -20,10 +29,7 @@ export function Body_Mani_() {
 
     return (
         <Tabs defaultValue="switch1" className="p-1 h-full flex flex-col" value={selectedTab} onValueChange={setSelectedTab} >
-            <TabsList className="self-start">
-                <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
-                <TabsTrigger value="switch2" className="text-xs">{`${hasCpass ? 'Password change' : 'No password change'}`}</TabsTrigger>
-            </TabsList>
+            <ManiTabList hasCpass={hasCpass} />
 
             <div className="flex-1 min-h-0 mt-1 p-2 pr-0 max-w-4xl rounded border-muted-foreground/20 border">
                 <div className="h-full w-full overflow-hidden" ref={ref}>
@@ -56,10 +62,7 @@ export function Body_Mani() {
 
     return (
         <Tabs defaultValue="switch1" className="p-1 h-full flex flex-col">
-            <TabsList className="self-start">
-                <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
-                <TabsTrigger value="switch2" className="text-xs">{`${hasCpass ? 'Password change' : 'No password change'}`}</TabsTrigger>
-            </TabsList>
+            <ManiTabList hasCpass={hasCpass} />
 
             <div className="flex-1 min-h-0 mt-1 p-2 pr-0 max-w-4xl rounded border-muted-foreground/20 border">
                 <div className="h-full w-full overflow-hidden" ref={ref}>
