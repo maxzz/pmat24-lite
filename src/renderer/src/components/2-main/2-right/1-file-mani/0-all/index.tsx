e9ccd7e0d3b9d0bfbd9411c70d802384d32d8887
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-24 18:38:02 -0700

    WIP. mani-editor. 5. w/ caching, but ..., hiding content w/ classes wo/ tab-content components

--- src/renderer/src/components/2-main/2-right/1-file-mani/0-all/index.tsx
+++ src/renderer/src/components/2-main/2-right/1-file-mani/0-all/index.tsx
@@ -3,7 +3,8 @@ import useResizeObserver from "use-resize-observer";
 import { ScrollArea, Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui";
 import { rightPanelSelectedContentAtom } from "@/store";
 import { FormEditor } from "../1-form-editor";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { classNames } from "@/utils";
 
 export function Body_Mani() {
     const { ref, width, height } = useResizeObserver();
@@ -14,7 +15,45 @@ export function Body_Mani() {
     }
 
     const hasCpass = fileUs.meta?.length === 2;
-    
+
+    const [selectedTab, setSelectedTab] = useState('switch1');
+
+    return (
+        <Tabs defaultValue="switch1" className="p-1 h-full flex flex-col" value={selectedTab} onValueChange={setSelectedTab} >
+            <TabsList className="self-start">
+                <TabsTrigger value="switch1" className="text-xs">Login</TabsTrigger>
+                <TabsTrigger value="switch2" className="text-xs">{`Password change${hasCpass ? '' : ': empty'}`}</TabsTrigger>
+            </TabsList>
+
+            <div className="flex-1 min-h-0 mt-1 p-2 pr-0 max-w-4xl rounded border-muted-foreground/20 border">
+                <div className="h-full w-full overflow-hidden" ref={ref}>
+                    <ScrollArea style={{ width, height }} horizontal fullheight>
+
+                        <div className={classNames(selectedTab !== "switch1" && "hidden")}>
+                            <FormEditor fileUs={fileUs} formIdx={0} />
+                        </div>
+
+                        <div className={classNames(selectedTab !== "switch2" && "hidden")}>
+                            <FormEditor fileUs={fileUs} formIdx={1} />
+                        </div>
+
+                    </ScrollArea>
+                </div>
+            </div>
+        </Tabs>
+    );
+}
+
+export function Body_Mani__() {
+    const { ref, width, height } = useResizeObserver();
+
+    const fileUs = useAtomValue(rightPanelSelectedContentAtom);
+    if (!fileUs) {
+        return null;
+    }
+
+    const hasCpass = fileUs.meta?.length === 2;
+
     return (
         <Tabs defaultValue="switch1" className="p-1 h-full flex flex-col">
             <TabsList className="self-start">
