725ad028176c390a88e8d1f0dc32c9874a9fa0a4
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-29 16:27:11 -0700

    WIP. mani-editor. fields. propagate createAtomsParams

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/index.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/index.ts
@@ -1,35 +1,33 @@
 import { FileUs, FileUsAtom, FormIdx } from "@/store/store-types";
-import { FormAtoms, ManiAtoms, ManiChangesAtom } from "../9-types";
+import { CreateAtomsParams, FormAtoms, ManiAtoms, ManiChangesAtom } from "../9-types";
 import { FieldsState } from "../1-fields/1-fields-atoms";
 import { SubmitState } from "../2-submit";
 import { PolicyState } from "../3-policy";
 import { OptionsState } from "../4-options";
 import { atom } from "jotai";
 
-function createFormAtoms(fileUs: FileUs, fileUsAtom: FileUsAtom, formIdx: FormIdx, changesAtom: ManiChangesAtom): FormAtoms | undefined {
+function createFormAtoms(createAtomsParams: CreateAtomsParams): FormAtoms | undefined {
 
-    const metaForm = fileUs.meta?.[formIdx];
+    const metaForm = createAtomsParams.fileUs.meta?.[createAtomsParams.formIdx];
     if (!metaForm) {
         return;
     }
 
-    const createParams = { fileUs, fileUsAtom, formIdx, changesAtom };
+    const fieldsAtoms = FieldsState.createUiAtoms(createAtomsParams);
 
-    const fieldsAtoms = FieldsState.createUiAtoms(createParams);
-
-    const submitAtoms = SubmitState.createUiAtoms(createParams,
+    const submitAtoms = SubmitState.createUiAtoms(createAtomsParams,
         ({ get, set }) => {
             SubmitState.debouncedCombinedResultFromAtoms(submitAtoms, get, set);
         }
     );
 
-    const policyAtoms = PolicyState.createUiAtoms(createParams,
+    const policyAtoms = PolicyState.createUiAtoms(createAtomsParams,
         ({ get, set }) => {
             PolicyState.debouncedCombinedResultFromAtoms(policyAtoms, get, set);
         }
     );
 
-    const optionsAtoms = OptionsState.createAtoms(createParams,
+    const optionsAtoms = OptionsState.createAtoms(createAtomsParams,
         ({ get, set }) => {
             //console.log('options changed', field, field.mani.displayname);
             OptionsState.debouncedCombinedResultFromAtoms(optionsAtoms, get, set);
@@ -42,17 +40,17 @@ function createFormAtoms(fileUs: FileUs, fileUsAtom: FileUsAtom, formIdx: FormId
         policyAtoms,
         optionsAtoms,
 
-        changesAtom,
-        fileUsAtom,
-        formIdx,
+        changesAtom: createAtomsParams.changesAtom,
+        fileUsAtom: createAtomsParams.fileUsAtom,
+        formIdx: createAtomsParams.formIdx,
     };
 }
 
 export function createManiAtoms(fileUs: FileUs, fileUsAtom: FileUsAtom): ManiAtoms {
-    const changesAtom = atom(0);
 
-    const loginAtoms = createFormAtoms(fileUs, fileUsAtom, FormIdx.login, changesAtom);
-    const cpassAtoms = createFormAtoms(fileUs, fileUsAtom, FormIdx.cpass, changesAtom);
+    const changesAtom = atom(0);
+    const loginAtoms = createFormAtoms({fileUs, fileUsAtom, formIdx: FormIdx.login, changesAtom});
+    const cpassAtoms = createFormAtoms({fileUs, fileUsAtom, formIdx: FormIdx.cpass, changesAtom});
 
     return [loginAtoms, cpassAtoms, changesAtom];
 }
