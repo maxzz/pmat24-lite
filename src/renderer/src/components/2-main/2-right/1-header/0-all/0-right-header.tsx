a04d07d3b1ecf597a502d0bbe5377c04e173b2d1
Max Zakharzhevskiy <maxzz@msn.com>
2024-04-29 17:51:44 -0700

    WIP. mani-editor. fix connect changes button. code clean up

--- src/renderer/src/components/2-main/2-right/1-header/0-all/0-right-header.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/0-right-header.tsx
@@ -29,7 +29,6 @@ function SaveButtonAccess({ fileUs }: { fileUs: FileUs; }) {
 }
 
 export function R_PanelHeader() {
-    
     const fileUs = useAtomValue(rightPanelContentAtom);
     if (!fileUs) {
         return (
@@ -38,7 +37,6 @@ export function R_PanelHeader() {
             </div>
         );
     }
-
     return (
         <div className={`${panelHeaderClasses} flex items-start justify-between`}>
             <TitleWithFileUs fileUs={fileUs} />
@@ -51,37 +49,3 @@ export function R_PanelHeader() {
         </div>
     );
 }
-
-
-// export function R_PanelHeader() {
-//     const fileUs = useAtomValue(rightPanelContentAtom);
-//     if (!fileUs) {
-//         return (
-//             <div className={`${panelHeaderClasses} h-10`}>
-//                 <TitleNoFile />
-//             </div>
-//         );
-//     }
-
-//     const atoms = useAtomValue(fileUs.atomsAtom);
-//     if (!atoms) {
-//         return null;
-//     }
-
-//     const changesCount = useAtomValue(atoms[2]);
-//     const hasChanges = !!changesCount;
-
-//     return (
-//         <div className={`${panelHeaderClasses} flex items-start justify-between`}>
-//             <TitleWithFileUs fileUs={fileUs} />
-
-//             <div className="flex items-center gap-2">
-//                 {hasChanges && (
-//                     <Button className="text-background bg-orange-400">Save</Button>
-//                 )}
-
-//                 <R_PanelMenu />
-//             </div>
-//         </div>
-//     );
-// }
