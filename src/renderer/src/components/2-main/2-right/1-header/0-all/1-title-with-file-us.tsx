4e692b2c8387b36461d2967011c1502d2af2fed4
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-01 13:10:15 -0700

    WIP. right-header. update header layout. 2

--- src/renderer/src/components/2-main/2-right/1-header/0-all/1-title-with-file-us.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/1-title-with-file-us.tsx
@@ -7,18 +7,18 @@ export function TitleWithFileUs({ fileUs }: { fileUs: FileUs; }) {
     const Icon = appTypeToIcon(icon, hasBailOut);
 
     return (
-        <div className="py-1 text-muted-foreground space-y-1">
+        <div className="py-1 text-muted-foreground space-y-1 cursor-default">
             
             <div className="flex items-center gap-1">
                 <Icon className="size-5" />
                 {fileUs.stats.domain
-                    ? `website ${fileUs.stats.domain}`
-                    : 'Windows App'
+                    ? `Website: ${fileUs.stats.domain}`
+                    : 'Windows application'
                 }
             </div>
 
-            <div className="">Title {fileUs.stats.title}</div>
-            <div className="">Name {fileUs.fname}</div>
+            <div className="" title="Title from file">{fileUs.stats.title}</div>
+            <div className="" title="Filename">{fileUs.fname}</div>
         </div>
     );
 }
