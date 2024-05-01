0f56ab947b40cfe6ab061127fe70bc1703575c93
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-01 13:18:01 -0700

    WIP. right-header. update header layout. 3. app type

--- src/renderer/src/components/2-main/2-right/1-header/0-all/1-title-with-file-us.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/1-title-with-file-us.tsx
@@ -1,6 +1,13 @@
 import { FileUs } from "@/store/store-types";
 import { appTypeToIcon, fileUsToAppType } from "@/store/store-utils";
 
+function TitleFinename({ fileUs }: { fileUs: FileUs; }) {
+    const name = fileUs.fname;
+    return (
+        <div className="" title="Filename">{name}</div>
+    );
+}
+
 export function TitleWithFileUs({ fileUs }: { fileUs: FileUs; }) {
 
     const { icon, hasBailOut } = fileUsToAppType(fileUs);
@@ -8,17 +15,23 @@ export function TitleWithFileUs({ fileUs }: { fileUs: FileUs; }) {
 
     return (
         <div className="py-1 text-muted-foreground space-y-1 cursor-default">
-            
+
             <div className="flex items-center gap-1">
                 <Icon className="size-5" />
-                {fileUs.stats.domain
-                    ? `Website: ${fileUs.stats.domain}`
-                    : 'Windows application'
+
+                {!fileUs.stats.domain
+                    ? 'Windows application'
+                    : (
+                        <>
+                            Website: <div className="1text-foreground text-sm 1font-semibold">{fileUs.stats.domain}</div>
+                        </>
+                    )
                 }
             </div>
 
             <div className="" title="Title from file">{fileUs.stats.title}</div>
-            <div className="" title="Filename">{fileUs.fname}</div>
+
+            <TitleFinename fileUs={fileUs} />
         </div>
     );
 }
