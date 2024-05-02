b38cc153d7b868fceca5b2489d5a7fd75ef015d4
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-01 18:40:40 -0700

    WIP. right-header. update header layout. 8. update components layout

--- src/renderer/src/components/2-main/2-right/1-header/0-all/2-mani-explanation.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/2-mani-explanation.tsx
@@ -1,23 +1,36 @@
 import { FileUs } from "@/store/store-types";
 import { isManual } from "@/store/store-utils";
 
+const ManiUrlPartsClasses = "\
+1text-foreground \
+hover:text-foreground \
+underline \
+underline-offset-2 \
+1underline-offset-auto \
+1text-sm \
+1font-semibold \
+";
+
 function ManiUrlParts({ url, domain }: { url: string | undefined; domain: string; }) {
     return (<>
         Login is defined for the site
 
-        <div className="1text-foreground hover:text-foreground underline underline-offset-2 1underline-offset-auto 1text-sm 1font-semibold">
+        <div className={ManiUrlPartsClasses}>
             {domain}
         </div>
     </>);
 }
 
 export function ManiExplanation({ fileUs }: { fileUs: FileUs; }) {
-    const manual = isManual(fileUs);
-    return (<>
-        {!fileUs.stats.domain
-            ? manual
-                ? 'Manually defined login for a Windows application'
-                : 'Login for a Windows application'
-            : <ManiUrlParts url={fileUs.stats.url} domain={fileUs.stats.domain} />}
-    </>);
+
+    if (!fileUs.stats.domain) {
+        const manual = isManual(fileUs);
+        return manual
+            ? 'Manually defined login for a Windows application'
+            : 'Login for a Windows application';
+    }
+
+    return (
+        <ManiUrlParts url={fileUs.stats.url} domain={fileUs.stats.domain} />
+    );
 }
