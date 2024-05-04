b0d7ecda7813cc704bcf64f472b3092d90504199
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-04 16:48:28 -0700

    WIP. mani-editor. connect policies forAtoms(). 4. code clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/3-policy/0-all/1-tab-all-policy.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/3-policy/0-all/1-tab-all-policy.tsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from "react";
-import { atom, useAtom, useAtomValue } from "jotai";
-import { Meta } from "@/store/manifest";
-import { PolicyState, TabSectionProps } from "@/store/atoms/3-file-mani-atoms";
+import { TabSectionProps } from "@/store/atoms/3-file-mani-atoms";
 import { TableHeader } from "./2-grid-header";
 import { PoliciesGrid } from "./3-policies-grid";
 import { NoPasswordsForPolies } from "./9-no-polies";
 
-export function TabPolicy({ formAtoms, formIdx }: TabSectionProps) {
+export function TabPolicy({ formAtoms }: TabSectionProps) {
 
     const policies = formAtoms.policyAtoms;
     if (!policies.length) {
@@ -15,30 +12,10 @@ export function TabPolicy({ formAtoms, formIdx }: TabSectionProps) {
         );
     }
 
-    // const policiesAtom = useState(() => atom<Meta.Field[]>([]))[0];
-    // const [policies, setPolicies] = useAtom(policiesAtom);
-
-    // const fileUs = formAtoms.params.fileUs;
-    // const metaForm = formAtoms.params.fileUs.meta?.[formIdx]!; // We are under createFormAtoms umbrella, so we can safely use ! here
-
-    // useEffect(
-    //     () => {
-    //         const fieldsWPolicy = metaForm?.fields?.filter((field) => field.mani.policy || field.mani.policy2) || []; // and add psw fields that may have policy
-    //         setPolicies(fieldsWPolicy);
-    //     }, [fileUs]
-    // );
-
     return (
         <div className="ml-1">
             <div className="pl-3 pr-2 py-2 grid grid-cols-[auto_minmax(0,1fr)_auto] gap-x-1 items-stretch rounded">
                 <TableHeader />
-
-                {/* {policies.map(
-                    (policy, idx) => (
-                        <PolicyItem key={idx} policyAtoms={policy} />
-                    )
-                )} */}
-
                 <PoliciesGrid policies={policies} />
             </div>
         </div>
