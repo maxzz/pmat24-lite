6afeee821076193419cbbf35725650bac7760f82
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-05 16:58:15 -0700

    WIP. mani-editor. options. add new RawInput w/ experimants, 6. code clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/4-options/4-controls/5-row-input-w-atom.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/4-options/4-controls/5-row-input-w-atom.tsx
@@ -1,8 +1,12 @@
-import { InputHTMLAttributes, useEffect, useState } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
 import { PrimitiveAtom, atom, useAtom, useAtomValue } from 'jotai';
 import { classNames } from '@/utils';
 import { Label, Tooltip, TooltipContent, TooltipPortal, TooltipProvider, TooltipTrigger, inputRingClasses } from '@/ui';
 import { SymbolWarning } from '@/ui/icons';
+
+function validateError(value: string) {
+    return value === '111' ? '' : `Value ${value} is invalid, should be 111`;
+}
 
 const rowInputClasses = "\
 px-2 py-1 h-6 w-full \
@@ -50,19 +54,20 @@ function RawInput({ label, stateAtom, className, ...rest }: RowInputProps) {
     );
 }
 
-export function RowInputWAtom({ label, valueAtom, className, ...rest }: RowInputWAtomProps) {
-    // const [value, setValue] = useAtom(valueAtom);
-    const [openTooltip, setOpenTooltip] = useState(false);
+type RowInputWAtomProps = InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+    valueAtom: PrimitiveAtom<string>;
+};
 
-    // const [touched, setTouched] = useState(false);
-    // const [error, setError] = useState('');
+export function RowInputWAtom({ label, valueAtom, className, ...rest }: RowInputWAtomProps) {
+    const [openTooltip, setOpenTooltip] = useState(false);
 
     const stateAtom = useState(() => atom<RowInputState<string>>({ data: 'value', error: undefined, touched: undefined, validate: validateError }))[0];
     const state = useAtomValue(stateAtom);
 
     return (
         <Label className="grid grid-cols-subgrid col-span-2 items-center text-xs font-light">
-            <div className="">
+            <div>
                 {label}
             </div>
 
@@ -71,22 +76,6 @@ export function RowInputWAtom({ label, valueAtom, className, ...rest }: RowInput
 
                     <div className="relative">
                         <RawInput label={label} stateAtom={stateAtom} className={className} {...rest} />
-
-                        {/* <input
-                        className={classNames(rowInputClasses, inputRingClasses/*, error && "ring-1 ring-red-500/70"* /, className)}
-                        value={value}
-                        onChange={(e) => {
-                            setValue(e.target.value);
-                            const errorMsg = validateError?.(e.target.value) ?? '';
-                            setError(errorMsg);
-                        }}
-                        onBlur={() => {
-                            setTouched(true);
-                            const errorMsg = validateError?.(value) ?? '';
-                            setError(errorMsg);
-                        }}
-                        {...rest}
-                    /> */}
 
                         <TooltipTrigger asChild>
                             <div>
@@ -108,67 +97,5 @@ export function RowInputWAtom({ label, valueAtom, className, ...rest }: RowInput
                 </Tooltip>
             </TooltipProvider>
         </Label>
-    );
-}
-
-
-
-
-function validateError(value: string) {
-    return value === '111' ? '' : `Value ${value} is invalid, should be 111`;
-}
-
-type RowInputWAtomProps = InputHTMLAttributes<HTMLInputElement> & {
-    label: string;
-    valueAtom: PrimitiveAtom<string>;
-};
-
-export function RowInputWAtom_Old({ valueAtom, className, ...rest }: RowInputWAtomProps) {
-    const [value, setValue] = useAtom(valueAtom);
-    const [openTooltip, setOpenTooltip] = useState(false);
-
-    const [touched, setTouched] = useState(false);
-    const [error, setError] = useState('');
-
-    return (
-        <TooltipProvider>
-            <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
-
-                <div className="relative">
-                    <input
-                        className={classNames(rowInputClasses, inputRingClasses/*, error && "ring-1 ring-red-500/70"*/, className)}
-                        value={value}
-                        onChange={(e) => {
-                            setValue(e.target.value);
-                            const errorMsg = validateError?.(e.target.value) ?? '';
-                            setError(errorMsg);
-                        }}
-                        onBlur={() => {
-                            setTouched(true);
-                            const errorMsg = validateError?.(value) ?? '';
-                            setError(errorMsg);
-                        }}
-                        {...rest}
-                    />
-
-                    <TooltipTrigger asChild>
-                        <div>
-                            {error && (
-                                <SymbolWarning className="absolute mt-px mr-px right-3 top-1/2 transform -translate-y-1/2 size-4 text-red-500/90" />
-                            )}
-                        </div>
-                    </TooltipTrigger>
-                </div>
-
-                {error && touched && (
-                    <TooltipPortal container={document.getElementById("portal")}>
-                        <TooltipContent align="end" sideOffset={-2}>
-                            {error}
-                        </TooltipContent>
-                    </TooltipPortal>
-                )}
-
-            </Tooltip>
-        </TooltipProvider>
     );
 }
