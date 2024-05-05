fd883a9f706746a3fe1b0b5404b9a52e58251b1d
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-05 14:24:45 -0700

    WIP. mani-editor. options. add 10-option-row.tsx

---
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/4-options/4-controls/10-option-row.tsx
@@ -0,0 +1,54 @@
+import { InputHTMLAttributes } from 'react';
+import { classNames } from '@/utils';
+import { inputRingClasses } from '@/ui';
+import { PrimitiveAtom, useAtom } from 'jotai';
+
+export type AccessAtom = PrimitiveAtom<string | number>;
+
+export type OptionRowInputProps<T> = InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+    accessAtom: T;
+};
+
+const optionRowInputClasses = "\
+px-2 py-1 h-6 \
+\
+text-mani-foreground bg-mani-background \
+\
+border-mani-border-muted border \
+\
+rounded-sm \
+outline-none";
+
+export function OptionRowInput<T extends PrimitiveAtom<string | number>>({ label, accessAtom, className, ...rest }: OptionRowInputProps<T>) {
+    const [value, setValue] = useAtom(accessAtom);
+    return (<>
+        <div className="">
+            {label}
+        </div>
+
+        <input
+            className={classNames(optionRowInputClasses, inputRingClasses, className)}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            {...rest}
+        />
+    </>);
+}
+
+export function OptionRowCheck<T extends PrimitiveAtom<boolean>>({ label, accessAtom, className, ...rest }: OptionRowInputProps<T>) {
+    const [value, setValue] = useAtom(accessAtom);
+    return (<>
+        <div className="">
+            {label}
+        </div>
+
+        <input
+            className={classNames("place-self-center size-4 dark-checkbox", className)}
+            type="checkbox"
+            checked={value}
+            onChange={() => setValue(v => !v)}
+            {...rest}
+        />
+    </>);
+}
