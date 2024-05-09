152810fdc6c764da535a6fb1427579b2262e887e
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-09 14:24:53 -0700

    WIP. mani-editor. add MenuItem_SaveCurrent

---
+++ src/renderer/src/components/1-header/1-main-menu/10-file/15-save-current.tsx
@@ -0,0 +1,19 @@
+import { useAtomValue, useSetAtom } from "jotai";
+import { DropdownMenuItem } from "@/ui/shadcn";
+import { rightPanelAtom } from "@/store";
+import { doSaveOneAtom } from "@/store/atoms/3-file-mani-atoms/8-save-reset-one";
+
+export function MenuItem_SaveCurrent() {
+
+    const fileUsAtom = useAtomValue(rightPanelAtom);
+    const doSaveOne = useSetAtom(doSaveOneAtom);
+
+    return (<>
+        <DropdownMenuItem onClick={() => fileUsAtom && doSaveOne(fileUsAtom)} disabled={!fileUsAtom}>
+            Save
+            {/* <DropdownMenuShortcut className={menuShortcutClasses}>{shortcutNameCreate}</DropdownMenuShortcut> */}
+        </DropdownMenuItem>
+    </>);
+}
+
+//TODO: add shortcut Ctrl+S
