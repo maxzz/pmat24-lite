eae3f65de3cfa2290330acb5d5359fa772a90bec
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-08 17:20:00 -0700

    WIP. mani-editor. options. changes indicator. 3. OK. add customization with css properties

--- src/renderer/src/ui/icons/symbols/all-other/24-fire.tsx
+++ src/renderer/src/ui/icons/symbols/all-other/24-fire.tsx
@@ -4,24 +4,19 @@ import { classNames } from "@/utils";
 export function SvgSymbolFire() {
     return (<>
         <symbol id="icon-fire" viewBox="0 0 24 24">
-            <path className="fill-[var(--fill-a,none)] stroke-[var(--stroke-a,current)]" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
-            <path className="fill-[var(--fill-b,none)] stroke-[var(--stroke-b,current)]" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+            <path className="fill-[var(--fill-a,none)] stroke-[var(--stroke-a,currentColor)]" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+            <path className="fill-[var(--fill-b,none)] stroke-[var(--stroke-b,currentColor)]" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
         </symbol>
     </>);
 }
 
-// export function SvgSymbolFire() {
-//     return (<>
-//         <symbol id="icon-fire" viewBox="0 0 24 24">
-//             <path className="fill-orange-600" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
-//             <path className="fill-orange-50" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
-//         </symbol>
-//     </>);
-// }
+//fill-orange-600 #ea580c
+//fill-orange-50  #fff7ed
+const fireIconClasses = "fill-none stroke-current stroke-[1.5] [--fill-a:#ea580c] [--fill-b:#fff7ed]";
 
 export function SymbolFire({ className, children, title, ...rest }: SVGAttributes<SVGSVGElement> & HTMLAttributes<SVGSVGElement>) {
     return (
-        <svg className={classNames("fill-none stroke-current stroke-[1.5] [--fill1-a:red]", className)} strokeLinecap="round" strokeLinejoin="round" {...rest}>
+        <svg className={classNames(fireIconClasses, className)} strokeLinecap="round" strokeLinejoin="round" {...rest}>
             {title && <title>{title}</title>}
             {children}
             <use xlinkHref="#icon-fire" />
