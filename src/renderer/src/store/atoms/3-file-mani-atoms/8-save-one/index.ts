863d8ab40533d1fb23d871825d4a7f5a36c4d121
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-09 13:49:28 -0700

    WIP. mani-editor. add saveOneAtoms

---
+++ src/renderer/src/store/atoms/3-file-mani-atoms/8-save-one/index.ts
@@ -0,0 +1,23 @@
+import { atom } from "jotai";
+import { FileUs } from "@/store/store-types";
+import { ManiAtoms } from "../9-types";
+
+const saveOneAtoms = atom(null,
+    (get, set, maniAtoms: ManiAtoms) => {
+        const loginFormAtoms = maniAtoms[0];
+        const cpassFormAtoms = maniAtoms[1];
+
+        if (loginFormAtoms) {
+            loginFormAtoms.params.fileUsAtom
+
+            loginFormAtoms.fieldsAtoms
+            loginFormAtoms.submitAtoms
+            loginFormAtoms.policyAtoms
+            loginFormAtoms.optionsAtoms
+        }
+    }
+)
+
+//TODO: file save/reset from atoms
+//TODO: submit editor
+//TODO: policy editor as part of fields editor
