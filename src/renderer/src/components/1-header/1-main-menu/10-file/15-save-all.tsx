2fd7f5829a9e5b2acfbc008332892cf3f3519004
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-09 14:14:45 -0700

    WIP. mani-editor. add doSaveAllAtom UI condition

--- src/renderer/src/components/1-header/1-main-menu/10-file/15-save-all.tsx
+++ src/renderer/src/components/1-header/1-main-menu/10-file/15-save-all.tsx
@@ -1,15 +1,18 @@
 import { useSetAtom } from "jotai";
-import { doOpenCreateDialogAtom, shortcutNameCreate } from "@/store/atoms/7-dialogs";
-import { DropdownMenuItem, DropdownMenuShortcut } from "@/ui/shadcn";
-import { menuShortcutClasses } from "@/ui/shared-styles";
+import { useSnapshot } from "valtio";
+import { DropdownMenuItem } from "@/ui/shadcn";
 import { doSaveAllAtom } from "@/store/atoms/4-save-reset-all";
+import { allFileUsChanges } from "@/store/atoms/3-file-mani-atoms";
 
 export function MenuItem_SaveAll() {
     const doSaveAll = useSetAtom(doSaveAllAtom);
+    const hasChanges = !!useSnapshot(allFileUsChanges).size;
     return (<>
-        <DropdownMenuItem onClick={doSaveAll}>
+        <DropdownMenuItem onClick={doSaveAll} disabled={!hasChanges}>
             Save All
-            <DropdownMenuShortcut className={menuShortcutClasses}>{shortcutNameCreate}</DropdownMenuShortcut>
+            {/* <DropdownMenuShortcut className={menuShortcutClasses}>{shortcutNameCreate}</DropdownMenuShortcut> */}
         </DropdownMenuItem>
     </>);
 }
+
+//TODO: add shortcut
