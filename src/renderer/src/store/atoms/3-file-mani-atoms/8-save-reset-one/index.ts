ba34fc98588fff9b9dc5ddbf7106dc8df1650633
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-09 13:57:23 -0700

    WIP. mani-editor. saveOneAtom new params

--- src/renderer/src/store/atoms/3-file-mani-atoms/8-save-reset-one/index.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/8-save-reset-one/index.ts
@@ -1,9 +1,19 @@
 import { atom } from "jotai";
-import { FileUs } from "@/store/store-types";
+import { FileUs, FileUsAtom } from "@/store/store-types";
 import { ManiAtoms } from "../9-types";
 
-export const saveOneAtoms = atom(null,
-    (get, set, maniAtoms: ManiAtoms) => {
+export const saveOneAtom = atom(null,
+    (get, set, fileUsAtom: FileUsAtom) => {
+        const fileUs = get(fileUsAtom);
+
+        const maniAtoms = get(fileUs.atomsAtom);
+        if (!maniAtoms) {
+            return;
+        }
+
+        /*
+        (get, set, maniAtoms: ManiAtoms) => {
+
         const loginFormAtoms = maniAtoms[0];
         const cpassFormAtoms = maniAtoms[1];
 
@@ -15,8 +25,9 @@ export const saveOneAtoms = atom(null,
             loginFormAtoms.policyAtoms
             loginFormAtoms.optionsAtoms
         }
+        */
     }
-)
+);
 
 //TODO: file save/reset from atoms
 //TODO: submit editor
