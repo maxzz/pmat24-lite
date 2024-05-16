3f09495a4422f8ed83b75ca59c3ccdd9584b705f
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-15 20:00:03 -0700

    WIP. mani-editor. policy dlg. test area 8. ---------- remove old accordion trigger

--- src/renderer/src/components/4-dialogs/7-policy/2-complexity/1-all.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/2-complexity/1-all.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { atom, useAtom } from "jotai";
 import { namesConstrainSet } from "@/store/manifest";
 import { PolicyDlgConv } from "../0-all/0-conv";
@@ -6,7 +7,6 @@ import { classNames } from "@/utils";
 import { Button, Input, Label } from "@/ui";
 import { SectionMinMaxLength } from "./2-min-max-length";
 import { SectionTestRoom } from "./4-test-room";
-import { useState } from "react";
 
 const selectNames = [...namesConstrainSet, 'Custom rule'];
 
@@ -31,24 +31,22 @@ export function SectionRuleTypes({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms
                 <SectionMinMaxLength atoms={atoms} />
             </div>
 
-            <div className={classNames("flex items-center gap-2", !isCustom && "invisible pointer-events-none")}>
-                <div className={classNames("flex-1 relative h-8 flex items-center justify-between space-x-2")}>
-                    <Input className="flex-1" />
-
-                    <Button className="absolute right-2 aspect-square rounded-full" variant="outline" size="xs" tabIndex={-1} title="Explanation" >
-                        ?
+            <div>
+                <div className={classNames("flex items-center gap-2", !isCustom && "invisible pointer-events-none")}>
+                    <div className={classNames("flex-1 relative h-8 flex items-center justify-between space-x-2")}>
+                        <Input className="flex-1" />
+                        <Button className="absolute right-2 aspect-square rounded-full" variant="outline" size="xs" tabIndex={-1} title="Explanation" >
+                            ?
+                        </Button>
+                    </div>
+                    <Button onClick={() => setIsTestAreaOpen(isTestAreaOpen.length ? [] : ['policy'])}>
+                        Test area
                     </Button>
                 </div>
 
-                <Button onClick={() => setIsTestAreaOpen(isTestAreaOpen.length ? [] : ['policy'])}>
-                    Test area
-                </Button>
-
-
+                <SectionTestRoom atoms={atoms} isTestAreaOpenAtom={isTestAreaOpenAtom} />
             </div>
-
-            <SectionTestRoom atoms={atoms} isTestAreaOpenAtom={isTestAreaOpenAtom} />
-
+            
         </div>
     );
 }
--- src/renderer/src/components/4-dialogs/7-policy/2-complexity/4-test-room.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/2-complexity/4-test-room.tsx
@@ -1,19 +1,12 @@
-import { ReactNode, useState } from "react";
+import { ReactNode } from "react";
+import { PrimitiveAtom, useAtom } from "jotai";
+import { Accordion, AccordionContent, AccordionItem } from "@/ui";
 import { PolicyDlgConv } from "../0-all/0-conv";
 import { TestAreaBody } from "./3-test-area-body";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/ui/shadcn/accordion";
-import { Button } from "@/ui";
-import { PrimitiveAtom, useAtom } from "jotai";
 
-function TestAreaSection({ value, label, children }: { value: string; label: ReactNode; children: ReactNode; }) {
+function TestAreaSection({ value, children }: { value: string; children: ReactNode; }) {
     return (
         <AccordionItem value={value} className="border-none">
-            {/* <div className="inline-block">
-                <AccordionTrigger iconFirst leftDown className="pl-2 pr-4 py-2 text-xs border-border border rounded shadow">
-                    {label}
-                </AccordionTrigger>
-            </div> */}
-
             <AccordionContent>
                 {children}
             </AccordionContent>
@@ -22,19 +15,18 @@ function TestAreaSection({ value, label, children }: { value: string; label: Rea
 }
 
 function TestAreaOpenState({ children, isTestAreaOpenAtom }: { children: ReactNode; isTestAreaOpenAtom: PrimitiveAtom<string[]>; }) {
-    //const [value, setvalue] = useState<string[]>([]);
     const [isTestAreaOpen, setIsTestAreaOpen] = useAtom(isTestAreaOpenAtom);
-    return (<>
+    return (
         <Accordion type="multiple" value={isTestAreaOpen} onValueChange={(v) => setIsTestAreaOpen(v)}>
             {children}
         </Accordion>
-    </>);
+    );
 }
 
 export function SectionTestRoom({ atoms, isTestAreaOpenAtom }: { atoms: PolicyDlgConv.PolicyUiAtoms; isTestAreaOpenAtom: PrimitiveAtom<string[]>; }) {
     return (
         <TestAreaOpenState isTestAreaOpenAtom={isTestAreaOpenAtom}>
-            <TestAreaSection label="Test area" value="policy">
+            <TestAreaSection value="policy">
 
                 <TestAreaBody />
 
