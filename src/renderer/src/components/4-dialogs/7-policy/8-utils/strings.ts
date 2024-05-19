72c6826738ec18ce292efbbb000fc753afce248a
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-19 14:04:00 -0700

    WIP. policy cpp port. 9. explanation. 3. add PolicyFunctions

--- src/renderer/src/components/4-dialogs/7-policy/8-utils/strings.ts
+++ src/renderer/src/components/4-dialogs/7-policy/8-utils/strings.ts
@@ -47,3 +47,16 @@ export namespace PolicyStrings {
     export const IDS_PSW_VALUE_EMPTY		/**/ = "Enter Password";
     export const IDS_PSW_VALUES_SAME        /**/ = "Current and new password fields have same values.";
 }
+
+export namespace PolicyFunctions {
+    export const IDS_PSW_POLICY_LENGTH = (min: number, max: number) =>          /**/ `   Password length must be between ${min} and ${max} characters.\n`;
+    export const IDS_PSW_POLICY_ACHSET = (s: string) =>	                        /**/ `   Password must contain a character from ${s}.\n`;
+    export const IDS_PSW_POLICY_MINCHSET = (d: number, s: string)	            /**/ => `   Password must contain at least ${d} character(s) from ${s}.\n`;
+    export const IDS_PSW_POLICY_MAXCHSET = (d: number, s: string)	            /**/ => `   Password must contain only ${d} character(s) from ${s}.\n`;
+    export const IDS_PSW_POLICY_MMCHSET = (d1: number, d2: number, s: string)   /**/ => `   Password must contain at least ${d1} character(s) and not more than  ${d2} character(s) from ${s}.\n`;
+    export const IDS_PSW_POLICY_REPEAT = ()	                                    /**/ => "   Password must contain repeated occurrence of:\n";
+    export const IDS_PSW_POLICY_NOREPEAT = ()	                                /**/ => "   Each password character must only be used one time.\n";
+    export const IDS_PSW_VALUES_DIFFER = ()                                     /**/ => "You must enter the same password in both new password fields.";
+    export const IDS_PSW_VALUE_EMPTY = ()		                                /**/ => "Enter Password";
+    export const IDS_PSW_VALUES_SAME = ()                                       /**/ => "Current and new password fields have same values.";
+}
