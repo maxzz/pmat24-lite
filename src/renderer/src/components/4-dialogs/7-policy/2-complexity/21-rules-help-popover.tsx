11b84af181dcd2202e26136257ead7cf5b0521d5
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-24 13:59:27 -0700

    WIP. policy cpp port. add RulesHelpPopover scroll area

--- src/renderer/src/components/4-dialogs/7-policy/2-complexity/21-rules-help-popover.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/2-complexity/21-rules-help-popover.tsx
@@ -1,12 +1,12 @@
 import { Fragment } from "react";
-import { Button, Popover, PopoverArrorWoBottom, PopoverClose, PopoverContent, PopoverPortal, PopoverTrigger } from "@/ui";
+import { Button, Popover, PopoverArrorWoBottom, PopoverClose, PopoverContent, PopoverPortal, PopoverTrigger, ScrollArea } from "@/ui";
 import { helpRules } from "./20-help-rules";
 import { SymbolCross } from "@/ui/icons";
 
 //TODO: it should be popup with trigger button
 export function RulesHelpPopover() {
     return (
-        <Popover>
+        <Popover open>
             {/* <PopoverTrigger className="absolute right-2 h-6 aspect-square rounded-full">
                     ?
                 </PopoverTrigger>
@@ -18,21 +18,24 @@ export function RulesHelpPopover() {
             </PopoverTrigger>
 
             <PopoverPortal>
-                <PopoverContent className="relative text-foreground bg-background border-border border shadow" sideOffset={5} align="center">
+                <PopoverContent className="relative p-0 w-96 text-foreground bg-background border-border border shadow" sideOffset={5} align="center">
 
-                    <div className="p-4 max-w-2xl text-xs">
+                    <div className="p-4 text-xs">
                         <div className="mb-4 text-center">
                             Custom rule
                         </div>
 
-                        <div className="grid grid-cols-[auto,auto] gap-2">
-                            {helpRules.map((rule, idx) => (
-                                <Fragment key={idx}>
-                                    <div className="font-bold">{rule.c1}</div>
-                                    <div>{rule.c2}</div>
-                                </Fragment>
-                            ))}
-                        </div>
+                        <ScrollArea className="h-64">
+
+                            <div className="p-4 grid grid-cols-[auto,auto] gap-2">
+                                {helpRules.map((rule, idx) => (
+                                    <Fragment key={idx}>
+                                        <div className="font-bold">{rule.c1}</div>
+                                        <div>{rule.c2}</div>
+                                    </Fragment>
+                                ))}
+                            </div>
+                        </ScrollArea>
                     </div>
 
                     <PopoverClose className="absolute top-4 right-4">
