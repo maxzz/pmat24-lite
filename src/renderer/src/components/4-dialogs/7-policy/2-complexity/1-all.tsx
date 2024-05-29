9c4b3a51499c881b720c6645fb93c8f206764659
Max Zakharzhevskiy <maxzz@msn.com>
2024-05-28 17:14:12 -0700

    WIP. policy cpp port. SectionRuleTypes new layout. 3. split to components

--- src/renderer/src/components/4-dialogs/7-policy/2-complexity/1-all.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/2-complexity/1-all.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { atom, useAtom } from "jotai";
+import { atom, useAtom, useAtomValue } from "jotai";
 import { namesConstrainSet } from "@/store/manifest";
 import { PolicyDlgConv } from "../0-all/0-conv";
 import { Dropdown } from "../9-constrols";
@@ -11,53 +11,66 @@ import { RulesHelpPopover } from "./21-rules-help-popover";
 
 const selectNames = [...namesConstrainSet, 'Use custom rule'];
 
-export function SectionRuleTypes({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms; }) {
-
+function RuleSelectSection({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms; }) {
     const [selected, setSelected] = useAtom(atoms.constrainSetAtom);
     const isCustom = +selected === selectNames.length - 1;
-    const [isCustomRule, setIsCustomRule] = useAtom(atoms.isCustomRuleAtom);
+    const isTestAreaOpenAtom = useState(() => atom<string[]>([]))[0];
+    return (
+        <div className="space-y-1">
+            <div className="">
+                Password complexity rule
+            </div>
+
+            <div className="flex items-center justify-between gap-2">
+                <Label className="flex-1 text-xs flex flex-col 1items-center">
+                    <Dropdown items={selectNames} value={selected} onValueChange={setSelected} />
+                </Label>
+
+                {!isCustom && (
+                    <SectionMinMaxLength atoms={atoms} />
+                )}
+            </div>
+        </div>
+    );
+}
+
+function CustomRuleSection({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms; }) {
+    const selected = useAtomValue(atoms.constrainSetAtom);
+    const isCustom = +selected === selectNames.length - 1;
+    //const [isCustomRule, setIsCustomRule] = useAtom(atoms.isCustomRuleAtom);
 
     const isTestAreaOpenAtom = useState(() => atom<string[]>([]))[0];
     const [isTestAreaOpen, setIsTestAreaOpen] = useAtom(isTestAreaOpenAtom);
+    return (
+        <div>
+            <div className={classNames("flex items-center gap-2", !isCustom && "invisible pointer-events-none")}>
 
+                <div className="w-full space-y-1">
+                    <div className="">Custom rule</div>
+
+                    <div className={classNames("flex-1 relative h-8 flex items-center justify-between space-x-2")}>
+                        <Input className="flex-1 h-8" />
+                        <RulesHelpPopover />
+                    </div>
+
+                    <Button size="sm" onClick={() => setIsTestAreaOpen(isTestAreaOpen.length ? [] : ['policy'])}>
+                        Test area
+                    </Button>
+                </div>
+            </div>
+
+            {isCustom && (
+                <SectionTestRoom atoms={atoms} isTestAreaOpenAtom={isTestAreaOpenAtom} />
+            )}
+        </div>
+    );
+}
+
+export function SectionRuleTypes({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms; }) {
     return (
         <div className="flex flex-col gap-2 select-none">
-
-            <div className="space-y-1">
-                <div className="">Rule for determining password complexity</div>
-                <div className="flex items-center justify-between gap-2">
-                    <Label className="flex-1 text-xs flex flex-col 1items-center">
-                        <Dropdown items={selectNames} value={selected} onValueChange={setSelected} />
-                    </Label>
-
-                    {!isCustom && (
-                        <SectionMinMaxLength atoms={atoms} />
-                    )}
-                </div>
-            </div>
-
-            <div>
-                <div className={classNames("flex items-center gap-2", !isCustom && "invisible pointer-events-none")}>
-
-                    <div className="w-full space-y-1">
-                        <div className="">Custom rule</div>
-
-                        <div className={classNames("flex-1 relative h-8 flex items-center justify-between space-x-2")}>
-                            <Input className="flex-1 h-8" />
-                            <RulesHelpPopover />
-                        </div>
-
-                        <Button size="sm" onClick={() => setIsTestAreaOpen(isTestAreaOpen.length ? [] : ['policy'])}>
-                            Test area
-                        </Button>
-                    </div>
-                </div>
-
-                {isCustom && (
-                    <SectionTestRoom atoms={atoms} isTestAreaOpenAtom={isTestAreaOpenAtom} />
-                )}
-            </div>
-
+            <RuleSelectSection atoms={atoms} />
+            <CustomRuleSection atoms={atoms} />
         </div>
     );
 }
--- src/renderer/src/components/4-dialogs/7-policy/2-complexity/2-min-max-length.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/2-complexity/2-min-max-length.tsx
@@ -21,3 +21,23 @@ export function SectionMinMaxLength({ atoms }: { atoms: PolicyDlgConv.PolicyUiAt
         </div>
     );
 }
+
+export function SectionMinMaxLength_Old({ atoms }: { atoms: PolicyDlgConv.PolicyUiAtoms; }) {
+    const [min, setMin] = useAtom(atoms.minLengthAtom);
+    const [max, setMax] = useAtom(atoms.maxLengthAtom);
+    return (
+        <div className="flex items-center space-x-2">
+            <div>
+                Length
+            </div>
+
+            <Input className="h-8 text-xs max-w-[6ch]" value={`${min}`} onChange={(e) => setMin(+e.target.value)} />
+
+            <div className="">
+                to
+            </div>
+
+            <Input className="h-8 text-xs max-w-[6ch]" value={`${max}`} onChange={(e) => setMax(+e.target.value)} />
+        </div>
+    );
+}
