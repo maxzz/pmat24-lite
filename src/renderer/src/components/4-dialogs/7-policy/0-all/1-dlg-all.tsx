adac5708f99d2984bef58c801e1a4e69c026487c
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-03 19:08:09 -0700

    WIP. PolicyEditorNewDlg. add applyPolicyChanges()

--- src/renderer/src/components/4-dialogs/7-policy/0-all/1-dlg-all.tsx
+++ src/renderer/src/components/4-dialogs/7-policy/0-all/1-dlg-all.tsx
@@ -1,37 +1,35 @@
 import { useMemo, useState } from "react";
-import { PrimitiveAtom, atom, useAtom, useAtomValue, useSetAtom } from "jotai";
+import { Getter, PrimitiveAtom, Setter, atom, useAtom, useAtomValue, useSetAtom } from "jotai";
 import { Mani } from "pm-manifest";
 import { createUiAtoms, debouncedCombinedResultFromAtoms } from "./0-create-ui-atoms";
 import { Dialog, DialogCloseButton, DialogContent } from "@/ui";
 import { PolicyEditorBody } from "./2-dlg-body";
 import { PolicyDlgConv } from "./0-conv";
-import { policyToStrings } from "@/store/manifest";
 
 type PolicyEditorNewDlgProps = {
     openAtom: PrimitiveAtom<boolean>;
     policiesAtom: PrimitiveAtom<Mani.FieldPolicy>;
 };
 
+function applyPolicyChanges(get: Getter, set: Setter, dlgUiAtoms: PolicyDlgConv.PolicyUiAtoms) {
+    const state = PolicyDlgConv.fromAtoms(dlgUiAtoms, get, set);
+
+    if (!dlgUiAtoms.changed) {
+        return;
+    }
+    const strings = PolicyDlgConv.forMani(state);
+
+    //TODO: get access to setManiChanges()
+
+    console.log(`PolicyEditorNewDlg changed=${dlgUiAtoms.changed}`, JSON.stringify(state, null, 2));
+    console.log(`PolicyEditorNewDlg changed=${dlgUiAtoms.changed}`, JSON.stringify(strings, null, 2));
+}
+
 export function PolicyEditorNewDlg({ openAtom, policiesAtom }: PolicyEditorNewDlgProps) {
     const [isOpen, setIsOpen] = useAtom(openAtom);
     const policies = useAtomValue(policiesAtom);
 
-    const doSetResultAtom = useState(
-        () => atom(
-            null,
-            (get, set, dlgUiAtoms: PolicyDlgConv.PolicyUiAtoms) => {
-                const state = PolicyDlgConv.fromAtoms(dlgUiAtoms, get, set);
-
-                if (!dlgUiAtoms.changed) {
-                    return;
-                }
-                const strings = PolicyDlgConv.forMani(state);
-
-                console.log(`PolicyEditorNewDlg changed=${dlgUiAtoms.changed}`, JSON.stringify(state, null, 2));
-                console.log(`PolicyEditorNewDlg changed=${dlgUiAtoms.changed}`, JSON.stringify(strings, null, 2));
-            }
-        )
-    )[0];
+    const doSetResultAtom = useState(() => atom(null, applyPolicyChanges))[0];
     const doSetResult = useSetAtom(doSetResultAtom);
 
     const dlgUiAtoms = useMemo(
