86b0c6c3701fb47f3e08d934304cbf84248a30bc
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-06 20:40:32 -0700

    WIP. PolicyEditorNewDlg. test area layout. 1

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-test-area-body.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-test-area-body.tsx
@@ -1,20 +1,22 @@
 import { Button, Input } from "@/ui";
 
+const localInputClasses = "text-mani-foreground bg-mani-background border-mani-border-muted";
+
 export function TestAreaBody() {
     return (
         <div className="mt-2 flex flex-col space-y-2">
-            <div className="h-9 flex items-center space-x-2">
-                <Input className="text-mani-foreground bg-mani-background border-mani-border-muted" />
+            <div className="h-8 flex items-center space-x-2">
+                <Input className={localInputClasses} />
 
-                <Button className="h-full" variant="outline" size="sm" tabIndex={-1} title="Explanation" >
+                <Button className="min-w-20" variant="outline" size="sm" tabIndex={-1} title="Explanation" >
                     Verify
                 </Button>
             </div>
 
-            <div className="h-9 flex items-center space-x-2">
-                <Input className="text-mani-foreground bg-mani-background border-mani-border-muted" />
+            <div className="h-8 flex items-center space-x-2">
+                <Input className={localInputClasses} />
 
-                <Button className="h-full" variant="outline" size="sm" tabIndex={-1} title="Explanation" >
+                <Button className="min-w-20" variant="outline" size="sm" tabIndex={-1} title="Explanation" >
                     Generate
                 </Button>
             </div>
