4746baf519bc977a21ffd638abc786d93cf176ca
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-07 14:55:55 -0700

    WIP. PolicyEditorNewDlg. test area layout. 11. simplify accordion

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-custom-rule/3-test-area-accordion.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-custom-rule/3-test-area-accordion.tsx
@@ -2,33 +2,17 @@ import { ReactNode } from "react";
 import { PrimitiveAtom, useAtom } from "jotai";
 import { Accordion, AccordionContent, AccordionItem } from "@/ui";
 
-function TestAreaSection({ value, children }: { value: string; children: ReactNode; }) {
-    return (
-        <AccordionItem value={value} className="border-none">
-            <AccordionContent>
-                {children}
-            </AccordionContent>
-        </AccordionItem>
-    );
-}
-
-function TestAreaOpenState({ children, isTestAreaOpenAtom }: { children: ReactNode; isTestAreaOpenAtom: PrimitiveAtom<string[]>; }) {
+export function TestRoomAccordion({ children, isTestAreaOpenAtom }: { children: ReactNode; isTestAreaOpenAtom: PrimitiveAtom<string[]>; }) {
     const [isTestAreaOpen, setIsTestAreaOpen] = useAtom(isTestAreaOpenAtom);
     return (
         <Accordion type="multiple" value={isTestAreaOpen} onValueChange={(v) => setIsTestAreaOpen(v)}>
-            {children}
+
+            <AccordionItem value="policy" className="border-none">
+                <AccordionContent>
+                    {children}
+                </AccordionContent>
+            </AccordionItem>
+
         </Accordion>
-    );
-}
-
-export function TestRoomAccordion({ children, isTestAreaOpenAtom }: { children: ReactNode; isTestAreaOpenAtom: PrimitiveAtom<string[]>; }) {
-    return (
-        <TestAreaOpenState isTestAreaOpenAtom={isTestAreaOpenAtom}>
-            <TestAreaSection value="policy">
-
-                {children}
-
-            </TestAreaSection>
-        </TestAreaOpenState>
     );
 }
