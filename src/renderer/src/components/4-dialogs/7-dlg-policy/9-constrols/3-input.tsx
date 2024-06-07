bd244acbffb3c2c28a328cab3b4a52ce3a740258
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-06 20:35:39 -0700

    WIP. PolicyEditorNewDlg. remove unused Input

--- src/renderer/src/components/4-dialogs/7-dlg-policy/9-constrols/3-input.tsx
+++
@@ -1,8 +0,0 @@
-import { InputHTMLAttributes } from "react";
-import { classNames } from '@/utils';
-
-export function Input({ className, ...rest }: InputHTMLAttributes<HTMLInputElement>) {
-    return (
-        <input className={classNames("p-2 h-9 text-primary-300 bg-primary-700 rounded", className)} {...rest} />
-    );
-}
--- src/renderer/src/components/4-dialogs/7-dlg-policy/9-constrols/index.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/9-constrols/index.tsx
@@ -1,4 +1,3 @@
 export * from "./1-check";
 export * from "./2-radio";
-export * from "./3-input";
 export * from "./4-dropdown";
