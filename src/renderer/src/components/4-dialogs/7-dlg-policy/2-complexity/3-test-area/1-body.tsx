e99bc3c23b0dcd5e967841918a2b2d8eb532be82
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-07 15:09:03 -0700

    WIP. PolicyEditorNewDlg. test area explanation. 1. with TextareaAutoGrow

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-test-area/1-body.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-test-area/1-body.tsx
@@ -1,11 +1,35 @@
-import { Button, Input } from "@/ui";
+import { Button, Input, TextareaAutoGrow } from "@/ui";
 import { PolicyDlgConv } from "../../0-all/0-conv";
+import { useState } from "react";
 
 const localInputClasses = "h-8 text-mani-foreground bg-mani-background border-mani-border-muted";
 
 export function TestAreaBody({ dlgUiAtoms }: { dlgUiAtoms: PolicyDlgConv.PolicyUiAtoms; }) {
+    const [explanation, setExplanation] = useState('');
     return (
-        <div className="mt-2 flex flex-col space-y-2">
+        <div className="relative mt-4 px-4 py-3 border-border border rounded flex flex-col space-y-2">
+
+            <div className="absolute left-0 -top-2">
+                Test area
+            </div>
+
+            <div className="">
+                <div className="">
+                    Explanation
+                </div>
+
+                <TextareaAutoGrow
+                    value={explanation}
+                    onChange={(e) => setExplanation(e.target.value)}
+                    rows={1}
+                    className="min-h-0 text-xs"
+                    // textareaPaddingFont={cn(textareaPaddingFontDebugClasses, textareaPaddingFontClasses)}
+                    placeholder="Paste theme vars here"
+                    spellCheck={false}
+                />
+
+            </div>
+
             <div className="h-8 flex items-center space-x-2">
                 <Input className={localInputClasses} />
 
