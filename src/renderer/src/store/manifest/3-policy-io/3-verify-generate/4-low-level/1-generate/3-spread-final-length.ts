3fe2d3ce666010e6d9e78b6c4a8b383ee1497b76
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-14 16:43:36 -0700

    WIP. policy-io. generate. update spreadLenForUndefs chSetExtra.max detection

--- src/renderer/src/store/manifest/3-policy-io/3-verify-generate/4-low-level/1-generate/3-spread-final-length.ts
+++ src/renderer/src/store/manifest/3-policy-io/3-verify-generate/4-low-level/1-generate/3-spread-final-length.ts
@@ -8,7 +8,7 @@ function sortAscendingByCharSetLength(a: ChSetExtra, b: ChSetExtra): number {
     return isLowerCharSetLength ? -1 : 1;
 }
 
-export function spreadLenForUndef(toGenerate: ChSetExtra[], pswLenGenerated: number, targetMin: number, targetMax: number): number {
+export function spreadLenForUndefs(toGenerate: ChSetExtra[], lenAlreadyGenerated: number, targetMin: number, targetMax: number): number {
     // Sort ruleEntries whose max is undefined in ascending order of their character set length.
     toGenerate.sort(sortAscendingByCharSetLength);
 
@@ -16,38 +16,36 @@ export function spreadLenForUndef(toGenerate: ChSetExtra[], pswLenGenerated: num
 
     toGenerate.forEach(
         (chSetExtra, idx) => {
-            const lenForThisUndef = Math.floor((targetMax - pswLenGenerated) / (entriesCount > 0 ? entriesCount : 1));
+            const lenForThisUndef = Math.floor((targetMax - lenAlreadyGenerated) / (entriesCount > 0 ? entriesCount : 1));
 
             if (lenForThisUndef <= 0) { // No more extra characters available so set minimum length
                 chSetExtra.max = chSetExtra.min;
             } else {
                 const isLastEntry = idx === toGenerate.length - 1;
                 if (isLastEntry) {
-                    let moreLengthToGenerate = 0; // Minimum more characters to satisfy the minimum length requirement.
-
-
                     // We have rule entries for whom password has to be generated.
-                    if (pswLenGenerated < targetMin) {
-                        moreLengthToGenerate = targetMin - pswLenGenerated;
+                    if (targetMin > lenAlreadyGenerated) {
+                        let moreLengthToGenerate = targetMin - lenAlreadyGenerated; // Minimum more characters to satisfy the minimum length requirement.
 
                         const minimumLenToSatisfyRange = Math.max(moreLengthToGenerate, chSetExtra.min);
                         chSetExtra.min = Math.min(minimumLenToSatisfyRange, chSetExtra.chSet.chars.length);
                     }
                 }
 
-                chSetExtra.max = Math.max(chSetExtra.min, Math.min(lenForThisUndef, chSetExtra.chSet.chars.length));
+                //chSetExtra.max = Math.max(chSetExtra.min, Math.min(lenForThisUndef, chSetExtra.chSet.chars.length));
+                chSetExtra.max = Math.max(chSetExtra.min, lenForThisUndef);
 
-                if (isLastEntry && chSetExtra.max > targetMax - pswLenGenerated) {
-                    chSetExtra.max = targetMax - pswLenGenerated;
+                if (isLastEntry && chSetExtra.max > targetMax - lenAlreadyGenerated) {
+                    chSetExtra.max = targetMax - lenAlreadyGenerated;
                 }
             }
 
             if (chSetExtra.checkWasLenGenerated()) {
-                pswLenGenerated += chSetExtra.generatedLen;
+                lenAlreadyGenerated += chSetExtra.generatedLen;
                 entriesCount--;
             }
         }
     );
 
-    return pswLenGenerated;
+    return lenAlreadyGenerated;
 }
