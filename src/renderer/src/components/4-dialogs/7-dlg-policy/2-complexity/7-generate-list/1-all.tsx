24307b605a87266ddb0c0e8bf2a2a9a1e4e0ae4b
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-15 15:11:36 -0700

    WIP. policy-io. GeneratedList. 2. fixed

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/7-generate-list/1-all.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/7-generate-list/1-all.tsx
@@ -1,12 +1,17 @@
 import { PrimitiveAtom, useAtom } from "jotai";
-import { Popover, PopoverArrorWoBottom, PopoverClose, PopoverContent, PopoverPortal, ScrollArea } from "@/ui";
+import { Popover, PopoverArrorWoBottom, PopoverClose, PopoverContent, PopoverPortal, PopoverTrigger, ScrollArea } from "@/ui";
 import { SymbolCross } from "@/ui/icons";
 import { GeneratedListBody } from "./3-body";
+import React from "react";
 
 export function ButtonGeneratedList({ openAtom }: { openAtom: PrimitiveAtom<boolean>; }) {
     const [open, setOpen] = useAtom(openAtom);
     return (
         <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <div />
+            </PopoverTrigger>
+
             <PopoverPortal>
                 <PopoverContent className="relative mx-4 p-0 w-[460px] text-foreground bg-background border-border border shadow" sideOffset={5} align="center">
 
