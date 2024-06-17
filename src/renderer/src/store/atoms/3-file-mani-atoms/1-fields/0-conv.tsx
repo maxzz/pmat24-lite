32694ea1704c2dc265865f5ecc363f343d518b89
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-16 18:56:20 -0700

    WIP. mani store. add ManiFile namespace

--- src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/0-conv.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/0-conv.tsx
@@ -137,7 +137,76 @@ export namespace FieldConv {
         TransformValue.valueLife2Mani(from.valueLife, rv);
         return rv;
     }
+
+    export function forManiClean(from: ThisType): ThisType {
+        const rv: ThisType = {
+            ...(from.useit && { useit: true }),
+            displayname: from.displayname,
+            dbname: from.dbname,
+            type: from.type,
+            password: from.password,
+
+            // ...fieldTyp2Obj(from.type),
+            // policy: from.policies.policy,
+            // policy2: from.policies.policy2,
+            // options: from.policies.options,
+        };
+
+        // TransformValue.valueLife2Mani(from.valueLife, rv);
+        return rv;
+    }
 }
 
 //TODO: filter out undefined values when saving manifest
 //TODO: we need to correlate policies with password change form
+
+namespace ManiFile {
+    export type FieldValueValue = {
+        value?: string;
+        choosevalue?: string;           // This does not exist in field catalog yet but we can added it to field catalog (as 2023 extension).
+        password?: boolean,             // In file it's undefined | '1'. Only field catalog or manual mode can change this value.
+        askalways?: boolean,            // In file it's undefined | '1'.
+        onetvalue?: boolean,            // In file it's undefined | '1'.
+    };
+
+    export type FieldValueIds = {
+        displayname: string,            // It should be '' (in momory) if undefined and empty won't be stored in file (for localization). In filed catalog this is "dispname" and is required so we mark it here as required as well.
+        dbname: string;
+        ownernote?: string;             // This is not stored in Field and may appear in Field Catalog only.
+    };
+
+    export type FieldValue = FieldValueValue & FieldValueIds;
+
+    export type FieldPolicySome = {
+        policy?: string | undefined;    // This is standard rule: "[p4]g:8:8:withspecial:different_ap"
+        policy2?: string | undefined;   // This is custom rule like: "[e1]g:(a{4,4}d{2,2}A{1,1}[@#$%!]{1,1})&lt;8,8&gt;"; both can present at the same time. It's defined in file, but not in c++.
+        options?: string | undefined;   // see FieldPolicyOptiion type
+    };
+
+    export type FieldPolicy = Required<FieldPolicySome>;
+
+    export type FieldPolicyOptions = {  // Names are case-sensitive here as it comes from file.
+        chgpolopts: {                   // Field options (e.g. password change field policy options stringify'd JSON object). "norep" and "chkppos"
+            norep: boolean;             // TODO: Who puts FieldPolicyOptiion into manifest?
+            chkppos: boolean;           //       These options are part of policy2, not field itself. This should not exist in manifest, but...
+        }
+    };
+    
+    export type FieldDirection = {
+        rfield?: 'in' | 'out';
+        rfieldindex?: number;           // "2"
+        rfieldform?: string;            // refs from login form
+    };
+
+    export type Field = FieldValue & FieldPolicySome & FieldDirection & {
+        type: Mani.FieldTypeStr;        // This does not exist in field catalog
+
+        path_ext?: string;              // path to this control with accessiblity info if exists
+
+        submit?: boolean,               // "1"
+        useit?: boolean,                // "1"
+
+        controltosubmitdata?: boolean;
+        ids?: string;
+    };
+}
