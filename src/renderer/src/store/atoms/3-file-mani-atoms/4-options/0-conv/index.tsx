8c9a7431d4787ac118e05ae7829a9c40f2e079a5
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-19 18:39:42 -0700

    WIP. mani final. 5. add atoms for missing detection options

--- src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/index.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/index.tsx
@@ -7,40 +7,44 @@ import { FileUsParams } from "../../9-types";
 export namespace OptionsConv {
 
     type UiPart1General = {
-        name: string;       // login name
-        desc: string;       // login description; Mani.Options.sidekick
-        hint: string;       // user hint; Mani.Options.ownernote
-        balloon: string;    // show balloon # times; note: value should be a number, but it's stored as string
+        name: string;               // login name
+        desc: string;               // login description; Mani.Options.sidekick
+        hint: string;               // user hint; Mani.Options.ownernote
+        balloon: string;            // show balloon # times; note: value should be a number, but it's stored as string
     };
 
     type UiPart2ScreenDetection = {
-        url: string;        // URL
-        caption: string;    // Windows Caption
-        monitor: boolean;   // Monitor screen changes
+        url: string;                // URL
+        caption: string;            // Windows Caption
+        monitor: boolean;           // Monitor screen changes
+
+        dlg_tab: string;
+        dlg_class: string;
+        dlg_checkexe: boolean;
     };
 
     type UiPart3Authentication = {
-        aim: boolean;       // Start authentication immediately
-        lock: boolean;      // Lock out login fields
+        aim: boolean;               // Start authentication immediately
+        lock: boolean;              // Lock out login fields
     };
 
     type UiPart4QL = {
-        dashboard: boolean; // Display on mini-dashboard
-        qName: string;      // Quick Link Name
-        qUrl: string;       // Quick Link URL
-        qUse: boolean;      // Use Quick Link
+        dashboard: boolean;         // Display on mini-dashboard
+        qName: string;              // Quick Link Name
+        qUrl: string;               // Quick Link URL
+        qUse: boolean;              // Use Quick Link
     };
 
     type UiPart5PasswordManagerIcon = {
-        id: string;         // Location ID
-        loc: string;        // Location
+        id: string;                 // Location ID
+        loc: string;                // Location
     };
 
     export type OptionsForAtoms = {
         uiPart1General: UiPart1General;
-        uiPart4QL: UiPart4QL;
         uiPart2ScreenDetection: UiPart2ScreenDetection;
         uiPart3Authentication: UiPart3Authentication;
+        uiPart4QL: UiPart4QL;
         uiPart5PasswordManagerIcon: UiPart5PasswordManagerIcon;
 
         isWeb: boolean;
@@ -48,9 +52,9 @@ export namespace OptionsConv {
 
     export type FormOptionsAtoms = {
         uiPart1General: AtomizeWithType<UiPart1General, RowInputState>;
-        uiPart4QL: AtomizeWithType<UiPart4QL, RowInputState>;
         uiPart2ScreenDetection: AtomizeWithType<UiPart2ScreenDetection, RowInputState>;
         uiPart3Authentication: AtomizeWithType<UiPart3Authentication, RowInputState>;
+        uiPart4QL: AtomizeWithType<UiPart4QL, RowInputState>;
         uiPart5PasswordManagerIcon: AtomizeWithType<UiPart5PasswordManagerIcon, RowInputState>;
 
         isWebAtom: PrimitiveAtom<boolean>;
@@ -78,6 +82,10 @@ export namespace OptionsConv {
                 caption: detection.caption || '',                   //TODO: show only for Win32
                 monitor: options.recheckwindowafterfillin === '1',  //TODO: strange name for monitor changes
                 url: detection.web_ourl || '',
+
+                dlg_tab: detection.dlg_tab || '',
+                dlg_class: detection.dlg_class || '',
+                dlg_checkexe: detection.dlg_checkexe === '1',
             },
             uiPart3Authentication: {
                 aim: options.autoprompt === '1',
@@ -114,6 +122,10 @@ export namespace OptionsConv {
                 captionAtom: newAtomForInput(uiPart2ScreenDetection.caption, onChange('caption')),
                 monitorAtom: newAtomForCheck(uiPart2ScreenDetection.monitor, onChange('monitor')),
                 urlAtom: newAtomForInput(uiPart2ScreenDetection.url, onChange('url')),
+
+                dlg_tabAtom: newAtomForInput(uiPart2ScreenDetection.dlg_tab, onChange('dlg_tab')),
+                dlg_classAtom: newAtomForInput(uiPart2ScreenDetection.dlg_class, onChange('dlg_class')),
+                dlg_checkexeAtom: newAtomForCheck(uiPart2ScreenDetection.dlg_checkexe, onChange('dlg_checkexe')),
             },
             uiPart3Authentication: {
                 aimAtom: newAtomForCheck(uiPart3Authentication.aim, onChange('aim')),
@@ -126,8 +138,8 @@ export namespace OptionsConv {
                 qUseAtom: newAtomForCheck(uiPart4QL.qUse, onChange('use')),
             },
             uiPart5PasswordManagerIcon: {
-                idAtom: newAtomForInput(uiPart5PasswordManagerIcon.id || '', onChange('id')),
-                locAtom: newAtomForInput(uiPart5PasswordManagerIcon.loc || '', onChange('loc')),
+                idAtom: newAtomForInput(uiPart5PasswordManagerIcon.id, onChange('id')),
+                locAtom: newAtomForInput(uiPart5PasswordManagerIcon.loc, onChange('loc')),
             },
 
             isWebAtom: atomWithCallback(initialState.isWeb, onChange('isWeb')),
@@ -150,6 +162,10 @@ export namespace OptionsConv {
                 caption: get(uiPart2ScreenDetection.captionAtom).data,
                 monitor: get(uiPart2ScreenDetection.monitorAtom).data === '1',
                 url: get(uiPart2ScreenDetection.urlAtom).data,
+
+                dlg_tab: get(uiPart2ScreenDetection.dlg_tabAtom).data,
+                dlg_class: get(uiPart2ScreenDetection.dlg_classAtom).data,
+                dlg_checkexe: get(uiPart2ScreenDetection.dlg_checkexeAtom).data === '1',
             },
             uiPart3Authentication: {
                 aim: get(uiPart3Authentication.aimAtom).data === '1',
