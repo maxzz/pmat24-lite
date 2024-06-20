3fa4e3a98f8b3bbf6ddd2179586e3deceda2c4c7
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-19 19:28:49 -0700

    WIP. mani final. 5. done with detectionAndOptionsForMani()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-save-main-atom/53-conv-mani-options.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-save-main-atom/53-conv-mani-options.ts
@@ -1,39 +1,53 @@
-import { Mani } from "pm-manifest";
 import { FileMani } from "@/store/manifest";
 import { OptionsConv } from "../../4-options";
 
-/** /
 function detectionForMani(options: OptionsConv.OptionsForAtoms): FileMani.Detection {
     const rv: FileMani.Detection = {
-        caption: options.uiPart2ScreenDetection.caption,
-        variablecaption: options.uiPart2ScreenDetection.caption,
-        
-        web_ourl: options.uiPart2ScreenDetection.url,
-        web_murl: options.uiPart2ScreenDetection.url,
-        web_qurl: options.uiPart4QL.qUrl,
-        web_checkurl: options.uiPart4QL.qUse ? '1' : undefined,
+        caption: options.p2Detect.caption,
+        variablecaption: options.p2Detect.caption,
 
-        dlg_class: options.ui
+        web_ourl: options.p2Detect.url,
+        web_murl: options.p2Detect.url,
+        web_qurl: options.p4QL.qUrl,
+        web_checkurl: options.p4QL.qUse ? '1' : undefined,
+
+        dlg_class: options.p2Detect.dlg_class,
+        dlg_tab: options.p2Detect.dlg_tab,
+        dlg_checkexe: options.p2Detect.dlg_checkexe ? '1' : undefined,
+
+        emu_pattern: options.p2Detect.emu_pattern,
+        names: options.p2Detect.names,
+        names_ext: options.p2Detect.names_ext,
+        processname: options.p2Detect.processname,
+        commandline: options.p2Detect.commandline,
     };
     return rv;
 }
 
 function optionsForMani(options: OptionsConv.OptionsForAtoms): FileMani.Options {
+
+    const submitType = options.p1General.submitType === 'dosubmit' || options.p1General.submitType === 'dosubmit'
+        ? options.p1General.submitType
+        : undefined;
+
     const rv: FileMani.Options = {
-        choosename: options.uiPart1General.name,
-        sidekick: options.uiPart1General.desc,
-        ownernote: options.uiPart1General.hint,
-        quicklink: options.uiPart4QL.dashboard,
-        auth_pl: options.uiPart3Authentication.aim,
-        balooncount: options.uiPart1General.balloon,
-        autoprompt: options.uiPart3Authentication.aim,
-        lockfields: options.uiPart3Authentication.lock,
-        submittype: options.uiPart5PasswordManagerIcon.id,
-        iconkey: options.uiPart5PasswordManagerIcon.id,
-        iconlocation: options.uiPart5PasswordManagerIcon.loc,
-        usequicklink: options.uiPart4QL.dashboard,
-        recheckwindowafterfillin: options.uiPart2ScreenDetection.monitor ? '1' : '0',
-        qlwocred: options.uiPart4QL.url,
+        choosename: options.p1General.name,
+        sidekick: options.p1General.desc,
+        ownernote: options.p1General.hint,
+        quicklink: options.p1General.qlName,
+
+        auth_pl: options.p3Auth.auth_pl,
+        balooncount: options.p1General.balloon,
+        autoprompt: options.p3Auth.aim ? '1' : undefined,
+        lockfields: options.p3Auth.lock ? '1' : undefined,
+        submittype: submitType,
+        iconkey: options.p5Icon.id,
+        iconlocation: options.p5Icon.loc,
+
+        usequicklink: options.p4QL.qUse ? '1' : '2',
+        recheckwindowafterfillin: options.p2Detect.reCheck ? '1' : undefined,
+        qlwocred: options.p1General.qlWoCred ? '1' : undefined,
+
         //unknownattributes
     };
     return rv;
@@ -51,4 +65,3 @@ export function detectionAndOptionsForMani(options: OptionsConv.OptionsForAtoms)
     };
     return rv;
 }
-/**/
--- src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/index.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/index.tsx
@@ -1,7 +1,6 @@
 import { Getter, PrimitiveAtom, Setter } from "jotai";
 import { AtomizeWithType, OnValueChange, atomWithCallback } from '@/util-hooks';
 import { RowInputState, newAtomForCheck, newAtomForInput, validateManifestName } from "@/ui";
-import { FileUsAtom, FormIdx } from '@/store/store-types';
 import { FileUsParams } from "../../9-types";
 
 export namespace OptionsConv {
@@ -11,6 +10,10 @@ export namespace OptionsConv {
         desc: string;               // login description; Mani.Options.sidekick
         hint: string;               // user hint; Mani.Options.ownernote
         balloon: string;            // show balloon # times; note: value should be a number, but it's stored as string
+        submitType: string;         // "dosubmit" | "nosubmit"
+        qlName: string;             // Quick Link Name
+        qlWoCred: boolean;          // Quick reauthentication enable/disable (QL wo/ crededntials). I'm not sure why it's here at all
+        unkAttrs: string;           // unknown attributes
     };
 
     type p2Detect = {               // Part Screen Detection
@@ -28,11 +31,14 @@ export namespace OptionsConv {
         names_ext: string;
         processname: string;
         commandline: string;
+
+        reCheck: boolean;           // re-check windowa fter fillin
     };
 
     type p3Auth = {                 // Part Authentication
         aim: boolean;               // Start authentication immediately
         lock: boolean;              // Lock out login fields
+        auth_pl: string;            // extended policy (see AuthTokenValues); only one bit as hex string (auth_pl="100"); used only for login form
     };
 
     type p4QL = {                   // Part Quick Links
@@ -84,6 +90,10 @@ export namespace OptionsConv {
                 desc: options.sidekick || '',
                 hint: options.ownernote || '',
                 balloon: options.balooncount || '3',
+                submitType: options.submittype || '',
+                qlName: options.quicklink || '',
+                qlWoCred: options.qlwocred === '1',
+                unkAttrs: options.unkAttrs || '',
             },
             p2Detect: {
                 caption: detection.caption || '',                   //TODO: show only for Win32
@@ -99,10 +109,13 @@ export namespace OptionsConv {
                 names_ext: detection.names_ext || '',
                 processname: detection.processname || '',
                 commandline: detection.commandline || '',
+
+                reCheck: options.recheckwindowafterfillin === '1',
             },
             p3Auth: {
                 aim: options.autoprompt === '1',
                 lock: options.lockfields === '1',
+                auth_pl: options.auth_pl || '',
             },
             p4QL: {
                 dashboard: true,
@@ -130,6 +143,10 @@ export namespace OptionsConv {
                 descAtom: newAtomForInput(p1General.desc, onChange('desc')),
                 hintAtom: newAtomForInput(p1General.hint, onChange('hint')),
                 balloonAtom: newAtomForInput(p1General.balloon, onChange('balloon')),
+                submitTypeAtom: newAtomForInput(p1General.submitType, onChange('submitType')),
+                qlNameAtom: newAtomForInput(p1General.qlName, onChange('qlName')),
+                qlWoCredAtom: newAtomForCheck(p1General.qlWoCred, onChange('qlWoCred')),
+                unkAttrsAtom: newAtomForInput(p1General.unkAttrs, onChange('unkAttrs')),
             },
             p2Detect: {
                 captionAtom: newAtomForInput(p2Detect.caption, onChange('caption')),
@@ -145,10 +162,13 @@ export namespace OptionsConv {
                 names_extAtom: newAtomForInput(p2Detect.names_ext, onChange('names_ext')),
                 processnameAtom: newAtomForInput(p2Detect.processname, onChange('processname')),
                 commandlineAtom: newAtomForInput(p2Detect.commandline, onChange('commandline')),
+
+                reCheckAtom: newAtomForCheck(p2Detect.reCheck, onChange('recheckwindowafterfillin')),
             },
             p3Auth: {
                 aimAtom: newAtomForCheck(p3Auth.aim, onChange('aim')),
                 lockAtom: newAtomForCheck(p3Auth.lock, onChange('lock')),
+                auth_plAtom: newAtomForInput(p3Auth.auth_pl, onChange('auth_pl')),
             },
             p4QL: {
                 dashboardAtom: newAtomForCheck(p4QL.dashboard, onChange('dashboard')),
@@ -167,7 +187,7 @@ export namespace OptionsConv {
         return rv;
     }
 
-    export function fromAtoms(atoms: FormOptionsAtoms, get: Getter, set: Setter): OptionsForAtoms{
+    export function fromAtoms(atoms: FormOptionsAtoms, get: Getter, set: Setter): OptionsForAtoms {
         const { p1General: p1General, p2Detect, p3Auth, p4QL, p5Icon } = atoms;
 
         const rv: OptionsForAtoms = {
@@ -176,6 +196,10 @@ export namespace OptionsConv {
                 desc: get(p1General.descAtom).data,
                 hint: get(p1General.hintAtom).data,
                 balloon: get(p1General.balloonAtom).data,
+                qlName: get(p1General.qlNameAtom).data,
+                submitType: get(p1General.submitTypeAtom).data,
+                qlWoCred: get(p1General.qlWoCredAtom).data === '1',
+                unkAttrs: get(p1General.unkAttrsAtom).data,
             },
             p2Detect: {
                 caption: get(p2Detect.captionAtom).data,
@@ -191,10 +215,13 @@ export namespace OptionsConv {
                 names_ext: get(p2Detect.names_extAtom).data,
                 processname: get(p2Detect.processnameAtom).data,
                 commandline: get(p2Detect.commandlineAtom).data,
+
+                reCheck: get(p2Detect.reCheckAtom).data === '1',
             },
             p3Auth: {
                 aim: get(p3Auth.aimAtom).data === '1',
                 lock: get(p3Auth.lockAtom).data === '1',
+                auth_pl: get(p3Auth.auth_plAtom).data,
             },
             p4QL: {
                 dashboard: get(p4QL.dashboardAtom).data === '1',
