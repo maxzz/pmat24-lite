47d74503265efc94a476088929bef8dca0239504
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-23 15:18:26 -0700

    WIP. options. 7. grid checkbox. failed to limit click area. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/1-options-row/2-row-label.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/1-options-row/2-row-label.tsx
@@ -1,13 +1,23 @@
 import { ReactNode } from "react";
 import { Label } from "@/ui";
 
-export function RowLabel({ label, children }: { label: string; children: ReactNode;}) {
+export function RowLabel({ label, children }: { label: string; children: ReactNode; }) {
     return (
-        <Label className={"col-span-2 py-0.5 pr-0.5 text-xs grid grid-cols-subgrid items-center"}>
+        <Label className={"col-span-2 py-0.5 pr-0.5 text-xs grid grid-cols-subgrid items-center"} onClick={
+            (e) => {
+                console.log("RowLabel.onClick", e.target);
+
+                if (e.target instanceof HTMLInputElement && e.target.tagName !== "INPUT") {
+                    console.log("RowLabel.onClick", e.target.tagName);
+                    
+                    e.stopPropagation();
+                }
+            }
+            }>
             <div className="font-light">
                 {label}
             </div>
-            
+
             {children}
         </Label>
     );
