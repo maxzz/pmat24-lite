ec06b2e7104f3305b0e1e31b9170e3415b180a91
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-23 14:44:37 -0700

    WIP. options. 7. fix SectionTitle sub-grid

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-section-header/1-section-title.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-section-header/1-section-title.tsx
@@ -1,6 +1,6 @@
 export function SectionTitle({ label }: { label: string; }) {
     return (
-        <div className="mt-2 first:mt-0 text-sm font-semibold">
+        <div className="col-span-2 mt-2 first:mt-0 text-sm font-semibold">
             {label}
         </div>
     );
