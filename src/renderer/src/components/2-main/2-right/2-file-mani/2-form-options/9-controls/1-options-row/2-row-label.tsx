164e83025546f714e73af4eb0eb09c543d8a8be1
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-23 14:43:57 -0700

    WIP. options. 7. RowLabel clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/1-options-row/2-row-label.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/1-options-row/2-row-label.tsx
@@ -1,18 +1,6 @@
 import { ReactNode } from "react";
 import { Label } from "@/ui";
 
-export function RowLabel0({ label, children }: { label: string; children: ReactNode;}) {
-    return (
-        <Label className="col-span-2 py-0.5 pr-0.5 text-xs grid grid-cols-subgrid items-center">
-            <div className="font-light">
-                {label}
-            </div>
-            
-            {children}
-        </Label>
-    );
-}
-
 export function RowLabel({ label, children }: { label: string; children: ReactNode;}) {
     return (
         <Label className={"col-span-2 py-0.5 pr-0.5 text-xs grid grid-cols-subgrid items-center"}>
