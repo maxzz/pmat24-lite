009f7d371b527674b77fd426c273031bb1d0155a
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-23 13:33:57 -0700

    WIP. options. 7. add ability to be nested sub-grid to Part1General

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/1-general.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/1-general.tsx
@@ -1,16 +1,26 @@
 import { OptionsState } from "@/store/atoms/3-file-mani-atoms/4-options";
 import { RowInputWLabel } from "../9-controls";
 
-export function Part1General({ atoms }: { atoms: OptionsState.Atoms; }) {
-
+export function Part1General0({ atoms }: { atoms: OptionsState.Atoms; }) {
     const { nameAtom, descAtom, hintAtom, balloonAtom } = atoms.p1General;
-    
     return (<>
         <RowInputWLabel stateAtom={nameAtom} label="Managed login name" />
         <RowInputWLabel stateAtom={descAtom} label="Description" />
         <RowInputWLabel stateAtom={hintAtom} label="User hint" />
         <RowInputWLabel stateAtom={balloonAtom} label="Show balloon" />
     </>);
+}
+
+export function Part1General({ atoms }: { atoms: OptionsState.Atoms; }) {
+    const { nameAtom, descAtom, hintAtom, balloonAtom } = atoms.p1General;
+    return (
+        <div className="col-span-2 grid grid-cols-subgrid gap-x-2 gap-y-1">
+            <RowInputWLabel stateAtom={nameAtom} label="Managed login name" />
+            <RowInputWLabel stateAtom={descAtom} label="Description" />
+            <RowInputWLabel stateAtom={hintAtom} label="User hint" />
+            <RowInputWLabel stateAtom={balloonAtom} label="Show balloon" />
+        </div>
+    );
 }
 
 //TODO: add validation
