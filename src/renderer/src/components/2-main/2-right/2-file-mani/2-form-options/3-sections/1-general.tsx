0981f72065467b82f79905557fd1704293c7bc23
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-24 13:33:05 -0700

    WIP. options. 7. add Part1General trigger

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/1-general.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/1-general.tsx
@@ -1,6 +1,9 @@
 import { OptionsState } from "@/store/atoms/3-file-mani-atoms/4-options";
 import { RowInputWLabel } from "../9-controls";
 import { UiAccordion } from "../9-controls/ui-accordion";
+import { Button } from "@/ui";
+import { useState } from "react";
+import { atom, useAtom } from "jotai";
 
 export function Part1General0({ atoms }: { atoms: OptionsState.Atoms; }) {
     const { nameAtom, descAtom, hintAtom, balloonAtom } = atoms.p1General;
@@ -26,7 +29,7 @@ export function Part1General1({ atoms }: { atoms: OptionsState.Atoms; }) {
     );
 }
 
-export function Part1General({ atoms }: { atoms: OptionsState.Atoms; }) {
+export function Part1General2({ atoms }: { atoms: OptionsState.Atoms; }) {
     const { nameAtom, descAtom, hintAtom, balloonAtom } = atoms.p1General;
     return (
         <UiAccordion open={true}>
@@ -36,6 +39,27 @@ export function Part1General({ atoms }: { atoms: OptionsState.Atoms; }) {
             <RowInputWLabel stateAtom={balloonAtom} label="Show balloon" />
         </UiAccordion>
     );
+}
+
+export function Part1General({ atoms }: { atoms: OptionsState.Atoms; }) {
+    const { nameAtom, descAtom, hintAtom, balloonAtom } = atoms.p1General;
+
+    const openAtom = useState(() => atom(false))[0];
+    const [open, setOpen] = useAtom(openAtom);
+
+    return (<>
+        <RowInputWLabel stateAtom={nameAtom} label="Managed login name" />
+
+        <Button className="mr-0.5 col-start-2 place-self-end" onClick={() => setOpen(v => !v)}>
+            More...
+        </Button>
+
+        <UiAccordion open={open}>
+            <RowInputWLabel stateAtom={descAtom} label="Description" />
+            <RowInputWLabel stateAtom={hintAtom} label="User hint" />
+            <RowInputWLabel stateAtom={balloonAtom} label="Show balloon" />
+        </UiAccordion>
+    </>);
 }
 
 //TODO: add validation
