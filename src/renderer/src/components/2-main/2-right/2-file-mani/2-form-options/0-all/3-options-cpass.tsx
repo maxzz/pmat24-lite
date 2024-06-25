3341b02b1d22a31157bdc15efccc30ee7fc3943d
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-24 17:47:55 -0700

    WIP. options. 7. add 4-options-common-parts.tsx

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/0-all/3-options-cpass.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/0-all/3-options-cpass.tsx
@@ -1,26 +1,11 @@
-import { useAtomValue } from "jotai";
 import { TabSectionProps } from "@/store/atoms/3-file-mani-atoms";
-import { Part2ScreenDetection, Part3Authentication, Part4QL, Part5PasswordManagerIcon } from "../3-sections";
-import { SubSectionTitle } from "../9-controls";
+import { OptionsDetection, OptionsAuth, OptionsQuicklink, OptionsIcon } from "./4-options-common-parts";
 
-export function GroupCpass({ formAtoms }: TabSectionProps) {
-
-    const optionsAtoms = formAtoms.optionsAtoms;
-    const isWeb = useAtomValue(optionsAtoms.isWebAtom);
-
+export function GroupCpass(props: TabSectionProps) {
     return (<>
-        <SubSectionTitle label="Screen detection" />
-        <Part2ScreenDetection atoms={optionsAtoms} />
-
-        <SubSectionTitle label="Authentication" />
-        <Part3Authentication atoms={optionsAtoms} />
-
-        <SubSectionTitle label="Quick link" />
-        <Part4QL atoms={optionsAtoms} />
-
-        {!isWeb && (<>
-            <SubSectionTitle label="Password Manager Icon" />
-            <Part5PasswordManagerIcon atoms={optionsAtoms} />
-        </>)}
+        <OptionsDetection {...props} />
+        <OptionsAuth {...props} />
+        <OptionsQuicklink {...props} />
+        <OptionsIcon {...props} />
     </>);
 }
---
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/0-all/4-options-common-parts.tsx
@@ -0,0 +1,36 @@
+import { useAtomValue } from "jotai";
+import { TabSectionProps } from "@/store/atoms/3-file-mani-atoms";
+import { Part2ScreenDetection, Part3Authentication, Part4QL, Part5PasswordManagerIcon } from "../3-sections";
+import { SubSectionTitle } from "../9-controls";
+
+export function OptionsDetection({ formAtoms }: TabSectionProps) {
+    return (<>
+        <SubSectionTitle label="Screen detection" />
+        <Part2ScreenDetection atoms={formAtoms.optionsAtoms} />
+    </>);
+}
+
+export function OptionsAuth({ formAtoms }: TabSectionProps) {
+    return (<>
+        <SubSectionTitle label="Authentication" />
+        <Part3Authentication atoms={formAtoms.optionsAtoms} />
+    </>);
+}
+
+export function OptionsQuicklink({ formAtoms }: TabSectionProps) {
+    return (<>
+        <SubSectionTitle label="Quick link" />
+        <Part4QL atoms={formAtoms.optionsAtoms} />
+    </>);
+}
+
+export function OptionsIcon({ formAtoms }: TabSectionProps) {
+    const optionsAtoms = formAtoms.optionsAtoms;
+    const isWeb = useAtomValue(optionsAtoms.isWebAtom);
+    return (<>
+        {!isWeb && (<>
+            <SubSectionTitle label="Password Manager Icon" />
+            <Part5PasswordManagerIcon atoms={optionsAtoms} />
+        </>)}
+    </>);
+}
