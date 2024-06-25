2b0a0238e22716caf1225ed53a19cedcc4e45393
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-24 17:37:32 -0700

    WIP. options. 7. add SubSectionTitle0 and SubSectionTitle

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/0-all/1-options-general.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/0-all/1-options-general.tsx
@@ -1,11 +1,11 @@
 import { TabSectionProps } from "@/store/atoms/3-file-mani-atoms";
 import { Part1General } from "../3-sections";
-import { SubSectionTitle } from "../9-controls";
+import { SubSectionTitle, SubSectionTitle0 } from "../9-controls";
 
 export function GroupGeneral({ formAtoms }: TabSectionProps) {
     const optionsAtoms = formAtoms.optionsAtoms;
     return (<>
-        <SubSectionTitle label="General" />
+        <SubSectionTitle0 label="General" />
         <Part1General atoms={optionsAtoms} />
     </>);
 }
--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-section-header/2-sub-section-title.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-section-header/2-sub-section-title.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+import { atom, useAtomValue } from "jotai";
 import { SymbolInfo } from "@/ui/icons";
+import { SlidersButton } from "../3-sliders-button";
 
-const sectionClasses = "\
+const sectionClasses0 = "\
 col-span-2 \
 \
 first:mt-1.5 mt-2 mb-0.5 pb-1 \
@@ -13,9 +16,9 @@ border-b";
 // text-[#32ffdaa0] \
 // border-[#32ffda40] \
 
-export function SubSectionTitle({ label }: { label: string; }) {
+export function SubSectionTitle0({ label }: { label: string; }) {
     return (
-        <div className={sectionClasses}>
+        <div className={sectionClasses0}>
             {label}
             {/* <SymbolInfo className="mr-3 size-4 text-muted-foreground 1opacity-50" /> */}
         </div>
@@ -23,3 +26,28 @@ export function SubSectionTitle({ label }: { label: string; }) {
 }
 
 //TODO: show info icon on section with focus
+
+const sectionClasses = "\
+col-span-2 \
+\
+first:mt-1.5 mt-2 mb-0.5 pb-1 \
+\
+font-normal \
+text-mani-title \
+border-mani-title \
+border-b \
+flex items-end justify-between\
+\
+\
+";
+
+export function SubSectionTitle({ label }: { label: string; }) {
+    const openAtom = useState(() => atom(false))[0];
+    const open = useAtomValue(openAtom);
+    return (
+        <div className={sectionClasses}>
+            {label}
+            <SlidersButton openAtom={openAtom} />
+        </div>
+    );
+}
