5eba235d5357eb8ea8ddfb6038c05e5af9906aca
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-25 19:20:00 -0700

    WIP. options. 7. add missing Extended Authentication policy

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/3-authentication.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/3-authentication.tsx
@@ -3,10 +3,12 @@ import { RowInputWLabel } from "../9-controls";
 
 export function Part3Authentication({ atoms }: { atoms: OptionsState.Atoms; }) {
 
-    const { aimAtom, lockAtom } = atoms.p3Auth;
+    const { aimAtom, lockAtom, auth_plAtom } = atoms.p3Auth;
 
     return (<>
         <RowInputWLabel stateAtom={aimAtom} label="Authenticate immediately" asCheckbox />
         <RowInputWLabel stateAtom={lockAtom} label="Lock out login fields" asCheckbox />
+
+        <RowInputWLabel stateAtom={auth_plAtom} label="Extended Authentication policy" />
     </>);
 }
