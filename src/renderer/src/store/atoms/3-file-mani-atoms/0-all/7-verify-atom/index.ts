937585c1757676dd1cabd7e018accb515f8a1628
Max Zakharzhevskiy <maxzz@msn.com>
2024-06-29 14:23:06 -0700

    WIP. options. verify. return verifyOptionsErrorAtom as set atom result

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/7-verify-atom/index.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/7-verify-atom/index.ts
@@ -2,32 +2,30 @@ import { atom } from "jotai";
 import { ManiAtoms } from "../../9-types";
 import { OptionsConv } from "../../4-options";
 
-export const verifyOptionsErrorAtom = atom('');
-
 export const verifyOptionsAtom = atom(null,
-    (get, set, { maniAtoms }: { maniAtoms: ManiAtoms; }) => {
+    (get, set, { maniAtoms }: { maniAtoms: ManiAtoms; }): string[] | undefined => {
         
         const [login, cpass] = maniAtoms;
         if (!login && !cpass) {
             return;
         }
 
+        const rv: string[] = []; 
+
         if (login) {
             const errors = OptionsConv.verifyAtoms(login.optionsAtoms, get, set);
             if (errors.length) {
-                const error = errors[0];
-                set(verifyOptionsErrorAtom, error);
+                rv.push(...errors);
             }
         }
 
         if (cpass) {
             const errors = OptionsConv.verifyAtoms(cpass.optionsAtoms, get, set);
             if (errors.length) {
-                const error = errors[0];
-                set(verifyOptionsErrorAtom, error);
+                rv.push(...errors);
             }
         }
 
-        set(verifyOptionsErrorAtom, '');
+        return rv.length ? rv : undefined;
     }
 );
