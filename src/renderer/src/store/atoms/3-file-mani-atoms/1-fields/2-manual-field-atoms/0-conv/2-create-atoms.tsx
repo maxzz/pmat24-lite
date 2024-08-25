7de9984b03a655c83387585b7c79e3b9767ba139
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-25 15:13:39 -0700

    WIP. manual atoms. add ExtraForAtoms

--- src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/2-create-atoms.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/2-create-atoms.tsx
@@ -1,14 +1,16 @@
 import { type Atomize } from "@/util-hooks";
-import { ScriptChunkEditorData } from "pm-manifest";
+import { ScriptChunkEditorData, uuid } from "pm-manifest";
 import { NormalFieldConv, type NormalField } from "../../1-normal-field-atoms/0-conv";
 import { newAtomForCheck, newAtomForInput, OnChangeValueWithPpdateName, validateNumber } from "@/ui/local-ui/1-input-validate";
 import { type ManualField } from "./9-types";
 
 function createAtom(chunk: ScriptChunkEditorData, idx: number, onChange: OnChangeValueWithPpdateName) {
+    const uid5 = uuid.asRelativeNumber();
     switch (chunk.type) {
         case "kbd": {
             const rv: ManualField.KbdForAtoms = {
                 type: 'kbd',
+                uid5,
                 charAtom: newAtomForInput(chunk.char, onChange('man-kbd-key')),
                 repeatAtom: newAtomForInput(chunk.repeat, onChange('man-kbd-repeat'), { validate: validateNumber }),
                 shiftAtom: newAtomForInput(chunk.shift, onChange('man-kbd-shift')),
@@ -20,6 +22,7 @@ function createAtom(chunk: ScriptChunkEditorData, idx: number, onChange: OnChang
         case "pos": {
             const rv: ManualField.PosForAtoms = {
                 type: 'pos',
+                uid5,
                 xAtom: newAtomForInput(chunk.x, onChange('man-pos-x'), { validate: validateNumber }),
                 yAtom: newAtomForInput(chunk.y, onChange('man-pos-y'), { validate: validateNumber }),
                 unitsAtom: newAtomForCheck(chunk.units, onChange('man-pos-units')),
@@ -30,6 +33,7 @@ function createAtom(chunk: ScriptChunkEditorData, idx: number, onChange: OnChang
         case "dly": {
             const rv: ManualField.DlyForAtoms = {
                 type: 'dly',
+                uid5,
                 nAtom: newAtomForInput(chunk.n, onChange('man-dly-dly'), { validate: validateNumber }),
             };
             return rv;
@@ -46,6 +50,7 @@ function createAtom(chunk: ScriptChunkEditorData, idx: number, onChange: OnChang
 
             const rv: ManualField.FldForAtoms = {
                 type: 'fld',
+                uid5,
                 field: embFld,
             };
             return rv;
--- src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/9-types.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/9-types.tsx
@@ -1,32 +1,43 @@
 import { AtomizeWithType } from "@/util-hooks";
-import { EditorDataForKbd, EditorDataForPos, EditorDataForDly, EditorDataForFld, ScriptChunkEditorData, Meta } from "pm-manifest";
+import { EditorDataForKbd, EditorDataForPos, EditorDataForDly, EditorDataForFld, ScriptChunkEditorData, Meta, ChunkKey } from "pm-manifest";
 import { NormalField } from "../../1-normal-field-atoms/0-conv";
 import { RowInputState } from "@/ui";
 
 export namespace ManualField {
 
-    export type KbdForAtoms = Prettify<AtomizeWithType<Omit<EditorDataForKbd, 'type'>, RowInputState> & {
+    type ExtraForAtoms = {
+        type: ChunkKey;
+        uid5: number;       // unique id for each row
+    };
+
+    export type KbdForAtoms = Prettify<ExtraForAtoms & AtomizeWithType<Omit<EditorDataForKbd, 'type'>, RowInputState> & {
         type: 'kbd';
     }>;
 
-    export type PosForAtoms = Prettify<AtomizeWithType<Omit<EditorDataForPos, 'type'>, RowInputState> & {
+    export type PosForAtoms = Prettify<ExtraForAtoms & AtomizeWithType<Omit<EditorDataForPos, 'type'>, RowInputState> & {
         type: 'pos';
     }>;
 
-    export type DlyForAtoms = Prettify<AtomizeWithType<Omit<EditorDataForDly, 'type'>, RowInputState> & {
+    export type DlyForAtoms = Prettify<ExtraForAtoms & AtomizeWithType<Omit<EditorDataForDly, 'type'>, RowInputState> & {
         type: 'dly';
     }>;
 
-    export type FldForAtoms = {
+    export type FldForAtoms =  Prettify<ExtraForAtoms& {
         type: 'fld';
         field: NormalField.FieldAtoms;
-    };
+    }>;
 
     export type ForAtoms = KbdForAtoms | PosForAtoms | DlyForAtoms | FldForAtoms;
 
     type FieldForAtoms = {
         scr: ScriptChunkEditorData;
     };
+
+    // type Extra2ForAtoms = {
+    //     metaField: Meta.Field;      // all fields from original to combine with fields from atoms to create new field
+    //     fromFile: FieldForAtoms;    // original state to compare with
+    //     changed: boolean;           // state from atoms is different from original state
+    // };
 
     type ScriptAtoms = Prettify<
         & ForAtoms
