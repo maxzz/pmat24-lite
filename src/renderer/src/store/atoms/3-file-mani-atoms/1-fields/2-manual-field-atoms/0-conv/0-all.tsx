12c37dc1450b2ff1cb45280f52e6193d93dfa49c
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-24 17:58:08 -0700

    WIP. ManualFieldsGrid. add singular versionfromAtom()

--- src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/0-all.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-fields/2-manual-field-atoms/0-conv/0-all.tsx
@@ -67,63 +67,62 @@ export function createAtoms(initialState: ScriptChunkEditorData[], onChange: OnC
     return scriptAtoms;
 }
 
-export function fromAtoms(scriptItems: ManualField.ScriptTypesAtoms[], get: Getter, set: Setter): ScriptChunkEditorData[] {
-    const chunks = scriptItems.map(
-        (scriptItem) => {
-            switch (scriptItem.type) {
-                case "pos": {
-                    const x = get(scriptItem.xAtom);
-                    const y = get(scriptItem.yAtom);
-                    const units = get(scriptItem.unitsAtom);
-                    const res = get(scriptItem.resAtom);
-                    const rv: EditorDataForPos = {
-                        type: 'pos',
-                        x: +x.data,
-                        y: +y.data,
-                        units: !!units.data,
-                        res: +res.data,
-                    };
-                    return rv;
-                }
-                case "kbd": {
-                    const char = get(scriptItem.charAtom);
-                    const repeat = get(scriptItem.repeatAtom);
-                    const shift = get(scriptItem.shiftAtom);
-                    const ctrl = get(scriptItem.ctrlAtom);
-                    const alt = get(scriptItem.altAtom);
-                    const rv: EditorDataForKbd = {
-                        type: 'kbd',
-                        char: char.data,
-                        repeat: +repeat.data,
-                        shift: +shift.data,
-                        ctrl: +ctrl.data,
-                        alt: +alt.data,
-                    };
-                    return rv;
-                }
-                case "dly": {
-                    const n = get(scriptItem.nAtom);
-                    const rv: EditorDataForDly = {
-                        type: 'dly',
-                        n: +n.data,
-                    };
-                    return rv;
-                }
-                case "fld": {
-                    // const fromAtomValues: NormalFieldConv.FieldForAtoms = NormalFieldConv.fromAtoms(atom.field, get, set);
-                    // const maniValues = NormalFieldConv.forMani(fromAtomValues);
-                    // const fileValues = ManiConv.fieldForFileMani(maniValues, fieldAtoms.metaField, undefined, false);
-                    
-                    //TODO: this is not correct, need to get the field values from the atom
-                    const rv: EditorDataForFld = {
-                        type: 'fld',
-                        field: scriptItem.field.metaField,
-                    };
-                    return rv;
-                }
-            }
+function fromAtom(scriptItem: ManualField.ScriptTypesAtoms, get: Getter, set: Setter): ScriptChunkEditorData {
+    switch (scriptItem.type) {
+        case "pos": {
+            const x = get(scriptItem.xAtom);
+            const y = get(scriptItem.yAtom);
+            const units = get(scriptItem.unitsAtom);
+            const res = get(scriptItem.resAtom);
+            const rv: EditorDataForPos = {
+                type: 'pos',
+                x: +x.data,
+                y: +y.data,
+                units: !!units.data,
+                res: +res.data,
+            };
+            return rv;
         }
-    );
+        case "kbd": {
+            const char = get(scriptItem.charAtom);
+            const repeat = get(scriptItem.repeatAtom);
+            const shift = get(scriptItem.shiftAtom);
+            const ctrl = get(scriptItem.ctrlAtom);
+            const alt = get(scriptItem.altAtom);
+            const rv: EditorDataForKbd = {
+                type: 'kbd',
+                char: char.data,
+                repeat: +repeat.data,
+                shift: +shift.data,
+                ctrl: +ctrl.data,
+                alt: +alt.data,
+            };
+            return rv;
+        }
+        case "dly": {
+            const n = get(scriptItem.nAtom);
+            const rv: EditorDataForDly = {
+                type: 'dly',
+                n: +n.data,
+            };
+            return rv;
+        }
+        case "fld": {
+            // const fromAtomValues: NormalFieldConv.FieldForAtoms = NormalFieldConv.fromAtoms(atom.field, get, set);
+            // const maniValues = NormalFieldConv.forMani(fromAtomValues);
+            // const fileValues = ManiConv.fieldForFileMani(maniValues, fieldAtoms.metaField, undefined, false);
 
+            //TODO: this is not correct, need to get the field values from the atom
+            const rv: EditorDataForFld = {
+                type: 'fld',
+                field: scriptItem.field.metaField,
+            };
+            return rv;
+        }
+    }
+}
+
+export function fromAtoms(scriptItems: ManualField.ScriptTypesAtoms[], get: Getter, set: Setter): ScriptChunkEditorData[] {
+    const chunks = scriptItems.map((scriptItem) => fromAtom(scriptItem, get, set));
     return chunks;
 }
