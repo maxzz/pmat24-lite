d19f9f328e6658e1c37cfe367243e5627b1757f9
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-29 16:12:09 -0700

    WIP. manual ui. connect PanelActionsTitle

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all-action.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all-action.tsx
@@ -1,17 +1,33 @@
-// import { useEffect } from "react";
-// import { useInitSelectedIdx } from "@/store";
-// import { PanelActionsTitle } from "./1-panel-title";
-// import { PanelActionsList } from "./2-panel-items";
-// import { classNames } from "@/utils";
-// import { editorFrameClasses } from "@/components/ui/shared-styles";
+import { useEffect } from "react";
+import { useAtomValue } from "jotai";
+import { type MFormContextProps } from "@/store/atoms/3-file-mani-atoms";
 
-// export function PanelActions() {
-//     const cb = useInitSelectedIdx();
-//     useEffect(() => { cb(); }, []);
-//     return (
-//         <div className={classNames("h-full min-h-[20rem] flex flex-col space-y-1 select-none", editorFrameClasses/*, focusWithinClasses*/)}>
-//             <PanelActionsTitle />
-//             <PanelActionsList />
-//         </div>
-//     );
-// };
+import { PanelActionsTitle } from "./1-panel-title";
+//import { PanelActionsList } from "./2-panel-items";
+import { classNames } from "@/utils";
+
+import { useInitSelectedIdx } from "@/store/atoms/3-file-mani-atoms";
+import { editorFrameClasses } from "../shared-styles";
+
+export function ManualPanelActions({ maniAtoms, formAtoms, formIdx }: MFormContextProps) {
+    // const cb = useInitSelectedIdx();
+    // useEffect(() => { cb(); }, []);
+
+    const items = useAtomValue(formAtoms.manual.chunksAtom);
+    return (
+        <div className={classNames("h-full min-h-[20rem] flex flex-col space-y-1 select-none", editorFrameClasses/*, focusWithinClasses*/)}>
+            <PanelActionsTitle />
+
+            {/* <PanelActionsList /> */}
+            <div className="">
+                {items.map(
+                    (item, idx) => (
+                        <div key={idx}>
+                            {item.type}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
--- src/renderer/src/store/atoms/3-file-mani-atoms/2-manual-fields/index.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/2-manual-fields/index.tsx
@@ -1,2 +1,3 @@
 export * from "./0-conv";
 export * from "./1-fields-atoms";
+export * from "./3-panels";
