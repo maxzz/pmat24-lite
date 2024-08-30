5629d77939d9c71e25bb0305d4b55f69db7ac1ea
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-30 13:45:29 -0700

    WIP. manual ui. props. PropsEditorDly done

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/panel-delay/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/panel-delay/index.tsx
@@ -1,27 +1,39 @@
 import { HTMLAttributes } from "react";
-import { useAtom } from "jotai";
 import { ManualFieldState } from "@/store/atoms/3-file-mani-atoms";
-import { InputField, propsBoxClasses } from "../../8-ui";
+import { propsBoxClasses } from "../../8-ui";
+import { RowInputWLabel } from "@/components/2-main/2-right/2-file-mani/2-form-options/9-controls";
+
+// export function InputDelay({ item }: { item: ManualFieldState.DlyForAtoms; }) {
+//     const [n, setN] = useAtom(item.nAtom);
+
+//     function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+//         let n = +e.target.value;
+//         if (Number.isNaN(n)) {
+//             n = 1;
+//         }
+//         setN(n);
+//     }
+
+//     return (
+//         <div className="flex items-end space-x-1" title="Delay in ms before performing the next action">
+//             <InputField
+//                 className="w-12" horizontal
+//                 label="Delay"
+//                 value={`${n}`}
+//                 onChange={onChange}
+//             />
+
+//             <div className="pb-1">
+//                 ms
+//             </div>
+//         </div>
+//     );
+// }
 
 export function InputDelay({ item }: { item: ManualFieldState.DlyForAtoms; }) {
-    const [n, setN] = useAtom(item.nAtom);
-
-    function onChange(e: React.ChangeEvent<HTMLInputElement>) {
-        let n = +e.target.value;
-        if (Number.isNaN(n)) {
-            n = 1;
-        }
-        setN(n);
-    }
-
     return (
         <div className="flex items-end space-x-1" title="Delay in ms before performing the next action">
-            <InputField
-                className="w-12" horizontal
-                label="Delay"
-                value={`${n}`}
-                onChange={onChange}
-            />
+            <RowInputWLabel stateAtom={item.nAtom} label="Description" />
 
             <div className="pb-1">
                 ms
