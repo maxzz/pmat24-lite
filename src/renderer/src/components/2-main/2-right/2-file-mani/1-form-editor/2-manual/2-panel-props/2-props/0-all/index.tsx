dffb2970cb518c933b07605126872660e59b4499
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-31 14:54:20 -0700

    WIP. manual ui. ManualPanelActions. update ScriptItemPropsEditor layout

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/0-all/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/0-all/index.tsx
@@ -1,8 +1,8 @@
 import { ManualFieldState } from "@/store/atoms/3-file-mani-atoms";
-import { PropsEditorDly } from "../panel-delay";
-import { PropsEditorFld } from "../panel-field";
-import { PropsEditorKey } from "../panel-key";
-import { PropsEditorPos } from "../panel-pos";
+import { PropsEditorDly } from "../3-panel-delay";
+import { PropsEditorFld } from "../1-panel-field";
+import { PropsEditorKey } from "../2-panel-key";
+import { PropsEditorPos } from "../4-panel-pos";
 
 export function ScriptItemPropsEditor({ item }: { item: ManualFieldState.ForAtoms; }) {
     switch (item.type) {
