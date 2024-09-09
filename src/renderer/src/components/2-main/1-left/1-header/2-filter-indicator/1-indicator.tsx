07e266c309bb6ae22fe883790950ca60f24096f0
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-08 18:25:24 -0700

    WIP. files list. update L_PanelHeader.CurrentFilter

--- src/renderer/src/components/2-main/1-left/1-header/2-filter-indicator/1-indicator.tsx
+++ src/renderer/src/components/2-main/1-left/1-header/2-filter-indicator/1-indicator.tsx
@@ -9,9 +9,9 @@ export function CurrentFilter() {
         return null;
     }
     return (
-        <div className="flex items-center gap-0.5">
-            <span className="">Filter:</span>
-            <span className="">{text}</span>
+        <div className="px-2 border-border border rounded flex items-center gap-0.5">
+            <span>Filter:</span>
+            <span>{text}</span>
             <Button className="h-6" variant="ghost" onClick={() => setText('')}>
                 <IconClose className="size-3" />
             </Button>
