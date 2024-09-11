efda224650c4cc8913cd70d6c4ba59436d3f7f83
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-10 18:40:37 -0700

    WIP. save mani. update 2-do-save-main-atom params. 4. add getOptions()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/1-all-do-save-one-atom/1-pack-manifest/1-pack-manifest-data.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/1-all-do-save-one-atom/1-pack-manifest/1-pack-manifest-data.ts
@@ -1,14 +1,14 @@
 import { type FileMani } from "@/store/manifest";
 import { type NormalField, type NormalFieldsState, NormalFieldConv, SubmitConv } from "../../../../1-normal-fields";
 import { ManiConv } from "../../0-conv/1-normal/2-conv-mani";
-import { OptionsConv } from "../../../../4-options";
+import { ManiOptions, OptionsConv } from "../../../../4-options";
 import { type DAOForMani, detectionAndOptionsForMani } from "../../0-conv/3-options/53-conv-mani-options";
 import { filterEmptyValues } from "./7-filter-empty-values";
 import { PackManifestDataParams } from "./9-types";
 import { NFormCtx } from "@/store/atoms/3-file-mani-atoms/9-types";
 
 export function getNormalFields(formCtx: NFormCtx, packParams: PackManifestDataParams) {
-    const { get, set } = packParams;
+    const { rvManifest, get, set } = packParams;
 
     // 1. Submits
 
@@ -36,6 +36,27 @@ export function getNormalFields(formCtx: NFormCtx, packParams: PackManifestDataP
     // console.log('maniValues', JSON.stringify(fields, null, 2));
 }
 
+export function getOptions(optionsP: ManiOptions.FormOptionsAtoms, packParams: PackManifestDataParams) {
+    const { rvManifest, get, set } = packParams;
+
+    const detectionAndOptionsRow = OptionsConv.fromAtoms(optionsP, get, set);
+
+    const { detection, options } = detectionAndOptionsForMani(detectionAndOptionsRow);
+
+    const detectionFiltered = filterEmptyValues(detection);
+    const optionsFiltered = filterEmptyValues(options);
+
+    const detectionAndOptions: Partial<DAOForMani> = {
+        ...(detectionFiltered && { detection: detectionFiltered }),
+        ...(optionsFiltered && { options: optionsFiltered }),
+    };
+
+    const optionStr = JSON
+        .stringify(detectionAndOptions, null, 2)
+        .replace(/"names_ext":\s".*",/, '"names_ext": "...",');
+    console.log('options', optionStr);
+}
+
 export function packManifestData(packParams: PackManifestDataParams) {
     const { fileUs, fileUsAtom, maniAtoms, rvManifest, get, set } = packParams;
 
@@ -49,22 +70,7 @@ export function packManifestData(packParams: PackManifestDataParams) {
 
         // 3. Options
 
-        const detectionAndOptionsRow = OptionsConv.fromAtoms(loginFormAtoms.options, get, set);
-
-        const { detection, options } = detectionAndOptionsForMani(detectionAndOptionsRow);
-
-        const detectionFiltered = filterEmptyValues(detection);
-        const optionsFiltered = filterEmptyValues(options);
-
-        const detectionAndOptions: Partial<DAOForMani> = {
-            ...(detectionFiltered && { detection: detectionFiltered }),
-            ...(optionsFiltered && { options: optionsFiltered }),
-        };
-
-        const optionStr = JSON
-            .stringify(detectionAndOptions, null, 2)
-            .replace(/"names_ext":\s".*",/, '"names_ext": "...",');
-        console.log('options', optionStr);
+        getOptions(loginFormAtoms.options, packParams);
 
         // 4. The rest
 
