2f87cf3996c5539eee7195efbbc0b6aafbb255d3
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-10 18:12:25 -0700

    WIP. save mani. update 2-do-save-main-atom files layout. 3

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/1-all-do-save-one-atom/0-all-pack-manifest-data/1-pack-manifest-data.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/1-all-do-save-one-atom/0-all-pack-manifest-data/1-pack-manifest-data.ts
@@ -2,9 +2,9 @@ import { type Getter, type Setter } from "jotai";
 import { type FileUs, type FileUsAtom } from "@/store/store-types";
 import { type FileMani } from "@/store/manifest";
 import { type NormalField, type NormalFieldsState, NormalFieldConv, SubmitConv } from "../../../../1-normal-fields";
-import { ManiConv } from "../1-normal/2-conv-mani";
+import { ManiConv } from "../../0-conv/1-normal/2-conv-mani";
 import { OptionsConv } from "../../../../4-options";
-import { type DAOForMani, detectionAndOptionsForMani } from "../3-options/53-conv-mani-options";
+import { type DAOForMani, detectionAndOptionsForMani } from "../../0-conv/3-options/53-conv-mani-options";
 import { filterEmptyValues } from "./7-filter-empty-values";
 
 export function packManifestData(get: Getter, set: Setter, fileUs: FileUs, fileUsAtom: FileUsAtom, newFilename?: string) {
