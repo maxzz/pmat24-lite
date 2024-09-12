f89ae36985fb245809a5c61b09adb47e83865db1
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-11 19:25:58 -0700

    WIP. save mani. add getNormalFieldValues()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/1-get-normal-fields.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/1-get-normal-fields.ts
@@ -1,31 +1,22 @@
-import { type FileMani } from "@/store/manifest";
 import { type PackManifestDataParams } from "../9-types";
 import { type NFormCtx } from "@/store/atoms/3-file-mani-atoms/9-types";
 import { type NormalField, type NormalFieldsState, NormalFieldConv } from "../../../../1-normal-fields";
-import { fieldForFileMani } from "./7-conv-mani-field";
 
-export function packNormalFields(formCtx: NFormCtx, packParams: PackManifestDataParams) {
+export function getNormalFieldValues(formCtx: NFormCtx, packParams: PackManifestDataParams): NormalField.ThisType[] {
     const { rvManifest, get, set } = packParams;
 
     // 2. Fields
 
-    const fields: FileMani.Field[] = formCtx.fieldsAtoms.map(
+    const rv = formCtx.fieldsAtoms.map(
         (fieldAtoms: NormalFieldsState.Atoms) => {
             const metaField = fieldAtoms.metaField;
 
             const fromAtomValues: NormalField.ForAtoms = NormalFieldConv.fromAtoms(fieldAtoms, get, set);
             const maniValues: NormalField.ThisType = NormalFieldConv.forMani(fromAtomValues);
 
-            const fileValues: FileMani.Field = fieldForFileMani({
-                from: maniValues,
-                maniField: metaField.mani,
-                ftyp: metaField.ftyp,
-                rdir: undefined,
-                isSubmit: false,
-            });
-            return fileValues;
+            return maniValues;
         }
     );
 
-    // console.log('maniValues', JSON.stringify(fields, null, 2));
+    return rv;
 }
