5165ba95dede26520887e3e611184482c4086f0b
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-12 14:59:18 -0700

    WIP. save mani. combine createFieldsCtx with callback

--- src/renderer/src/store/atoms/3-file-mani-atoms/1-normal-fields/1-field-items/1-create-normal/1-create-fields-context.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-normal-fields/1-field-items/1-create-normal/1-create-fields-context.tsx
@@ -1,8 +1,9 @@
 import { type Getter, type Setter } from 'jotai';
-import { FieldTyp, type Meta } from '@/store/manifest';
-import { type NormalField } from '../0-conv';
-import { type FileUsCtx, type ManiAtoms } from "../../../9-types";
-import { NormalFieldState } from './2-field-atoms';
+import { fieldForEditor, FieldTyp, type Meta } from '@/store/manifest';
+import { NormalFieldConv, type NormalField } from '../0-conv';
+import { type OnChangeProps, setManiChanges, type FileUsCtx, type ManiAtoms } from "../../../9-types";
+import { type OnValueChangeAny } from '@/util-hooks';
+import { debounce } from '@/utils';
 
 export namespace NormalFieldsState {
 
@@ -18,16 +19,49 @@ export namespace NormalFieldsState {
         const nonButtonFields = fields.filter((field) => field.ftyp !== FieldTyp.button);
 
         function mapMetaFieldToFieldAtoms(field: Meta.Field, idx: number): Atoms {
-
             function onChange({ get, set }: { get: Getter, set: Setter }) {
-                NormalFieldState.onChangeWithScopeDebounced(idx, { fileUsCtx: fileUsCtx, maniAtoms, get, set });
+                onChangeWithScopeDebounced(idx, { fileUsCtx, maniAtoms, get, set });
             }
 
-            const rowAtoms = NormalFieldState.createUiAtoms(field, onChange);
+            const rowAtoms = createUiAtoms(field, onChange);
             return rowAtoms;
         }
 
         const rv = nonButtonFields.map(mapMetaFieldToFieldAtoms) || [];
         return rv;
     }
+}
+
+function onChangeWithScope(fieldIdx: number, { fileUsCtx, maniAtoms, get, set }: OnChangeProps) {
+    const nomalFormAtoms = maniAtoms[fileUsCtx.formIdx]!.normal;
+    if (!nomalFormAtoms) {
+        return;
+    }
+
+    const atoms: NormalField.FieldAtoms = nomalFormAtoms.fieldsAtoms[fieldIdx];
+
+    const fromUi = NormalFieldConv.fromAtoms(atoms, get, set);
+    const changed = !NormalFieldConv.areTheSame(fromUi, atoms.fromFile);
+    atoms.changed = changed;
+
+    const changes = setManiChanges(fileUsCtx, changed, `${fileUsCtx.formIdx ? 'c' : 'l'}-f-${fieldIdx}`);
+
+    /** /
+    const str1 = JSON.stringify(state.policies, null, 2);
+    const str2 = JSON.stringify(atoms.fromFile.policies, null, 2);
+    const str3 = changes.size ? `\nstate ${str1}\nfile ${str2}` : '';
+    console.log(`%cCombineField: [${[...changes.keys()]}]%c${str3}`, 'background-color: navy; color: bisque', 'background-color: #282828; color: white');
+    /**/
+}
+
+const onChangeWithScopeDebounced = debounce(onChangeWithScope);
+
+function createUiAtoms(field: Meta.Field, onChange: OnValueChangeAny): NormalField.FieldAtoms {
+    const forAtoms = fieldForEditor(field.mani);
+    return {
+        ...NormalFieldConv.createAtoms(forAtoms, onChange),
+        metaField: field,
+        fromFile: forAtoms,
+        changed: false,
+    };
 }
