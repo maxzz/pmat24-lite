1b958d0563b4b00bcb28de8a56b383b3e93789da
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-11 19:40:00 -0700

    WIP. save mani. split normal fields before update from submit

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
@@ -1,9 +1,14 @@
 import { type PackManifestDataParams } from "../9-types";
 import { type NFormCtx } from "@/store/atoms/3-file-mani-atoms/9-types";
-import { packNormalFields } from "./1-pack-normal-fields";
-import { packNormalSubmit } from "./2-pack-normal-submit";
+import { packNormalFields } from "./2-pack-normal-fields";
+import { packNormalSubmit } from "./3-pack-normal-submit";
+import { getNormalFieldValues } from "./1-get-normal-fields";
 
 export function packNormalFieldsAndSubmit(formCtx: NFormCtx, packParams: PackManifestDataParams) {
-    packNormalFields(formCtx, packParams); // This should be before packNormalSubmit
+
+    const editAndMeta = getNormalFieldValues(formCtx, packParams);
+
+    packNormalFields(editAndMeta, packParams); // This should be before packNormalSubmit
+    
     packNormalSubmit(formCtx, packParams);
 }
--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/2-pack-normal-fields.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/2-pack-normal-fields.ts
@@ -1,31 +1,28 @@
 import { type FileMani } from "@/store/manifest";
 import { type PackManifestDataParams } from "../9-types";
-import { type NFormCtx } from "@/store/atoms/3-file-mani-atoms/9-types";
-import { type NormalField, type NormalFieldsState, NormalFieldConv } from "../../../../1-normal-fields";
+import { type EditorFieldAndMeta } from "./1-get-normal-fields";
 import { fieldForFileMani } from "./7-conv-mani-field";
 
-export function packNormalFields(editorFields: NormalField.ThisType[], packParams: PackManifestDataParams): FileMani.Field[] {
+export function packNormalFields(editorFields: EditorFieldAndMeta[], packParams: PackManifestDataParams): FileMani.Field[] {
     const { rvManifest, get, set } = packParams;
 
     // 2. Fields
 
     const fields: FileMani.Field[] = editorFields.map(
-        (editorField: NormalField.ThisType) => {
-            const metaField = fieldAtoms.metaField;
-
-            const fromAtomValues: NormalField.ForAtoms = NormalFieldConv.fromAtoms(fieldAtoms, get, set);
-            const maniValues: NormalField.ThisType = NormalFieldConv.forMani(fromAtomValues);
+        (editorField: EditorFieldAndMeta) => {
+            const metaField = editorField.metaField;
 
             const fileValues: FileMani.Field = fieldForFileMani({
-                from: maniValues,
+                from: editorField.editField,
                 maniField: metaField.mani,
                 ftyp: metaField.ftyp,
                 rdir: undefined,
                 isSubmit: false,
             });
+
             return fileValues;
         }
     );
 
-    // console.log('maniValues', JSON.stringify(fields, null, 2));
+    return fields;
 }
