634322552b7319778ee3d375615627b9d2027f96
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-13 19:20:09 -0700

    WIP. save mani. update 2-pack files layout

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
@@ -1,8 +1,8 @@
 import { type PackManifestDataParams } from "../9-types";
 import { type NFormCtx } from "@/store/atoms/3-file-mani-atoms/9-types";
-import { packNormalFields } from "./2-pack-normal-fields";
-import { packNormalSubmit } from "./3-pack-normal-submit";
-import { getNormalFieldValues } from "./1-get-normal-fields";
+import { packNormalFields } from "./1-pack-normal-fields";
+import { packNormalSubmit } from "./5-pack-normal-submit";
+import { getNormalFieldValues } from "./2-get-normal-fields";
 
 export function packNormalFieldsAndSubmit(formCtx: NFormCtx, packParams: PackManifestDataParams) {
 
--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/1-pack-normal-fields.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/1-pack-normal-fields.ts
@@ -1,7 +1,7 @@
 import { type FileMani } from "@/store/manifest";
 import { type PackManifestDataParams } from "../9-types";
-import { type EditorFieldAndMeta } from "./1-get-normal-fields";
-import { fieldForFileMani } from "./7-conv-mani-field";
+import { type EditorFieldAndMeta } from "./2-get-normal-fields";
+import { fieldForFileMani } from "./3-conv-mani-field";
 
 export function packNormalFields(editorFields: EditorFieldAndMeta[], packParams: PackManifestDataParams): FileMani.Field[] {
     const { rvManifest, get, set } = packParams;
