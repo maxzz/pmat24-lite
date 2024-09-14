f29bcf3fc57aaffa0b214fb887261f38b84242c8
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-13 19:40:03 -0700

    WIP. save mani. add updatedSubmitFields()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
@@ -3,6 +3,7 @@ import { type SubmitConvTypes, type NFormCtx } from "@/store/atoms/3-file-mani-a
 import { packNormalFields } from "./1-pack-normal-fields";
 import { packNormalSubmit } from "./5-pack-normal-submit";
 import { getNormalFieldValues } from "./2-get-normal-fields";
+import { duplicateManiField } from "./7-duplicate-mani-field";
 
 export function packNormalFieldsAndSubmit(formCtx: NFormCtx, packParams: PackManifestDataParams) {
 
@@ -11,4 +12,14 @@ export function packNormalFieldsAndSubmit(formCtx: NFormCtx, packParams: PackMan
     packNormalFields(editAndMeta, packParams); // This should be before packNormalSubmit
     
     const submits: SubmitConvTypes.SubmitForAtoms = packNormalSubmit(formCtx, packParams); // Options should called before submit or set form submit here
+
+    const selected = submits.selected;
+
+    const updatedSubmitFields = submits.buttonNames.map((submit) => {
+        return {
+            name: submit.name,
+            field: submit.metaField?.mani && duplicateManiField(submit.metaField?.mani),
+        };
+    });
+
 }
