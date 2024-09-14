fc70772bf0988e8e9dd14d9c795f336484987e94
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-13 19:18:37 -0700

    WIP. save mani. simplify duplicateManiField()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/7-duplicate-mani-field.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/7-duplicate-mani-field.ts
@@ -1,6 +1,14 @@
 import { Mani } from "pm-manifest";
 
+/**
+ * Mani.Field has no nested objects, so we can use destructuring to copy the object
+ * instead of JSON.parse(JSON.stringify(field)).
+ * Note: We don't care about chgpolopts (FieldPolicyOptions) because it's not used in the editor
+ * and initially should not exist at all.
+ */
 export function duplicateManiField(field: Mani.Field): Mani.Field {
-    const rv = JSON.parse(JSON.stringify(field));
+    const rv = {
+        ...field
+    };
     return rv;
 }
