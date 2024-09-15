ca77f5f5016731c9979e6cd3f33a8ee0e9406ce0
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-15 15:51:37 -0700

    WIP. save mani. pack options. done w/ packFormOptions()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/1-pack-manifest.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/1-pack-manifest.ts
@@ -1,9 +1,8 @@
 import { FormIdx } from "@/store/store-types";
-import { AnyFormAtoms } from "../../../9-types";
+import { type AnyFormAtoms } from "../../../9-types";
 import { type PackManifestDataParams, packManualFields, packNormalFieldsAndSubmit, packFormOptions } from "../2-pack";
 
 export function packManifest(packParams: PackManifestDataParams) {
-    
     const { maniAtoms } = packParams;
     const [loginFormAtoms, cpassFormAtoms] = maniAtoms;
 
@@ -14,10 +13,15 @@ export function packManifest(packParams: PackManifestDataParams) {
 function packForm(form: AnyFormAtoms | undefined, formIdx: FormIdx, packParams: PackManifestDataParams) {
     if (form) {
         const { newMani } = packParams;
-        
-        newMani.forms = newMani.forms || [];
 
-        packFormOptions(form.options, formIdx, packParams); // This should be before packNormalFieldsAndSubmit or create nessary options inside packNormalFieldsAndSubmit
+        const { detection, options } = packFormOptions(form.options, packParams);
+
+        newMani.forms = newMani.forms || [];
+        newMani.forms[formIdx] = {
+            detection,
+            options,
+            fields: [],
+        };
 
         if (form.normal) {
             packNormalFieldsAndSubmit(form.normal, packParams);
--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/3-options/0-all.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/3-options/0-all.ts
@@ -1,28 +1,18 @@
 import { type ManiOptions, OptionsConv } from "../../../../4-options";
 import { type PackManifestDataParams } from "../9-types";
-import { type DAOForMani } from "./8-conv-options-to-file";
-import { filterEmptyValues } from "../../0-save-atom/7-filter-empty-values";
 import { formDetectionForMani } from "./7-conv-form-detection";
 import { formOptionsForMani } from "./7-conv-form-options";
-import { FormIdx } from "@/store/store-types";
 
-export function packFormOptions(optionsAtoms: ManiOptions.FormOptionsAtoms, formIdx: FormIdx, packParams: PackManifestDataParams) {
+export function packFormOptions(optionsAtoms: ManiOptions.FormOptionsAtoms, packParams: PackManifestDataParams) {
     const { newMani, get, set } = packParams;
 
     const detectionAndOptionsRow = OptionsConv.fromAtoms(optionsAtoms, get, set);
 
-    if (!newMani.forms) {
-        return;
-    }
-
-    newMani.forms[formIdx] = {
+    const rv = {
         detection: formDetectionForMani(detectionAndOptionsRow),
         options: formOptionsForMani(detectionAndOptionsRow),
-        fields: [],
     };
+    // console.log('options', JSON.stringify(rv, null, 2).replace(/"names_ext":\s".*",/, '"names_ext": "...",'));
 
-    // const optionStr = JSON
-    //     .stringify(detectionAndOptions, null, 2)
-    //     .replace(/"names_ext":\s".*",/, '"names_ext": "...",');
-    // console.log('options', optionStr);
+    return rv;
 }
