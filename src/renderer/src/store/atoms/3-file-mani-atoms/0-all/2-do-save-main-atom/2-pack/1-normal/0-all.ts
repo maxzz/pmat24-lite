329ede24cd06117ff66e8d3a2a5d36851b843761
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-16 13:25:19 -0700

    WIP. save mani. remove 1-pack-normal-fields.ts

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/0-all.ts
@@ -1,15 +1,33 @@
+import { type Mani } from "@/store/manifest";
 import { type PackManifestDataParams } from "../9-types";
+import { type EditorFieldAndMeta } from "./2-get-normal-fields";
 import { type SubmitConvTypes, type NFormCtx } from "@/store/atoms/3-file-mani-atoms";
-import { packNormalFields } from "./1-pack-normal-fields";
 import { packNormalSubmit } from "./5-pack-normal-submit";
 import { getNormalFieldValues } from "./2-get-normal-fields";
 import { duplicateManiField } from "./7-duplicate-mani-field";
+import { fieldForFileMani } from "./7-conv-mani-field";
 
 export function packNormalFieldsAndSubmit(formCtx: NFormCtx, packParams: PackManifestDataParams) {
 
     const editAndMeta = getNormalFieldValues(formCtx, packParams);
 
-    packNormalFields(editAndMeta, packParams); // This should be before packNormalSubmit
+    const fields: Mani.Field[] = editAndMeta.map(
+        (editorField: EditorFieldAndMeta) => {
+            const metaField = editorField.metaField;
+
+            const fileValues: Mani.Field = fieldForFileMani({
+                from: editorField.editField,
+                maniField: metaField.mani,
+                ftyp: metaField.ftyp,
+                rdir: undefined,
+                isSubmit: false,
+            });
+
+            return fileValues;
+        }
+    );
+
+    //packNormalFields(editAndMeta, packParams); // This should be before packNormalSubmit
     
     const submits: SubmitConvTypes.SubmitForAtoms = packNormalSubmit(formCtx, packParams); // Options should called before submit or set form submit here
 
