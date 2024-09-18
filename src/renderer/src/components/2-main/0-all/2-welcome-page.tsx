c4bae99648870505b01710e98c19f3df07fe9ce8
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-17 19:36:05 -0700

    WIP. welcome. update WelcomePage. 2

--- src/renderer/src/components/2-main/0-all/2-welcome-page.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page.tsx
@@ -4,16 +4,17 @@ import { useAtomValue } from "jotai";
 
 export function WelcomePage() {
     const files = useAtomValue(filesAtom);
-    
-    if (files.length === 0) {
-        return (
-            <div className="h-full bg-muted-background flex flex-col items-center justify-center">
-                <div className="text-2xl">
-                    Welcome to the app!
-                </div>
-                <Button className="mt-4">Open a file</Button>
-            </div>
-        );
+
+    if (!files.length) {
+        return null;
     }
-    return null;
+
+    return (
+        <div className="h-full bg-muted-background flex flex-col items-center justify-center">
+            <div className="text-2xl">
+                Welcome to the app!
+            </div>
+            <Button className="mt-4">Open a file</Button>
+        </div>
+    );
 }
