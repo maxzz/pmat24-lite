f96172fe92d7893943e49cf2d2c7a7c1163fd944
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-19 13:29:03 -0700

    WIP. save mani. add ButtonQuickXml state feedback

--- src/renderer/src/components/2-main/2-right/1-header/0-all/3-btn-quick-xml.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/3-btn-quick-xml.tsx
@@ -2,6 +2,7 @@ import { useSnapshot } from "valtio";
 import { appSettings, RightPanelViewType } from "@/store";
 import { Button } from "@/ui";
 import { SymbolCode } from "@/ui/icons";
+import { classNames } from "@/utils";
 
 export function ButtonQuickXml() {
 
@@ -12,12 +13,14 @@ export function ButtonQuickXml() {
         return null;
     }
 
+    const isXml = activeView === RightPanelViewType.xml;
+
     function chageView() {
-        return appSettings.right.activeView = activeView === RightPanelViewType.forms ? RightPanelViewType.xml : RightPanelViewType.forms;
+        return appSettings.right.activeView = isXml ? RightPanelViewType.forms : RightPanelViewType.xml;
     }
 
     return (
-        <Button variant="ghost" className="-mr-4" onClick={chageView}>
+        <Button variant="ghost" className={classNames("-mr-4", isXml && "bg-muted")} onClick={chageView}>
             <SymbolCode className="size-4 fill-current" />
         </Button>
     );
