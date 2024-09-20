81789b98eb2e1abd97ed0a14971f2585cf3244e7
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-19 19:06:43 -0700

    WIP. save mani. submit UI. layout for web vs. win32. 4

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/2-submit/1-tab-all-submit-new.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/2-submit/1-tab-all-submit-new.tsx
@@ -2,7 +2,7 @@ import { useAtomValue, useAtom } from "jotai";
 import { type NFormContextProps } from "@/store/atoms/3-file-mani-atoms";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/ui/shadcn/select";
 
-function ManiSection2_Submit({ ctx }: { ctx: NFormContextProps; }) {
+function SubmitSelector({ ctx }: { ctx: NFormContextProps; }) {
 
     const buttonNameItems = useAtomValue(ctx.formAtoms.normal.submitAtoms.buttonNameItemsAtom);
     const [selected, setSelected] = useAtom(ctx.formAtoms.normal.submitAtoms.selectedAtom);
@@ -10,7 +10,7 @@ function ManiSection2_Submit({ ctx }: { ctx: NFormContextProps; }) {
     return (
         <Select value={selected.toString()} onValueChange={(value) => setSelected(+value)}>
 
-            <SelectTrigger className="px-2 py-1 h-7 w-max text-xs gap-1">
+            <SelectTrigger className="pl-2 pr-1 py-1 h-7 w-max text-xs gap-1">
                 <SelectValue placeholder={"Don't submit"} />
             </SelectTrigger>
 
@@ -33,9 +33,9 @@ function ManiSection2_Submit({ ctx }: { ctx: NFormContextProps; }) {
 function BodyWin32({ ctx }: { ctx: NFormContextProps; }) {
     const selected = useAtomValue(ctx.formAtoms.normal.submitAtoms.selectedAtom) !== 0;
     return (<>
-        After filling in the fields, 
+        After filling in the form fields, 
         {selected ? " the" : ""}
-        <ManiSection2_Submit ctx={ctx} /> 
+        <SubmitSelector ctx={ctx} /> 
         {selected ? " button will be used to submit the form data." : " the form data."}
     </>);
 }
@@ -46,8 +46,8 @@ export function TabSubmit({ ctx }: { ctx: NFormContextProps; }) {
         <div className="p-1 flex items-center gap-1">
             {isWeb
                 ? (<>
-                    After filling in the fields, 
-                    <ManiSection2_Submit ctx={ctx} />
+                    After filling in the form fields, 
+                    <SubmitSelector ctx={ctx} />
                     the form data.
                 </>) 
                 : (
@@ -57,11 +57,3 @@ export function TabSubmit({ ctx }: { ctx: NFormContextProps; }) {
         </div>
     );
 }
-
-// After filling in the fields click on the <ManiSection2_Submit ctx={ctx} /> button to submit the form data.
-                    
-// After filling in the fields, "YY" button will be used to submit the form data.
-// After filling in the fields, the "YY" button will be used to submit the form data.
-
-// After filling in the fields, Don't submit the form data.
-// After filling in the fields, do not submit the form data.
