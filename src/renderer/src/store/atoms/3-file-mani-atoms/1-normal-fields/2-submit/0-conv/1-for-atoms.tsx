01c59ab2aea1944e8d49e9c438223567f2d35071
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-20 13:05:24 -0700

    WIP. save mani. submit. update getSubmitChoices result. 3

--- src/renderer/src/store/atoms/3-file-mani-atoms/1-normal-fields/2-submit/0-conv/1-for-atoms.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/1-normal-fields/2-submit/0-conv/1-for-atoms.tsx
@@ -1,17 +1,95 @@
-import { type Meta } from "@/store/manifest";
+import { FieldTyp, SUBMIT, type Meta } from "@/store/manifest";
 import { type SubmitConvTypes } from "./9-types";
-import { getSubmitChoices } from "../9-get-submit-choices";
 
 export function forAtoms(metaForm: Meta.Form): SubmitConvTypes.SubmitForAtoms {
     
-    const { buttonNameItems, initialSelected, doSubmit, isSubmitTypeUndefined } = getSubmitChoices(metaForm);
+    const { buttonNameItems, selected, doSubmit, isSubmitTypeUndefined } = getSubmitChoices(metaForm);
 
     const rv: SubmitConvTypes.SubmitForAtoms = {
+        buttonNameItems,
+        selected,
+        doSubmit,
+        isSubmitTypeUndefined,
+    };
+    
+    return rv;
+}
+
+export function getSubmitChoices(metaForm: Meta.Form): SubmitConvTypes.SubmitForAtoms {
+    const isWeb = !!metaForm?.mani.detection.web_ourl;
+
+    const buttonFields = getButtonFields(metaForm);
+    const buttonNameItems = getButtonNameItems(buttonFields, isWeb);
+    
+    const doSubmit = metaForm.mani.options?.submittype === 'dosubmit';
+    const isSubmitTypeUndefined = typeof metaForm.mani.options?.submittype === 'undefined';
+
+    const selectedButtonIdx = getSelectedButtonIdx(isWeb, buttonFields);
+    const heuristicSubmit = isHeuristicSubmit(metaForm);
+
+    const initialSelected = (
+        heuristicSubmit || selectedButtonIdx !== -1
+            ? isWeb
+                ? 0
+                : selectedButtonIdx
+            : -1
+    ) + 1;
+
+    return {
         buttonNameItems,
         selected: initialSelected,
         doSubmit,
         isSubmitTypeUndefined,
     };
-    
+}
+
+function getButtonFields(metaForm: Meta.Form): Meta.Field[] {
+    return metaForm.fields?.filter(
+        (field) => field.ftyp === FieldTyp.button || field.mani.submit // we collect IE <a> so they are marked as submit
+    ) || [];
+}
+
+function getButtonNameItems(buttonFields: Meta.Field[], isWeb: boolean): SubmitConvTypes.ButtonNameItem[] {
+    const noSubmitOption = !isWeb && !buttonFields.length;
+
+    const rv: SubmitConvTypes.ButtonNameItem[] = [{
+        name: noSubmitOption ? "There is no control to submit" : "Don't submit",
+        metaField: null,
+    }];
+
+    if (isWeb) {
+        rv.push({
+            name: "Submit",
+            metaField: null,
+        });
+    } else {
+        let NameIdx = 0;
+        rv.push(...buttonFields.map(
+            (field) => ({
+                name: field.mani.displayname || `No name ${++NameIdx}`,
+                metaField: field,
+            })
+        ));
+    }
+
+    return rv;
+}
+
+function getSelectedButtonIdx(isWeb: boolean, buttonFields: Meta.Field[]): number {
+    let rv = -1;
+
+    buttonFields.forEach(
+        (field, idx) => field.mani.useit && (rv = idx)
+    );
+
+    return rv;
+}
+
+function isHeuristicSubmit(metaForm: Meta.Form): boolean {
+    return metaForm?.mani?.options?.submittype === SUBMIT.dosumbit;
+}
+
+function getUseItButtonIdx(buttonFields: Meta.Field[]): number {
+    let rv = buttonFields.findIndex((field) => (field.mani.type === 'button' || field.mani.submit) && field.mani.useit);
     return rv;
 }
