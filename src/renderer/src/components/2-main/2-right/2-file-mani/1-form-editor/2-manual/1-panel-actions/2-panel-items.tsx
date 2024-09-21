3f1fb2dbd74129c18340f4f0865890018b6bb88d
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-21 14:06:46 -0700

    WIP. save mani. manual. framer-motion. add the remove item animation

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/2-panel-items.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/2-panel-items.tsx
@@ -4,9 +4,9 @@ import { SingleRow } from "./3-single-row";
 import { MenuState } from "./4-row-popup-menu";
 import { rowParentActiveClasses } from "../8-manual-shared-styles";
 import { classNames } from "@/utils";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
-export function PanelActionsList({ ctx: ctxForm }: {ctx: MFormContextProps}) {
+export function PanelActionsList({ ctx: ctxForm }: { ctx: MFormContextProps; }) {
     const ctx = ctxForm.formAtoms.manual;
     const chunks = useAtomValue(ctx.chunksAtom);
 
@@ -18,34 +18,37 @@ export function PanelActionsList({ ctx: ctxForm }: {ctx: MFormContextProps}) {
     return (<>
         <div className={classNames("min-h-10 outline-none", rowParentActiveClasses)} tabIndex={0} onKeyDown={(e) => selectByKey(ctx, e.key)}>
 
-            {chunks.map(
-                (chunk, idx) => {
-                    const lastItemIdx = chunks.length - 1;
+            <AnimatePresence initial={false}>
+                {chunks.map(
+                    (chunk, idx) => {
+                        const lastItemIdx = chunks.length - 1;
 
-                    const menuState: MenuState = {
-                        onDelete: (e) => { e.stopPropagation(); deleteItem(ctx, idx); },
-                        onUp: (e) => { e.stopPropagation(); swapItems(ctx, idx, idx - 1); },
-                        onDn: (e) => { e.stopPropagation(); swapItems(ctx, idx, idx + 1); },
-                        hasUp: idx > 0,
-                        hasDn: idx < lastItemIdx,
-                    };
+                        const menuState: MenuState = {
+                            onDelete: (e) => { e.stopPropagation(); deleteItem(ctx, idx); },
+                            onUp: (e) => { e.stopPropagation(); swapItems(ctx, idx, idx - 1); },
+                            onDn: (e) => { e.stopPropagation(); swapItems(ctx, idx, idx + 1); },
+                            hasUp: idx > 0,
+                            hasDn: idx < lastItemIdx,
+                        };
 
-                    return (
-                        <SingleRow
-                            initial={{ opacity: 0, height: 0 }}
-                            animate={{ opacity: 1, height: "auto" }}
-                            transition={{ type:"spring", bounce: .5, duration: 1 }}
-                            ctx={ctx}
-                            chunk={chunk}
-                            menuState={menuState}
-                            idx={idx}
-                            onClick={(e) => selectItem(ctx, idx, (v) => e.ctrlKey ? !v : true)}
-                            key={chunk.uid5}
-                        />
-                    );
-                })
-            }
+                        return (
+                            <SingleRow
+                                initial={{ opacity: 0, height: 0 }}
+                                animate={{ opacity: 1, height: "auto" }}
+                                exit={{ opacity: 0, height: 0 }}
+                                transition={{ opacity: { duration: .2 }, type: "spring", duration: 1 }}
 
+                                ctx={ctx}
+                                chunk={chunk}
+                                menuState={menuState}
+                                idx={idx}
+                                onClick={(e) => selectItem(ctx, idx, (v) => e.ctrlKey ? !v : true)}
+                                key={chunk.uid5}
+                            />
+                        );
+                    })
+                }
+            </AnimatePresence>
         </div>
     </>);
 }
