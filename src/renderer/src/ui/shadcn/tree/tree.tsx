9972e18cab288b67d058338c9f9a99a1aeed4555
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-21 15:57:27 -0700

    WIP. tree. add update styles for state data-tree-item-selected

--- src/renderer/src/ui/shadcn/tree/tree.tsx
+++ src/renderer/src/ui/shadcn/tree/tree.tsx
@@ -231,8 +231,9 @@ const Leaf = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement> & LeafFol
         return (
             <div
                 ref={ref}
-                className={cn(leafBaseClasses, className, selected && leafSelectedClasses)}
+                className={cn(leafBaseClasses, selected && leafSelectedClasses, className)}
                 data-tree-id={item.id}
+                {...(selected && { 'data-tree-item-selected': '' })}
                 {...rest}
             >
                 <IconTextRender item={item} Icon={Icon} hideFolderIcon={false} iconClasses={leafIconClasses} />
@@ -247,8 +248,9 @@ const Folder = forwardRef<HTMLButtonElement, HTMLAttributes<HTMLButtonElement> &
         const { selected } = useSnapshot(item.state);
         return (
             <FolderTrigger
-                className={cn(treeItemBaseClasses, selected && treeItemSelectedClasses)}
+                className={cn(treeItemBaseClasses, selected && treeItemSelectedClasses, className)}
                 data-tree-folder-trigger={TypeTreeFolderTrigger}
+                {...(selected && { 'data-tree-item-selected': '' })}
                 arrowFirst={arrowFirst}
                 ref={ref}
                 {...rest}
