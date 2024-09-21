dd439cce16d02409c56579497fb800f9f53ed2fd
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-21 14:40:25 -0700

    WIP. save mani. manual. framer-motion. add insert new item after cursor. code clean up. done

--- src/renderer/src/store/atoms/3-file-mani-atoms/2-manual-fields/3-panels/1-panel-actions/5-op-atoms/6-do-create-item.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/2-manual-fields/3-panels/1-panel-actions/5-op-atoms/6-do-create-item.ts
@@ -10,13 +10,11 @@ export const doCreateItemAtom = atom(
     (get, set, ctx: MFormCtx, type: ChunkKey, password: boolean) => {
         const newItem = createScriptItem(type, password, ctx.onChangeItem);
 
-        let selectedIdx = get(ctx.selectedIdxStoreAtom);
-
         deselectCurrent(ctx, get, set);
 
         const chunks = get(ctx.chunksAtom);
 
-        //selectedIdx = Math.min(selectedIdx + 1, Math.max(0, chunks.length - 1));
+        let selectedIdx = get(ctx.selectedIdxStoreAtom);
         selectedIdx = clamp(selectedIdx + 1, 0, chunks.length - 1);
 
         const newChunks = [...chunks];
@@ -27,17 +25,3 @@ export const doCreateItemAtom = atom(
         set(doSelectIdxAtom, ctx, selectedIdx);
     }
 );
-
-// export const doCreateItemAtom = atom(
-//     null,
-//     (get, set, ctx: MFormCtx, type: ChunkKey, password: boolean) => {
-//         const newItem = createScriptItem(type, password, ctx.onChangeItem);
-
-//         const chunks = get(ctx.chunksAtom);
-//         const newChunks = [...chunks];
-//         newChunks.push(newItem);
-//         set(ctx.chunksAtom, newChunks);
-
-//         set(doSelectIdxAtom, ctx, newChunks.length - 1);
-//     }
-// );
