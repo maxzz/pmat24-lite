96b6760c59462a3c6daf0dc1e2794f4784fc0453
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-22 17:16:13 -0700

    WIP. reset mani. add options valuesToAtoms()

--- src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/2-create-atoms.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/4-options/0-conv/2-create-atoms.tsx
@@ -1,6 +1,8 @@
 import { atomWithCallback } from '@/util-hooks';
 import { type OnChangeValueWithUpdateName, createAtomForCheck, createAtomForInput, validateManifestName, validateNumber } from "@/ui";
 import { type ManiOptions } from "./9-types";
+import { Getter, Setter } from 'jotai';
+import { a } from '@react-spring/web';
 
 export function createAtoms(initialState: ManiOptions.OptionsForAtoms, onChange: OnChangeValueWithUpdateName): ManiOptions.FormOptionsAtoms {
     const { p1General, p2Detect, p3Auth, p4QL, p5Icon } = initialState;
@@ -56,3 +58,46 @@ export function createAtoms(initialState: ManiOptions.OptionsForAtoms, onChange:
 
     return rv;
 }
+
+export function valuesToAtoms(values: ManiOptions.OptionsForAtoms, atoms: ManiOptions.FormOptionsAtoms, get: Getter, set: Setter) {
+    set(atoms.p1General.nameAtom, (v) => ({ ...v, value: values.p1General.name }));
+    set(atoms.p1General.descAtom, (v) => ({ ...v, value: values.p1General.desc }));
+    set(atoms.p1General.hintAtom, (v) => ({ ...v, value: values.p1General.hint }));
+    set(atoms.p1General.balloonAtom, (v) => ({ ...v, value: values.p1General.balloon }));
+    set(atoms.p1General.submitTypeAtom, (v) => ({ ...v, value: values.p1General.submitType }));
+    set(atoms.p1General.qlNameAtom, (v) => ({ ...v, value: values.p1General.qlName }));
+    set(atoms.p1General.qlWoCredAtom, (v) => ({ ...v, value: values.p1General.qlWoCred }));
+    set(atoms.p1General.unkAttrsAtom, (v) => ({ ...v, value: values.p1General.unkAttrs }));
+
+    set(atoms.p2Detect.captionAtom, (v) => ({ ...v, value: values.p2Detect.caption }));
+    set(atoms.p2Detect.monitorAtom, (v) => ({ ...v, value: values.p2Detect.monitor }));
+    set(atoms.p2Detect.ourlAtom, (v) => ({ ...v, value: values.p2Detect.ourl }));
+    set(atoms.p2Detect.murlAtom, (v) => ({ ...v, value: values.p2Detect.murl }));
+
+    set(atoms.p2Detect.dlg_tabAtom, (v) => ({ ...v, value: values.p2Detect.dlg_tab }));
+    set(atoms.p2Detect.dlg_classAtom, (v) => ({ ...v, value: values.p2Detect.dlg_class }));
+    set(atoms.p2Detect.dlg_checkexeAtom, (v) => ({ ...v, value: values.p2Detect.dlg_checkexe }));
+
+    set(atoms.p2Detect.emu_patternAtom, (v) => ({ ...v, value: values.p2Detect.emu_pattern }));
+    set(atoms.p2Detect.namesAtom, (v) => ({ ...v, value: values.p2Detect.names }));
+    set(atoms.p2Detect.names_extAtom, (v) => ({ ...v, value: values.p2Detect.names_ext }));
+    set(atoms.p2Detect.processnameAtom, (v) => ({ ...v, value: values.p2Detect.processname }));
+    set(atoms.p2Detect.commandlineAtom, (v) => ({ ...v, value: values.p2Detect.commandline }));
+
+    set(atoms.p2Detect.reCheckAtom, (v) => ({ ...v, value: values.p2Detect.reCheck }));
+
+    set(atoms.p3Auth.aimAtom, (v) => ({ ...v, value: values.p3Auth.aim }));
+    set(atoms.p3Auth.lockAtom, (v) => ({ ...v, value: values.p3Auth.lock }));
+    set(atoms.p3Auth.auth_plAtom, (v) => ({ ...v, value: values.p3Auth.auth_pl }));
+
+    set(atoms.p4QL.dashboardAtom, (v) => ({ ...v, value: values.p4QL.dashboard }));
+    set(atoms.p4QL.qNameAtom, (v) => ({ ...v, value: values.p4QL.qName }));
+    set(atoms.p4QL.qUrlAtom, (v) => ({ ...v, value: values.p4QL.qUrl }));
+    set(atoms.p4QL.qUseAtom, (v) => ({ ...v, value: values.p4QL.qUse }));
+
+    set(atoms.p5Icon.idAtom, (v) => ({ ...v, value: values.p5Icon.id }));
+    set(atoms.p5Icon.locAtom, (v) => ({ ...v, value: values.p5Icon.loc }));
+
+    set(atoms.isWebAtom, values.isFormWeb);
+}
+
