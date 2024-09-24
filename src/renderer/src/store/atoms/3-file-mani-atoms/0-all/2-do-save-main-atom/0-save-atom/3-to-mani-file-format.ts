ae64ebd58442122ed56d7ca85d7441473f90a309
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-23 19:25:07 -0700

    WIP. save mani. toManiFileFormat(). add convertDetection() and convertOptions()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/3-to-mani-file-format.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/3-to-mani-file-format.ts
@@ -1,34 +1,36 @@
-import { type FileMani, type Mani } from "@/store/manifest";
+import { SUBMIT, type FileMani, type Mani } from "@/store/manifest";
 
 export function toManiFileFormat(newMani: Partial<Mani.Manifest>): FileMani.Manifest {
 
     const fileMani: FileMani.Manifest = {
-        descriptor: newMani.descriptor,
+        descriptor: newMani.descriptor!,
         options: newMani.options,
         forms: newMani.forms?.map(
             (form) => {
                 const newForm: FileMani.Form = {
-                    detection: form.detection,
-                    options: form.options,
-                    fields: form.fields.map(field => {
-                        const newField: FileMani.Field = {
-                            detection: field.detection,
-                            options: field.options,
-                        };
-                        if (field.normal) {
-                            newField.normal = {
-                                detection: field.normal.detection,
-                                options: field.normal.options,
+                    detection: convertDetection(form.detection),
+                    options: convertOptions(form.options),
+                    fields: form.fields.map(
+                        (field) => {
+                            const newField: FileMani.Field = {
+                                detection: field.detection,
+                                options: field.options,
                             };
+                            if (field.normal) {
+                                newField.normal = {
+                                    detection: field.normal.detection,
+                                    options: field.normal.options,
+                                };
+                            }
+                            if (field.manual) {
+                                newField.manual = {
+                                    detection: field.manual.detection,
+                                    options: field.manual.options,
+                                };
+                            }
+                            return newField;
                         }
-                        if (field.manual) {
-                            newField.manual = {
-                                detection: field.manual.detection,
-                                options: field.manual.options,
-                            };
-                        }
-                        return newField;
-                    }),
+                    ),
                 };
                 return newForm;
             }
@@ -36,4 +38,19 @@ export function toManiFileFormat(newMani: Partial<Mani.Manifest>): FileMani.Mani
     };
 
     return fileMani;
+}
+
+function convertDetection(detection: Mani.Detection): FileMani.Detection {
+    return {
+        ...detection as FileMani.Detection,
+        ...(detection.web_checkurl && { web_checkurl: '1' }),
+    };
+}
+
+function convertOptions(options: Mani.Options): FileMani.Options {
+    return {
+        ...options as FileMani.Options,
+        ...(options.submittype && { submittype: options.submittype as 'dosubmit' | `nosubmit` }),
+        ...(options.autoprompt && { autoprompt: '1' }),
+    };
 }
