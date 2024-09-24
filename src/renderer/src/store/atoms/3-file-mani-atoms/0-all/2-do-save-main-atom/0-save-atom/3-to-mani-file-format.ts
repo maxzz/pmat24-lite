7d22e7622d5e13955a3e0b3d081e95c7f862ce14
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-23 19:29:51 -0700

    WIP. save mani. toManiFileFormat(). fixing typescript

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/3-to-mani-file-format.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/3-to-mani-file-format.ts
@@ -1,56 +1,50 @@
 import { SUBMIT, type FileMani, type Mani } from "@/store/manifest";
+import { filterEmptyValues } from "./7-filter-empty-values";
 
 export function toManiFileFormat(newMani: Partial<Mani.Manifest>): FileMani.Manifest {
 
     const fileMani: FileMani.Manifest = {
         descriptor: newMani.descriptor!,
         options: newMani.options,
-        forms: newMani.forms?.map(
-            (form) => {
-                const newForm: FileMani.Form = {
-                    detection: convertDetection(form.detection),
-                    options: convertOptions(form.options),
-                    fields: form.fields.map(
-                        (field) => {
-                            const newField: FileMani.Field = {
-                                detection: field.detection,
-                                options: field.options,
-                            };
-                            if (field.normal) {
-                                newField.normal = {
-                                    detection: field.normal.detection,
-                                    options: field.normal.options,
-                                };
-                            }
-                            if (field.manual) {
-                                newField.manual = {
-                                    detection: field.manual.detection,
-                                    options: field.manual.options,
-                                };
-                            }
-                            return newField;
-                        }
-                    ),
-                };
-                return newForm;
-            }
-        ),
+        forms:
+            newMani.forms?.
+                map(
+                    (form) => {
+                        const newForm: FileMani.Form = {
+                            detection: convertDetection(form.detection),
+                            options: convertOptions(form.options),
+                            fields: form.fields.map(
+                                (field) => {
+                                    const newField: FileMani.Field = {
+                                        ...field as FileMani.Field,
+                                    };
+                                    return newField;
+                                }
+                            ),
+                        };
+                        return newForm;
+                    }
+                )
+                .filter(Boolean),
     };
 
     return fileMani;
 }
 
 function convertDetection(detection: Mani.Detection): FileMani.Detection {
-    return {
+    const rv: FileMani.Detection = {
         ...detection as FileMani.Detection,
         ...(detection.web_checkurl && { web_checkurl: '1' }),
     };
+    return rv;
 }
 
 function convertOptions(options: Mani.Options): FileMani.Options {
-    return {
+    //filterEmptyValues
+    const rv: FileMani.Options = {
         ...options as FileMani.Options,
         ...(options.submittype && { submittype: options.submittype as 'dosubmit' | `nosubmit` }),
         ...(options.autoprompt && { autoprompt: '1' }),
     };
+    return rv;
 }
