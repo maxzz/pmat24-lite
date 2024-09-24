166b7dc7e233dfd29c6428d03ca4f7cffc9082ed
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-23 18:41:46 -0700

    WIP. save mani. done with packDescriptor()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/2-pack-descriptor.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/2-pack-descriptor.ts
@@ -1,4 +1,4 @@
-import { createGuid, TimeUtils} from "@/store/manifest";
+import { createGuid, Mani, TimeUtils } from "@/store/manifest";
 import { PackManifestDataParams } from "../2-pack";
 
 export function packDescriptor(packParams: PackManifestDataParams) {
@@ -6,35 +6,16 @@ export function packDescriptor(packParams: PackManifestDataParams) {
 
     const { fileUs } = packParams;
 
-    // const createdDate = Date.now();
-    // const createdDate = fileUs.mani?.descriptor?.created || Date.now();
-    // const createdDate = fileUs.mani?.descriptor?.created || "1d43331 1aaabb19";
+    let descriptor = fileUs.mani?.descriptor;
 
-    const now = TimeUtils.dateToFileTime(new Date());
-    const hex = now.toString(16);
-    const pos = hex.length - 8;
-    const str = hex.slice(0, pos) + ' ' + hex.slice(pos);
-    console.log(`${pos} now: ${now}, hex: ${hex}, str: ${str} ui: "${TimeUtils.dpTimeToShow(str)}"`); // now: 133716029940500000, hex: 1db0e0551610620, str: 1db0e05 51610620 ui: "09.23.2024 03:09:54 PM"
+    descriptor = descriptor
+        ? { ...descriptor, }
+        : { id: `{${createGuid()}}`, created: TimeUtils.timeNowAsDpTime(), } as Mani.Descriptor;
 
-    console.log(`date 1: "${TimeUtils.dpTimeToShow("1d43331 1aaabb19")}"`);                    // "08.13.2018 11:11:49 AM"
-    console.log(`date 2: "${TimeUtils.dpTimeToShow(TimeUtils.dateToFileTime(new Date()))}"`);  // "09.23.2024 02:33:31 PM"
-    console.log(`date 3: "${TimeUtils.fileTimeToDate("1d43331 1aaabb19")}"`);                  // "Mon Aug 13 2018 11:11:49 GMT-0700 (Pacific Daylight Time)"
-    console.log(`date 4: "${TimeUtils.fileTimeToDate()}"`);                                    // "Mon Sep 23 2024 14:33:31 GMT-0700 (Pacific Daylight Time)"
+    descriptor.modified = TimeUtils.timeNowAsDpTime();
+    descriptor.version = '2.4.5';
 
-    // let oldDescriptor = fileUs.mani?.descriptor;
-    // if (!oldDescriptor) {
-    //     oldDescriptor = {
-    //         id: createGuid(),
-    //         created: new Date().getTime(),
-    //         modified: new Date().getTime(),
-    //         version: '2.4.5', // Old PMAT created files with version 2.4.3
-    //     };
-    // }
-
-    const newDescriptor = {
-        ...fileUs.mani?.descriptor,
-    };
-
+    newMani.descriptor = descriptor;
 }
 
 // function makeGuid(): string {
