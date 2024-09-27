a6495f74e62cdf0e210d5ff70faeb1ce0bb59d11
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-26 19:31:37 -0700

    WIP. extended policy. add prototype: 2-fido

--- src/renderer/src/components/1-header/0-all/index.tsx
+++ src/renderer/src/components/1-header/0-all/index.tsx
@@ -3,7 +3,7 @@ import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
 import { MainDropdownMenu } from "../1-main-menu";
 import { Button } from "@/ui";
 import { doOpenCreateDialogAtom, doOpenDrawerAtom } from "@/store/atoms/7-dialogs";
-import { IconExPolFace } from "@/ui/icons/normal/extended-policy";
+import { IconExPol02Fido, IconExPolFace } from "@/ui/icons/normal/extended-policy";
 
 export function SectionHeader() {
     const doOpenCreateDialog = useSetAtom(doOpenCreateDialogAtom);
@@ -23,6 +23,7 @@ export function SectionHeader() {
                 </Button>
 
                 <IconExPolFace className="size-5" />
+                <IconExPol02Fido className="size-5" />
             </div>
 
             <ThemeSwitch />
