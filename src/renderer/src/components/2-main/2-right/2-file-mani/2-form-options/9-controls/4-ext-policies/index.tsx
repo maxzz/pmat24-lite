e70d70bccf6f94cdc8ed1f41ae3dca62a992da6a
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-27 16:14:36 -0700

    WIP. extended policy. define ts tokens type

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/4-ext-policies/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/4-ext-policies/index.tsx
@@ -1,10 +1,14 @@
-//---------------------------------------------------------------------------
-/* 
- AUTHENTICATION POLICIES
- The low-order word specifies the Authentication Tokens enabled in the policy. See Authentication Tokens supported. 
- The high-order word specifies the relationship between Authentication Tokens. See Authentication Token relationships supported. 
-*/
-/* Authentication Tokens supported */
+// tokens.push_back(SToken(IDI_ICON_PASSWORD,    rstring(IDS_PASSWORD_TOKEN).c_str(),          DP_AUTH_TOKEN_PASSWORD));
+// tokens.push_back(SToken(IDI_ICON_FINGERPRINT, rstring(IDS_FINGERPRINTS_TOKEN).c_str(),      DP_AUTH_TOKEN_FINGERPRINT));
+// tokens.push_back(SToken(IDI_ICON_SMARTCARD,   rstring(IDS_SMARTCARD_TOKEN).c_str(),         DP_AUTH_TOKEN_SMARTCARD));
+// tokens.push_back(SToken(IDI_ICON_CONTACTLESS, rstring(IDS_CONTACTLESSCARD_TOKEN).c_str(),   DP_AUTH_TOKEN_CONTACTLESS));
+// tokens.push_back(SToken(IDI_ICON_PIN,         rstring(IDS_PIN_TOKEN).c_str(),               DP_AUTH_TOKEN_PIN));
+// tokens.push_back(SToken(IDI_ICON_OTP,         rstring(IDS_OTP_TOKEN).c_str(),               DP_AUTH_TOKEN_OTP));
+// tokens.push_back(SToken(IDI_ICON_PROXIMITY,   rstring(IDS_PROXIMITYCARD_TOKEN).c_str(),     DP_AUTH_TOKEN_PROXIMITY));
+// tokens.push_back(SToken(IDI_ICON_FIDOKEY,     rstring(IDS_FIDOKEY_TOKEN).c_str(),           DP_AUTH_TOKEN_U2F));
+// tokens.push_back(SToken(IDI_ICON_BLUETOOTH,   rstring(IDS_BLUETOOTH_TOKEN).c_str(),         DP_AUTH_TOKEN_BLUETOOTH));
+// tokens.push_back(SToken(IDI_ICON_FACE,        rstring(IDS_FACE_TOKEN).c_str(),              DP_AUTH_TOKEN_FACE));
+
 // #define DP_AUTH_TOKEN_PASSWORD       0x0001 // Password based Authentication Token
 // #define DP_AUTH_TOKEN_FINGERPRINT    0x0002 // Fingerprint based Authentication Token
 // #define DP_AUTH_TOKEN_SMARTCARD      0x0004 // Smart Card based Authentication Token
@@ -21,26 +25,6 @@
 // #define DP_AUTH_TOKEN_WIA            0x2000 // Windows Integrated Authentication (WIA) Authentication Token
 // #define DP_AUTH_TOKEN_MAIL           0x4000 // E-Mail Authentication Token
 // #define DP_AUTH_TOKEN_U2F            0x8000 // U2F Token
-/* 
- Authentication Tokens Relationships
-*/
-// #define DP_AUTH_RULE_OR     0x0  // Authentication rule is OR
-// #define DP_AUTH_RULE_AND    0x1  // Authentication rule is AND
-
-//---------------------------------------------------------------------------
-
-// tokens.push_back(SToken(IDI_ICON_PASSWORD,    rstring(IDS_PASSWORD_TOKEN).c_str(),          DP_AUTH_TOKEN_PASSWORD));
-// tokens.push_back(SToken(IDI_ICON_FINGERPRINT, rstring(IDS_FINGERPRINTS_TOKEN).c_str(),      DP_AUTH_TOKEN_FINGERPRINT));
-// tokens.push_back(SToken(IDI_ICON_SMARTCARD,   rstring(IDS_SMARTCARD_TOKEN).c_str(),         DP_AUTH_TOKEN_SMARTCARD));
-// tokens.push_back(SToken(IDI_ICON_CONTACTLESS, rstring(IDS_CONTACTLESSCARD_TOKEN).c_str(),   DP_AUTH_TOKEN_CONTACTLESS));
-// tokens.push_back(SToken(IDI_ICON_PIN,         rstring(IDS_PIN_TOKEN).c_str(),               DP_AUTH_TOKEN_PIN));
-// tokens.push_back(SToken(IDI_ICON_OTP,         rstring(IDS_OTP_TOKEN).c_str(),               DP_AUTH_TOKEN_OTP));
-// tokens.push_back(SToken(IDI_ICON_PROXIMITY,   rstring(IDS_PROXIMITYCARD_TOKEN).c_str(),     DP_AUTH_TOKEN_PROXIMITY));
-// tokens.push_back(SToken(IDI_ICON_FIDOKEY,     rstring(IDS_FIDOKEY_TOKEN).c_str(),           DP_AUTH_TOKEN_U2F));
-// tokens.push_back(SToken(IDI_ICON_BLUETOOTH,   rstring(IDS_BLUETOOTH_TOKEN).c_str(),         DP_AUTH_TOKEN_BLUETOOTH));
-// tokens.push_back(SToken(IDI_ICON_FACE,        rstring(IDS_FACE_TOKEN).c_str(),              DP_AUTH_TOKEN_FACE));
-
-//---------------------------------------------------------------------------
 
 // IDS_PASSWORD_TOKEN           "Password"
 // IDS_FINGERPRINTS_TOKEN       "Fingerprints"
@@ -53,10 +37,6 @@
 // IDS_BLUETOOTH_TOKEN          "Bluetooth"
 // IDS_FACE_TOKEN               "Face"
 
-//---------------------------------------------------------------------------
-
-// IDI_PASSWORDMANAGER   ICON   "Res\\Digital Persona PM Icon.ico"
-// IDI_DLLMAIN32         ICON   "Res\\Digital Persona Icon.ico"
 // IDI_ICON_BLUETOOTH    ICON   "Res\\Bluetooth.ico"
 // IDI_ICON_CONTACTLESS  ICON   "Res\\CONTACTLESS.ico"
 // IDI_ICON_FACE         ICON   "Res\\Face.ico"
@@ -68,4 +48,20 @@
 // IDI_ICON_PROXIMITY    ICON   "Res\\PROX.ico"
 // IDI_ICON_SMARTCARD    ICON   "Res\\Smartcard.ico"
 
-//---------------------------------------------------------------------------
+type TokenType = {
+    displayName: string;
+    value: number;
+};
+
+const tokens: TokenType[] = [
+    { displayName: 'Password', value: 0x0001 },
+    { displayName: 'Fingerprints', value: 0x0002 },
+    { displayName: 'PKI smart card', value: 0x0004 },
+    { displayName: 'Contactless writable card', value: 0x0020 },
+    { displayName: 'PIN', value: 0x0080 },
+    { displayName: 'One-Time Password', value: 0x0800 },
+    { displayName: 'Proximity ID card', value: 0x0100 },
+    { displayName: 'FIDO Key', value: 0x8000 },
+    { displayName: 'Bluetooth', value: 0x0200 },
+    { displayName: 'Face', value: 0x0010 },
+];
