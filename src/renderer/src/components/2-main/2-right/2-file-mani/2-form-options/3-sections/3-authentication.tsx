180edf77d3d5d8f7cf7856870f2c5d0b582a5d9b
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-29 13:33:47 -0700

    WIP. extended policy. update section Part3Authentication

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/3-authentication.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/3-sections/3-authentication.tsx
@@ -1,15 +1,20 @@
 import { type OFormContextProps } from "@/store/atoms/3-file-mani-atoms";
 import { RowInputWTitle } from "../9-controls";
 import { ExtPolicySelect } from "../9-controls/4-ext-policies/1-input-select";
+import { FormIdx } from "pm-manifest";
 
 export function Part3Authentication({ ctx }: { ctx: OFormContextProps; }) {
 
-    const { aimAtom, lockAtom, auth_plAtom } = ctx.oAllAtoms.options.p3Auth;
+    const { options } = ctx.oAllAtoms;
+    const { aimAtom, lockAtom, auth_plAtom } = options.p3Auth;
+    const isLogin = options.formIdx === FormIdx.login;
 
     return (<>
         <RowInputWTitle stateAtom={aimAtom} label="Authenticate immediately" asCheckbox />
         <RowInputWTitle stateAtom={lockAtom} label="Lock out login fields" asCheckbox />
 
-        <ExtPolicySelect stateAtom={auth_plAtom} />
+        {isLogin && (
+            <ExtPolicySelect stateAtom={auth_plAtom} />
+        )}
     </>);
 }
