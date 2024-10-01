0754a4da9227ed2724febc9f493eb7dabcc46794
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-30 18:37:41 -0700

    WIP. save. add createXml()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/0-all.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/0-all.tsx
@@ -1,4 +1,4 @@
-import { atom } from "jotai";
+import { atom, Getter, Setter } from "jotai";
 import { type FileUsAtom } from "@/store/store-types";
 import { convertToXml, type Mani } from "@/store/manifest";
 import { stopIfAnyErrors } from "../1-stop-if-errors";
@@ -6,6 +6,37 @@ import { packManifest } from "./1-pack-manifest";
 import { printTestManifest } from "./8-print-test-manifest";
 import { toManiFileFormat } from "./3-to-mani-file-format";
 import { saveContentToFile } from "./4-save-content";
+import { ManiAtoms } from "../../../9-types";
+
+function createXml(fileUsAtom: FileUsAtom, get: Getter, set: Setter): string | undefined {
+    const fileUs = get(fileUsAtom);
+
+    const maniAtoms = get(fileUs.maniAtomsAtom);
+    if (!maniAtoms || stopIfAnyErrors(maniAtoms, get, set)) {
+        return;
+    }
+
+    // Now validation done
+
+    const newMani: Partial<Mani.Manifest> = {
+        forms: [],
+    };
+
+    packManifest({ fileUs, fileUsAtom, maniAtoms, newMani, get, set });
+
+    const fileMani = toManiFileFormat(newMani);
+
+    const {xml, error} = convertToXml(fileMani)
+
+    console.log('xml', xml);
+
+    if (error || !xml) {
+        console.error('Error converting to xml', error);
+        return;
+    }
+
+    return xml;
+}
 
 export const doSaveOneAtom = atom(
     null,
