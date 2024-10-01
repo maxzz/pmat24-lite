f54015196fd317f95c02c0b12f45cce8a353add6
Max Zakharzhevskiy <maxzz@msn.com>
2024-09-30 18:42:12 -0700

    WIP. save. add createXml(). 4. integrated

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/0-all.tsx
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/0-all.tsx
@@ -1,44 +1,7 @@
-import { atom, Getter, Setter } from "jotai";
+import { atom } from "jotai";
 import { type FileUsAtom } from "@/store/store-types";
-import { convertToXml, type Mani } from "@/store/manifest";
-import { stopIfAnyErrors } from "../1-stop-if-errors";
-import { packManifest } from "./1-pack-manifest";
-import { printTestManifest } from "./8-print-test-manifest";
-import { toManiFileFormat } from "./3-to-mani-file-format";
-import { saveContentToFile } from "./4-save-content";
-
-function createXml(fileUsAtom: FileUsAtom, get: Getter, set: Setter): string | undefined {
-    const fileUs = get(fileUsAtom);
-
-    const maniAtoms = get(fileUs.maniAtomsAtom);
-    if (!maniAtoms || stopIfAnyErrors(maniAtoms, get, set)) {
-        return;
-    }
-
-    // Now validation done
-
-    const newMani: Partial<Mani.Manifest> = {
-        forms: [],
-    };
-
-    packManifest({ fileUs, fileUsAtom, maniAtoms, newMani, get, set });
-
-    const fileMani = toManiFileFormat(newMani);
-
-    const { xml, error } = convertToXml(fileMani);
-
-    console.log('xml', xml);
-
-    if (error || !xml) {
-        console.error('Error converting to xml', error);
-        return;
-    }
-
-    // printTestManifest(fileMani);
-    // printTestManifest(newMani);
-
-    return xml;
-}
+import { saveContentToFile } from "./5-save-content";
+import { createXmlText } from "./4-create-xml-text";
 
 export const doSaveOneAtom = atom(
     null,
@@ -50,7 +13,7 @@ export const doSaveOneAtom = atom(
             return;
         }
 
-        const xml = createXml(fileUsAtom, get, set);
+        const xml = createXmlText(fileUsAtom, get, set);
         if (!xml) {
             return;
         }
