ac94fdf82b7b4c6f01489a0963aeaa2385d93be3
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-02 19:05:53 -0700

    WIP. save. add getFilesRecursively() test

--- src/renderer/src/store/atoms/1-files/2-do-web-deliver/web-file-entries.ts
+++ src/renderer/src/store/atoms/1-files/2-do-web-deliver/web-file-entries.ts
@@ -66,3 +66,21 @@ export async function getAllFileEntries(dataTransferItemList: DataTransferItemLi
 
     return rv;
 }
+
+async function* getFilesRecursively(entry) {
+    if (entry.kind === "file") {
+        const file = await entry.getFile();
+        if (file !== null) {
+            file.relativePath = getRelativePath(entry);
+            yield file;
+        }
+    } else if (entry.kind === "directory") {
+        for await (const handle of entry.values()) {
+            yield* getFilesRecursively(handle);
+        }
+    }
+}
+
+for await (const fileHandle of getFilesRecursively(directoryHandle)) {
+    console.log(fileHandle);
+}
