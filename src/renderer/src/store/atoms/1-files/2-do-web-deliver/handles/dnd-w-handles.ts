6cdf96373bd16fa8f12352b3a832a65f8c7b3ae0
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-05 14:37:34 -0700

    WIP. save. problem. flat-drop-files. fix collectDNDHandles() types and add folders at any level

--- src/renderer/src/store/atoms/1-files/2-do-web-deliver/handles/dnd-w-handles.ts
+++ src/renderer/src/store/atoms/1-files/2-do-web-deliver/handles/dnd-w-handles.ts
@@ -1,48 +1,51 @@
+type FsHandle =
+    | { kind: 'file'; } & FileSystemFileHandle
+    | { kind: 'directory'; } & FileSystemDirectoryHandle;
+
+export function isFsFileHandle(handle: FileSystemHandle | FsHandle): handle is FileSystemFileHandle {
+    return handle.kind === 'file';
+}
+
+export function isFsDirectoryHandle(handle: FileSystemHandle | FsHandle): handle is FileSystemDirectoryHandle {
+    return handle.kind === 'directory';
+}
+
 /**
  * Traverses through a directory and yields files and folders (in undefined order)
  * https://github.com/umstek/listen/blob/main/src/util/fileSystem.ts
  *
  * @param folder folder to traverse
  */
-async function* getEntriesRecursively(folder: FileSystemDirectoryHandle): AsyncGenerator<[string[], FileSystemFileHandle | FileSystemDirectoryHandle], void, unknown> {
+async function* getEntriesRecursively(folder: FileSystemDirectoryHandle): AsyncGenerator<[string[], FsHandle], void, unknown> {
     for await (const entry of folder.values()) {
-        if (entry.kind === 'directory') {
+        if (entry.kind === 'file') {
             yield [[folder.name], entry];
+        }
+        else if (entry.kind === 'directory') {
+            yield [[folder.name, entry.name], entry];
 
             for await (const [path, file] of getEntriesRecursively(entry)) {
                 yield [[folder.name, ...path], file];
             }
-        } else {
-            yield [[folder.name], entry];
         }
     }
 }
 
-type FSHandle =
-    | { kind: 'file'; } & FileSystemFileHandle
-    | { kind: 'directory'; } | FileSystemDirectoryHandle;
-
-export function isFileSystemFileHandle(handle: FileSystemHandle | FSHandle): handle is FileSystemFileHandle {
-    return handle.kind === 'file';
-}
-
-export function isFileSystemDirectoryHandle(handle: FileSystemHandle | FSHandle): handle is FileSystemDirectoryHandle {
-    return handle.kind === 'directory';
-}
-
 export async function collectDNDHandles(dataTransferItems: DataTransferItemList) {
-    const fileHandlesPromises: Promise<FileSystemHandle | null>[] = [...dataTransferItems]
+    const fileHandlesPromises = [...dataTransferItems]
         .filter((item) => item.kind === 'file')
-        .map((item) => item.getAsFileSystemHandle());
+        .map((item) => item.getAsFileSystemHandle() as Promise<FsHandle | null>);
 
-    const rv: [path: string[], handle: FSHandle][] = [];
+    const rv: [path: string[], handle: FsHandle][] = [];
 
     for await (const handle of fileHandlesPromises) {
         if (handle) {
-            if (isFileSystemFileHandle(handle)) {
+            if (handle.kind === 'file') {
                 rv.push([[], handle]);
-            } else if (isFileSystemDirectoryHandle(handle)) {
-                for await (const subEntry of getEntriesRecursively(handle)) {
+            } else {
+                rv.push([[handle.name], handle]);
+                
+                for await (const subEntry of getEntriesRecursively(handle as FileSystemDirectoryHandle)) {
                     rv.push(subEntry);
                 }
             }
@@ -50,7 +53,7 @@ export async function collectDNDHandles(dataTransferItems: DataTransferItemList)
     }
 
     for (const [path, handle] of rv) {
-        console.log(`path: "${path.join('/')}", handle: %o`, handle);
+        console.log(`%cpath: "${path.join('/')}"%c, handle: %o`, `color: ${isFsFileHandle(handle) ? 'olive': 'fuchsia'}`, 'color: tan', handle);
     }
 
     return rv;
