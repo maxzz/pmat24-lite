32b71cded22e5f46b6077437aa76a7529665f33c
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-07 16:15:12 -0700

    WIP. save. problem. flat-drop-files. add new getFilesFromDataTransferItems()

--- src/renderer/src/store/atoms/1-files/2-do-web-deliver/3-legacy-entries/2-dnd-w-entries.ts
+++ src/renderer/src/store/atoms/1-files/2-do-web-deliver/3-legacy-entries/2-dnd-w-entries.ts
@@ -1,102 +1,14 @@
 import { isEntryDirectory, isEntryFile, type FsHandle } from "../9-fs-types";
 import { getFilePromisify, getReadEntriesPromisify } from "./8-promisify-entry-utils";
 
-/**
- * Derived from 'flat-drop-files'
- * https://github.com/placemark/flat-drop-files/blob/main/index.ts
- * Changes:
- *      1. FileWithHandleAndPath is exported
- *      2. ingnore link can be done outside of the getFilesFromDataTransferItems() function
- *      3. load files content should be conditioned on the file extension
- */
-
 export interface FileWithHandleAndPath extends File {
     handle?: FsHandle | null;
     path: string;
 }
 
-/**
- * Derived from 'junk'
- * https://github.com/sindresorhus/junk
- */
-/** /
-const ignoreList = [
-    // # All
-    "^npm-debug\\.log$", // Error log for npm
-    "^\\..*\\.swp$", // Swap file for vim state
-
-    // # macOS
-    "^\\.DS_Store$", // Stores custom folder attributes
-    "^\\.AppleDouble$", // Stores additional file resources
-    "^\\.LSOverride$", // Contains the absolute path to the app to be used
-    "^Icon\\r$", // Custom Finder icon: http://superuser.com/questions/298785/icon-file-on-os-x-desktop
-    "^\\._.*", // Thumbnail
-    "^\\.Spotlight-V100(?:$|\\/)", // Directory that might appear on external disk
-    "\\.Trashes", // File that might appear on external disk
-    "^__MACOSX$", // Resource fork
-
-    // # Linux
-    "~$", // Backup file
-
-    // # Windows
-    "^Thumbs\\.db$", // Image file cache
-    "^ehthumbs\\.db$", // Folder config file
-    "^Desktop\\.ini$", // Stores custom folder attributes
-    "@eaDir$", // Synology Diskstation "hidden" folder where the server stores thumbnails
-];
-
-export const junkRegex = new RegExp(ignoreList.join("|"));
-/**/
-
 type LoadFilter = (filename: string) => boolean; // filename wo/ path; returns true if the file content should be loaded
 const defaultLoadFilter: LoadFilter = (filename: string) => true; // load all files 
 let currentLoadFilter: LoadFilter = defaultLoadFilter; // we can make scopeed or member on getFilesFromEntry(), but so far its OK.
-
-/**
- * There are a bunch of similar-looking things here.
- *
- * - DataTransferItem: something that is _directly_ selected in a
- *   drag & drop operation. Can be a file, folder, or string.
- * - FileSystemEntry: only refers to a file or folder, but
- *   unlike a DataTransferItem, you can get a FileSystemEntry
- *   by traversing the files in a folder (files that were
- *   not explicitly selected). This is also the way
- *   that you can get a FileSystemHandle.
- * - File: the way that you can actually read a file. You can
- *   get a File object from a DataTransferItem directly with .getAsFile(), or
- *   from a FileSystemEntry with .file(). The File object is
- *   very similar to the Blob object.
- * - FileSystemFileHandle: an object that lets you read from
- *   and write to a file on a user's hard drive. This is so that,
- *   for example, you can hit Cmd+S and save back to the
- *   file that you opened.
- */
-
-/**
- * A few things this doesn't do but might want to in the future:
- *
- * - Get handles for directories, and add directories to the list.
- *   With a directory handle, you can save into a directory.
- */
-
-/**
- * Really nothing here but a method that routes to readFile or readDir.
- */
-function getFilesFromEntry(entry: FileSystemEntry, item: DataTransferItem | undefined, path = ""): Promise<FileWithHandleAndPath[]> {
-    if (isEntryFile(entry)) {
-        if (currentLoadFilter(entry.name)) {
-            return getFileAccess(entry, item, path).then((file) => [file]);
-        }
-    }
-    else if (isEntryDirectory(entry)) {
-        return readDir(entry, path, item);
-    }
-    return Promise.resolve([]);
-}
-
-/**
- * Promise adapters ----------------------------------------------------------
- */
 
 function getHandle(item: DataTransferItem | undefined): Promise<FsHandle | null> {
     if (!item?.getAsFileSystemHandle) { // Currently only Chromium browsers support getAsFileSystemHandle.
@@ -119,63 +31,10 @@ function getFileAccess(entry: FileSystemFileEntry, item: DataTransferItem | unde
     );
 }
 
-async function dirReadEntries(dirReader: FileSystemDirectoryReader, path: string): Promise<FileWithHandleAndPath[]> {
-    return getReadEntriesPromisify(dirReader).then(
-        (entries) => {
-            /**
-             * Recursion here! getFilesFromEntry will,
-             * for a directory, call readDir, which will call dirReadEntries all
-             * over again.
-             */
-            const getFilesPromises = entries.map(
-                (entry) => getFilesFromEntry(entry, undefined, path)
-            );
-
-            return Promise.all(getFilesPromises).then((nested) => nested.flat());
-        }
-    );
-}
-
-/**
- * This method is odd because
- *
- * - The .readEntries method only returns batches of 100,
- *   and signals when it's done because it returns a batch of 0.
- */
-async function readDir(entry: FileSystemDirectoryEntry, path: string, item: DataTransferItem | undefined): Promise<FileWithHandleAndPath[]> {
-    const dirReader = entry.createReader();
-    const newPath = path + entry.name + "/";
-    let files: FileWithHandleAndPath[] = [];
-    let newFiles: FileWithHandleAndPath[];
-
-    if (item) {
-        const handle = await getHandle(item) as FileSystemDirectoryHandle | null;
-        if (handle) {
-            (entry as any).handle = handle;
-            console.log('dir handle', handle);
-
-            // const entries = getEntriesRecursively(handle);
-            // for await (const [key, value] of entries) {
-            //     console.log('children all', { key, value });
-            // }
-
-            for await (const [key, value] of handle.entries()) {
-                console.log('children', { key, value }); // <- children { key: '{10250eb8-d616-4370-b3ab-39aedb8c6950}.dpm', value: FileSystemFileHandle }
-            }
-        }
-    }
-
-    do {
-        newFiles = await dirReadEntries(dirReader, newPath);
-        files = files.concat(newFiles);
-    } while (newFiles.length > 0);
-    return files;
-}
-
 async function* getEntriesRecursively(folder: FileSystemDirectoryEntry): AsyncGenerator<[string, FileWithHandleAndPath], void, unknown> {
     let entries: FileSystemEntry[] = [];
 
-    const dirReader = folder.createReader();
+    const dirReader = folder.createReader(); // The .readEntries returns batches of 100 entries and returns a 0 length when complete.
     let entriesPart: FileSystemEntry[];
     do {
         entriesPart = await getReadEntriesPromisify(dirReader);
@@ -216,6 +75,7 @@ export async function getFilesFromDataTransferItems(files: DataTransferItem[], l
      * handle after getting the first one.
      */
     const rv: FileWithHandleAndPath[] = [];
+
     for await (const [entry, dtItem] of inputs) {
         if (isEntryFile(entry)) {
             if (currentLoadFilter(entry.name)) {
@@ -228,15 +88,6 @@ export async function getFilesFromDataTransferItems(files: DataTransferItem[], l
             }
         }
     }
+    
     return rv;
-
-    // for await (const [path, file] of getEntriesRecursively(inputs[0][0] as FileSystemDirectoryEntry)) {
-    //     console.log(`%cpath: "${path}"%o`, `color: ${file ? 'tan' : 'fuchsia'}`, file);
-    // }
-
-    // return Promise.all(
-    //     inputs.map(([entry, item]) => getFilesFromEntry(entry, item))
-    // ).then((nested) => {
-    //     return nested.flat(); // return nested.flat().filter((file) => !junkRegex.test(file.name));
-    // });
 }
