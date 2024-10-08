a5fe7394f8c97ec405e43151b0ab91788e1f161b
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-08 14:50:55 -0700

    WIP. save. problem. flat-drop-files. ---------- backup experimints before remove. 2

--- src/renderer/src/store/atoms/1-files/2-do-web-deliver/3-legacy-entries/1-collect-dnd-entries.ts
+++ src/renderer/src/store/atoms/1-files/2-do-web-deliver/3-legacy-entries/1-collect-dnd-entries.ts
@@ -1,21 +1,9 @@
 import { FileWithHandleAndPath, getFilesFromDataTransferItems } from "./2-dnd-w-entries";
-//import { getFilesFromDataTransferItems2 } from "./2-dnd-w-entries-2";
-//import { getAllFileSystemEntries, readAllDirectoryEntries } from "./8-nun-old-read-entries";
-
-export type EntryHandle = {
-    legacyEntry: FileSystemFileEntry;
-    modernHandle: FileSystemFileHandle | null;
-};
+import { getFilesFromDataTransferItems2 } from "./2-dnd-w-entries-2";
 
 // function printFileHandles(handles: FileWithHandleAndPath[]) {
 //     for (const file of handles) {
 //         console.log(`%cpath: "${file.path}"%o`, `color: ${file.handle?.kind === 'directory' ? 'fuchsia' : 'tan'}`, { file, handle: file.handle });
-//     }
-// }
-
-// function printFileHandles(handles: FileWithHandleAndPath[]) {
-//     for (const file of handles) {
-//         console.log(`%cpath: "${file.path}"%o`, 'color: tan', { file });
 //     }
 // }
 
@@ -31,26 +19,9 @@ function printEntryFiles(handles: FileWithHandleAndPath[]) {
     }
 }
 
-// export async function getAllFileEntries(fileDataTransferItems: DataTransferItem[]): Promise<FileSystemEntry[]> {
 export async function getAllFileEntries(fileDataTransferItems: DataTransferItem[]): Promise<FileWithHandleAndPath[]> {
-    /*5* /
-    const handles = await collectDndItems(dataTransferItemList);
-
-    for (const [path, handle] of handles) {
-        console.log(`%cpath: "${path.join('/')}"%o`, `color: ${isFsFileHandle(handle) ? 'tan': 'fuchsia'}`, handle);
-    }
-    /**/
-
-    /*4* /
-    const rv: FileSystemEntry[] = await getAllFileSystemEntries(fileDataTransferItems);
-    printFileSystemEntries(rv);
-    return rv;
-    /**/
-
-    /*7*/
     const rv: FileWithHandleAndPath[] = await getFilesFromDataTransferItems(fileDataTransferItems);
     // const rv: FileWithHandleAndPath[] = await getFilesFromDataTransferItems2(fileDataTransferItems);
     printEntryFiles(rv);
     return rv;
-    /**/
 }
