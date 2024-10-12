b205343e7eb31f6d89f0f790e95736f9ad52f0fd
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-11 17:19:15 -0700

    WIP. modern pickers. switch to ButtonFilesPicker

--- src/renderer/src/components/1-header/0-all/8-test-open-files.tsx
+++ src/renderer/src/components/1-header/0-all/8-test-open-files.tsx
@@ -1,16 +1,20 @@
 import { useSetAtom } from "jotai";
 import { doSetFilesFromModernDialogAtom } from "@/store";
 import { Button } from "@/ui/shadcn";
+import { ButtonFilesPicker } from "@/components/2-main/0-all/2-welcome-page/2-button-files-picker";
 
 export function TestOpenFiles() {
     const doSetFilesFromModernDialog = useSetAtom(doSetFilesFromModernDialogAtom);
     return (<>
-        <Button variant="outline" className="text-[.65rem]" onClick={() => doSetFilesFromModernDialog({ openFiles: true })}>
+        <ButtonFilesPicker />
+        <ButtonFilesPicker openAsFolder />
+
+        {/* <Button variant="outline" className="text-[.65rem]" onClick={() => doSetFilesFromModernDialog({ openFiles: true })}>
             Open files
         </Button>
 
         <Button variant="outline" className="text-[.65rem]" onClick={() => doSetFilesFromModernDialog({ openFiles: false })}>
             Open folder
-        </Button>
+        </Button> */}
     </>);
 }
