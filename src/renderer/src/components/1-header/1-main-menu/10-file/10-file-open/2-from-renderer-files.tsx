f6995263c4cef4617079a3bf020399c7d86d5dfb
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-12 14:22:57 -0700

    WIP. modern pickers. DropdownMenuItem_Folder_FromRenderer code clean up

--- src/renderer/src/components/1-header/1-main-menu/10-file/10-file-open/2-from-renderer-files.tsx
+++ src/renderer/src/components/1-header/1-main-menu/10-file/10-file-open/2-from-renderer-files.tsx
@@ -1,46 +1,14 @@
-import { useRef, useState } from "react";
-import { useSetAtom } from "jotai";
-import { doSetFilesFromLegacyDialogAtom, maniMenuOpenAtom } from "@/store";
 import { DropdownMenuItem } from "@/ui";
-import { IdOpenFiles } from "./4-menu-items_persistent";
+import { IdOpenFiles, IdOpenFolders } from "./4-menu-items_persistent";
 
 export function DropdownMenuItem_Files_FromRenderer({ openFolder }: { openFolder: boolean; }) {
-    const [dlgOpen, setDlgOpen] = useState(false);
-    const setMenuOpen = useSetAtom(maniMenuOpenAtom);
-    const doSetFilesFromLegacyDialog = useSetAtom(doSetFilesFromLegacyDialogAtom);
-
-    const ref = useRef<HTMLInputElement>(null);
-
     function onClickToOpen() {
-        setDlgOpen(true);
-        //ref.current?.click();
-        const input = document.getElementById(IdOpenFiles);
-        input && input.click();
+        document.getElementById(openFolder ? IdOpenFolders: IdOpenFiles)?.click();
     }
 
-    // function onFocus() {
-    //     dlgOpen && setMenuOpen(false);
-    //     setDlgOpen(false);
-    // }
-
-    // function onChange(event: ChangeEvent<HTMLInputElement>) {
-    //     setMenuOpen(false);
-    //     doSetFilesFromLegacyDialog(event.target.files);
-    // }
-
     return (
-        <DropdownMenuItem
-            asChild
-            // onSelect={(e) => e.preventDefault()}
-            // onFocus={onFocus}
-        >
-            <div className="" onClick={onClickToOpen}>
-                {/* <InputFileAsDlg
-                    ref={ref}
-                    accept=".dpm,.dpn"
-                    onChange={onChange}
-                    openAsFolder={false}
-                /> */}
+        <DropdownMenuItem asChild>
+            <div onClick={onClickToOpen}>
                 {openFolder ? 'Open Folder...' : 'Open Files...'}
             </div>
         </DropdownMenuItem>
--- src/renderer/src/components/1-header/1-main-menu/10-file/10-file-open/3-from-renderer-folders.tsx
+++ src/renderer/src/components/1-header/1-main-menu/10-file/10-file-open/3-from-renderer-folders.tsx
@@ -1,39 +1,16 @@
-import { useState } from "react";
-import { useSetAtom } from "jotai";
-import { maniMenuOpenAtom } from "@/store";
 import { DropdownMenuItem } from "@/ui";
-import { IdOpenFolders } from "./4-menu-items_persistent";
+import { IdOpenFiles, IdOpenFolders } from "./4-menu-items_persistent";
 
 export function DropdownMenuItem_Folder_FromRenderer({ openFolder }: { openFolder: boolean; }) {
-    const setMenuOpen = useSetAtom(maniMenuOpenAtom);
-    const [dlgOpen, setDlgOpen] = useState(false);
-
     function onClickToOpen() {
-        setDlgOpen(true);
-        const input = document.getElementById(IdOpenFolders);
-        input && input.click();
+        document.getElementById(openFolder ? IdOpenFolders: IdOpenFiles)?.click();
     }
 
-    // function onFocus() {
-    //     dlgOpen && setMenuOpen(false);
-    //     setDlgOpen(false);
-    // }
-
     return (
-        <DropdownMenuItem
-            asChild
-            // onSelect={(e) => e.preventDefault()}
-            // onFocus={onFocus}
-        >
+        <DropdownMenuItem asChild>
             <div onClick={onClickToOpen}>
                 {openFolder ? 'Open Folder...' : 'Open Files...'}
             </div>
-
-            {/* <label htmlFor={IdOpenFolders} onClick={onClickToOpen}>
-                {openFolder ? 'Open Folder...' : 'Open Files...'}
-            </label> */}
         </DropdownMenuItem>
     );
 }
-
-
