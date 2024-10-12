485979936755cb96530be89fa4ac558c4105cafd
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-12 13:48:26 -0700

    WIP. modern pickers. move all app globals AppGlobals

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,8 @@
 import { SectionHeader } from "./components/1-header";
 import { SectionMain } from "./components/2-main";
 import { SectionFooter } from "./components/3-footer";
-import { doSetFilesFromDropAtom } from "./store";
-import { OnAppMount, WorldToReactListener } from "./xternal-to-main";
-import { DropItDoc, Toaster } from "./ui";
 import { UISymbolDefs } from "@ui/icons";
-import { AppGloabals } from "./components/4-dialogs";
-import { SpyAllIcons } from "./util-hooks";
+import { AppGlobals } from "./components/4-dialogs";
 
 export function App(): JSX.Element {
     return (<>
@@ -19,13 +15,7 @@ export function App(): JSX.Element {
             <SectionFooter />
         </div>
 
-        <AppGloabals />
-        <Toaster />
-
-        <DropItDoc doSetFilesFromDropAtom={doSetFilesFromDropAtom} />
-
-        <WorldToReactListener />
-        <OnAppMount />
+        <AppGlobals />
     </>);
 }
 
--- src/renderer/src/components/4-dialogs/0-global/0-all.tsx
+++ src/renderer/src/components/4-dialogs/0-global/0-all.tsx
@@ -1,8 +1,11 @@
+import { OnAppMount, WorldToReactListener } from "@/xternal-to-main";
+import { doSetFilesFromDropAtom } from "@/store";
+import { DropItDoc, Toaster } from "@/ui";
 import { ManiCreateDialogs } from "../2-dlg-create-login";
 import { OptionsFilesDialog } from "../8-dlg-options";
 import { AppGlobalShortcuts } from "./1-global-shortcuts";
 
-export function AppGloabals() {
+export function AppGlobals() {
     return (<>
         {/* Global dialogs */}
         <ManiCreateDialogs />
@@ -10,5 +13,13 @@ export function AppGloabals() {
 
         {/* Global shortcuts */}
         <AppGlobalShortcuts />
+
+        {/* Rest */}
+        <Toaster />
+
+        <DropItDoc doSetFilesFromDropAtom={doSetFilesFromDropAtom} />
+
+        <WorldToReactListener />
+        <OnAppMount />
     </>);
 }
