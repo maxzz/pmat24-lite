194d7efee766453264b3a4da5bd221b47654fa11
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-13 15:35:57 -0700

    WIP. field-catalog. WelcomePage layout. 3. done for now

--- src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
@@ -8,7 +8,7 @@ import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 const titleStyle = {
     color: 'black',
     WebkitTextFillColor: 'transparent',
-    WebkitTextStroke: '0.1px hsl(var(--border))',
+    WebkitTextStroke: '0.1px hsl(var(--foreground))',
     WebkitFontSmoothing: 'antialiased',
 };
 
@@ -35,13 +35,30 @@ export function WelcomePage() {
                         exit={{ opacity: 0, scale: 0.9 }}
                     >
                         <div className="w-full h-full flex flex-col items-center justify-center gap-y-4">
-                            <div className="text-2xl font-extrabold" style={titleStyle}>
+                            <div className="text-2xl font-extrabold opacity-30" style={titleStyle}>
                                 {/* Password Manager Admin Tool */}
                                 Welcome to the Password Manager Admin Tool
                             </div>
 
+                            {allowHandleFiles
+                                ? 'Open a file or folder to start working with the app.'
+                                : 'Open a folder to start working with the app.'
+                            }
+
                             {allowHandleFiles && <ButtonFilesPicker />}
                             <ButtonFilesPicker openAsFolder />
+
+                            <div className="text-xs space-y-1">
+                                <div className="font-semibold">
+                                    Resently used folders:
+                                </div>
+                                <div className="">
+                                    Folder 1 (placeholder)
+                                </div>
+                                <div className="">
+                                    Folder 2 (placeholder)
+                                </div>
+                            </div>
 
                             <DontShowNext className="absolute left-0 bottom-0 p-4" />
                         </div>
