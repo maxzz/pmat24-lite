c7d872f8df5844bfbc08da55862ad2bd774c2420
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-14 18:45:27 -0700

    WIP. field-catalog. add real MiddleBody

--- src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import { atom, useAtomValue, useSetAtom } from "jotai";
-import { doCloseFldCatDialogAtom, fldCatTriggerAtom } from "@/store";
+import { doCancelFldCatDialogAtom, doCloseFldCatDialogAtom, fldCatTriggerAtom } from "@/store";
 import { type CatalogItem } from "@/store/manifest";
 import * as D from "@/ui/shadcn/dialog";
 import { Button } from "@/ui";
+import { MiddleBody } from "../1-body/3-middle-body";
 
 export function DialogFieldCatalogBody() {
-    const fldCatTrigger = useSetAtom(fldCatTriggerAtom);
+    const doCancelFldCatDialog = useSetAtom(doCancelFldCatDialogAtom);
 
-    function cancelDialog() {
-        fldCatTrigger(null);
-    }
-
-    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
+    const doCloseFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
 
     const inData = useAtomValue(fldCatTriggerAtom);
     const needSelect = !!inData?.outBoxAtom;
@@ -28,15 +25,16 @@ export function DialogFieldCatalogBody() {
                     Field Catalog (TODO: for folder?)
                 </div>
 
-                <D.DialogCloseButton className="right-1 -top-0.5 p-4 hover:bg-muted hover:rounded-md" tabIndex={-1} onClick={cancelDialog} />
+                <D.DialogCloseButton className="right-1 -top-0.5 p-4 hover:bg-muted hover:rounded-md" tabIndex={-1} onClick={doCancelFldCatDialog} />
             </D.DialogHeader>
 
             <div className="px-4 py-4 grid grid-cols-1 gap-6">
                 456
             </div>
+            <MiddleBody selectedItemAtom={selectedItemAtom} closeDlg={doCloseFldCatDialog} />
 
             <div className="py-4 boreder-border border-t text-center">
-                <Button variant="default" size={"sm"} onClick={cancelDialog}>
+                <Button variant="default" size={"sm"} onClick={doCancelFldCatDialog}>
                     Close
                 </Button>
             </div>
