a54cfe3cde6765ebd2dda824c12b9e95b6a67c1d
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-16 14:56:37 -0700

    WIP. field-catalog. update DialogFieldCatalogBody layout

--- src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
@@ -1,27 +1,38 @@
 import { useState } from "react";
 import { atom, useAtomValue, useSetAtom } from "jotai";
-import { doCancelFldCatDialogAtom, doCloseFldCatDialogAtom, fldCatItemsAtom, fldCatTriggerAtom, type SelectedItemAtom } from "@/store";
+import { doCancelFldCatDialogAtom, doCloseFldCatDialogAtom, fldCatItemsAtom, type SelectedItemAtom } from "@/store";
 import { type CatalogItem } from "@/store/manifest";
 import * as D from "@/ui/shadcn/dialog";
 import { BottomButtons } from "./3-bottom-buttons";
 import { Button } from "@/ui/shadcn";
 import { classNames } from "@/utils";
-import { SelectedItemBody } from "../2-selected-item-props";
 import { FldCatItemsGrid } from "../1-items-grid";
+import { SelectedItemBody } from "../2-selected-item-props";
+
+const subSectionClasses = 'text-sm text-foreground bg-background border-border border-b';
+
+function TitleItems() {
+    return (
+        <div className={classNames(subSectionClasses, "mb-1 flex items-center justify-between")}>
+            <div>
+                Field catlog items
+            </div>
+
+            <Button className="mb-1 aspect-square" variant="outline" size="xs">
+                +
+            </Button>
+        </div>
+    );
+}
 
 export function DialogFieldCatalogBody() {
+
+    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
     const doCancelFldCatDialog = useSetAtom(doCancelFldCatDialogAtom);
+    const totalItems = useAtomValue(fldCatItemsAtom).length;
 
     const selectedItemAtom = useState<SelectedItemAtom>(() => atom<CatalogItem | null>(null))[0];
     const selectedItem = useAtomValue(selectedItemAtom);
-
-
-
-    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
-    const totalItems = useAtomValue(fldCatItemsAtom).length;
-
-    const inData = useAtomValue(fldCatTriggerAtom);
-    const needSelect = !!inData?.outBoxAtom;
 
     return (
         <div className="min-h-56 text-xs select-none">
@@ -45,10 +56,12 @@ export function DialogFieldCatalogBody() {
                     />
                 </div>
 
-                <TitleProps />
+                <div className={subSectionClasses}>
+                    Selected item
+                </div>
                 <SelectedItemBody selectedItemAtom={selectedItemAtom} />
 
-                <div className={dlgHeaderClasses}>
+                <div className="my-1 text-xs font-thin">
                     {totalItems} item{totalItems === 1 ? '' : 's'} in field catalog
                 </div>
 
@@ -58,35 +71,6 @@ export function DialogFieldCatalogBody() {
         </div>
     );
 }
-
-const subSectionClasses = 'text-sm text-foreground bg-background border-border border-b';
-
-function TitleItems() {
-    return (
-        <div className={classNames(subSectionClasses, "mb-1 flex items-center justify-between")}>
-            <div>
-                Catlog items
-            </div>
-
-            <Button className="mb-1 aspect-square" variant="outline" size="xs">
-                +
-            </Button>
-        </div>
-    );
-}
-
-
-
-function TitleProps() {
-    return (
-        <div className={subSectionClasses}>
-            Selected item
-        </div>
-    );
-}
-
-
-const dlgHeaderClasses = 'my-1 text-xs font-thin';
 
 //TODO: show folder of the field catalog
 
