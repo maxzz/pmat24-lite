347e0b039244280a28d015b0c9a869e8fc6395de
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-16 13:16:55 -0700

    WIP. field-catalog. simplify buttons layout

--- src/renderer/src/components/4-dialogs/4-field-catalog/0-all/3-bottom-buttons.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/0-all/3-bottom-buttons.tsx
@@ -1,38 +1,30 @@
-import { ButtonHTMLAttributes } from "react";
 import { useAtomValue, useSetAtom } from "jotai";
 import { doCancelFldCatDialogAtom, doCloseFldCatDialogAtom, fldCatTriggerAtom, type SelectedItemAtom } from "@/store";
 import { Button } from "@/ui/shadcn";
 import { inputFocusClasses } from "../2-selected-item-props/8-inputs";
 
-type BottomButtonsProps = {
-    selectedItemAtom: SelectedItemAtom;
-};
+export function BottomButtons({ selectedItemAtom }: { selectedItemAtom: SelectedItemAtom; }) {
 
-export function BottomButtons({ selectedItemAtom }: BottomButtonsProps) {
     const inData = useAtomValue(fldCatTriggerAtom);
     const showSelectBtn = !!inData?.outBoxAtom;
 
+    const selectedItem = useAtomValue(selectedItemAtom);
+
+    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
     const doCancelFldCatDialog = useSetAtom(doCancelFldCatDialogAtom);
+
     return (
         <div className="pt-4 flex items-center justify-end gap-x-2">
             {showSelectBtn && (
-                <SelectButton selectedItemAtom={selectedItemAtom} />
+                <Button disabled={!selectedItem} onClick={() => closeFldCatDialog({ fldCatItem: selectedItem })}>
+                    Select
+                </Button>
             )}
 
             <Button className={inputFocusClasses} onClick={doCancelFldCatDialog}>
                 {showSelectBtn ? 'Cancel' : 'Close'}
             </Button>
         </div>
-    );
-}
-
-function SelectButton({ selectedItemAtom, ...rest }: {selectedItemAtom: SelectedItemAtom}) {
-    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
-    const selectedItem = useAtomValue(selectedItemAtom);
-    return (
-        <Button disabled={!selectedItem} onClick={() => closeFldCatDialog({ fldCatItem: selectedItem })} {...rest}>
-            Select
-        </Button>
     );
 }
 
