0e958bb516aba97ce9489e93df4a1a1c05cc4b67
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-15 17:33:01 -0700

    WIP. field-catalog. update dialog middle body

--- src/renderer/src/components/4-dialogs/4-field-catalog/1-body/3-middle-body.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/1-body/3-middle-body.tsx
@@ -1,18 +1,23 @@
-import { PrimitiveAtom } from "jotai";
-import { CatalogItem } from "@/store/manifest";
-import { FldCatOutData } from "@/store/atoms/7-dialogs";
+import { type PrimitiveAtom, useSetAtom } from "jotai";
+import { type CatalogItem } from "@/store/manifest";
+import { doCloseFldCatDialogAtom } from "@/store";
 import { FldCatItemsGrid } from "../2-items-grid";
 import { SelectedItemBody } from "./4-selected-item-body";
+import { Button } from "@/ui";
+import { classNames } from "@/utils";
 
-const subSectionClasses = 'mt-4 pb-0.5 text-sm text-primary-400 bg-primary-800 border-primary-700 border-b';
+const subSectionClasses = 'text-sm text-foreground bg-background border-border border-b';
 
 function SubTitleA() {
     return (
-        <div className={subSectionClasses}>
-            <div className="flex items-center justify-between">
-                <div className="">Catlog items</div>
-                <button className="px-2 border border-primary-700/50 rounded">+</button>
+        <div className={classNames(subSectionClasses, "mb-1 flex items-center justify-between")}>
+            <div>
+                Catlog items
             </div>
+
+            <Button className="mb-1 aspect-square" variant="outline" size="xs">
+                +
+            </Button>
         </div>
     );
 }
@@ -27,22 +32,20 @@ function SubTitleB() {
 
 type MiddleBodyProps = {
     selectedItemAtom: PrimitiveAtom<CatalogItem | null>;
-    closeDlg: (data: FldCatOutData) => void;
 };
 
-export function MiddleBody({ selectedItemAtom, closeDlg }: MiddleBodyProps) {
-    return (
-        <div>
-            <SubTitleA />
-            <div className="h-[50vh] min-h-[120px]">
-                <FldCatItemsGrid
-                    selectedItemAtom={selectedItemAtom}
-                    onDoubleClick={(item: CatalogItem) => closeDlg({ fldCatItem: item })}
-                />
-            </div>
-
-            <SubTitleB />
-            <SelectedItemBody selectedItemAtom={selectedItemAtom} />
+export function MiddleBody({ selectedItemAtom }: MiddleBodyProps) {
+    const closeFldCatDialog = useSetAtom(doCloseFldCatDialogAtom);
+    return (<>
+        <SubTitleA />
+        <div className="h-[50vh] min-h-[120px]">
+            <FldCatItemsGrid
+                selectedItemAtom={selectedItemAtom}
+                onDoubleClick={(item: CatalogItem) => closeFldCatDialog({ fldCatItem: item })}
+            />
         </div>
-    );
+
+        <SubTitleB />
+        <SelectedItemBody selectedItemAtom={selectedItemAtom} />
+    </>);
 }
