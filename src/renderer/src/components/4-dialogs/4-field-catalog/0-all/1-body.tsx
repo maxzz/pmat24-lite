b49534e25ffab22d19eaa521dc4f73bc7fb4f70f
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-21 13:50:41 -0700

    WIP. field-catalog. fix FldCatItemsGrid overflow

--- src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/0-all/1-body.tsx
@@ -17,42 +17,44 @@ export function DialogFieldCatalogBody() {
     const totalItems = useAtomValue(fldCatItemsAtom).length;
     const selectedItemAtom = useState<SelectedItemAtom>(() => atom<CatalogItem | null>(null))[0];
 
-    return (<div>
-        <D.DialogHeader className="relative py-3 text-sm font-bold border-border border-b flex items-center">
-            Field Catalog
+    return (
+        <div className="grid grid-rows-[auto_1fr]">
+            <D.DialogHeader className="relative py-3 text-sm font-bold border-border border-b flex items-center">
+                Field Catalog
 
-            <D.DialogCloseButton
-                className="absolute right-1 top-1/2 -translate-y-1/2 p-2 hover:bg-muted hover:rounded-sm"
-                onClick={doCancelFldCatDialog}
-                tabIndex={-1}
-            />
-        </D.DialogHeader>
-
-        <div className="px-3 pb-3">
-            <FieldCatalogToolbar className="py-1 1bg-red-300/10" />
-
-            {/* <div className="1bg-green-300/20 min-h-80 border-border border rounded grid grid-cols-[minmax(200px,1fr)_200px]"> */}
-            <div className="h-full 1bg-green-300/20 border-border border rounded flex">
-                <FldCatItemsGrid
-                    selectedItemAtom={selectedItemAtom}
-                    onItemDoubleClick={(item: CatalogItem) => closeFldCatDialog({ fldCatItem: item })}
+                <D.DialogCloseButton
+                    className="absolute right-1 top-1/2 -translate-y-1/2 p-2 hover:bg-muted hover:rounded-sm"
+                    onClick={doCancelFldCatDialog}
+                    tabIndex={-1}
                 />
+            </D.DialogHeader>
 
-                <RightPanelGuard
-                    className="1relative 1bg-blue-300/10 px-2 py-2 border-border border-l 1z-10"
-                    selectedItemAtom={selectedItemAtom}
-                />
-            </div>
+            <div className="px-3 pb-3 h-full grid grid-rows-[auto_1fr]">
+                <FieldCatalogToolbar className="py-1 1bg-red-300/10" />
 
-            <div className="font-thin">
-                {totalItems} item{totalItems === 1 ? '' : 's'} in field catalog
-            </div>
+                {/* <div className="1bg-green-300/20 min-h-80 border-border border rounded grid grid-cols-[minmax(200px,1fr)_200px]"> */}
+                <div className="h-full 1bg-green-300/20 border-border border rounded flex">
+                    <FldCatItemsGrid
+                        selectedItemAtom={selectedItemAtom}
+                        onItemDoubleClick={(item: CatalogItem) => closeFldCatDialog({ fldCatItem: item })}
+                    />
 
-            <div className="flex items-center justify-end gap-x-2">
-                <BottomButtons selectedItemAtom={selectedItemAtom} />
+                    <RightPanelGuard
+                        className="1relative 1bg-blue-300/10 px-2 py-2 border-border border-l 1z-10"
+                        selectedItemAtom={selectedItemAtom}
+                    />
+                </div>
+
+                <div className="font-thin">
+                    {totalItems} item{totalItems === 1 ? '' : 's'} in field catalog
+                </div>
+
+                <div className="flex items-center justify-end gap-x-2">
+                    <BottomButtons selectedItemAtom={selectedItemAtom} />
+                </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 //TODO: show folder of the field catalog
