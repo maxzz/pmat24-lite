01bc1972af1ad00b5369e0dd3bee4baf9d135ae7
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-25 15:07:53 -0700

    WIP. field catalog. footer. re-work LoadedCounter

--- src/renderer/src/components/3-footer/1-loaded-counter.tsx
+++ src/renderer/src/components/3-footer/1-loaded-counter.tsx
@@ -1,7 +1,8 @@
-import { treeFilesAtom } from "@/store";
+import { totalManis, treeFilesAtom } from "@/store";
 import { useAtomValue } from "jotai";
+import { useSnapshot } from "valtio";
 
-export function LoadedCounter() {
+export function LoadedCounterNun() {
     const treeFiles = useAtomValue(treeFilesAtom);
 
     if (!treeFiles?.length) {
@@ -22,7 +23,47 @@ export function LoadedCounter() {
     );
 }
 
-function SubSection({label, counter}: {label: string; counter: number}) {
+export function LoadedCounter() {
+    const treeFiles = useAtomValue(treeFilesAtom);
+
+    if (!treeFiles?.length) {
+        return <div className="text-[.65rem]">No files loaded</div>;
+    }
+
+    const { normal, manual, empty } = useSnapshot(totalManis);
+
+    const suffix = ` file${treeFiles.length > 1 ? 's' : ''}`;
+    return (
+        <div className="text-[.65rem] leading-5 text-muted-foreground flex items-center gap-1">
+            Loaded{' '}
+
+            <span className="text-text/60">
+                {treeFiles.length}
+            </span>
+
+            {suffix}
+
+            {' ('}
+            {subCounter('normal:', normal)}
+            {', '}
+            {subCounter('manual:', manual)}
+            {', '}
+            {subCounter('empty:', empty)}
+            {')'}
+            {/* <SubSection label="normal" counter={normal} />
+            <SubSection label="manual" counter={manual} />
+            <SubSection label="empty" counter={empty} /> */}
+            {')'}
+        </div>
+    );
+}
+
+function subCounter(label: string, counter: number, doSuffix: boolean = false): string {
+    return `${label} ${counter} ${doSuffix ? `file${counter > 1 ? 's' : ''}`:''}`;
+}
+
+
+function SubSection({ label, counter }: { label: string; counter: number; }) {
 
     const suffix = ` file${counter > 1 ? 's' : ''}`;
     return (
