5fdbe81d2919f41f1702a33dac298b6d085ec84d
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-24 19:21:17 -0700

    WIP. field catalog. split FilterOptions monolith. 2

--- src/renderer/src/components/4-dialogs/1-dlg-filter-files/2-right-side-options/1-filter-options.tsx
+++ src/renderer/src/components/4-dialogs/1-dlg-filter-files/2-right-side-options/1-filter-options.tsx
@@ -1,25 +1,8 @@
 import { Button } from "@ui/shadcn";
 import * as Dr from "@ui/shadcn/drawer";
-import { SymbolDot } from "@/ui/icons";
-import { CheckAscending } from "./2-order";
-import { CheckSortBy } from "./3-sort-by";
-
-const popupContentDotClasses = "w-3 h-3 inline fill-foreground/70 stroke-foreground/50 stroke-2";
-const popupContentTextClasses = "inline-block font-bold font-mono tracking-tight w-8";
-
-function DrawerItems() {
-    return (
-        <div className="text-xs px-1">
-            <div className="pb-2 font-bold">Search options:</div>
-            <div className="pb-1">Use the search prefix to dispay only:</div>
-            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>win:</span> logins for Windows apps</div>
-            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>web:</span> logins for web apps</div>
-            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>why:</span> logins with problems to check why</div>
-            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>cap:</span> logins with window caption</div>
-            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>cls:</span> logins with window classname</div>
-        </div>
-    );
-}
+import { CheckAscending } from "./3-order";
+import { CheckSortBy } from "./4-sort-by";
+import { DrawerItems } from "./2-drawer-items";
 
 export function FilterOptions() {
     return (
---
+++ src/renderer/src/components/4-dialogs/1-dlg-filter-files/2-right-side-options/2-drawer-items.tsx
@@ -0,0 +1,17 @@
+import { SymbolDot } from "@/ui/icons";
+
+const popupContentDotClasses = "w-3 h-3 inline fill-foreground/70 stroke-foreground/50 stroke-2";
+const popupContentTextClasses = "inline-block font-bold font-mono tracking-tight w-8";
+export function DrawerItems() {
+    return (
+        <div className="text-xs px-1">
+            <div className="pb-2 font-bold">Search options:</div>
+            <div className="pb-1">Use the search prefix to dispay only:</div>
+            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>win:</span> logins for Windows apps</div>
+            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>web:</span> logins for web apps</div>
+            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>why:</span> logins with problems to check why</div>
+            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>cap:</span> logins with window caption</div>
+            <div className=""><SymbolDot className={popupContentDotClasses} /><span className={popupContentTextClasses}>cls:</span> logins with window classname</div>
+        </div>
+    );
+}
