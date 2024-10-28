45870956eb13c53fcc8973bce519d391f95dc279
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-28 15:07:57 -0700

    WIP. field catalog. add CatBody prototype

--- src/renderer/src/components/2-main/2-right/3-file-cat/index.tsx
+++ src/renderer/src/components/2-main/2-right/3-file-cat/index.tsx
@@ -1,15 +1,36 @@
 import { type HTMLAttributes } from 'react';
+import { useAtomValue } from 'jotai';
 import { ScrollArea } from '@/ui';
 import useResizeObserver from 'use-resize-observer';
 import { classNames } from '@/utils';
+import { type FileUsAtom } from '@/store/store-types';
+import { rightPanelAtom } from '@/store';
 
-export function Body_Cat({ className, ...rest }: {  } & HTMLAttributes<HTMLDivElement>) {
+export function Body_Cat({ className, ...rest }: {} & HTMLAttributes<HTMLDivElement>) {
     const { ref, width, height } = useResizeObserver();
     return (<>
         <div className={classNames("h-full w-full", className)} ref={ref} {...rest}>
             <ScrollArea style={{ width, height }} horizontal>
-                123
+                <Guard />
             </ScrollArea>
         </div>
     </>);
+}
+
+export function Guard() {
+    const fileUsAtom = useAtomValue(rightPanelAtom);
+    if (!fileUsAtom) {
+        return null;
+    }
+    return (
+        <CatBody fileUsAtom={fileUsAtom} />
+    );
+}
+
+function CatBody({ fileUsAtom, className, ...rest }: { fileUsAtom: FileUsAtom; } & HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div className={classNames("h-full w-full", className)}{...rest}>
+            1234
+        </div>
+    );
 }
