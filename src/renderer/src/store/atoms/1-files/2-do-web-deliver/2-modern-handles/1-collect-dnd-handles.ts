9331da9ff7e367296e46ff6c98a6abc7015d884c
Max Zakharzhevskiy <maxzz@msn.com>
2024-10-28 18:03:11 -0700

    WIP. field catalog. collectDndHandles() path problem. 2

--- src/renderer/src/store/atoms/1-files/2-do-web-deliver/2-modern-handles/1-collect-dnd-handles.ts
+++ src/renderer/src/store/atoms/1-files/2-do-web-deliver/2-modern-handles/1-collect-dnd-handles.ts
@@ -9,12 +9,18 @@ import { type FsHandle } from "../9-fs-types";
 async function* getEntriesRecursively(folder: FileSystemDirectoryHandle): AsyncGenerator<[path: string[], handle: FsHandle, dir: FileSystemDirectoryHandle], void, unknown> {
     for await (const entry of folder.values()) {
         if (entry.kind === 'file') {
+            console.log('file:', entry.name, { folder, entry });
+
             yield [[folder.name], entry, folder];
         }
         else if (entry.kind === 'directory') {
+            console.log('dir:', entry.name, { folder, entry });
+
             yield [[folder.name, entry.name], entry, folder];
 
             for await (const [path, file, folder] of getEntriesRecursively(entry)) {
+                console.log('yield:', path, file, folder);
+
                 yield [[folder.name, ...path], file, folder];
             }
         }
