6d2e6ff2e9010e7ef5cc17a85c2f1caaf927e90d
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-06 16:31:01 -0800

    WIP. field catalog. add assignFceAtoms()

--- src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/1-create-fce-roots.ts
+++ src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/1-create-fce-roots.ts
@@ -1,8 +1,12 @@
-import { type FileUs } from "@/store/store-types";
+import { FileUsStats, type FileUs } from "@/store/store-types";
 import { type FileContent } from "@shared/ipc-types";
 import { type FceRoots } from "../9-types-fc";
+import { rootDir } from "../../1-files";
 import { createEmptyFceRoot, fieldCatToFceRoot } from "./8-fc-file-to-fc-root";
 import { finalizeFileContent } from "@/store/store-utils";
+import { extensionWoDot } from "@/utils";
+import { atom } from "jotai";
+import { ManiAtoms } from "../../3-file-mani-atoms";
 
 // Field catalog should have name "field_catalog.dpn" and should be in the root folder.
 // Root "field_catalog.dpn" have impact on the root folder and A/B/C subfolders.
@@ -48,4 +52,58 @@ export function createFldCatRoot(fileUsItems: FileUs[]) {
     let rootFc = createEmptyFceRoot(rootFcFileUs?.fileCnt);
 
 
+}
+
+export function createEmptyFceAtoms(): FileUs {
+    const fileCnt: FileContent = finalizeFileContent(null);
+
+    const rv: FileUs = {
+        fileCnt,
+        parsedSrc: {
+            mani: undefined,
+            meta: undefined,
+            fcat: undefined,
+            stats: {} as FileUsStats,
+        },
+        uiState: {
+            isGroupAtom: atom<boolean>(false),
+            isCurrentAtom: atom<boolean>(false),
+        },
+        maniAtomsAtom: atom<ManiAtoms | null>(null),
+        fceAtomsRef: undefined,
+        fceAtoms: undefined,
+    };
+    return rv;
+}
+
+export function assignFceAtoms(fileUsItems: FileUs[]): void {
+
+    const rootPath = rootDir.rpath.toLowerCase();
+
+    let rootFc: FileUs | undefined;
+
+    const onlyFcs = fileUsItems.reduce((map, fileUs) => {
+        if (fileUs.parsedSrc.fcat) {
+            const fpath = fileUs.fileCnt.fpath.toLowerCase();
+            const fname = fileUs.fileCnt.fname.toLowerCase();
+            const isRoot = fname === 'field_catalog.dpn' && fpath === rootPath;
+            if (isRoot) {
+                rootFc = fileUs;
+            } else {
+                map[`${fpath}/${fname}`] = fileUs;
+            }
+        }
+        return map;
+    }, {} as Record<string, FileUs>);
+
+    rootFc = rootFc || createEmptyFceAtoms();
+
+    fileUsItems.forEach(
+        (fileUs) => {
+            const goodForFc = !fileUs.parsedSrc.fcat && fileUs.fileCnt.fpath.toLowerCase().match(RegExp(`^${rootPath}/([a-c])`));
+            if (goodForFc) {
+                fileUs.fceAtomsRef = rootFc?.fceAtoms;
+            }
+        }
+    );
 }
