a77acc529acf391558c4cc419719c97ba4301390
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-07 19:04:25 -0800

    WIP. field catalog. add new dialog in/out types

--- src/renderer/src/store/atoms/4-field-catalogs/9-types/3-types-dlg.ts
+++ src/renderer/src/store/atoms/4-field-catalogs/9-types/3-types-dlg.ts
@@ -18,3 +18,22 @@ export type Fce0DlgIn = {
 export type Fce0DlgOut = {
     selectedItem: FceItem | null; // selected item
 };
+
+// v1
+
+/**
+ * Field catalog dialog in data
+ */
+export type FceDlgIn = {
+    dbid?: string | undefined;      // dbid of the field catalog item to initially select
+    showTxt: boolean;               // show text fields only
+    showPsw: boolean;               // show password fields only
+    outBoxAtom?: PrimitiveAtom<FceDlgOut | null>; // selected item output atom
+};
+
+/**
+ * Field catalog dialog output data
+ */
+export type FceDlgOut = {
+    selectedItem: FceItem | null; // selected item
+};
