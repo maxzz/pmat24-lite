43b5369afedc36cb2daacd35ed0a2b59d8f88f56
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-09 13:50:36 -0800

    WIP. field catalog. connect doDiscardAllFileUsLinksAtom

--- src/renderer/src/store/store-utils/1-file-system-utils/8-discard-file-us.ts
+++ src/renderer/src/store/store-utils/1-file-system-utils/8-discard-file-us.ts
@@ -1,10 +1,8 @@
+import { filesAtom } from "@/store/atoms";
 import { type FileUs } from "@/store/store-types";
 import { type Getter, type Setter, atom } from "jotai";
 
-/**
- * Discard FileUs links
- */
-export function discardFileUs(get: Getter, set: Setter, fileUs: FileUs) {
+function discardFileUs(get: Getter, set: Setter, fileUs: FileUs) {
 
     const maniAtoms = get(fileUs.maniAtomsAtom);
     if (maniAtoms) {
@@ -25,9 +23,28 @@ export function discardFileUs(get: Getter, set: Setter, fileUs: FileUs) {
     }
 }
 
+/**
+ * Discard FileUs links atom
+ */
 export const doDiscardFileUsAtom = atom(
     null,
     (get, set, fileUs: FileUs) => {
         discardFileUs(get, set, fileUs);
+    }
+);
+
+/**
+ * Discard all array of FileUs atom
+ */
+export const doDiscardAllFileUsLinksAtom = atom(
+    null,
+    (get, set) => {
+        const all = get(filesAtom);
+        all.forEach(
+            (fileUsAtom) => {
+                const fileUs = get(fileUsAtom);
+                discardFileUs(get, set, fileUs);
+            }
+        );
     }
 );
--- src/renderer/src/store/store-utils/1-file-system-utils/index.ts
+++ src/renderer/src/store/store-utils/1-file-system-utils/index.ts
@@ -1,1 +1,2 @@
 export * from './1-finalize-file-content';
+export * from './8-discard-file-us';
