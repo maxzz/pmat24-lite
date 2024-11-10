412fc14fde7abd8342d0e5a62d8213b80b0d13fe
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-09 18:54:50 -0800

    WIP. field catalog. update fieldCatToFceAtoms()

--- src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/5-fc-file-to-fc-atoms.ts
+++ src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/5-fc-file-to-fc-atoms.ts
@@ -1,10 +1,17 @@
 import { atom } from 'jotai';
 import { proxy } from 'valtio';
 import { catalogItemInFileToFieldValue, type CatalogItemEdit, type CatalogFile, uuid } from '@/store/manifest';
-import { type Fce0Atoms, type Fce0Ctx, type FceItem } from '../9-types';
+import { type FceAtoms, type FceCtx, type FceItem } from '../9-types';
 import { type FileContent } from '@shared/ipc-types';
+import { type FileUs } from '@/store/store-types';
+import { createFceAtoms } from './2-create-empty-fce-fileus';
 
-export function fieldCatToFceAtoms(fileCnt: FileContent, fcat: CatalogFile.Root): Fce0Atoms {
+export function fieldCatToFceAtoms(fileUs: FileUs): FceAtoms {
+
+    const fcat = fileUs.parsedSrc.fcat;
+    if (!fcat) {
+        throw new Error('Field catalog not found');
+    }
 
     // 1. Prepare items for the field catalog editor
 
@@ -26,13 +33,7 @@ export function fieldCatToFceAtoms(fileCnt: FileContent, fcat: CatalogFile.Root)
 
     // 2. Finalize the field catalog editor root
 
-    const rv: Fce0Atoms = {
-        fileCnt: fileCnt,
-        fceCtxAtom: atom<Fce0Ctx | null>(null),
-        descriptor: fcat.descriptor,
-        items: atom<FceItem[]>(items),
-    };
-
+    const rv: FceAtoms = createFceAtoms({ fileUs, desc: fcat.descriptor, items });
     return rv;
 }
 
