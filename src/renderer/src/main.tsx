89721ac910d33ceb590ba26d6a12debbdd38c3fc
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-12 18:41:11 -0800

    WIP. field catalog. fc view. update index.css location

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,7 +2,7 @@ import "./utils/x-devtool-install-block";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import './assets/index.css';
+import './assets/css/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
