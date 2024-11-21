a645e06e391328dbbcea0c59300356d5153aaa8f
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-21 13:58:43 -0800

    WIP. field catalog. props edit. ---------- remove BottomPanelGuard animation

--- src/renderer/src/components/2-main/2-right/3-file-cat/1-cat-body.tsx
+++ src/renderer/src/components/2-main/2-right/3-file-cat/1-cat-body.tsx
@@ -4,28 +4,23 @@ import { type FileUsAtom } from "@/store/store-types";
 import { FldCatItemsGrid } from "@/components/4-dialogs/4-field-catalog/2-items-grid";
 import { classNames } from "@/utils";
 import { SelectedItemPropsBody } from "@/components/4-dialogs/4-field-catalog/3-selected-item-props/1-props-body";
-import { BottomPanelGuard } from "./2-nun-cat-props";
 
 export function FcViewBody({ fileUsAtom, className, ...rest }: { fileUsAtom: FileUsAtom; } & HTMLAttributes<HTMLDivElement>) {
 
     const fileUs = useAtomValue(fileUsAtom);
     const fceCtx = fileUs.fceAtoms?.viewFceCtx;
     if (!fceCtx) {
-        return <div className="">No body</div>;
+        return <div>No body</div>;
     }
     
     return (
         <div className={classNames("h-full w-full max-w-4xl grid grid-rows-[1fr,auto]", className)} {...rest}>
 
-            <FldCatItemsGrid className="" fceCtx={fceCtx} />
+            <FldCatItemsGrid fceCtx={fceCtx} />
 
             <div className="relative mx-3 my-2 p-2 border-border border rounded-md">
                 <SelectedItemPropsBody fceCtx={fceCtx} />
             </div>
-
-            {/* <div className="relative">
-                <BottomPanelGuard className="mx-3 my-2 p-2 border-border border rounded-md" fceCtx={fceCtx} />
-            </div> */}
         </div>
     );
 }
