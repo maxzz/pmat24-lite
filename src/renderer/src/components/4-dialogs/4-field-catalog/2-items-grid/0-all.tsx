642bcae358c7fd26865f1404bd2172b4f4df1c17
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-23 15:21:33 -0800

    WIP. field catalog. FldCatItemsGrid to reduce rerenders. 4. code clean up

--- src/renderer/src/components/4-dialogs/4-field-catalog/2-items-grid/0-all.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/2-items-grid/0-all.tsx
@@ -6,56 +6,25 @@ import { classNames } from "@/utils";
 import { doScrollToItemAtom, type FceCtx } from "@/store";
 import { FldCatItemsBody } from "./1-body";
 
-function FldCatItemsGridGuarded({ fceCtx, className, ...rest }: { fceCtx: FceCtx; } & HTMLAttributes<HTMLDivElement>) {
-
-    const { ref: refRootCb, width, height } = useResizeObserver();
-
-    // const refRoot = useRef<HTMLDivElement | null>(null);
-
-    // const doScrollToSelected = useSetAtom(doScrollToItemAtom);
-
-    // const [selectedItem, doSetSelectItem] = useAtom(fceCtx.selectedItemAtom);
-
-    // useEffect(
-    //     () => {
-    //         doSetSelectItem(doScrollToSelected({ container: refRoot.current, fceCtx }));
-    //     }, [refRoot.current]
-    // );
-
-    // useEffect(
-    //     () => {
-    //         doScrollToSelected({ container: refRoot.current, fceCtx });
-    //     }, [selectedItem]
-    // );
-    const { ref: refRoot } = useScrollToSelected(fceCtx);
-
-    return (
-        <div className={classNames("relative w-full", className)} {...rest}>
-            <div className={`absolute inset-0 py-px flex flex-col`} ref={(elm) => { refRootCb(elm); refRoot(elm); }}>
-                <ScrollArea style={{ width, height }}>
-
-                    {/* <FldCatItemsBody
-                        className={classNames("grid grid-cols-[auto_auto_minmax(0px,1fr)] outline-none select-none")}
-                        tabIndex={0}
-                        fceCtx={fceCtx}
-                    /> */}
-                    <GridBody fceCtx={fceCtx} />
-
-                </ScrollArea>
-            </div>
-        </div>
-    );
-}
-
 export function FldCatItemsGrid({ fceCtx, className, ...rest }: { fceCtx: FceCtx; } & HTMLAttributes<HTMLDivElement>) {
     return (
-        <FldCatItemsGridGuard fceCtx={fceCtx} className={className} {...rest}>
-            <GridBody fceCtx={fceCtx} />
-        </FldCatItemsGridGuard>
+        <FceScrollGuard fceCtx={fceCtx} className={className} {...rest}>
+            <FceBody fceCtx={fceCtx} />
+        </FceScrollGuard>
     );
 }
 
-function FldCatItemsGridGuard({ fceCtx, className, children, ...rest }: { fceCtx: FceCtx; children: ReactNode; } & HTMLAttributes<HTMLDivElement>) {
+function FceBody({ fceCtx }) {
+    return (
+        <FldCatItemsBody
+            className={classNames("grid grid-cols-[auto_auto_minmax(0px,1fr)] outline-none select-none")}
+            tabIndex={0}
+            fceCtx={fceCtx}
+        />
+    );
+}
+
+function FceScrollGuard({ fceCtx, className, children, ...rest }: { fceCtx: FceCtx; children: ReactNode; } & HTMLAttributes<HTMLDivElement>) {
     const { ref: refRootCb, width, height } = useResizeObserver();
     const { ref: refRoot } = useScrollToSelected(fceCtx);
     return (
@@ -68,18 +37,6 @@ function FldCatItemsGridGuard({ fceCtx, className, children, ...rest }: { fceCtx
         </div>
     );
 }
-
-function GridBody({ fceCtx }) {
-    return (
-        <FldCatItemsBody
-            className={classNames("grid grid-cols-[auto_auto_minmax(0px,1fr)] outline-none select-none")}
-            tabIndex={0}
-            fceCtx={fceCtx}
-        />
-    );
-}
-
-//TODO: move to hook state from FldCatItemsGrid to reduce rerenders
 
 function useScrollToSelected<T extends HTMLElement>(fceCtx: FceCtx): { ref: (elm: T | null) => void; } {
     const ref = useRef<T | null>(null);
