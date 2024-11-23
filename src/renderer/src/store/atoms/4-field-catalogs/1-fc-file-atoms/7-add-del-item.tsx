916a9cfb03265f8aeebaa0bcea51ef36aad2d444
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-22 17:19:06 -0800

    WIP. field catalog. toolbar. add doDeleteItemIdxAtom

--- src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/7-add-del-item.tsx
+++ src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/7-add-del-item.tsx
@@ -1,24 +1,56 @@
-import { atom, type Getter, type Setter } from "jotai";
+import { atom } from "jotai";
 import { type FceItem, type FceCtx } from "@/store";
 import { createEmptyFceItem, FieldTyp } from "@/store/manifest";
 import { doSelectIdxAtom } from "./4-do-set-selected";
+import { setManiChanges } from "../../3-file-mani-atoms";
 
 export const doAddItemAtom = atom(
     null,
-    (get, set, ctx: FceCtx, fType: FieldTyp): FceItem | undefined => {
+    (get, set, fceCtx: FceCtx, fType: FieldTyp): FceItem | undefined => {
         const newItem = createEmptyFceItem(fType);
 
-        let items = [...get(ctx.fceAtoms.itemsAtom), newItem];
+        let items = [...get(fceCtx.fceAtoms.itemsAtom), newItem];
 
         newItem.fceMeta.index = items.length - 1;
         newItem.beforeEdit.displayname = `New ${fType === FieldTyp.edit ? 'text' : 'password'} ${newItem.fceMeta.index + 1}`;
         newItem.fieldValue.displayname = newItem.beforeEdit.displayname;
 
-        set(ctx.fceAtoms.itemsAtom, items);
+        set(fceCtx.fceAtoms.itemsAtom, items);
 
-        set(doSelectIdxAtom, ctx, items.length - 1, true);
-        set(ctx.selectedItemAtom, newItem);
+        set(doSelectIdxAtom, fceCtx, items.length - 1, true);
+        set(fceCtx.selectedItemAtom, newItem);
+
+        setManiChanges(fceCtx.fceAtoms, true, `add-${newItem.fceMeta.uuid}`);
 
         return newItem;
+    }
+);
+
+//TODO: for delete remove 'add' and add 'del' changes
+export const doDeleteItemIdxAtom = atom(
+    null,
+    (get, set, fceCtx: FceCtx, idx: number) => {
+        let items = get(fceCtx.fceAtoms.itemsAtom);
+        const item = items[idx];
+
+        if (!item) {
+            return;
+        }
+
+        items = items.slice(idx, idx + 1);
+
+        set(fceCtx.fceAtoms.itemsAtom, items);
+
+        const newIdx = idx > 0 ? idx - 1 : 0;
+        set(doSelectIdxAtom, fceCtx, newIdx, true);
+        set(fceCtx.selectedItemAtom, undefined);
+
+        const changesSet = fceCtx.fceAtoms.fileUs.fileCnt.changesSet;
+        const hasAdd = changesSet.has(`add-${item.fceMeta.uuid}`);
+        if (hasAdd) {
+            changesSet.delete(`add-${item.fceMeta.uuid}`);
+        } else {
+            changesSet.add(`del-${item.fceMeta.uuid}`);
+        }
     }
 );
