75709a62e557b8751e21facbe0816dbc4a76be2d
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-24 15:21:15 -0800

    WIP. field catalog. right panel header. update Row1ChooseName

--- src/renderer/src/components/2-main/2-right/1-header/0-all/1-row1-title-with-file-us.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/1-row1-title-with-file-us.tsx
@@ -3,11 +3,20 @@ import { type FileUs } from "@/store/store-types";
 
 export function Row1ChooseName({ fileUs }: { fileUs: FileUs; }) {
 
-    let chooseName = useAtomValue(fileUs.parsedSrc.stats.loginFormChooseNameAtom);
+    const chooseName = useAtomValue(fileUs.parsedSrc.stats.loginFormChooseNameAtom);
 
     if (fileUs.parsedSrc.stats.isFCat) {
         const isMaster = fileUs.fceAtoms?.viewFceCtx?.isMaster;
-        chooseName = `${isMaster ? 'Primary' : 'Secondary'} Field Catalog`;
+        const hint = isMaster ? '' : 'The active field catalog is located at the root of the open folder \nand has the file name "field_catalog.dpn".';
+        return (
+            <div className="text-sm font-semibold" title="Name from manifest file">
+                Field Catalog
+                {' '}
+                <span className="text-xs font-normal text-orange-500 dark:text-yellow-500" title={hint}>
+                    {isMaster ? '' : '(inactive)'}
+                </span>
+            </div>
+        );
     }
 
     return (
