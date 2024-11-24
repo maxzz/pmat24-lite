2df1c9caeecb75915dda82064add0a051c1dc711
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-24 15:26:07 -0800

    WIP. field catalog. right panel header. update Row1ChooseName. less rerenders

--- src/renderer/src/components/2-main/2-right/1-header/0-all/1-row1-title-with-file-us.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/1-row1-title-with-file-us.tsx
@@ -2,26 +2,32 @@ import { useAtomValue } from "jotai";
 import { type FileUs } from "@/store/store-types";
 
 export function Row1ChooseName({ fileUs }: { fileUs: FileUs; }) {
+    return (<>
+        {fileUs.parsedSrc.stats.isFCat
+            ? <FcName fileUs={fileUs} />
+            : <ManiName fileUs={fileUs} />}
+    </>);
+}
 
+function ManiName({ fileUs }: { fileUs: FileUs; }) {
     const chooseName = useAtomValue(fileUs.parsedSrc.stats.loginFormChooseNameAtom);
-
-    if (fileUs.parsedSrc.stats.isFCat) {
-        const isMaster = fileUs.fceAtoms?.viewFceCtx?.isMaster;
-        const hint = isMaster ? '' : 'The active field catalog is located at the root of the open folder \nand has the file name "field_catalog.dpn".';
-        return (
-            <div className="text-sm font-semibold" title="Name from manifest file">
-                Field Catalog
-                {' '}
-                <span className="text-xs font-normal text-orange-500 dark:text-yellow-500" title={hint}>
-                    {isMaster ? '' : '(inactive)'}
-                </span>
-            </div>
-        );
-    }
-
     return (
         <div className="pl-1 text-sm font-semibold" title="Name from manifest file">
             {chooseName}
+        </div>
+    );
+}
+
+function FcName({ fileUs }: { fileUs: FileUs; }) {
+    const isMaster = fileUs.fceAtoms?.viewFceCtx?.isMaster;
+    const hint = isMaster ? '' : 'The active field catalog is located at the root of the open folder \nand has the file name "field_catalog.dpn".';
+    return (
+        <div className="text-sm font-semibold" title="Name from manifest file">
+            Field Catalog
+            {' '}
+            <span className="text-xs font-normal text-orange-500 dark:text-yellow-500" title={hint}>
+                {isMaster ? '' : '(inactive)'}
+            </span>
         </div>
     );
 }
