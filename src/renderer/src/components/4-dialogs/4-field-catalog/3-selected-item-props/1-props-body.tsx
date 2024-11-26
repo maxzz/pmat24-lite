c3751e3ea14b7f3ed44ae7f429a8585f84b9d4a0
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-26 15:43:08 -0800

    WIP. field catalog. ----------back to simple SelectedItemPropsBody

--- src/renderer/src/components/4-dialogs/4-field-catalog/3-selected-item-props/1-props-body.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/3-selected-item-props/1-props-body.tsx
@@ -1,27 +1,15 @@
 import { useAtomValue } from "jotai";
 import { FieldTyp } from "@/store/manifest";
-import { hasSelectedItemAtom, useSelectedUpdates, type FceCtx } from "@/store";
+import { hasSelectedItemAtom, type FceCtx } from "@/store";
 import { PropText, PropValue, PropTextarea, NewLabel } from "./8-inputs";
-import { classNames } from "@/utils";
 import { SelectedIdxView, SelectedIdView } from "./7-selected-views";
-
-export function SelectedItemPropsBody({ fceCtx }: { fceCtx: FceCtx; }) {
-    return (<>
-        {/* <FakeSelectedUpdates fceCtx={fceCtx} /> */}
-        <SelectedItemPropsContent fceCtx={fceCtx} />
-    </>);
-}
-
-function FakeSelectedUpdates({ fceCtx }: { fceCtx: FceCtx; }) {
-    useSelectedUpdates({ fceCtx });
-    return null;
-}
+import { classNames } from "@/utils";
 
 const itemClasses = "pt-2 flex flex-col disabled:opacity-25 disabled:pointer-events-none";
 const disabledClasses = "opacity-0 pointer-events-none cursor-not-allowed";
 const mergeStateClasses = (enabled: boolean) => classNames(itemClasses, !enabled && disabledClasses);
 
-function SelectedItemPropsContent({ fceCtx }: { fceCtx: FceCtx; }) {
+export function SelectedItemPropsBody({ fceCtx }: { fceCtx: FceCtx; }) {
 
     const hasSelected = useAtomValue(hasSelectedItemAtom)({ fceCtx });
     const valueLife = useAtomValue(fceCtx.fcePropAtoms.valueLifeAtom);
