f0f7a27d5a680ff2440290af3629a522aabbede3
Max Zakharzhevskiy <maxzz@msn.com>
2024-11-30 19:17:10 -0800

    WIP. manual mode props. updaate ManualModeView; add scroll on actions

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/0-view.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/0-view.tsx
@@ -1,22 +1,31 @@
 import { type MFormContextProps } from "@/store/atoms/3-file-mani-atoms";
 import { ManualPanelActions } from "../1-panel-actions";
 import { ManualPanelProps } from "../2-panel-props";
+import { Scroll } from "lucide-react";
+import { ScrollArea } from "@/ui";
 
 const manualModeViewClasses = "\
-pl-0.5 pr-2.5 min-w-60 \
+pl-0.5 pr-2.5 min-w-60 h-full bg-green-400 \
 \
-grid grid-cols-1 grid-rows-[minmax(0,_1fr),auto] \
+grid grid-cols-1 grid-rows-[minmax(100px,_1fr),auto] min-h-0 \
 @[600px]:grid-cols-2 \
 gap-1 \
 ";
 
-export function ManualModeView({ ctx }: {ctx: MFormContextProps}) {
+export function ManualModeView({ ctx }: { ctx: MFormContextProps; }) {
     return (<>
         <div className={manualModeViewClasses}>
 
-            <ManualPanelActions className="@container/actions h-full min-h-[23rem]" ctx={ctx} />
+            <div className="relative">
+                <div className="absolute inset-0 bg-red-400 overflow-hidden">
+                    <ScrollArea className="h-full bg-red-400">
+                        <ManualPanelActions className="@container/actions h-full min-h-[23rem]" ctx={ctx} />
+                    </ScrollArea>
+                </div>
+            </div>
+
             <ManualPanelProps className="@container/props min-h-[340px] text-xs" ctx={ctx} />
-            
+
         </div>
     </>);
 }
