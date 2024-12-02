4e149e40d733cd82deee37a72682441107e8df34
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-01 18:23:49 -0800

    WIP. manual mode props. move classes to rows

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/2-rows/4-single-row.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/2-rows/4-single-row.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import { type MFormCtx, type ManualFieldState } from "@/store/atoms/3-file-mani-atoms";
 import { type MenuState, RowMenuButton } from "./5-row-popup-menu";
 import { RowColumnDetails, RowColumnIcon, rowColumnName } from "../3-row-details";
-import { rowClasses, rowSelectedClasses } from "./8-row-classes";
 import { classNames } from "@/utils";
 
 type SingleRowProps = HTMLAttributes<HTMLDivElement> & {
@@ -15,6 +14,24 @@ type SingleRowProps = HTMLAttributes<HTMLDivElement> & {
 };
 
 const singleRowClasses = "py-0.5 grid grid-cols-[min-content,5rem,1fr,min-content] items-center";
+
+export const rowParentActiveClasses = "[--parent-active:0] focus-within:[--parent-active:1]";
+
+export const rowClasses = "\
+leading-6 \
+hover:bg-primary-200/30 dark:hover:bg-primary-800/40";
+
+export const rowSelectedClasses = "\
+text-primary-800 dark:text-primary-200 \
+\
+bg-primary-400/20 dark:bg-primary-400/20 \
+hover:!bg-primary-400/30 \
+\
+outline-primary-400 \
+[outline-width:calc(var(--parent-active)_*_1px)] \
+outline rounded-[3px] \
+\
+cursor-default";
 
 function SingleRowWRef({ formCtx, chunk, menuState, idx, ...rest }: SingleRowProps, ref: Ref<HTMLDivElement>) {
 
