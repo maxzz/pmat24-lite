b8ccce3599244ed7fc4ccdb070830cd55f706878
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-01 16:24:28 -0800

    WIP. manual mode props. update style files layout

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/8-manual-shared-styles/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/8-manual-shared-styles/index.tsx
@@ -1,7 +1,7 @@
-export * from "./focus";
-export * from "./menu";
-export * from "./actions-list";
-export * from "./editor-title-w-frame";
+export * from "./3-focus";
+export * from "./4-menu";
+export * from "./2-paenl-actions-list";
+export * from "./1-editor-title-w-frame";
 
 export const editorFrameClasses = "p-1 border-border border rounded";
 
