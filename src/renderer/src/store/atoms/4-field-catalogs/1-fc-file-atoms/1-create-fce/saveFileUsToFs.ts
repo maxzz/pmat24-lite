d71ab6a14409adb99ed8afc0c71833ef06541fe6
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-15 15:50:59 -0800

    WIP. field catalog. doFcePropChangesAtom. 7. add doSaveFileUsToFsAtom

---
+++ src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/1-create-fce/saveFileUsToFs.ts
@@ -0,0 +1,28 @@
+import { atom, type Getter, type Setter } from "jotai";
+import type { FileUs } from "@/store/store-types";
+
+export const doSaveFileUsToFsAtom = atom(null,
+    (get, set, fileUs: FileUs) => {
+        try {
+            saveFileUsToFs(fileUs, get, set);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+);
+
+function saveFileUsToFs(fileUs: FileUs, get: Getter, set: Setter) {
+    const { fileCnt } = fileUs;
+    
+    const filePath = `${fileCnt.fpath}/${fileCnt.fname}`;
+    const cnt = fileCnt.raw;
+
+    console.log('saveFileToFs', filePath, cnt);
+
+    // const filePath = path.join(fpath, fname);
+    // fs.writeFile(filePath, raw, (err) => {
+    //     if (err) {
+    //         console.error(err);
+    //     }
+    // });
+}
