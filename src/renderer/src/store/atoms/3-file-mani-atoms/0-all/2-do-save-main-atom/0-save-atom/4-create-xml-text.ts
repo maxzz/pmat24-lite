2c0ec10b493e0e56bc43ede6e1b6131804bd811a
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-15 16:27:59 -0800

    WIP. field catalog. save. update createXmlText()

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/4-create-xml-text.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/0-save-atom/4-create-xml-text.ts
@@ -1,16 +1,30 @@
 import { Getter, Setter } from "jotai";
-import { type FileUsAtom } from "@/store/store-types";
+import { type FileUs, type FileUsAtom } from "@/store/store-types";
 import { convertToXml, type Mani } from "@/store/manifest";
 import { stopIfInvalidAny } from "../1-stop-if-validation-failed";
 import { packManifest } from "./1-pack-manifest";
 //import { printTestManifest } from "./8-print-test-manifest";
 import { toManiFileFormat } from "./3-to-mani-file-format";
+import { type ManiAtoms } from "../../../9-types";
 
 export function createXmlText(fileUsAtom: FileUsAtom, get: Getter, set: Setter): string | undefined {
     const fileUs = get(fileUsAtom);
 
+    if (fileUs.fceAtomsForFcFile) {
+        const rv = getFcContent(fileUs, fileUsAtom, get, set);
+        return rv;
+    }
+
     const maniAtoms = get(fileUs.maniAtomsAtom);
-    if (!maniAtoms || stopIfInvalidAny(maniAtoms, get, set)) {
+    if (maniAtoms) {
+        const rv = getManiContent(fileUs, fileUsAtom, maniAtoms, get, set);
+        return rv;
+    }
+}
+
+function getManiContent(fileUs: FileUs, fileUsAtom: FileUsAtom, maniAtoms: ManiAtoms, get: Getter, set: Setter): string | undefined {
+    
+    if (stopIfInvalidAny(maniAtoms, get, set)) {
         return;
     }
 
@@ -37,4 +51,9 @@ export function createXmlText(fileUsAtom: FileUsAtom, get: Getter, set: Setter):
     // printTestManifest(newMani);
 
     return xml;
+}
+
+function getFcContent(fileUs: FileUs, fileUsAtom: FileUsAtom, get: Getter, set: Setter): string | undefined {
+    const fceAtoms = fileUs.fceAtomsForFcFile;
+    return;
 }
