3fa11c83d8d3f4ed3c54885e1201bd6a7ec8941a
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-22 19:05:32 -0800

    WIP. dev-options. 3. move menu to files. split app header

--- src/renderer/src/components/1-header/0-all/0-all.tsx
+++ src/renderer/src/components/1-header/0-all/0-all.tsx
@@ -1,20 +1,17 @@
 import { useSetAtom } from "jotai";
 import { useSnapshot } from "valtio";
-import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
-import { TopMainDropdownMenu } from "./1-nun-old-main-menu-all";
-import { Button } from "@/ui";
+import { appSettings } from "@/store";
 import { doOpenCreateDialogAtom, doOpenDrawerAtom } from "@/store/atoms/7-dialogs";
 import { TestOpenFiles } from "./8-1-test-open-files";
 import { TestOpenFieldCatalog } from "./8-2-test-open-field-catalog";
-import { appSettings } from "@/store";
-// import { TestPolicyIcons } from "./8-test-policy-icons";
+import { Button } from "@/ui";
+import { ThemeSwitch } from "@/ui/shadcn/theme-toggle-switch";
+// import { TopMainDropdownMenu } from "./1-nun-old-main-menu-all";
+// import { TestPolicyIcons } from "./8-3-test-policy-icons";
 
 export function SectionHeader() {
-    const doOpenCreateDialog = useSetAtom(doOpenCreateDialogAtom);
-    const doOpenDrawer = useSetAtom(doOpenDrawerAtom);
-
+    
     const { showUiHeader } = useSnapshot(appSettings.appUi.uiAdvanced);
-
     if (!showUiHeader) {
         return <div />;
     }
@@ -24,13 +21,10 @@ export function SectionHeader() {
             <div className="flex items-center gap-2">
                 {/* <TopMainDropdownMenu /> */}
 
-                <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenCreateDialog(true)}>
-                    Create mani
-                </Button>
+                <TestCreateManifest />
 
-                <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenDrawer(true)}>
-                    Create
-                </Button>
+                <TestCreatewithDrawer />
+
 
                 <TestOpenFiles />
                 <TestOpenFieldCatalog />
@@ -40,5 +34,23 @@ export function SectionHeader() {
 
             <ThemeSwitch />
         </div>
+    );
+}
+
+function TestCreateManifest() {
+    const doOpenCreateDialog = useSetAtom(doOpenCreateDialogAtom);
+    return (
+        <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenCreateDialog(true)}>
+            Create mani
+        </Button>
+    );
+}
+
+function TestCreatewithDrawer() {
+    const doOpenDrawer = useSetAtom(doOpenDrawerAtom);
+    return (
+        <Button variant="outline" className="text-[.65rem]" onClick={() => doOpenDrawer(true)}>
+            Create
+        </Button>
     );
 }
