3bf1c447eaa5c5edfdfd89b432cec598985a95c9
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-23 18:26:28 -0800

    WIP. files. update tree getFileListDisplayText()

--- src/renderer/src/store/atoms/1-files/3-tree-files/3-tree-files.ts
+++ src/renderer/src/store/atoms/1-files/3-tree-files/3-tree-files.ts
@@ -1,7 +1,7 @@
 import { atom } from "jotai";
 import { type FileUsAtom } from "@/store/store-types";
 import { fileListOptionsAtom } from "@/store";
-import { appTypeToIcon, fileUsToAppType, getFileListDisplayName } from "@/store/store-utils";
+import { appTypeToIcon, fileUsToAppType, getFileListDisplayText } from "@/store/store-utils";
 import { filteredAtom } from "./1-filtered-files";
 import { type DataItemCore, type DataItemNavigation } from "@/ui/shadcn/tree";
 
@@ -30,7 +30,7 @@ export const treeFilesAtom = atom( // files to show in the tree
 
                 const rv: TreeFileItem = {
                     id: fileUs.fileCnt.unid,
-                    name: getFileListDisplayName(fileUs, fileListOptions.itemsState, get),
+                    name: getFileListDisplayText(fileUs, fileListOptions.itemsState, get),
                     icon: appTypeToIcon(fileUsToAppType(fileUs, showIeWranIcon)),
                     fileUsAtom,
                 };
--- src/renderer/src/store/store-utils/2-ui-utils/9-file-list-display-text.ts
+++ src/renderer/src/store/store-utils/2-ui-utils/9-file-list-display-text.ts
@@ -2,7 +2,7 @@ import { Getter } from "jotai";
 import { FileUs } from "@/store/store-types";
 import { FileListItemsState } from "../../atoms/9-ui-state/1-files-list/3-ui-file-items";
 
-export function getFileListDisplayName(fileUs: FileUs, options: FileListItemsState, get: Getter) {
+export function getFileListDisplayText(fileUs: FileUs, options: FileListItemsState, get: Getter) {
     const { showChosen, showFname } = options;
 
     const name =
