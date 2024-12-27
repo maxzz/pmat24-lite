b5996015597b3e711d0bba2bdc2154dd5117fd52
Max Zakharzhevskiy <maxzz@msn.com>
2024-12-26 18:16:27 -0800

    WIP. field catalog. dlg-fc-main. update Header. 1

--- src/renderer/src/components/4-dialogs/4-field-catalog/0-all/3-header.tsx
+++ src/renderer/src/components/4-dialogs/4-field-catalog/0-all/3-header.tsx
@@ -1,27 +1,17 @@
 import { useSetAtom } from "jotai";
 import { type FceCtx, doCancelFceDlgAtom } from "@/store";
 import { DialogCloseButton } from "@/ui/shadcn/dialog";
-// import { SymbolFolder } from "@/ui/icons";
+import { SymbolFolder } from "@/ui/icons";
 
 export function Header({ fceCtx }: { fceCtx: FceCtx; }) {
-
     const doCancelFldCatDialog = useSetAtom(doCancelFceDlgAtom);
-
-    // const openMainDlg = !fceCtx.inData?.openSelectItemDlg;
-    // const fname = fceCtx.fceAtoms?.fileUs.fileCnt?.fpath; //TODO: if hasMain() then show path relative to main fc file, or none if main fc file
-
     return (
         <div className="relative py-2 border-border border-b flex flex-col items-center">
             <div className="text-xs font-semibold">
                 Field Catalog
             </div>
 
-            {/* {!openMainDlg &&
-                <div className="opacity-50 flex items-center justify-start gap-1">
-                    <SymbolFolder className="size-4" />
-                    {fname ? fname : 'root'}
-                </div>
-            } */}
+            <HeaderFolder fceCtx={fceCtx} />
 
             <DialogCloseButton
                 className="absolute right-1 top-1/2 -translate-y-1/2 p-2 hover:bg-muted hover:rounded-sm"
@@ -30,4 +20,17 @@ export function Header({ fceCtx }: { fceCtx: FceCtx; }) {
             />
         </div>
     );
+}
+
+function HeaderFolder({ fceCtx }: { fceCtx: FceCtx; }) {
+    const openMainDlg = !fceCtx.inData?.openSelectItemDlg;
+    const fname = fceCtx.fceAtoms?.fileUs.fileCnt?.fpath; //TODO: if hasMain() then show path relative to main fc file, or none if main fc file
+    return (<>
+        {!openMainDlg &&
+            <div className="opacity-50 flex items-center justify-start gap-1">
+                <SymbolFolder className="size-4" />
+                {fname ? fname : 'root'}
+            </div>
+        }
+    </>);
 }
