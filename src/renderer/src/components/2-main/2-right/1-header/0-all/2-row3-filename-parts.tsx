cfb8e0416d400cb053f6d721356e4a2511a0bc2f
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-01 15:27:18 -0800

    WIP. field catalog. dlg-fc-main. update Row3FnameParts

--- src/renderer/src/components/2-main/2-right/1-header/0-all/2-row3-filename-parts.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/2-row3-filename-parts.tsx
@@ -4,11 +4,11 @@ type ParsedFnameParams = {
 };
 
 const partClasses = {
-    container: "text-[0.7rem] overflow-hidden whitespace-nowrap overflow-ellipsis", //text-primary-300/80
-    sm: "opacity-50 font-sans text-[0.5rem]",
-    xs: "opacity-30",
-    lg: "px-px text-[0.72rem] text-primary-400 opacity-100 border-b border-dotted border-primary-500",
-    l2: "px-1 text-[.65rem] font-bold text-gray-600 opacity-100", // large for ManiInfoTooltip
+    container: /**/ "text-[0.7rem] truncate", //text-primary-300/80
+    sm:        /**/ "font-sans text-[0.6rem] opacity-50",
+    xs:        /**/ "opacity-20",
+    lg:        /**/ "px-px text-[0.7rem] text-foreground opacity-50 border-b border-dotted border-primary-500",
+    l2:        /**/ "px-1 text-[.7rem] font-bold text-foreground opacity-70", // large for ManiInfoTooltip
 };
 
 const reFilenameMatch = /^\{([0-9A-Za-z]{3,3})(.*)([0-9A-Za-z]{3,3})\}\.dpm$/; //TODO: handle '{guid} + extra.dpm' filenames
@@ -16,8 +16,8 @@ const reFilenameMatch = /^\{([0-9A-Za-z]{3,3})(.*)([0-9A-Za-z]{3,3})\}\.dpm$/; /
 export function Row3FnameParts({ fname, large }: ParsedFnameParams) {
     const { container, sm, xs, lg: large1, l2: large2 } = partClasses;
 
-    const match = (fname || '').match(reFilenameMatch);
-    if (!match) {
+    const matchToParts = (fname || '').match(reFilenameMatch);
+    if (!matchToParts) {
         return (
             <div className={container}>
                 <span className={sm}>
@@ -31,11 +31,16 @@ export function Row3FnameParts({ fname, large }: ParsedFnameParams) {
 
     return (
         <div className={container}>
+            Filename:{' '}
             <span className={xs}>{'{'}</span>
-            <span className={lg}>{match[1]}</span>
-            <span className={sm}>{match[2]}</span>
-            <span className={lg}>{match[3]}</span>
+            <span className={lg}>{matchToParts[1]}</span>
+            <span className={sm}>{matchToParts[2]}</span>
+            <span className={lg}>{matchToParts[3]}</span>
             <span className={xs}>{'}.dpm'}</span>
         </div>
     );
 }
+
+//TODO: add tooltip with filename path
+//TODO: add button copy filename (with or without path) to clipboard
+//TODO: reveal filename in explorer
