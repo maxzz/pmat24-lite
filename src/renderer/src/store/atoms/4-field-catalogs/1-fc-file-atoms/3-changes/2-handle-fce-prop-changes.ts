e6ff63ef9ec90295ced89f4bafb73cf5d7c277e5
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-05 15:46:40 -0800

    WIP. field catalog. dlg-fc-main changes. update handleFcePropChanges()

--- src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/3-changes/2-handle-fce-prop-changes.ts
+++ src/renderer/src/store/atoms/4-field-catalogs/1-fc-file-atoms/3-changes/2-handle-fce-prop-changes.ts
@@ -4,7 +4,8 @@ import { type FceItem } from "../../9-types";
 import { type FcePropChangesProps } from "./1-prop-changes-atom";
 import { setFileUsChangeFlag, hasFileUsAnyChanges } from "@/store/atoms/3-file-mani-atoms";
 
-export function handleFcePropChanges(selectedItem: FceItem, { fceCtx, name, nextValue }: FcePropChangesProps, get: Getter, set: Setter) {
+export function handleFcePropChanges(selectedItem: FceItem, ctx: FcePropChangesProps, get: Getter, set: Setter) {
+    const { fceCtx, name, nextValue } = ctx;
     const beforeEdit = selectedItem.beforeEdit;
     const uuid = selectedItem.fceMeta.uuid;
 
@@ -13,27 +14,29 @@ export function handleFcePropChanges(selectedItem: FceItem, { fceCtx, name, next
         return;
     }
 
+    let changed = false;
+    let changePrefix: string | undefined;
+
     switch (name) {
         case 'nameAtom': {
+            changePrefix = 'name';
             const displayname = nextValue as string;
-            const changed = displayname !== beforeEdit.displayname;
+            changed = displayname !== beforeEdit.displayname;
             //console.log('doFcePropChangesAtom', JSON.stringify({ name, changed, uuid, nextValue, current: selectedItem.fieldValue }, null, 2));
             selectedItem.fieldValue.displayname = displayname;
-            
-            setFileUsChangeFlag(fceCtx.fceAtoms, changed, `name-${uuid}`);
             break;
         }
         case 'ownernoteAtom': {
+            changePrefix = 'note';
             const ownernote = nextValue as string;
-            const changed = ownernote !== beforeEdit.ownernote;
+            changed = ownernote !== beforeEdit.ownernote;
             //console.log('doFcePropChangesAtom', JSON.stringify({ name, changed, uuid, nextValue, current: selectedItem.fieldValue }, null, 2));
             selectedItem.fieldValue.ownernote = ownernote;
-            
-            setFileUsChangeFlag(fceCtx.fceAtoms, changed, `note-${uuid}`);
             break;
         }
         case 'valueLifeAtom': {
-            const changed = !sameValueLife(nextValue as ValueLife, beforeEdit);
+            changePrefix = 'life';
+            changed = !sameValueLife(nextValue as ValueLife, beforeEdit);
             //console.log('doFcePropChangesAtom', JSON.stringify({ name, changed, uuid, nextValue, current: selectedItem.fieldValue }, null, 2));
 
             const { value, valueAs, isRef, isNon } = nextValue as ValueLife;
@@ -41,14 +44,22 @@ export function handleFcePropChanges(selectedItem: FceItem, { fceCtx, name, next
             selectedItem.fieldValue.valueAs = valueAs;
             selectedItem.fieldValue.isRef = isRef;
             selectedItem.fieldValue.isNon = isNon;
-
-            setFileUsChangeFlag(fceCtx.fceAtoms, changed, `life-${uuid}`);
             break;
         }
     }
 
+    if (changePrefix) {
+        setFileUsChangeFlag(fceCtx.fceAtoms, changed, `${changePrefix}-${uuid}`);
+    }
+
+    printItemChanges(selectedItem, ctx, changed, uuid);
+}
+
+function printItemChanges(selectedItem: FceItem, ctx: FcePropChangesProps, changed: boolean, uuid: number) {
+    const { fceCtx, name, nextValue } = ctx;
+
     if (fceCtx.fceAtoms.fileUs.parsedSrc.stats.isFCatRoot) {
-        const changed = hasFileUsAnyChanges(fceCtx.fceAtoms);
-        console.log('doFcePropChangesAtom', JSON.stringify({ name, changed, uuid, nextValue, current: selectedItem.fieldValue }, null, 2));
+        const fileChanged = hasFileUsAnyChanges(fceCtx.fceAtoms);
+        console.log('doFcePropChangesAtom', JSON.stringify({ name, changed, fileChanged, uuid, nextValue, current: selectedItem.fieldValue }, null, 2));
     }
 }
