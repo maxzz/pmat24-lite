7e0cff0423c879fd383d8cb203dc81f821412e72
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-04 19:36:47 -0800

    WIP. field catalog. dlg-fc-main. getFcContentText(). 7. mergeToManiField() code review

--- src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/7-merge-to-mani-field.ts
+++ src/renderer/src/store/atoms/3-file-mani-atoms/0-all/2-do-save-main-atom/2-pack/1-normal/7-merge-to-mani-field.ts
@@ -9,6 +9,7 @@ type MergeManiFieldsProps = {
 
 export function mergeToManiField({ from, maniField, ftyp, isSubmit }: MergeManiFieldsProps): Mani.Field {
     const restoreLink = from.rfieldform === Mani.FORMNAME.noname && maniField.rfieldform === Mani.FORMNAME.brokenFcLink; // It was broken now but who knows which fc will be loaded next time
+    //TODO: this is still not true if user intentionally changed it to not use fc. I guess we should remove maniField.rfieldform Mani.FORMNAME.brokenFcLink when user set it to not use fc
 
     const rv: Mani.Field = {
         ...maniField,
@@ -16,7 +17,7 @@ export function mergeToManiField({ from, maniField, ftyp, isSubmit }: MergeManiF
         submit: isSubmit,
         rfieldform: restoreLink ? Mani.FORMNAME.fieldcatalog : from.rfieldform, 
     };
-    
+
     rv.value = getFieldStringValue(from.value, ftyp);
 
     //TODO: we need to correlate policies with password change form
