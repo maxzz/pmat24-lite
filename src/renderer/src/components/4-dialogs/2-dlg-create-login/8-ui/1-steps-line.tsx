8ae0bcf83c146dc2fb19091af3b7bdad8f296bd4
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-09 16:13:44 -0800

    WIP. create-mani. connect steps-line demo. 4

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/1-steps-line.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/1-steps-line.tsx
@@ -9,41 +9,17 @@ export function Step({ step, currentStep }: { step: number; currentStep: number;
                 ? "inactive"
                 : "complete";
     return (
-        <motion.div animate={status} className="relative">
+        <motion.div className="relative" animate={status}>
             <motion.div
-                variants={{
-                    active: {
-                        scale: 1,
-                        transition: { delay: 0, duration: 0.2, },
-                    },
-                    complete: {
-                        scale: 1.25,
-                    },
-                }}
-                transition={{ duration: 0.6, delay: 0.2, type: "tween", ease: "circOut", }}
                 className="absolute inset-0 rounded-full bg-blue-200"
+                variants={frameVariants}
+                transition={{ duration: 0.6, delay: 0.2, type: "tween", ease: "circOut", }}
             />
 
             <motion.div
                 className="relative flex h-10 w-10 items-center justify-center rounded-full border-2 font-semibold"
                 initial={false}
-                variants={{
-                    inactive: {
-                        backgroundColor: "#fff", // neutral
-                        borderColor: "#e5e5e5", // neutral-200
-                        color: "#a3a3a3", // neutral-400
-                    },
-                    active: {
-                        backgroundColor: "#fff",
-                        borderColor: "#3b82f6", // blue-500
-                        color: "#3b82f6", // blue-500
-                    },
-                    complete: {
-                        backgroundColor: "#3b82f6", // blue-500
-                        borderColor: "#3b82f6", // blue-500
-                        color: "#3b82f6", // blue-500
-                    },
-                }}
+                variants={circleVariants}
                 transition={{ duration: 0.2 }}
             >
                 <div className="flex items-center justify-center">
@@ -56,6 +32,34 @@ export function Step({ step, currentStep }: { step: number; currentStep: number;
         </motion.div>
     );
 }
+
+const frameVariants = {
+    active: {
+        scale: 1,
+        transition: { delay: 0, duration: 0.2, },
+    },
+    complete: {
+        scale: 1.25,
+    },
+};
+
+const circleVariants = {
+    inactive: {
+        backgroundColor: "#fff", // neutral
+        borderColor: "#e5e5e5", // neutral-200
+        color: "#a3a3a3", // neutral-400
+    },
+    active: {
+        backgroundColor: "#fff",
+        borderColor: "#3b82f6", // blue-500
+        color: "#3b82f6", // blue-500
+    },
+    complete: {
+        backgroundColor: "#3b82f6", // blue-500
+        borderColor: "#3b82f6", // blue-500
+        color: "#3b82f6", // blue-500
+    },
+};
 
 function CheckIcon(props: ComponentProps<"svg">) {
     return (
