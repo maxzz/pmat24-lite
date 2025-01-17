51e890e51bfb2db318cf6522425b45dbf5c1346f
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-16 19:05:07 -0800

    WIP. create-mani. add initial GetTlwScreenshotsResult render

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/2-apps-grid/index.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/2-apps-grid/index.tsx
@@ -1,24 +1,27 @@
 import { type ComponentProps } from "react";
-// import reply from "@/assets/tests/25.01.16.25/TopLevelWindowsScreenshots.json";
+import reply from "@/assets/tests/25.01.16.25/TopLevelWindowsScreenshots.json";
+import { GetTlwScreenshotsResult } from "@shared/ipc-types";
 
-// console.log("AppsGrid", reply);
+console.log("AppsGrid", reply);
 
 export function AppsGrid({ className, ...rest }: ComponentProps<"div">) {
+    const data = reply as GetTlwScreenshotsResult;
     return (
-        <div className="grid grid-cols-1 1debug" {...rest}>
-            <div className="flex flex-col gap-4">
-                <div className="text-sm font-semibold">Apps</div>
-                <div className="text-xs">Select apps to train</div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-                <div className="text-sm font-semibold">Apps</div>
-                <div className="text-xs">Select apps to train</div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-                <div className="text-sm font-semibold">Apps</div>
-                <div className="text-xs">Select apps to train</div>
+        <div className={className} {...rest}>
+            <div className="grid grid-cols-1 1debug">
+                {data.map((item, idx) => {
+                    if (item.type === 'error') {
+                        // const res = null;
+                        const res = <div className="flex flex-col gap-4" key={idx}>error</div>;
+                        return res;
+                    }
+                    return (
+                        <div className="flex flex-col gap-4" key={idx}>
+                            <div className="text-sm font-semibold">{item.caption}</div>
+                            <div className="text-xs">{item.caption}</div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
