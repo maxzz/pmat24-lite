752dcfdda42d76b1ab1a1044c6a7081fd6eac1ca
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-16 19:09:02 -0800

    WIP. create-mani. GetTlwScreenshotsResult render. 2

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/2-apps-grid/index.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-ui/2-apps-grid/index.tsx
@@ -2,21 +2,25 @@ import { type ComponentProps } from "react";
 import reply from "@/assets/tests/25.01.16.25/TopLevelWindowsScreenshots.json";
 import { GetTlwScreenshotsResult } from "@shared/ipc-types";
 
-console.log("AppsGrid", reply);
+// console.log("AppsGrid", reply);
 
 export function AppsGrid({ className, ...rest }: ComponentProps<"div">) {
     const data = reply as GetTlwScreenshotsResult;
     return (
         <div className={className} {...rest}>
-            <div className="grid grid-cols-1 1debug">
+            <div className="grid grid-cols-1 gap-4 debug">
                 {data.map((item, idx) => {
                     if (item.type === 'error') {
                         // const res = null;
-                        const res = <div className="flex flex-col gap-4" key={idx}>error</div>;
+                        const res = (
+                            <div className="flex flex-col gap-1" key={idx}>
+                                Error. hwnd: {item.hwnd} error code: "{item.errorCode}"
+                            </div>
+                        );
                         return res;
                     }
                     return (
-                        <div className="flex flex-col gap-4" key={idx}>
+                        <div className="flex flex-col gap-1" key={idx}>
                             <div className="text-sm font-semibold">{item.caption}</div>
                             <div className="text-xs">{item.caption}</div>
                         </div>
