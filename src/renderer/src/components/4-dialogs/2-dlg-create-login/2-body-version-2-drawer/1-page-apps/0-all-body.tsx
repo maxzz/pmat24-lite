f3681bf4e8cadd7cd5526f5cd95edc9b40103bbe
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-17 18:44:27 -0800

    WIP. create-mani. GetTlwScreenshotsResult render. 4. add Page1Apps. 6. add observer

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
@@ -1,20 +1,23 @@
 import { ScrollArea } from "@/ui/shadcn";
+import useResizeObserver from "use-resize-observer";
 import { AppsGrid } from "./2-apps-grid";
 import { ButtonCreateFormSelector } from "@/components/4-dialogs/9-select-create-mani-type";
 
 export function Page1Apps() {
+    const { ref, width, height } = useResizeObserver();
     return (
         <div className="-my-4 px-4 text-xs border-l border-border grid grid-rows-[1fr,auto] 1debug">
 
             <div className="relative">
-                <div className="aboslute inset-0">
-                    <ScrollArea className="1h-full" fullHeight fixedWidth>
+                <div ref={ref} className="aboslute inset-0">
+                    <ScrollArea className="h-full" style={{ width, height }} fullHeight fixedWidth>
                         <AppsGrid />
                     </ScrollArea>
                 </div>
             </div>
 
             <ButtonCreateFormSelector triggerLabel="Create new manifest" />
+            
         </div>
     );
 }
