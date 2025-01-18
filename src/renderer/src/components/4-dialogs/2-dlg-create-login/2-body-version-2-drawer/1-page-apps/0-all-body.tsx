5df11f2ba427bc1c613de597dd4ed7668acff0bd
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-18 14:56:56 -0800

    WIP. create-mani. GetTlwScreenshotsResult render. 4. add Page1Apps. 7. alternative version wo/ observer is working too

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
@@ -4,14 +4,22 @@ import { AppsGrid } from "./2-apps-grid";
 import { ButtonCreateFormSelector } from "@/components/4-dialogs/9-select-create-mani-type";
 
 export function Page1Apps() {
-    const { ref, width, height } = useResizeObserver();
+    // const { ref, width, height } = useResizeObserver();
     return (
         <div className="1-my-4 px-4 text-xs border-l border-border grid grid-rows-[auto,1fr,auto] debug">
             <div className="text-xl bg-green-400">Description</div>
 
-            <div ref={ref} className="relative h-full w-full">
+            {/* <div ref={ref} className="relative h-full w-full">
                 <div className="absolute inset-0">
                     <ScrollArea style={{ width, height }} fullHeight fixedWidth>
+                        <AppsGrid />
+                    </ScrollArea>
+                </div>
+            </div> */}
+
+            <div className="relative h-full w-full">
+                <div className="absolute inset-0">
+                    <ScrollArea fullHeight fixedWidth>
                         <AppsGrid />
                     </ScrollArea>
                 </div>
