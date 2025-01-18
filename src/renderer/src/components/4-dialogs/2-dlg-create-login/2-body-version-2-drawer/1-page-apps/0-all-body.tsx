9bd58d7b142cd3adcf4868684226a433221fd5fb
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-18 15:18:26 -0800

    WIP. create-mani. GetTlwScreenshotsResult render. 4. add Page1Apps. 7. fix alternative version wo/ observer size

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
@@ -1,22 +1,34 @@
-import { ScrollArea } from "@/ui/shadcn";
 import useResizeObserver from "use-resize-observer";
+import { ScrollArea } from "@/ui/shadcn";
 import { AppsGrid } from "./2-apps-grid";
 import { ButtonCreateFormSelector } from "@/components/4-dialogs/9-select-create-mani-type";
 
 export function Page1Apps() {
+    // const { ref, width, height } = useResizeObserver();
     return (
-        <div className="1-my-4 px-4 text-xs border-l border-border grid grid-rows-[auto,1fr,auto] debug">
-            <div className="text-xl bg-green-400">Description</div>
+        <div className="1-my-4 text-xs border-l border-border grid grid-rows-[auto,1fr,auto] 1debug">
+            <div className="px-2 py-1 text-sm bg-muted/30">
+                Description
+            </div>
 
             <div className="relative h-full w-full">
                 <div className="absolute inset-0">
-                    <ScrollArea fullHeight fixedWidth>
+                    <ScrollArea className="px-2 py-1 size-full" fullHeight fixedWidth>
                         <AppsGrid />
                     </ScrollArea>
                 </div>
             </div>
 
-            <div className="text-xl bg-green-400">
+{/* <div ref={ref} className="relative h-full w-full">
+                <div className="absolute inset-0">
+                    <ScrollArea style={{ width, height }} fullHeight fixedWidth>
+                        <AppsGrid />
+                    </ScrollArea>
+                </div>
+            </div> */}
+
+
+            <div className="px-1 py-2 bg-muted/30 border-t border-border">
                 <ButtonCreateFormSelector triggerLabel="Create new manifest" />
             </div>
         </div>
