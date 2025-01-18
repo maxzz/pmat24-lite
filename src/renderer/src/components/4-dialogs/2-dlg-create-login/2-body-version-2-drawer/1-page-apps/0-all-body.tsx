c3cf69f4e51fd70eb15253927e27ae06c7c69c2b
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-17 18:37:55 -0800

    WIP. create-mani. GetTlwScreenshotsResult render. 4. add Page1Apps. 5

---
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/0-all-body.tsx
@@ -0,0 +1,20 @@
+import { ScrollArea } from "@/ui/shadcn";
+import { AppsGrid } from "./2-apps-grid";
+import { ButtonCreateFormSelector } from "@/components/4-dialogs/9-select-create-mani-type";
+
+export function Page1Apps() {
+    return (
+        <div className="-my-4 px-4 text-xs border-l border-border grid grid-rows-[1fr,auto] 1debug">
+
+            <div className="relative">
+                <div className="aboslute inset-0">
+                    <ScrollArea className="1h-full" fullHeight fixedWidth>
+                        <AppsGrid />
+                    </ScrollArea>
+                </div>
+            </div>
+
+            <ButtonCreateFormSelector triggerLabel="Create new manifest" />
+        </div>
+    );
+}
