9a797f83178e174fdcff440e857d5f582b9156d8
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-19 16:33:24 -0800

    WIP. create-mani. add RenaderTlwScreenshot

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/2-apps-grid.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-body-version-2-drawer/1-page-apps/2-apps-grid.tsx
@@ -1,54 +1,56 @@
 import { type ComponentProps } from "react";
-import reply from "@/assets/tests/25.01.16.25/TopLevelWindowsScreenshots.json";
-import { GetTlwScreenshotsResult } from "@shared/ipc-types";
+import { type GetTlwScreenshotsResult, type TlwScreenshot } from "@shared/ipc-types";
+import replyTest from "@/assets/tests/25.01.16.25/TopLevelWindowsScreenshots.json";
 import { classNames } from "@/utils";
 import { IconBrokenImage } from "@/ui/icons";
 
 // console.log("AppsGrid", reply);
 
 export function AppsGrid({ className, ...rest }: ComponentProps<"div">) {
-    const data = reply as GetTlwScreenshotsResult;
+    const data = replyTest as GetTlwScreenshotsResult;
     return (
         <div className={classNames("grid gap-4 grid-cols-[repeat(auto-fill,minmax(150px,1fr))] 1debug", className)} {...rest}>
-
             {data.map(
                 (item, idx) => {
-                    if (item.type === 'error') {
-                        // const res = null;
-                        const res = (
-                            <div className="pb-4 flex flex-col gap-1" key={idx}>
-                                <IconBrokenImage className="max-w-8" title="Broken image" />
-                                <div className="">Error. hwnd: {item.hwnd}, error code: "{item.errorCode}"</div>
-                            </div>
-                        );
-                        return res;
-                    }
-
-                    // const Img = data64ToImage(item.data);
-                    const dataUrl = `data:image/png;base64,${item.data}`;
-                    const res = (
-                        <div className="pb-4 flex flex-col gap-1" key={idx}>
-
-                            {/* <img className="w-full h-full" src={img} /> */}
-                            {/* <Img className="w-full h-full" /> */}
-                            {/* {Img} */}
-                            <img className="max-w-52" src={dataUrl} />
-                            <div className="text-xs font-semibold">{item.caption}</div>
-                        </div>
-                    );
-                    return res;
-
-                    // return (
-                    //     <div className="flex flex-col gap-1" key={idx}>
-                    //         <div className="text-xs font-semibold">{item.caption}</div>
-                    //         <div className="text-xs">{item.caption}</div>
-                    //     </div>
-                    // );
+                    return <RenaderTlwScreenshot item={item} idx={idx} />;
                 }
             )}
-
         </div>
     );
+}
+
+function RenaderTlwScreenshot({ item, idx }: { item: TlwScreenshot, idx: number; }) {
+    if (item.type === 'error') {
+        // const res = null;
+        const res = (
+            <div className="pb-4 flex flex-col gap-1" key={idx}>
+                <IconBrokenImage className="max-w-8" title="Broken image" />
+                <div className="">Error. hwnd: {item.hwnd}, error code: "{item.errorCode}"</div>
+            </div>
+        );
+        return res;
+    }
+
+    // const Img = data64ToImage(item.data);
+    const dataUrl = `data:image/png;base64,${item.data}`;
+    const res = (
+        <div className="pb-4 flex flex-col gap-1" key={idx}>
+
+            {/* <img className="w-full h-full" src={img} /> */}
+            {/* <Img className="w-full h-full" /> */}
+            {/* {Img} */}
+            <img className="max-w-52" src={dataUrl} />
+            <div className="text-xs font-semibold">{item.caption}</div>
+        </div>
+    );
+    return res;
+
+    // return (
+    //     <div className="flex flex-col gap-1" key={idx}>
+    //         <div className="text-xs font-semibold">{item.caption}</div>
+    //         <div className="text-xs">{item.caption}</div>
+    //     </div>
+    // );
 }
 
 function data64ToImage(data64: string): HTMLImageElement {
