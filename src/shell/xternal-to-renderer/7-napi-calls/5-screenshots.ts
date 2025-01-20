c101747591fb3c97af898709cd6955e3dec08e48
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-20 15:32:03 -0800

    WIP. create-mani. 5-screenshots.ts v1

--- src/shell/xternal-to-renderer/7-napi-calls/5-screenshots.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/5-screenshots.ts
@@ -1,50 +1,65 @@
 import { addon, type ImageFormatType, type TlwInfo } from '.';
 
+/**
+ * Demonstrates how to use Promise to get top-level Windows information from pmat_plugin_nodejs.node
+ */
 export class GetTopLevelWindowTest {
-    /// Demonstrates how to use Promise to get top-level Windows information from pmat_plugin_nodejs.node
     async getInfo(): Promise<TlwInfo[]> {
-        return new Promise<TlwInfo[]>((resolve: (arg0: TlwInfo[]) => void, reject: (arg0: string) => void) => {
-            addon.getTopLevelWindowsInfo('{}', (err: any, data: string) => {
-                if (err) {
-                    //console.error(err);
-                    reject(err);
-                }
-                else {
-                    //console.log(data);
-                    let iconContentObj = JSON.parse(data);
-                    resolve(iconContentObj);
-                }
-            });
-        });
+        return new Promise<TlwInfo[]>(
+            (resolve: (arg0: TlwInfo[]) => void, reject: (arg0: string) => void) => {
+                addon.getTopLevelWindowsInfo('{}',
+
+                    (err: any, data: string) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            try {
+                                resolve(JSON.parse(data));
+                            } catch (e) {
+                                reject(e instanceof Error ? e.message : e as string);
+                            }
+                        }
+                    }
+
+                );
+            }
+        );
     }
 }
-
+/*
 export async function test07_GetTopLevelWindowsInfo() {
     let getter = new GetTopLevelWindowTest();
     let infos = await getter.getInfo();
     return infos;
 }
+*/
 
-
+/**
+ * Demonstrates how to use Promise to get top-level Windows screenshots from pmat_plugin_nodejs.node
+*/
 export class GetTopLevelWindowsScreenshotsTest {
-    /// Demonstrates how to use Promise to get top-level Windows screenshots from pmat_plugin_nodejs.node
     async getScreenshots(windowsInfos: string): Promise<[Buffer, ImageFormatType]> {
-        return new Promise<[Buffer, ImageFormatType]>((resolve: (arg0: [Buffer, ImageFormatType]) => void, reject: (arg0: string) => void) => {
-            addon.getTopLevelWindowsScreenshots(windowsInfos, (err: any, data: string) => {
-                if (err) {
-                    //console.error(err);
-                    reject(err);
-                }
-                else {
-                    //console.log(data);
-                    let iconContentObj = JSON.parse(data);
-                    resolve(iconContentObj);
-                }
-            });
-        });
+        return new Promise<[Buffer, ImageFormatType]>(
+            (resolve: (arg0: [Buffer, ImageFormatType]) => void, reject: (arg0: string) => void) => {
+
+                addon.getTopLevelWindowsScreenshots(windowsInfos,
+                    (err: any, data: string) => {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            try {
+                                resolve(JSON.parse(data));
+                            } catch (e) {
+                                reject(e instanceof Error ? e.message : e as string);
+                            }
+                        }
+                    }
+                );
+
+            }
+        );
     }
 }
-
 /*
 export async function test08_GetTopLevelWindowsScreenshots(windowsInfos: TlwInfo[]) {
     const desiredWidth = 300;
