c3ad94ef2e744a5bf8cc82bfbf41aedfb4b65b87
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-21 16:49:19 -0800

    WIP. create-mani. RenaderTlwScreenshot selection on image vs item selection

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-create-ui/2-apps-grid/2-renader-tlw-data.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-create-ui/2-apps-grid/2-renader-tlw-data.tsx
@@ -10,11 +10,11 @@ export function RenderTwlData({ item, ...rest }: { item: TlwScreenshotInfo; } & 
     const tlwData = item.item as TlwData;
     return (
         <div
-            className={classNames("relative m-1 pb-4 flex flex-col gap-1", isSelected && itemSelectedClasses)}
+            className={classNames("relative m-1 pb-4 flex flex-col gap-1 overflow-hidden")}
             onClick={() => item.editor.selected = !isSelected}
             {...rest}
         >
-            <RenderData64 className="max-w-52" data64={tlwData.data} />
+            <RenderData64 className={classNames("m-1 max-w-52", isSelected && itemSelectedClasses)} data64={tlwData.data} />
 
             {isSelected
                 ? <CheckIcon className={checkboxClasses} />
@@ -31,7 +31,7 @@ const itemSelectedClasses = "ring-2 ring-sky-500 1ring-offset-1 ring-o rounded";
 
 const checkboxClasses = "\
 absolute left-2 top-1.5 p-1 size-6 \
-text-black bg-white border-2 border-black \
+text-black bg-white border-2 border-sky-500 \
 shadow-sm shadow-slate-500 \
 rounded-full";
 
