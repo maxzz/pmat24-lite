0cf54d4bccfb2cccf603e490f3afd9bdfa663a15
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-21 16:24:33 -0800

    WIP. create-mani. RenaderTlwScreenshot add selected state

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-create-ui/2-apps-grid/2-renader-tlw-data.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-create-ui/2-apps-grid/2-renader-tlw-data.tsx
@@ -1,14 +1,19 @@
 import { type ComponentProps } from "react";
 import { useSnapshot } from "valtio";
-import { TlwScreenshotInfo } from "@/store";
-import type { TlwData } from "@shared/ipc-types";
+import { type TlwScreenshotInfo } from "@/store";
+import { type TlwData } from "@shared/ipc-types";
+import { CheckIcon } from "lucide-react";
 
 export function RenderTwlData({ item, ...rest }: { item: TlwScreenshotInfo; } & ComponentProps<'div'>) {
     const isSelected = useSnapshot(item.editor).selected;
     const tlwData = item.item as TlwData;
     return (
-        <div className="pb-4 flex flex-col gap-1" {...rest}>
+        <div className="relative pb-4 flex flex-col gap-1" onClick={() => item.editor.selected = !isSelected} {...rest}>
             <RenderData64 className="max-w-52" data64={tlwData.data} />
+            {isSelected
+                ? <CheckIcon className="absolute left-3 top-2 p-1 size-6 text-black bg-white border-2 border-black rounded-full" />
+                : null
+            }
             <div className="text-xs font-semibold">{tlwData.caption}</div>
         </div>
     );
