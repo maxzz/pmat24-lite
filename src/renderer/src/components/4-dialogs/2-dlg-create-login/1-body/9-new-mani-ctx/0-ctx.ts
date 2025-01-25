120cb39ca583a048224bce47da5e310c0bc7b79f
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-24 18:42:51 -0800

    WIP. create-mani. ctx. add page and direction

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/9-new-mani-ctx/0-ctx.ts
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/9-new-mani-ctx/0-ctx.ts
@@ -2,45 +2,36 @@ import { atom, type Atom, type PrimitiveAtom } from "jotai";
 import { clamp } from "@/utils";
 import { wizardFirstPage, wizardLastPage, type WizardPage } from "./8-step-items-data";
 
-// export type NewManiCtx = {
-//     currentPageAtom: Atom<WizardPage>;  // current page
-//     appSelectedIdxAtom: PrimitiveAtom<number>;   // selected application index
-// };
-
 class NewManiCtx {
     currentPageAtom: Atom<WizardPage>;  // current page
     appSelectedIdxAtom: PrimitiveAtom<number>;   // selected application index
 
     constructor() {
-        this.currentPageAtom = atom((get) => get(_currentPageAtom));
+        this.currentPageAtom = atom((get) => {
+            return get(_currentPageAtom)[0];
+        });
         this.appSelectedIdxAtom = atom(0);
     }
 }
 
 export const newManiCtx: NewManiCtx = new NewManiCtx();
 
-// function createNewManiCtx(): NewManiCtx {
-//     const rv = {
-//         currentPageAtom: atom((get) => get(_currentPageAtom)),
-//         appSelectedIdxAtom: atom(0),
-//     };
-//     return rv;
-// }
+type CurrentPage = [page: WizardPage, direction: number];
 
-const _currentPageAtom = atom<WizardPage>(wizardFirstPage);
+const _currentPageAtom = atom<[page: WizardPage, direction: number]>([wizardFirstPage, 0]);
 
-export const doSetWizardPageAtom = atom(
-    null,
-    (get, set, page: WizardPage) => {
-        set(_currentPageAtom, page);
-    }
-);
+// export const doSetWizardPageAtom = atom(
+//     null,
+//     (get, set, page: WizardPage) => {
+//         set(_currentPageAtom, page);
+//     }
+// );
 
 export const doMoveWizardPageAtom = atom(
     null,
-    (get, set, {next}:{next: boolean}) => {
-        const currentPage = get(_currentPageAtom);
+    (get, set, { next }: { next: boolean; }) => {
+        const currentPage = get(_currentPageAtom)[0];
         const page = clamp(currentPage + (next ? 1 : -1), wizardFirstPage, wizardLastPage);
-        set(_currentPageAtom, page);
+        set(_currentPageAtom, [page, next ? 1 : -1]);
     }
 );
