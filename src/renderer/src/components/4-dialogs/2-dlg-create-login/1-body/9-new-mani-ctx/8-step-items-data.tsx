9df7ee3e0ddb2cf34a3d02febf54a8008e8ed2f8
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-25 14:54:20 -0800

    WIP. create-mani. ctx. update stepInfo. 2

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/9-new-mani-ctx/8-step-items-data.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/9-new-mani-ctx/8-step-items-data.tsx
@@ -12,12 +12,12 @@ export const wizardLastPage = WizardPage.save;
 
 // Data
 
-export const stepInfo: [title: string, explanation: string][] = [
-    ['Application window', 'Select the application window for training.'],
-    ['Fields and submit', 'Set up new form fields and submission method.'],
-    ['Options', 'Configure the new manifest settings.'],
-    ['Save', 'Name the manifest and save the file.'],
-];
+export const stepInfo: { [key in WizardPage]: [string, string] } = {
+    [WizardPage.apps]: ['Application window', 'Select the application window for training.'],
+    [WizardPage.fields]: ['Fields and submit', 'Set up new form fields and submission method.'],
+    [WizardPage.options]: ['Options', 'Configure the new manifest settings.'],
+    [WizardPage.save]: ['Save', 'Name the manifest and save the file.'],
+};
 
 const explanationClasses = "text-balance text-xs";
 
@@ -25,28 +25,28 @@ export const stepItems: StepItem[] = [
     {
         label: (
             <div className="flex flex-col gap-1">
-                <div className="text-sm font-semibold"><span className="text-foreground/50">1. </span>{stepInfo[WizardPage.apps][0]}</div>
+                <div className="text-sm font-semibold"><span className="text-foreground/50">1.{' '}</span>{stepInfo[WizardPage.apps][0]}</div>
                 <div className={explanationClasses}>{stepInfo[WizardPage.apps][1]}</div>
             </div>
         )
     }, {
         label: (
             <div className="flex flex-col gap-1">
-                <div className="font-semibold"><span className="text-foreground/50">2. </span>{stepInfo[WizardPage.fields][0]}</div>
+                <div className="font-semibold"><span className="text-foreground/50">2.{' '}</span>{stepInfo[WizardPage.fields][0]}</div>
                 <div className={explanationClasses}>{stepInfo[WizardPage.fields][1]}</div>
             </div>
         )
     }, {
         label: (
             <div className="flex flex-col gap-1">
-                <div className="font-semibold"><span className="text-foreground/50">3. </span>{stepInfo[WizardPage.options][0]}</div>
+                <div className="font-semibold"><span className="text-foreground/50">3.{' '}</span>{stepInfo[WizardPage.options][0]}</div>
                 <div className={explanationClasses}>{stepInfo[WizardPage.options][1]}</div>
             </div>
         )
     }, {
         label: (
             <div className="flex flex-col gap-1">
-                <div className="font-semibold"><span className="text-foreground/50">4. </span>{stepInfo[WizardPage.save][0]}</div>
+                <div className="font-semibold"><span className="text-foreground/50">4.{' '}</span>{stepInfo[WizardPage.save][0]}</div>
                 <div className={explanationClasses}>{stepInfo[WizardPage.save][1]}</div>
             </div>
         )
