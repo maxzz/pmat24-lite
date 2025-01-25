3089eec2f002f82c34da097bc4707c5d2982ddb2
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-25 14:28:06 -0800

    WIP. create-mani. ctx. PageWrapper. slide effect tweaking

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/1-wizard-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/1-wizard-body.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from "react";
 import { useAtomValue } from "jotai";
-import { AnimatePresence, motion } from "framer-motion"; //https://codesandbox.io/p/sandbox/framer-motion-image-gallery-pqvx3
+import { AnimatePresence, motion, MotionConfig } from "framer-motion"; //https://codesandbox.io/p/sandbox/framer-motion-image-gallery-pqvx3
 import { LeftPanelProgress, WizardBottomButtons } from "../../8-create-ui";
 import { newManiCtx, WizardPage } from "../../9-new-mani-ctx";
 import { Page1AppsBody } from "../1-page-apps";
@@ -10,25 +10,37 @@ import { Page4SaveBody } from "../4-page-save";
 
 export function WizardBody() {
     const currentStep = useAtomValue(newManiCtx.currentPageAtom);
+    const [page, direction] = useAtomValue(newManiCtx.pageAndDirectionAtom);
     return (
         <div className="h-full grid grid-cols-[auto_1fr]">
             <LeftPanelProgress className="p-4 bg-muted border-r border-foreground/20" />
 
-            <div className="h-full grid grid-rows-[1fr_auto] overflow-hidden">
-                <AnimatePresence initial={false} mode="wait">
-                    {currentStep === WizardPage.apps && <PageWrapper currentStep={currentStep} thisStep={WizardPage.apps} key={WizardPage.apps}><Page1AppsBody /></PageWrapper>}
-                    {currentStep === WizardPage.fields && <PageWrapper currentStep={currentStep} thisStep={WizardPage.fields} key={WizardPage.fields}><Page2FieldsBody /></PageWrapper>}
-                    {currentStep === WizardPage.options && <PageWrapper currentStep={currentStep} thisStep={WizardPage.options} key={WizardPage.options}><Page3OptionsBody /></PageWrapper>}
-                    {currentStep === WizardPage.save && <PageWrapper currentStep={currentStep} thisStep={WizardPage.save} key={WizardPage.save}><Page4SaveBody /></PageWrapper>}
-                </AnimatePresence>
+            <MotionConfig transition={TRANSITION}>
+                <div className="h-full grid grid-rows-[1fr_auto] overflow-hidden">
 
-                {/* <ButtonCreateFormSelector triggerLabel="Create new manifest" /> */}
+                    <div className="flex">
+                        <AnimatePresence initial={false} custom={direction}>
+                            {currentStep === WizardPage.apps && <PageWrapper direction={direction} currentStep={currentStep} thisStep={WizardPage.apps} key={WizardPage.apps}><Page1AppsBody /></PageWrapper>}
+                            {currentStep === WizardPage.fields && <PageWrapper direction={direction} currentStep={currentStep} thisStep={WizardPage.fields} key={WizardPage.fields}><Page2FieldsBody /></PageWrapper>}
+                            {currentStep === WizardPage.options && <PageWrapper direction={direction} currentStep={currentStep} thisStep={WizardPage.options} key={WizardPage.options}><Page3OptionsBody /></PageWrapper>}
+                            {currentStep === WizardPage.save && <PageWrapper direction={direction} currentStep={currentStep} thisStep={WizardPage.save} key={WizardPage.save}><Page4SaveBody /></PageWrapper>}
+                        </AnimatePresence>
+                    </div>
 
-                <WizardBottomButtons className="my-4" />
-            </div>
+                    {/* <ButtonCreateFormSelector triggerLabel="Create new manifest" /> */}
+
+                    <WizardBottomButtons className="my-4" />
+                </div>
+            </MotionConfig>
         </div>
     );
 }
+
+const TRANSITION = {
+    type: 'spring',
+    bounce: 0.05,
+    duration: 0.3,
+};
 
 const variants = {
     enter: (direction: number) => {
@@ -53,18 +65,20 @@ const variants = {
 
 //TODO: add loader after some time
 
-function PageWrapper({ children, currentStep, thisStep }: { children: ReactNode; currentStep: WizardPage; thisStep: WizardPage; }) {
-    const direction = currentStep >= thisStep ? '-100%' : '100%';
+function PageWrapper({ children, currentStep, thisStep, direction }: { children: ReactNode; currentStep: WizardPage; thisStep: WizardPage; direction: number; }) {
+    // const direction = currentStep >= thisStep ? '-100%' : '100%';
     return (
         <motion.div
+            custom={direction}
             variants={variants}
             initial="enter"
             animate="center"
             exit="exit"
-            transition={{
-                x: { type: "spring", stiffness: 300, damping: 30 },
-                opacity: { duration: 0.2 }
-            }}
+            // transition={{
+            //     x: { type: "spring", stiffness: 300, damping: 30 },
+            //     opacity: { duration: 2.2 }
+            // }}
+            className="w-full shrink-0"
 
         // initial={{ opacity: 1, x: direction }}
         // animate={{ opacity: 1, x: 0 }}
