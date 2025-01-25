d2c6cf9d7c1f5701afc7816deed7e5025ffbaf2f
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-25 15:07:12 -0800

    WIP. create-mani. ctx. update RenderTwlData

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/1-page-apps/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/1-page-apps/0-all-body.tsx
@@ -4,15 +4,14 @@ import { doSetScreenshotsAtom } from "@/store";
 import { ButtonCreateFormSelector } from "../../8-create-ui";
 import { AppsGrid } from "./1-apps-grid";
 import { ScrollArea } from "@/ui/shadcn";
+import { stepInfo, WizardPage } from "../../9-new-mani-ctx";
 
 export function Page1AppsBody() {
     // const doSetScreenshots = useSetAtom(doSetScreenshotsAtom);
     // useState(() => doSetScreenshots(300));
     return (
         <div className="h-full text-xs grid grid-rows-[auto,1fr,auto]">
-            <div className="px-2 py-1 text-sm bg-muted/30">
-                Select the window for which you want to create a manifest.
-            </div>
+            <PageHeader page={WizardPage.apps} />
 
             <div className="relative h-full w-full">
                 <div className="absolute inset-0">
@@ -25,6 +24,15 @@ export function Page1AppsBody() {
             <div className="px-1 py-2 bg-muted/30 border-t border-border">
                 <ButtonCreateFormSelector triggerLabel="Create new manifest" />
             </div>
+        </div>
+    );
+}
+
+function PageHeader({ page }: { page: WizardPage; }) {
+    return (
+        <div className="px-4 py-3 text-sm bg-muted/30 flex flex-col gap-1">
+            <div className="text-sm font-semibold">{stepInfo[page][0]}</div>
+            <div className="text-xs text-foreground/50">{stepInfo[page][1]}</div>
         </div>
     );
 }
