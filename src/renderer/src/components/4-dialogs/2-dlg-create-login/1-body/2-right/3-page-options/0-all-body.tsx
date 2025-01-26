b7732db9b9354518a67df8c3a103498ea9512305
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-25 17:45:56 -0800

    WIP. create-mani. ctx. propagate WizardPageHeader to all wizard pages

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/3-page-options/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/3-page-options/0-all-body.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { useSetAtom } from "jotai";
 import { doSetScreenshotsAtom } from "@/store";
-import { ButtonCreateFormSelector } from "../../8-create-ui";
+import { ButtonCreateFormSelector, WizardPageHeader } from "../../8-create-ui";
 import { ScrollArea } from "@/ui/shadcn";
 import { AppsGrid } from "../1-page-apps/1-apps-grid";
+import { WizardPage } from "../../9-new-mani-ctx";
 
 export function Page3OptionsBody() {
     // const doSetScreenshots = useSetAtom(doSetScreenshotsAtom);
     // useState(() => doSetScreenshots(300));
     return (
         <div className="h-full text-xs bg-yellow-300 grid grid-rows-[auto,1fr,auto]">
-            <div className="px-2 py-1 text-sm bg-muted/30">
-                Name the manifest and save the file.
-            </div>
+            <WizardPageHeader page={WizardPage.options} />
 
             <div className="relative h-full w-full">
                 <div className="absolute inset-0">
--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/4-page-save/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/4-page-save/0-all-body.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
 import { useSetAtom } from "jotai";
 import { doSetScreenshotsAtom } from "@/store";
-import { ButtonCreateFormSelector } from "../../8-create-ui";
+import { ButtonCreateFormSelector, WizardPageHeader } from "../../8-create-ui";
 import { ScrollArea } from "@/ui/shadcn";
 import { AppsGrid } from "../1-page-apps/1-apps-grid";
+import { WizardPage } from "../../9-new-mani-ctx";
 
 export function Page4SaveBody() {
     // const doSetScreenshots = useSetAtom(doSetScreenshotsAtom);
     // useState(() => doSetScreenshots(300));
     return (
         <div className="h-full text-xs bg-green-300 grid grid-rows-[auto,1fr,auto]">
-            <div className="px-2 py-1 text-sm bg-muted/30">
-                Select the login window for which you will create a manifest.
-            </div>
+            <WizardPageHeader page={WizardPage.save} />
 
             <div className="relative h-full w-full">
                 <div className="absolute inset-0">
