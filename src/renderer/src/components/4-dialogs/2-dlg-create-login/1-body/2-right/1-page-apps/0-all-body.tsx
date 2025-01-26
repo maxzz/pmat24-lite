736e656edb0c6abb62e0f305d5bf572e2cff2406
Max Zakharzhevskiy <maxzz@msn.com>
2025-01-25 17:43:01 -0800

    WIP. create-mani. ctx. add WizardPageHeader

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/1-page-apps/0-all-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/1-page-apps/0-all-body.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { useSetAtom } from "jotai";
 import { doSetScreenshotsAtom } from "@/store";
-import { ButtonCreateFormSelector } from "../../8-create-ui";
+import { ButtonCreateFormSelector, WizardPageHeader } from "../../8-create-ui";
 import { AppsGrid } from "./1-apps-grid";
 import { ScrollArea } from "@/ui/shadcn";
-import { stepInfo, WizardPage } from "../../9-new-mani-ctx";
+import { WizardPage } from "../../9-new-mani-ctx";
+
 
 export function Page1AppsBody() {
     // const doSetScreenshots = useSetAtom(doSetScreenshotsAtom);
     // useState(() => doSetScreenshots(300));
     return (
         <div className="h-full text-xs grid grid-rows-[auto,1fr,auto]">
-            <PageHeader page={WizardPage.apps} />
+            <WizardPageHeader page={WizardPage.apps} />
 
             <div className="relative h-full w-full">
                 <div className="absolute inset-0">
@@ -24,16 +25,6 @@ export function Page1AppsBody() {
             <div className="px-1 py-2 bg-muted/30 border-t border-border">
                 <ButtonCreateFormSelector triggerLabel="Create new manifest" />
             </div>
-        </div>
-    );
-}
-
-function PageHeader({ page }: { page: WizardPage; }) {
-    const [title, explanation] = stepInfo[page];
-    return (
-        <div className="px-4 py-3 text-sm bg-muted/30 flex flex-col gap-1">
-            <div className="text-sm font-semibold">{title}</div>
-            <div className="text-xs text-foreground/50">{explanation}</div>
         </div>
     );
 }
---
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/8-create-ui/3-page-header.tsx
@@ -0,0 +1,11 @@
+import { WizardPage, stepInfo } from "../9-new-mani-ctx";
+
+export function WizardPageHeader({ page }: { page: WizardPage; }) {
+    const [title, explanation] = stepInfo[page];
+    return (
+        <div className="px-4 py-3 text-sm bg-muted/30 flex flex-col gap-1">
+            <div className="text-sm font-semibold">{title}</div>
+            <div className="text-xs text-foreground/50">{explanation}</div>
+        </div>
+    );
+}
