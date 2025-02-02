643f49f9c22ac7a44e97c060986363388d42f11a
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-01 17:52:33 -0800

    WIP. build. update tsconfig and vite files

--- electron.vite.config-browser.mts
+++ electron.vite.config-browser.mts
@@ -2,21 +2,53 @@ import { join, resolve } from 'path';
 import { defineConfig, type PluginOption, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
-
 //console.log('------ electron.vite.config-browser.ts:__dirname =', __dirname);
 
-// const dd = [
-//     "@babel/runtime",
-//     "highlight",
-//     "lowlight",
-//     "prismjs",
-//     "refractor",
-// ]
+export default defineConfig((): UserConfig => { // https://vitejs.dev/config
+    return {
+        base: '',
+        root: resolve(__dirname, 'src/renderer'),
+        // build: { rollupOptions: { input: resolve(__dirname, 'src/renderer/index.html'), }, },
+
+        resolve: {
+            alias: {
+                "@shared": resolve(__dirname, 'src/shared'),
+                '@electron': resolve(__dirname, 'src/electron/app'),
+                "@shell": resolve(__dirname, 'src/shell/app'),
+
+                '@': resolve(__dirname, 'src/renderer/src'),
+                '@ui': resolve(__dirname, 'src/renderer/src/ui'),
+                '@renderer': resolve(__dirname, 'src/renderer/src'),
+            }
+        },
+
+        plugins: [
+            react(),
+            visualizer({
+                filename: 'visualization.html',
+                template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
+                gzipSize: true,
+                brotliSize: true,
+            }) as PluginOption,
+        ],
+
+        build: {
+            rollupOptions: { output: { manualChunks, } }, // minify: false,
+        },
+
+        server: { port: 3000, },
+    };
+});
 
 function manualChunks(id: string) { //https://rollupjs.org/configuration-options/#output-manualchunks
-    // if (dd.some((name) => id.includes(name)) ) {
-    //     return "rare";
-    // }
+    // const dd = [
+    //     "@babel/runtime",
+    //     "highlight",
+    //     "lowlight",
+    //     "prismjs",
+    //     "refractor",
+    // ]
+
     if (id.includes("react-dom")) {
         return "vendor-dom";
     }
@@ -33,51 +65,3 @@ function manualChunks(id: string) { //https://rollupjs.org/configuration-options
         return "rare";
     }
 }
-
-// https://vitejs.dev/config
-export default defineConfig((): UserConfig => {
-    return {
-        base: '',
-        root: resolve(__dirname, 'src/renderer'),
-        // build: {
-        //     rollupOptions: {
-        //         input: resolve(__dirname, 'src/renderer/index.html'),
-        //     },
-        // },
-
-        resolve: {
-            alias: {
-                '@': resolve(__dirname, 'src/renderer/src'),
-                '@ui': resolve(__dirname, 'src/renderer/src/ui'),
-                '@renderer': resolve(__dirname, 'src/renderer/src'),
-                '@electron': resolve(__dirname, 'src/electron/app'),
-                "@shared": resolve(__dirname, 'src/shared'),
-                "@shell": resolve(__dirname, 'src/shell/app'),
-            }
-        },
-
-        plugins: [
-            react(),
-            visualizer({
-                filename: 'visualization.html',
-                template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
-                gzipSize: true,
-                brotliSize: true,
-            })  as PluginOption,
-        ],
-
-        build: {
-            // minify: false,
-            rollupOptions: {
-                output: {
-                    manualChunks,
-                }
-            }
-        },
-    
-        server: {
-            port: 3000,
-        },
-
-    };
-});
--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,13 +1,62 @@
 import { join, resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
+//console.log('------ electron.vite.config.ts:__dirname =', __dirname);
 
-console.log('------ electron.vite.config.ts:__dirname =', __dirname);
+export default defineConfig({
+    main: {
+        build: {
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, 'src/shell/app/index.ts'),
+                },
+                // output: { dir: join(__dirname, 'dist-electron/main'), },
+                external: [
+                    'pmat_plugin_nodejs.node',
+                ]
+            }
+        },
+        resolve: {
+            alias: {
+                "@shared": resolve(__dirname, 'src/shared'),
+                '@electron': resolve(__dirname, 'src/shell/app'),
+                "@shell": resolve(__dirname, 'src/shell/app'),
+            }
+        },
+        plugins: [externalizeDepsPlugin()]
+    },
+
+    preload: {
+        build: {
+            rollupOptions: {
+                input: {
+                    index: resolve(__dirname, 'src/shell/preload/index.ts')
+                },
+                // output: { dir: join(__dirname, 'dist-electron/preload'), }
+            }
+        },
+        plugins: [externalizeDepsPlugin()]
+    },
+
+    renderer: {
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src/renderer/src'),
+                '@ui': resolve(__dirname, 'src/renderer/src/ui'),
+                '@renderer': resolve(__dirname, 'src/renderer/src'),
+                '@electron': resolve(__dirname, 'src/shell/app'),
+                "@shared": resolve(__dirname, 'src/shared'),
+                "@shell": resolve(__dirname, 'src/shell/app'),
+            }
+        },
+        build: {
+            rollupOptions: { output: { manualChunks, } }, // minify: false,
+        },
+        plugins: [react()]
+    }
+});
 
 function manualChunks(id: string) { //https://rollupjs.org/configuration-options/#output-manualchunks
-    // if (dd.some((name) => id.includes(name)) ) {
-    //     return "rare";
-    // }
     if (id.includes("react-dom")) {
         return "vendor-dom";
     }
@@ -24,63 +73,3 @@ function manualChunks(id: string) { //https://rollupjs.org/configuration-options
         return "rare";
     }
 }
-
-export default defineConfig({
-    main: {
-        build: {
-            rollupOptions: {
-                input: {
-                    index: resolve(__dirname, 'src/shell/app/index.ts'),
-                },
-                // output: {
-                //     dir: join(__dirname, 'dist-electron/main'),
-                // },
-                external: [
-                    'pmat_plugin_nodejs.node',
-                ]
-            }
-        },
-        resolve: {
-            alias: {
-                '@electron': resolve(__dirname, 'src/shell/app'),
-                "@shared": resolve(__dirname, 'src/shared'),
-                "@shell": resolve(__dirname, 'src/shell/app'),
-            }
-        },
-        plugins: [externalizeDepsPlugin()]
-    },
-    preload: {
-        build: {
-            rollupOptions: {
-                input: {
-                    index: resolve(__dirname, 'src/shell/preload/index.ts')
-                },
-                // output: {
-                //     dir: join(__dirname, 'dist-electron/preload'),
-                // }
-            }
-        },
-        plugins: [externalizeDepsPlugin()]
-    },
-    renderer: {
-        resolve: {
-            alias: {
-                '@': resolve(__dirname, 'src/renderer/src'),
-                '@ui': resolve(__dirname, 'src/renderer/src/ui'),
-                '@renderer': resolve(__dirname, 'src/renderer/src'),
-                '@electron': resolve(__dirname, 'src/shell/app'),
-                "@shared": resolve(__dirname, 'src/shared'),
-                "@shell": resolve(__dirname, 'src/shell/app'),
-            }
-        },
-        build: {
-            // minify: false,
-            rollupOptions: {
-                output: {
-                    manualChunks,
-                }
-            }
-        },
-        plugins: [react()]
-    }
-});
