ea94f47390128e47fbf5663f1461f2f61bd7e4d2
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-02 16:08:39 -0800

    WIP. create-mani. ctx. WizardBody. new layout. 4. done w/ header

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/1-wizard-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/1-wizard-body.tsx
@@ -3,17 +3,23 @@ import * as D from "@/ui/shadcn/dialog";
 import { LeftPanelProgress, WizardBottomButtons } from "../../8-create-ui";
 import { doOpenDrawerAtom } from "@/store";
 import { PagesBody } from "./2-pages-body";
+import { Button } from "@/ui";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 export function WizardBody() {
     const [doOpenDrawer, setDoOpenDrawer] = useAtom(doOpenDrawerAtom);
     return (
-        <div className="h-full">
-            <D.DialogHeader className="relative text-base font-bold flex items-center">
+        <div className="h-full flex flex-col">
+            <D.DialogHeader className="relative text-base font-bold border-b border-foreground/20 flex items-center">
                 <D.DialogTitle asChild>
-                    <div className="py-4">New manifest</div>
+                    <div className="py-3">New manifest</div>
                 </D.DialogTitle>
 
-                <D.DialogCloseButton onClick={() => setDoOpenDrawer(false)} tabIndex={-1} />
+                <Button className="absolute py-4 right-2 top-0 0 hover:text-white hover:bg-red-500" variant="ghost" size="xs" tabIndex={-1} onClick={() => setDoOpenDrawer(false)}>
+                    <Cross2Icon className="size-4" />
+                </Button>
+
+                {/* <D.DialogCloseButton className="top-2" onClick={() => setDoOpenDrawer(false)} tabIndex={-1} /> */}
             </D.DialogHeader>
 
             <div className="h-full grid grid-cols-[auto_1fr]">
@@ -21,7 +27,7 @@ export function WizardBody() {
                 <PagesBody />
             </div>
 
-            <WizardBottomButtons className="my-4" />
+            <WizardBottomButtons className="py-3 border-t border-foreground/20" />
         </div>
     );
 }
