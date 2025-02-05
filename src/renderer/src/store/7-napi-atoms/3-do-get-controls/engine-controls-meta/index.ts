7f344d375c6518e09d614c185b25f6fb944228f3
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-04 19:15:33 -0800

    WIP. create-mani. ctx. update 7-napi-atoms layout. 5 reduce shared scope

--- src/renderer/src/store/7-napi-atoms/3-do-get-controls/engine-controls-meta/index.ts
+++ src/renderer/src/store/7-napi-atoms/3-do-get-controls/engine-controls-meta/index.ts
@@ -16,57 +16,57 @@ export function controlsReplyToEngineControlWithMeta(reply: WindowControlsCollec
     };
 
     return rv;
+}
 
-    function addMetaToEngineControls(pool: string[], controls: EngineControl[]): EngineControlWithMeta[] {
-        const rv = controls.map((control) => {
-            const path = FieldPath.fieldPathItems(pool, control.path);
-            const rect = getControlTaretRect(path.loc);
-            const role = getRole(path.p4 || path.p4a);
-            const item = {
-                control,
-                meta: {
-                    uuid: uuid.asRelativeNumber(),
-                    path,
-                    ...(rect && { rect }),
-                    ...(role && { role }),
-                }
-            };
-            return item;
-        });
-        return rv;
-    }
-
-    function getRole(p4a: MPath.p4a[] | undefined): RoleStateNames | undefined {
-        if (!p4a?.length) {
-            return;
-        }
-        const lastP4a = p4a.at(-1);
-        const parts = lastP4a?.roleString?.split('_');
-
-        if (!lastP4a?.roleString || !parts?.length || !parts[0]) {
-            return;
-        }
-
-        const roleNum = parseInt(parts[0], 16);
-        const roleName = MSAA_ROLE[roleNum];
-        const stateNum = parts[1] || 0;
-
-        return {
-            raw: lastP4a?.roleString,
-            role: roleName,
-            states: [`${stateNum}`],
+function addMetaToEngineControls(pool: string[], controls: EngineControl[]): EngineControlWithMeta[] {
+    const rv = controls.map((control) => {
+        const path = FieldPath.fieldPathItems(pool, control.path);
+        const rect = getControlTaretRect(path.loc);
+        const role = getRole(path.p4 || path.p4a);
+        const item = {
+            control,
+            meta: {
+                uuid: uuid.asRelativeNumber(),
+                path,
+                ...(rect && { rect }),
+                ...(role && { role }),
+            }
         };
+        return item;
+    });
+    return rv;
+}
+
+function getRole(p4a: MPath.p4a[] | undefined): RoleStateNames | undefined {
+    if (!p4a?.length) {
+        return;
+    }
+    const lastP4a = p4a.at(-1);
+    const parts = lastP4a?.roleString?.split('_');
+
+    if (!lastP4a?.roleString || !parts?.length || !parts[0]) {
+        return;
     }
 
-    function getControlTaretRect(pathLoc: string | undefined): TargetClientRect | undefined {
-        const loc = FieldPath.loc.getControlRect(pathLoc);
-        if (loc) {
-            return {
-                left: loc[0],      // x1.x
-                top: loc[1],       // x1.y
-                right: loc[2],     // x2.x
-                bottom: loc[3],    // x2.y
-            };
-        }
+    const roleNum = parseInt(parts[0], 16);
+    const roleName = MSAA_ROLE[roleNum];
+    const stateNum = parts[1] || 0;
+
+    return {
+        raw: lastP4a?.roleString,
+        role: roleName,
+        states: [`${stateNum}`],
+    };
+}
+
+function getControlTaretRect(pathLoc: string | undefined): TargetClientRect | undefined {
+    const loc = FieldPath.loc.getControlRect(pathLoc);
+    if (loc) {
+        return {
+            left: loc[0],      // x1.x
+            top: loc[1],       // x1.y
+            right: loc[2],     // x2.x
+            bottom: loc[3],    // x2.y
+        };
     }
 }
