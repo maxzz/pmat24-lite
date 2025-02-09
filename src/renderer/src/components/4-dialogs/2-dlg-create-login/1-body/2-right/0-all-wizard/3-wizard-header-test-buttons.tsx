93b0df6b53c3f7581b32f5f6c0d2ad11e7399d8e
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-09 15:46:07 -0800

    WIP. create-mani. ctx. atoms. add DebugButtons. 5. add state types

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/3-wizard-header-test-buttons.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/0-all-wizard/3-wizard-header-test-buttons.tsx
@@ -3,8 +3,26 @@ import { atom, useAtom } from "jotai";
 import { classNames } from "@/utils";
 import { Label, RadioGroup, RadioGroupItem } from "@/ui";
 
-type TestScreenEnum = 'none' | 'A' | 'B'; // Test screenshots collection
-type TestAppEnum = 'none' | 'win32' | 'web'; // New manifest test content
+// const testScreens = ['none', 'A', 'B'];
+// const testApps = ['none', 'win32', 'web'];
+
+// type TestScreenEnum = 'none' | 'A' | 'B'; 
+// type TestAppEnum = 'none' | 'win32' | 'web'; 
+
+const testScreen2 = { // Test screenshots collection
+    A: 'A',
+    B: 'B',
+    none: 'none',
+};
+
+const testApp2 = { // New manifest test content
+    win32: 'win32',
+    web: 'web',
+    none: 'none',
+};
+
+type TestScreenEnum = keyof typeof testScreen2; // Test screenshots collection
+type TestAppEnum = keyof typeof testApp2; // New manifest test content
 
 const testScreenAtom = atom<TestScreenEnum>('none');
 const testAppAtom = atom<TestAppEnum>('none');
@@ -17,16 +35,16 @@ export function DebugButtons({ className, ...rest }: ComponentPropsWithoutRef<'d
 
             screen:
             <RadioGroup className="grid-cols-subgrid col-span-3" value={testScreen} onValueChange={(v) => setTestScreen(v as TestScreenEnum)}>
-                <Label className={classNames("col-start-1", labelClasses)}> <RadioGroupItem value="A" /> A </Label>
-                <Label className={classNames("col-start-2", labelClasses)}> <RadioGroupItem value="B" /> B </Label>
-                <Label className={classNames("col-start-3", labelClasses)}> <RadioGroupItem value="none" /> none </Label>
+                <Label className={classNames("col-start-1", labelClasses)}> <RadioGroupItem value={testScreen2.A} /> {testScreen2.A} </Label>
+                <Label className={classNames("col-start-2", labelClasses)}> <RadioGroupItem value={testScreen2.B} /> {testScreen2.B} </Label>
+                <Label className={classNames("col-start-3", labelClasses)}> <RadioGroupItem value={testScreen2.none} /> {testScreen2.none} </Label>
             </RadioGroup>
 
             content:
             <RadioGroup className="grid-cols-subgrid col-span-3" value={testApp} onValueChange={(v) => setTestApp(v as TestAppEnum)}>
-                <Label className={classNames("", labelClasses)}> <RadioGroupItem value="win32" /> win32 </Label>
-                <Label className={classNames("", labelClasses)}> <RadioGroupItem value="web" /> web </Label>
-                <Label className={classNames("", labelClasses)}> <RadioGroupItem value="none" /> none </Label>
+                <Label className={classNames("", labelClasses)}> <RadioGroupItem value={testApp2.win32} /> {testApp2.win32} </Label>
+                <Label className={classNames("", labelClasses)}> <RadioGroupItem value={testApp2.web} /> {testApp2.web} </Label>
+                <Label className={classNames("", labelClasses)}> <RadioGroupItem value={testApp2.none} /> {testApp2.none} </Label>
             </RadioGroup>
 
         </div>
