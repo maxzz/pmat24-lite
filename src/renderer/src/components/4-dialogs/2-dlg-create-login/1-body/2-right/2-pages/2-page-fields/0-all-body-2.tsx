3dcef8ed697ca670ead85ed2a38e42d429124687
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-14 19:03:32 -0800

    WIP. create-mani. ctx. atoms. add to Page2FieldsBody BodyRawXml

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/2-pages/2-page-fields/0-all-body-2.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/2-right/2-pages/2-page-fields/0-all-body-2.tsx
@@ -7,31 +7,36 @@ import { newManiCtx, WizardPage } from "../../../0-new-mani-ctx";
 import { WizardPageHeader } from "../../../8-create-ui";
 
 export function Page2FieldsBody() {
-    const maniXml = useAtomValue(newManiCtx.maniXmlAtom);
-    const { showUiHeader } = useSnapshot(appSettings.appUi.uiAdvanced);
     return (
         <div className="h-full text-xs bg-sky-300 grid grid-rows-[auto,1fr,auto]">
             <WizardPageHeader page={WizardPage.fields} />
 
             <div className="relative size-full">
                 <div className="absolute inset-0">
-
-                    {showUiHeader && (
-                        <Button
-                            className="absolute right-4 top-0.5 active:scale-y-95 z-10" variant="ghost"
-                            onClick={() => navigator.clipboard.writeText(maniXml || '')}
-                        >
-                            <Copy className="size-4" />
-                        </Button>
-                    )}
-
-                    <ScrollArea className="px-2 py-1 size-full" fullHeight fixedWidth>
-                        <div className="whitespace-pre">
-                            {maniXml || ''}
-                        </div>
-                    </ScrollArea>
+                    <BodyRawXml />
                 </div>
             </div>
         </div>
     );
+}
+
+function BodyRawXml() {
+    const maniXml = useAtomValue(newManiCtx.maniXmlAtom);
+    const { showUiHeader } = useSnapshot(appSettings.appUi.uiAdvanced);
+    return (<>
+        {showUiHeader && (
+            <Button
+                className="absolute right-4 top-0.5 active:scale-y-95 z-10" variant="ghost"
+                onClick={() => navigator.clipboard.writeText(maniXml || '')}
+            >
+                <Copy className="size-4" />
+            </Button>
+        )}
+
+        <ScrollArea className="px-2 py-1 size-full" fullHeight fixedWidth>
+            <div className="whitespace-pre">
+                {maniXml || ''}
+            </div>
+        </ScrollArea>
+    </>);
 }
