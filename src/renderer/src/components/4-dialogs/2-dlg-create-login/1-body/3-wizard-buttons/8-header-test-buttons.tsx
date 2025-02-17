eff0c161c34375a5e80776db3f45aab768ae44bc
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-16 19:37:03 -0800

    WIP. create-mani. ctx. atoms. add DelayInput multiplier

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/3-wizard-buttons/8-header-test-buttons.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/3-wizard-buttons/8-header-test-buttons.tsx
@@ -14,7 +14,7 @@ export function DebugButtons({ className, ...rest }: ComponentPropsWithoutRef<'d
     const doLoadFakeMani = useSetAtom(doLoadFakeManiAtom);
     const doDissmissNextToasts = useSetAtom(doDissmissNextToastsAtom);
 
-    const { testCreateAppsDelay, testCreateManiDelay } = useSnapshot(appSettings.appUi.uiAdvanced);
+    // const { testCreateAppsDelay, testCreateManiDelay } = useSnapshot(appSettings.appUi.uiAdvanced);
     // const settings = appSettings.appUi.uiAdvanced;
 
     return (
@@ -66,13 +66,21 @@ function DelayInput({ keyName }: { keyName: SettingsKey; }) {
     const value = useSnapshot(appSettings.appUi.uiAdvanced)[keyName];
     const settings = appSettings.appUi.uiAdvanced;
     return (
-        <div className="ml-2 flex items-center gap-1" title="delay >= 0 and < 10000 ms.">
+        <div 
+        className="ml-2 flex items-center gap-1" title="delay will be 100...10000 in ms or 1..100 in sec.">
             delay
             <input
-                className={inputClasses} type="number" value={value}
+                className={inputClasses}
+                type="number"
+                value={value}
                 onChange={(e) => {
-                    settings[keyName] = +e.target.value;
-                }} tabIndex={-1}
+                    let n = +e.target.value;
+                    if (n > 0 && n < 100) {
+                        n = n * 1000;
+                    }
+                    settings[keyName] = n;
+                }}
+                tabIndex={-1}
             />
         </div>
     );
