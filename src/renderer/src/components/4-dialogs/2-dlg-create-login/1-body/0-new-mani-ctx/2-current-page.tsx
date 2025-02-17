6ab0a43cd6debac8aa0392153d84eca28b7d28b3
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-16 18:49:20 -0800

    WIP. create-mani. ctx. atoms. move away moveFromAppsToNextPage()

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/0-new-mani-ctx/2-current-page.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-body/0-new-mani-ctx/2-current-page.tsx
@@ -1,13 +1,9 @@
-import { type Atom, atom, type Getter, type Setter } from "jotai";
-import { clamp, doAddNextToastIdAtom, doDissmissNextToastsAtom, errorToString } from "@/utils";
+import { type Atom, atom } from "jotai";
+import { clamp, doAddNextToastIdAtom, doDissmissNextToastsAtom } from "@/utils";
 import { toast } from "sonner";
-import { doGetWindowManiAtom, napiBuildState, sawManiXmlAtom } from "@/store/7-napi-atoms";
+import { napiBuildState } from "@/store/7-napi-atoms";
 import { WizardPage, wizardFirstPage, wizardLastPage } from "./8-step-items-data";
-import { appSelectedAppAtom } from "./4-selected-app";
-import { newManiCtx } from "./0-ctx";
-import { createFileContent, createFileUsFromFileContent } from "@/store/1-atoms/1-files/1-do-set-files/2-create-fileus";
-import { type FileContent } from "@shared/ipc-types";
-import { type FileUs } from "@/store";
+import { moveFromAppsToNextPage } from "./2-move-to-mani-page";
 
 // Page and direction
 
@@ -35,7 +31,6 @@ export function create_DoAdvancePageAtom() {
         async (get, set, { next }: { next: boolean; }) => {
 
             if (napiBuildState.buildRunning) {
-                //TODO: show message about build running
                 set(doAddNextToastIdAtom, toast.info('Build running...'));
                 return;
             }
@@ -64,57 +59,4 @@ export function create_DoAdvancePageAtom() {
             set(_pageAndDirectionAtom, [newPage, next ? 1 : -1]);
         }
     );
-}
-
-async function moveFromAppsToNextPage(get: Getter, set: Setter): Promise<boolean> {
-    const selectedApp = get(appSelectedAppAtom);
-    if (!selectedApp) {
-        set(doAddNextToastIdAtom, toast.error('First, select the application window.'));
-        return false;
-    }
-
-    const maniXml = get(newManiCtx.maniXmlAtom);
-
-    if (!maniXml) {
-        // 0. Claen up the context before parsing
-        set(newManiCtx.maniXmlAtom, undefined);
-        set(newManiCtx.fileUsAtom, undefined);
-
-        // 1. Get manifest as maniXml from the window
-        await set(doGetWindowManiAtom, { hwnd: selectedApp.item.hwnd, wantXml: true });
-        const sawManiXml = get(sawManiXmlAtom);
-
-        // 2. Save maniXml to the context
-        if (!sawManiXml) {
-            set(doAddNextToastIdAtom, toast.error('There are no input controls in the window.')); //TODO: you can define manifest content manually
-            return false;
-        }
-
-        set(newManiCtx.maniXmlAtom, sawManiXml);
-
-        //TODO: check created manifest content manually checkbox
-
-        // 3. Parse maniXml to fileUs
-        try {
-            const fileContent: FileContent = createFileContent(sawManiXml);
-            const fileUs: FileUs = createFileUsFromFileContent(fileContent);
-
-            set(newManiCtx.fileUsAtom, fileUs);
-
-            console.log('fileUs', fileUs);
-
-            //TODO: update loaded counters in the files list on the left
-        } catch (error) {
-            const msg = `Cannot parse manifest content.\n${errorToString(error)}`;
-            console.error(msg);
-            toast.error(msg);
-            return false;
-        }
-
-        // 4. done
-
-        console.log('sawManiXml', sawManiXml);
-    }
-
-    return true;
 }
