698b2a5639c52e53580236d19fae16fa32573247
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-21 19:14:44 -0800

    WIP. create-mani. new training 3. setSawMode() add position and size access

--- src/shell/xternal-to-renderer/commands/1-set-saw-mode.ts
+++ src/shell/xternal-to-renderer/commands/1-set-saw-mode.ts
@@ -1,9 +1,28 @@
 import { winApp } from '@shell/start-main-window/main-window';
 
+const prevPos: { pos: number[]; size: number[] } = {
+    pos: [],
+    size: [],
+}
+
 export function setSawMode(on: boolean): void {
     if (!winApp) {
         return;
     }
 
-    winApp.setAlwaysOnTop(on);
+    if (on) {
+        const pos = winApp.getPosition();
+        const size = winApp.getSize();
+        prevPos.pos  = pos;
+        prevPos.size = size;
+    
+        winApp.setAlwaysOnTop(true);
+        return;
+    } else {
+        winApp.setPosition(prevPos.pos[0], prevPos.pos[1]);
+        winApp.setSize(prevPos.size[0], prevPos.size[1]);
+
+        winApp.setAlwaysOnTop(false);
+    }
+   
 }
