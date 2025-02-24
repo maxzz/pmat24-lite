84304fa417f292c7e8a6a71b7e874cbf3a3f1641
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-23 19:51:36 -0800

    WIP. create-mani. new training 3. setSawModeOnMain(). conv DPI problem. 2

--- src/shell/xternal-to-renderer/commands/1-set-saw-mode-on-main.ts
+++ src/shell/xternal-to-renderer/commands/1-set-saw-mode-on-main.ts
@@ -10,26 +10,35 @@ export function setSawModeOnMain(winApp: BrowserWindow | null, on: boolean): voi
 
     if (on) {
         savedPos = getWindowRect(winApp);
-        
+
+        console.log('\nwindow rect:', savedPos, '\nbounds:', winApp.getBounds());
+
         let newPos: Electron.Rectangle = { x: 0, y: 0, width: 300, height: 200, };
 
         const displays = screen.getAllDisplays();
         const nearestDisplay = screen.getDisplayNearestPoint(savedPos);
         const matchingDisplay = screen.getDisplayMatching(savedPos);
 
+        // console.log('\nsetSawMode on:', '\ndisplays:', displays);
+        // console.log('\nsetSawMode on:', '\nnearest:', nearestDisplay);
+        console.log('\nsetSawMode on:', '\nmatching:', matchingDisplay);
+
         const newDisplay = matchingDisplay || nearestDisplay;
         newPos.x = newDisplay.bounds.x + newDisplay.bounds.width / 2 - savedPos.width / 2;
         newPos.y = newDisplay.bounds.y + newDisplay.bounds.height / 2 - savedPos.height / 2;
 
+        console.log(`newDisplay.bounds.x: ${newDisplay.bounds.x} halfWidth: ${savedPos.width / 2} winHalfWidth: ${newPos.width / 2}`);
+       
         // const newPos2 = screen.screenToDipRect(winApp, newPos);
         const newPos2 = screen.dipToScreenRect(winApp, newPos);
+        console.log('\nsetSawMode on: \nsavedPos:', savedPos, '\nnewPos:', newPos, '\nnewPos2:', newPos2);
+        
+        console.log('-------');
+        const newPos3 = screen.screenToDipRect(winApp, newDisplay.bounds);
+        const newPos4 = screen.dipToScreenRect(winApp, newDisplay.bounds);
+        console.log('\nsetSawMode on: \nnewPos3:', newPos3, '\nnewPos4:', newPos4);
 
-        // console.log('\nsetSawMode on:', '\ndisplays:', displays);
-        // console.log('\nsetSawMode on:', '\nnearest:', nearestDisplay);
-        console.log('\nsetSawMode on:', '\nmatching:', matchingDisplay);
-        console.log('\nsetSawMode on: \nsavedPos:', savedPos, '\nnewPos:', newPos2);
-
-        setWindowRect(winApp, newPos2);
+        setWindowRect(winApp, newPos);
         winApp.setAlwaysOnTop(true);
 
         mainStore.sawModeIsOn = true;
