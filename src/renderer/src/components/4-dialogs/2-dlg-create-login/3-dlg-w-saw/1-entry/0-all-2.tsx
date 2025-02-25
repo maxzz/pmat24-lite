6591e1a626c673cc0382d022c08d1c13bfa94c4a
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-24 18:51:23 -0800

    WIP. create-mani. new training 3. add MonitorOverlay

---
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry/0-all-2.tsx
@@ -0,0 +1,23 @@
+import { useAtom } from "jotai";
+import * as D from "@/ui/shadcn/dialog";
+import { doOpenCreateManiSawAtom } from "@/store/1-atoms/7-dialogs";
+import { DialogSawBody } from "./1-body";
+
+export function MonitorOverlay() {
+    const [isOpen, setIsOpen] = useAtom(doOpenCreateManiSawAtom);
+    return (<>
+        <D.Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <D.DialogContent className={dialogClasses} hiddenTitle="Create manifest" noClose>
+                <DialogSawBody />
+            </D.DialogContent>
+        </D.Dialog>
+    </>);
+}
+
+const dialogClasses = "\
+p-0 \
+!w-11/12 max-w-5xl \
+h-4/5 min-h-[60vh] max-h-[90vh] \
+rounded-md \
+data-[state=open]:[animation-duration:200ms] \
+";
