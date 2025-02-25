3802206bf026fccca5e4a01eebbb3cc3b1e64873
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-24 19:45:51 -0800

    WIP. create-mani. new training 3. update MonitorOverlay layout

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/0-all-saw.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/0-all-saw.tsx
@@ -1,20 +1,56 @@
 import { useAtomValue, useSetAtom } from "jotai";
-import { AnimatePresence, type AnimationProps, motion, type Transition } from "motion/react";
+import { type AnimationProps, type Transition, AnimatePresence, motion } from "motion/react";
 import { doOpenSawOverlayAtom, isOpenSawOverlayAtom } from "@/store/1-atoms/7-dialogs";
+import { doMonitoringAtom, monitorCounterAtom } from "@/store";
+import { useCallback, useEffect } from "react";
 
 export function MonitorOverlay() {
+
     const isOpen = useAtomValue(isOpenSawOverlayAtom);
     const doOpen = useSetAtom(doOpenSawOverlayAtom);
+
+    const doMonitoring = useSetAtom(doMonitoringAtom);
+
+    const monitorCounter = useAtomValue(monitorCounterAtom);
+
+    const callback = useCallback(
+        () => {
+            console.log('Monitoring callback');
+        }, []
+    );
+
+    useEffect(
+        () => {
+            if (isOpen) {
+                doMonitoring({ doStart: true, callback });
+                return () => doMonitoring({ doStart: false });
+            }
+        }, [isOpen]
+    );
+
     return (<>
         <AnimatePresence>
             {isOpen && (
                 <motion.div
-                    className="fixed inset-0 z-[100] bg-sky-500/20 backdrop-blur-sm"
+                    className="fixed inset-0 bg-sky-300 z-[100]"
                     {...animationProps}
                     onClick={() => doOpen(false)}
                 >
-                    <div className="h-full grid place-content-center">
-                        123
+                    <div className="h-full text-sm grid grid-cols-3 place-content-center">
+                        <div className="col-start-2 grid gap-y-4 text-center">
+                            <div className="text-balance">
+                                Launch the program or browse to the Web site that
+                                contains the login screen for which you want to create a managed logon.
+                            </div>
+
+                            <div className="">
+                                Login screen detected: 123
+                            </div>
+
+                            <div className="">{monitorCounter}</div>
+
+                        </div>
+
                     </div>
                 </motion.div>
             )}
@@ -38,7 +74,7 @@ const animationTransition: Transition = {
 };
 
 const animationProps: AnimationProps = {
-    initial: { opacity: 0, scale: 0.5 },
+    initial: { opacity: 0, scale: 0.75 },
     animate: { opacity: 1, scale: 1 },
-    exit: { opacity: 0, scale: 0.5 },
+    exit: { opacity: 0, scale: 0.75 },
 };
