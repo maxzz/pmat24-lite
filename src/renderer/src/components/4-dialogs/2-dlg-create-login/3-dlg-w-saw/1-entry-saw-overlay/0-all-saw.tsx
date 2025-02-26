69c399f263327c041cd713772ea961a5053aca69
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-25 18:37:24 -0800

    WIP. create-mani. new training 3. MonitorOverlay layout. 6. update MonitorOverlay

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/0-all-saw.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/0-all-saw.tsx
@@ -1,9 +1,9 @@
+import { useCallback, useEffect } from "react";
 import { useAtomValue, useSetAtom } from "jotai";
 import { type AnimationProps, type Transition, AnimatePresence, motion } from "motion/react";
 import { doOpenSawOverlayAtom, isOpenSawOverlayAtom } from "@/store/1-atoms/7-dialogs";
-import { doMonitoringAtom, monitorCounterAtom } from "@/store";
-import { useCallback, useEffect } from "react";
-import { Button, Checkbox, Label } from "@/ui";
+import { doMonitoringAtom } from "@/store";
+import { MonitorOverlayBody } from "./1-body";
 
 export function MonitorOverlay() {
 
@@ -30,67 +30,13 @@ export function MonitorOverlay() {
     return (<>
         <AnimatePresence>
             {isOpen && (
-                <motion.div
-                    className="fixed inset-0 bg-sky-300 z-[100]"
-                    {...animationProps}
-                    onClick={() => doOpen(false)}
-                >
+                <motion.div className="fixed inset-0 bg-sky-300 z-[100]" {...animationProps} onClick={() => doOpen(false)}>
                     <MonitorOverlayBody />
                 </motion.div>
             )}
         </AnimatePresence>
     </>);
 }
-
-function MonitorOverlayBody() {
-    const monitorCounter = useAtomValue(monitorCounterAtom);
-    return (
-        <div className="h-full text-sm grid grid-cols-[1fr_2fr_1fr] place-content-center">
-            <div className="col-start-2 grid gap-y-4 text-center">
-
-                <div className="">{monitorCounter}</div>
-
-                <div className="text-balance">
-                    Launch the program or browse to the Web site that
-                    contains the login screen for which you want to create a managed logon.
-                </div>
-
-                <div className="">
-                    Login screen detected:
-                </div>
-
-                <div className="">
-                    <div className="place-self-center size-24 border-border border rounded">
-                    </div>
-
-                    <div className="place-self-center">
-                        App name
-                    </div>
-                </div>
-
-                <Label className="place-self-center flex items-center gap-2">
-                    <Checkbox className="size-4" />
-                    Set up a managed logon manually
-                </Label>
-
-                <div className="">To continue click the button below.</div>
-
-                <Button className="place-self-center" variant="default" size="xs">
-                    Continue
-                </Button>
-
-            </div>
-        </div>
-    );
-}
-
-const dialogClasses = "\
-p-0 \
-!w-11/12 max-w-5xl \
-h-4/5 min-h-[60vh] max-h-[90vh] \
-rounded-md \
-data-[state=open]:[animation-duration:200ms] \
-";
 
 const animationTransition: Transition = {
     // type: "spring",
@@ -104,5 +50,3 @@ const animationProps: AnimationProps = {
     animate: { opacity: 1, scale: 1 },
     exit: { opacity: 0, scale: 0.75 },
 };
-
-//doOpenCreateManiSawAtom
