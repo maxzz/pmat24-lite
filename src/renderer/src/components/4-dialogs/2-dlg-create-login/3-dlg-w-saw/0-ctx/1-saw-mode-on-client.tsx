dcac6eefbd1d77f86648cba63d359521a3ae3949
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-26 19:31:03 -0800

    WIP. create-mani. new training 3. monitor. new layout. 5. add doTurnOffSawModeOnClientAtom

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/1-saw-mode-on-client.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/1-saw-mode-on-client.tsx
@@ -1,13 +1,15 @@
-import { atom } from "jotai";
+import { atom, PrimitiveAtom } from "jotai";
 import { doOpenCreateManiSawAtom, doOpenSawOverlayAtom } from "@/store";
 import { hasMain, sendToMain } from "@/xternal-to-main";
 
 /**
+ * @param turnOn - is to set the mode on or off
  * @param canceledByMain - is set if app close button was pressed from main process
+ * @param cancelAtom - cancel will be set to false when mode is turnned off by main process
  */
 export const sawModeOnClientAtom = atom(
     get => get(_sawModeAtom),
-    (get, set, { turnOn, canceledByMain }: { turnOn: boolean; canceledByMain: boolean; }) => {
+    (get, set, { turnOn, canceledByMain, cancelByMainAtom }: { turnOn: boolean; canceledByMain: boolean; cancelByMainAtom?: PrimitiveAtom<boolean>; }) => {
         const isOn = get(_sawModeAtom);
 
         if (turnOn) {
@@ -23,7 +25,10 @@ export const sawModeOnClientAtom = atom(
         } else {
             if (canceledByMain) {
                 // set(doOpenCreateManiSawAtom, false);
-                set(doOpenSawOverlayAtom, false); //TODO: avoid this dependency, by ...
+                // set(doOpenSawOverlayAtom, false); //TODO: avoid this dependency, by ...
+                if (cancelByMainAtom) {
+                    set(cancelByMainAtom, false);
+                }
             } else if (hasMain()) {
                 sendToMain({ type: 'r2m:set-saw-mode', setOn: false });
             }
@@ -33,3 +38,10 @@ export const sawModeOnClientAtom = atom(
 );
 
 const _sawModeAtom = atom<boolean>(false);
+
+export const doTurnOffSawModeOnClientAtom = atom(
+    null,
+    (get, set) => {
+        set(sawModeOnClientAtom, { turnOn: false, canceledByMain: false, cancelByMainAtom: doOpenSawOverlayAtom });
+    }
+);
