8d53d546e3201ad195863332bbf9a28b9540a828
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-28 15:20:33 -0800

    WIP. create-mani. new training 3. add moveToSecondDlgAtom. 2

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/2-move-to-second-dlg.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/2-move-to-second-dlg.tsx
@@ -1,17 +1,32 @@
 import { atom } from "jotai";
 import { getXmlCreateFileUs } from "../../0-ctx-new-mani";
+import { doOpenCreateManiSawAtom, doOpenSawOverlayAtom } from "@/store";
+import { doTurnOffSawModeOnClientAtom } from "./1-saw-mode-on-client";
 
 export const showProgressAtom = atom(false);
 
 export const moveToSecondDlgAtom = atom(
     null,
-    async (get, set, { hwnd}: { hwnd: string; }): Promise<boolean> => {
+    async (get, set, { cancel, hwnd }: { cancel: boolean; hwnd: string; }): Promise<boolean> => {
+        if (cancel) {
+            set(doOpenSawOverlayAtom, false);
+            set(doTurnOffSawModeOnClientAtom);
+            return false;
+        }
+
+        // before close and open next dialog:
+        //TODO: no controls
+        //TODO: manual mode
+
+        set(doOpenSawOverlayAtom, false);
+        set(doTurnOffSawModeOnClientAtom);
 
         const move = await getXmlCreateFileUs({ hwnd, showProgressAtom, get, set });
         if (!move) {
             return false;
         }
 
+        set(doOpenCreateManiSawAtom, true);
         return true;
     }
 );
