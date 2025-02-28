746b6ce1f62040dc25dcffddf0de1cb1d2f16f5e
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-27 18:16:05 -0800

    WIP. create-mani. new training 3. CtxContent. 2

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/2-dlg-w-screenshots/0-new-mani-ctx/0-ctx-content.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/2-dlg-w-screenshots/0-new-mani-ctx/0-ctx-content.tsx
@@ -1,10 +1,20 @@
-import { type Setter, type PrimitiveAtom as PA } from "jotai";
+import { type Setter, type PrimitiveAtom as PA, atom } from "jotai";
 import { type FileUs } from "@/store";
 
 export type CtxContent = {                       // Created content of the context regardless of current ctx state (like page, scroll, etc)
     maniXmlAtom: PA<string | undefined>;         // xml of the selected application
     fileUsAtom: PA<FileUs | undefined>;          // fileUs of the selected application
-}
+};
+
+export const ctxContent: CtxContent & { clear(set: Setter): void } = {
+    maniXmlAtom: atom<string | undefined>(undefined),
+    fileUsAtom: atom<FileUs | undefined>(undefined),
+
+    clear(set: Setter) {
+        set(this.maniXmlAtom, undefined);
+        set(this.fileUsAtom, undefined);
+    }
+};
 
 export function clearManiCtxManiData(ctxContent: CtxContent, set: Setter) {
     set(ctxContent.maniXmlAtom, undefined);
