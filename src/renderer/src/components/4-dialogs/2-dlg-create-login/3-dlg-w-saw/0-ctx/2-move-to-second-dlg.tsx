5cfea8c5116fce364401b9697256a4e87bee0541
Max Zakharzhevskiy <maxzz@msn.com>
2025-02-28 16:19:01 -0800

    WIP. create-mani. new training 3. add moveToSecondDlgAtom. 5. add toasts

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/2-move-to-second-dlg.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/0-ctx/2-move-to-second-dlg.tsx
@@ -1,32 +1,35 @@
 import { atom } from "jotai";
-import { getXmlCreateFileUs } from "../../0-ctx-new-mani";
+import { doAddNextToastIdAtom } from "@/utils";
+import { toast } from "sonner";
 import { doOpenCreateManiSawAtom, doOpenSawOverlayAtom } from "@/store";
 import { doTurnOffSawModeOnClientAtom } from "./1-saw-mode-on-client";
+import { getXmlCreateFileUs } from "../../0-ctx-new-mani";
 
 export const showProgressAtom = atom(false);
 
-export const moveToSecondDlgAtom = atom(
+export const doMoveToSecondDlgAtom = atom(
     null,
-    async (get, set, { cancel, hwnd }: { cancel: boolean; hwnd: string; }): Promise<boolean> => {
+    async (get, set, { cancel, hwnd }: { cancel: boolean; hwnd: string; }) => {
         if (cancel) {
             set(doOpenSawOverlayAtom, false);
             set(doTurnOffSawModeOnClientAtom);
-            return false;
+            return;
         }
 
         // before close and open next dialog:
         //TODO: no controls
+        set(doAddNextToastIdAtom, toast.error('There are no input controls in the window.'));
         //TODO: manual mode
+        return;
 
         set(doOpenSawOverlayAtom, false);
         set(doTurnOffSawModeOnClientAtom);
 
         const move = await getXmlCreateFileUs({ hwnd, showProgressAtom, get, set });
         if (!move) {
-            return false;
+            return;
         }
 
         set(doOpenCreateManiSawAtom, true);
-        return true;
     }
 );
