99e037854719888546538f13290d0b0ec693858f
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-03 14:49:17 -0800

    WIP. create-mani. new training 3. add 2-current-app.tsx

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/1-body.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/1-body.tsx
@@ -9,6 +9,7 @@ import { doTurnOffSawModeOnClientAtom } from "../0-ctx";
 import { doMoveToSecondDlgAtom } from "../0-ctx/2-move-to-second-dlg";
 import { DebugButtonsForSaw } from "../../8-test-buttons";
 import { hasMain } from "@/xternal-to-main";
+import { CurrentApp } from "./2-current-app";
 
 export function MonitorOverlayBody() {
     useDissmissNextToasts();
@@ -50,85 +51,6 @@ function ButtonContinue({ className, ...rest }: ComponentPropsWithoutRef<'div'>)
         </Button>
     );
 }
-
-function CurrentApp({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
-    return (
-        <div className={classNames("px-4 py-2 w-full border-border/30 border shadow rounded-md grid place-items-center gap-2", className)} {...rest}>
-            {/* <div className=""> Login screen detected: </div> */}
-            <div className="select-none">
-                Active application:
-            </div>
-
-            <AppIcon />
-
-            <CurrentAppCaption />
-        </div>
-    );
-}
-
-export function AppIcon({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
-    const imageElm = useAtomValue(sawIconAtom);
-    return (<>
-        {imageElm
-            ? (
-                <div className={classNames("size-8 grid place-items-center select-none", className)} {...rest}>
-                    <ImageHolder imageAtom={sawIconAtom} />
-                </div>
-            )
-            : (
-                <div className={classNames("h-8 flex items-center justify-center select-none", className)} {...rest}>
-                    No application selected
-                </div>
-            )
-        }
-    </>);
-}
-
-function CurrentAppCaption({ className, ...rest }: ComponentPropsWithoutRef<typeof motion.div>) {
-    const caption = useAtomValue(sawHandleCaptionAtom);
-    return (
-        <div className="relative overflow-clip">
-            <AnimatePresence mode="wait">
-                <motion.div
-                    className={classNames("w-full min-h-8 text-center line-clamp-2", className)}
-
-                    key={caption}
-                    // layoutDependency={caption}
-
-                    initial={{ opacity: 0, y: -5, }}
-                    animate={{ opacity: 1, y: 0, }}
-                    exit={{ opacity: 0, y: 0, transition: { duration: 0 } }}
-                    // transition={{ duration: 2 }}
-
-
-                    // layoutId={caption}
-                    // initial={{ opacity: 1, x: 100, }}
-                    // animate={{ opacity: 1, x: 0, }}
-                    // exit={{ opacity: 1, x: -100, }}
-                    // transition: { delay: .2, duration: .2 }
-                    title={caption} {...rest}
-                >
-                    <motion.div
-                    // layoutDependency={caption}
-
-                    // key={caption}
-                    // initial={{ opacity: 0, x: -100, }}
-                    // animate={{ opacity: 1, x: 0, }}
-                    // exit={{ opacity: 0, x: 100, }}
-                    // transition={{ duration: 2 }}
-
-                    // layout="position"
-                    >
-
-                        {caption}
-                    </motion.div>
-                </motion.div>
-            </AnimatePresence>
-        </div>
-    );
-}
-
-//TODO: app name text popup on update and multiline truncation
 
 function DebugBorder({ className, children, ...rest }: ComponentPropsWithoutRef<'div'>) {
     const doMoveToSecondDlg = useSetAtom(doMoveToSecondDlgAtom);
---
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/2-current-app.tsx
@@ -0,0 +1,83 @@
+import { type ComponentPropsWithoutRef } from "react";
+import { useAtomValue } from "jotai";
+import { AnimatePresence, motion } from "motion/react";
+import { classNames } from "@/utils";
+import { ImageHolder } from "@/ui";
+import { sawHandleCaptionAtom, sawIconAtom } from "@/store";
+
+export function CurrentApp({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
+    return (
+        <div className={classNames("px-4 py-2 w-full border-border/30 border shadow rounded-md grid place-items-center gap-2", className)} {...rest}>
+            {/* <div className=""> Login screen detected: </div> */}
+            <div className="select-none">
+                Active application:
+            </div>
+
+            <AppIcon />
+
+            <CurrentAppCaption />
+        </div>
+    );
+}
+
+function AppIcon({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
+    const imageElm = useAtomValue(sawIconAtom);
+    return (<>
+        {imageElm
+            ? (
+                <div className={classNames("size-8 grid place-items-center select-none", className)} {...rest}>
+                    <ImageHolder imageAtom={sawIconAtom} />
+                </div>
+            )
+            : (
+                <div className={classNames("h-8 flex items-center justify-center select-none", className)} {...rest}>
+                    No application selected
+                </div>
+            )
+        }
+    </>);
+}
+
+function CurrentAppCaption({ className, ...rest }: ComponentPropsWithoutRef<typeof motion.div>) {
+    const caption = useAtomValue(sawHandleCaptionAtom);
+    return (
+        <div className="relative overflow-clip">
+            <AnimatePresence mode="wait">
+                <motion.div
+                    className={classNames("w-full min-h-8 text-center line-clamp-2", className)}
+
+                    key={caption}
+                    // layoutDependency={caption}
+
+                    initial={{ opacity: 0, y: -5, }}
+                    animate={{ opacity: 1, y: 0, }}
+                    exit={{ opacity: 0, y: 0, transition: { duration: 0 } }}
+                    // transition={{ duration: 2 }}
+
+
+                    // layoutId={caption}
+                    // initial={{ opacity: 1, x: 100, }}
+                    // animate={{ opacity: 1, x: 0, }}
+                    // exit={{ opacity: 1, x: -100, }}
+                    // transition: { delay: .2, duration: .2 }
+                    title={caption} {...rest}
+                >
+                    <motion.div
+                    // layoutDependency={caption}
+
+                    // key={caption}
+                    // initial={{ opacity: 0, x: -100, }}
+                    // animate={{ opacity: 1, x: 0, }}
+                    // exit={{ opacity: 0, x: 100, }}
+                    // transition={{ duration: 2 }}
+
+                    // layout="position"
+                    >
+
+                        {caption}
+                    </motion.div>
+                </motion.div>
+            </AnimatePresence>
+        </div>
+    );
+}
