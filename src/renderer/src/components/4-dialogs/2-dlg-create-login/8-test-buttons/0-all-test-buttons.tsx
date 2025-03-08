aa5227a8174bffebb1145facdcf26e2491df8701
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-07 19:23:43 -0800

    WIP. create-mani. new training 3. update RowHwns imports

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/8-test-buttons/0-all-test-buttons.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/8-test-buttons/0-all-test-buttons.tsx
@@ -4,10 +4,9 @@ import { useSnapshot } from "valtio";
 import { classNames, doDissmissNextToastsAtom } from "@/utils";
 import { appSettings, debugSettings } from "@/store";
 import { Checkbox, Label, RadioGroup, RadioGroupItem } from "@/ui";
-import { defaultScreenshotWidth, doSetScreenshotsAtom } from "@/store/7-napi-atoms";
-import { testHwnd, TestHwndEnum, testMani, testScreen, type TestManiEnum, type TestScreenEnum } from "@/store/7-napi-atoms/8-create-mani-tests-w-fetch";
+import { defaultScreenshotWidth, doSetScreenshotsAtom, testHwnd, TestHwndEnum, testMani, testScreen, type TestManiEnum, type TestScreenEnum } from "@/store/7-napi-atoms";
 import { doUpdateHwndAndIconAtom } from "../3-dlg-w-saw/0-ctx";
-// import { doLoadFakeManiAtom } from "@/store/7-napi-atoms/8-create-mani-tests-w-fetch";
+// import { doLoadFakeManiAtom } from "@/store/7-napi-atoms";
 
 export function DebugButtonsForScreenshots({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
     return (
@@ -130,10 +129,13 @@ function DelayInput({ keyName }: { keyName: SettingsKey; }) {
     const value = useSnapshot(appSettings.appUi.uiAdvanced)[keyName];
     const settings = appSettings.appUi.uiAdvanced;
     return (
-        <div
-            className="ml-2 flex items-center gap-1" title="delay will be 100...10000 in ms or 1..100 in sec.">
+        <div className="ml-2 flex items-center gap-1" title="delay will be 100...10000 in ms or 1..100 in sec.">
             delay
-            <input className={inputClasses} type="number" tabIndex={-1} value={value} onChange={(e) => settings[keyName] = +e.target.value} />
+            <input
+                className={inputClasses} type="number" tabIndex={-1}
+                value={value}
+                onChange={(e) => settings[keyName] = +e.target.value}
+            />
         </div>
     );
 }
