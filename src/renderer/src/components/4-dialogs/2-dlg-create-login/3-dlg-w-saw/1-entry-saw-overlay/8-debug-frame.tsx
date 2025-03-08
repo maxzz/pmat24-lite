8f9e9c030c2ab6e84473fc2343fbf0fb19aebeb5
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-07 19:35:36 -0800

    WIP. create-mani. new training 3. update DebugFrame

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/8-debug-frame.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/3-dlg-w-saw/1-entry-saw-overlay/8-debug-frame.tsx
@@ -13,31 +13,31 @@ export function DebugFrame({ className, children, ...rest }: ComponentPropsWitho
 
     if (hasMain()) {
         return (
-            <div className={classNames("relative bg-muted/50 1bg-sky-400/20", className)} {...rest}>
+            <div className={classNames("relative bg-muted/10 1bg-sky-400/20", className)} {...rest}>
+                {children}
+            </div>
+        );
+    } else {
+        return (
+            <div className={classNames("relative w-[320px] bg-muted/10 1bg-sky-400/20 border-border/30 border shadow rounded-md", className)} {...rest}>
+
+                <div className="absolute left-0 -top-16 py-0.5 w-full text-right border-border/75 border rounded-md shadow opacity-50">
+                    <DebugButtonsForSaw className="scale-[.74] origin-left" />
+                </div>
+
+                <div className="relative px-3 py-3 w-full text-sm border-border/30 border-b flex items-center justify-center select-none">
+                    Select application
+
+                    <Button
+                        className="absolute right-2 top-1/2 -translate-y-1/2 hover:text-white hover:bg-red-500" variant="ghost" size="xs" tabIndex={-1}
+                        onClick={() => doMoveToSecondDlg({ cancel: true })}
+                    >
+                        <Cross2Icon className="size-4" />
+                    </Button>
+                </div>
+
                 {children}
             </div>
         );
     }
-
-    return (<>
-        <div className={classNames("relative w-[320px] bg-muted/10 1bg-sky-400/20 border-border/30 border shadow rounded-md", className)} {...rest}>
-
-            <div className="absolute left-0 -top-16 py-0.5 w-full text-right border-border/75 border rounded-md shadow opacity-50">
-                <DebugButtonsForSaw className="scale-[.74] origin-left" />
-            </div>
-
-            <div className="relative px-3 py-3 w-full text-sm border-border/30 border-b flex items-center justify-center select-none">
-                Select application
-
-                <Button
-                    className="absolute right-2 top-1/2 -translate-y-1/2 hover:text-white hover:bg-red-500" variant="ghost" size="xs" tabIndex={-1}
-                    onClick={() => doMoveToSecondDlg({ cancel: true })}
-                >
-                    <Cross2Icon className="size-4" />
-                </Button>
-            </div>
-
-            {children}
-        </div>
-    </>);
 }
