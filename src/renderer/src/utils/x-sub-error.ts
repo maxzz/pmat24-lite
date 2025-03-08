9777dc280d2d983fa3e6ef9bfa36bd55b6945b58
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-08 14:47:50 -0800

    WIP. create-mani. new training 3. daily code review

--- src/renderer/src/utils/x-sub-error.ts
+++ src/renderer/src/utils/x-sub-error.ts
@@ -3,7 +3,7 @@
  * "'get-saw-content' Error invoking remote method 'invoke-main': >>>Too many controls"
  * string 'Too many controls'.
  */
-export function getSubError(error: unknown) {
+export function getSubErrorMessage(error: unknown): string {
     const msg = error instanceof Error ? error.message : `${error}`;
     return msg.split('>>>').at(-1) || msg;
 }
