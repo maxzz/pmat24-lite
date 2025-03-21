a6760c736df872bc7738d767fe2ed7ece75cc212
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-20 19:37:53 -0700

    WIP. showFieldCatalog. 4 add rootFolderAtom

--- src/renderer/src/store/1-atoms/1-files/0-files-atom/index.ts
+++ src/renderer/src/store/1-atoms/1-files/0-files-atom/index.ts
@@ -4,3 +4,7 @@ import type { FileUsAtom } from "@/store/store-types";
 // Files
 
 export const filesAtom = atom<FileUsAtom[]>([]);
+
+// Root folder
+
+export const rootFolderAtom = atom(''); // root folder path //TODO: add for web version folder handle
