71753da5a52dcda71be686c5fbe80994b081cc7f
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-21 17:09:02 -0700

    WIP. showFieldCatalog. 5 add todo on bug were we check reason why

--- src/renderer/src/store/manifest/index.ts
+++ src/renderer/src/store/manifest/index.ts
@@ -4,3 +4,7 @@ export * from "./manifest-field-icons";
 export * from "./1-move-to-pm-manifest";
 export * from "./3-policy-io";
 export * from "./io";
+
+//TODO: 03.21.25
+// when we check for reason why complains about manifest, (for adp.com) we have one field
+// witout sid and second with sid. This is not a problem, since we have single field logon.
