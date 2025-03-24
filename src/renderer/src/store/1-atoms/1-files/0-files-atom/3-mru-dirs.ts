60a2963d744585913f13f3139365c105f1c1d783
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-23 17:55:34 -0700

    WIP. welcome. folder MRU. 3. connect idb-keyval

--- src/renderer/src/store/1-atoms/1-files/0-files-atom/3-mru-dirs.ts
+++ src/renderer/src/store/1-atoms/1-files/0-files-atom/3-mru-dirs.ts
@@ -1,3 +1,4 @@
+import { get, set } from "idb-keyval";
 import { hasMain } from "@/xternal-to-main";
 import { type PmatFolder } from "./9-types";
 import { appSettings } from "../../9-ui-state/0-local-storage-app";
@@ -8,28 +9,49 @@ import { appSettings } from "../../9-ui-state/0-local-storage-app";
 //          https://github.com/jakearchibald/idb-keyval
 
 export function addToDirsMru(folder: PmatFolder) {
-    addToDirsList(folder, hasMain());
-    printRootDir(folder);
+    try {
+        addToDirsList(folder, hasMain());
+        printRootDir(folder);
+    } catch (error) {
+        console.error('addToDirsMru', error);
+    }
 }
 
 function printRootDir(folder: PmatFolder) {
     console.log('%c setRootDir ', 'background-color: magenta; color: white', folder);
 }
 
-function addToDirsList(folder: PmatFolder, isWin: boolean = false) {
+async function addToDirsList(folder: PmatFolder, isWin: boolean = false) {
 
-    const list = isWin ? appSettings.appUi.mru.win : appSettings.appUi.mru.web;
-
-    // check if folder is already in the list
-    const idx = list.findIndex((item) => item.rpath === folder.rpath);
-    if (idx >= 0) {
-        list.splice(idx, 1);
+    if (!isWin) {
+        let currentList = await get<PmatFolder[]>('pmat25-mru-web');
+        if (currentList) {
+            if (currentList.find((item) => item.rpath === folder.rpath)) {
+                return;
+            }
+            if (currentList.length > 10) {
+                currentList.pop();
+            }
+            currentList.unshift(folder);
+        } else {
+            currentList = [folder];
+        }
+        set('pmat25-mru-web', currentList);
+        return;
     }
 
-    if (list.length > 10) {
-        list.shift();
-    }
+    // const list = isWin ? appSettings.appUi.mru.win : appSettings.appUi.mru.web;
 
-    // add folder in front of the list
-    list.unshift(folder);
+    // // check if folder is already in the list
+    // const idx = list.findIndex((item) => item.rpath === folder.rpath);
+    // if (idx >= 0) {
+    //     list.splice(idx, 1);
+    // }
+
+    // if (list.length > 10) {
+    //     list.shift();
+    // }
+
+    // // add folder in front of the list
+    // list.unshift(folder);
 }
