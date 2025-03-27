c1a9c1b445964bb29693cf1666b1e2977bcdda4f
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-26 18:18:18 -0700

    WIP. welcome. folder MRU. getFilesFromDataTransferItems() code review

--- src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/2-legacy-entries/1-collect-dnd-entries.ts
+++ src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/2-legacy-entries/1-collect-dnd-entries.ts
@@ -5,6 +5,27 @@ export interface FileWithPath extends File {
     path: string; // item path wo/ filename
 }
 
+export async function getFilesFromDataTransferItems(dtFileItems: DataTransferItem[]): Promise<FileWithPath[]> {
+
+    const inputs = dtFileItems.map((dtItem) => dtItem.webkitGetAsEntry()).filter(Boolean);
+
+    const rv: FileWithPath[] = [];
+
+    for await (const entry of inputs) {
+        if (isEntryFile(entry)) {
+            rv.push(await getFileAccess(entry as FileSystemFileEntry, ''));
+        }
+        else if (isEntryDirectory(entry)) {
+            for await (const [path, file] of getEntriesRecursively(entry as FileSystemDirectoryEntry)) {
+                file.path = path;
+                rv.push(file);
+            }
+        }
+    }
+
+    return rv;
+}
+
 async function getFileAccess(entry: FileSystemFileEntry, path: string): Promise<FileWithPath> {
     const file = await getFilePromisify(entry) as FileWithPath;
     file.path = path;
@@ -33,25 +54,4 @@ async function* getEntriesRecursively(folder: FileSystemDirectoryEntry): AsyncGe
             }
         }
     }
-}
-
-export async function getFilesFromDataTransferItems(dtFileItems: DataTransferItem[]): Promise<FileWithPath[]> {
-
-    const inputs = dtFileItems.map((dtItem) => dtItem.webkitGetAsEntry()).filter(Boolean);
-
-    const rv: FileWithPath[] = [];
-
-    for await (const entry of inputs) {
-        if (isEntryFile(entry)) {
-            rv.push(await getFileAccess(entry as FileSystemFileEntry, ''));
-        }
-        else if (isEntryDirectory(entry)) {
-            for await (const [path, file] of getEntriesRecursively(entry as FileSystemDirectoryEntry)) {
-                file.path = path;
-                rv.push(file);
-            }
-        }
-    }
-
-    return rv;
 }
