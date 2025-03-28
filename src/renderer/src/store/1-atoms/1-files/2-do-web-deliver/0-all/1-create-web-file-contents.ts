ff34ec84df7671b47069b763a33a1e937c550381
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-27 18:23:37 -0700

    WIP. welcome. folder MRU. createFileContents_WebAfterDnd code review. 3

--- src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/0-all/1-create-web-file-contents.ts
+++ src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/0-all/1-create-web-file-contents.ts
@@ -15,6 +15,108 @@ type DropItem = {
     notOur: boolean;                        // load of file content was blocked by allowedExt list.
 };
 
+/**
+ * Create FileContent items from open file/directory legacy web dialog or legacy drag and drop operation
+ * Modern drag and drop and dialog operations are not supported due to electronGetPaths() limitations.
+ * It should be File object not modified by JS.
+ */
+export async function createFileContents_From_Main(files: File[]): Promise<FileContent[] | undefined> {
+    const fileAndNames = electronGetPaths(files);
+    const fnames = fileAndNames.map((item) => item[1]);
+    //printFnameFiles(fnames, files);
+
+    if (fileAndNames.length) {
+        const rv: FileContent[] = await invokeLoadFiles(fnames, pmAllowedToOpenExt);
+        setRootFromMainFileContents(rv);
+        return rv;
+    }
+}
+
+export async function createFileContents_FromMru_Main(folder: PmatFolder): Promise<FileContent[] | undefined> {
+    if (folder.rpath) {
+        const rv: FileContent[] = await invokeLoadFiles([folder.rpath], pmAllowedToOpenExt);
+        setRootFromMainFileContents(rv);
+        return rv;
+    }
+}
+
+/**
+ * Create FileContent items from open file/directory web dialog
+ */
+export async function createFileContents_WebAfterDlgOpen(files: File[]): Promise<FileContent[]> {
+
+    let dropItems: DropItem[] = await mapToDropItems(files);
+    const rv = loadFilesAndCreateFileContents(dropItems);
+    return rv;
+
+    async function mapToDropItems(files: File[]): Promise<DropItem[]> {
+        let rv: DropItem[] = [];
+        try {
+            rv = await Promise.all(files.map(
+                async (file) => {
+                    const webFsItem = new WebFsItem({
+                        file,
+                        handle: isFileWithFileHandle(file) ? file.handle : null,
+                        parent: isFileWithDirectoryAndFileHandle(file) ? file.directoryHandle : null,
+                        path: pathWithoutFilename(file.webkitRelativePath), // webkitRelativePath is "C/D/E/{10250eb8-d616-4370-b3ab-39aedb8c6950}.dpm"
+                    });
+
+                    const rv: DropItem = {
+                        fname: file.name,
+                        fpath: webFsItem.path,
+                        fileWeb: file,
+                        webFsItem: webFsItem,
+                        notOur: false,
+                    };
+                    return rv;
+                }
+            ));
+        } catch (error) {
+            console.error('cannot read from File[]', files);
+        }
+        return rv;
+    }
+}
+
+/**
+ * Create FileContent items from web drag and drop operation
+ */
+export async function createFileContents_WebAfterDnd(fileDataTransferItems: DataTransferItem[]): Promise<{ fileContents: FileContent[], root: PmatFolder; }> {
+
+    const dndItems = (await collectWebDndItems(fileDataTransferItems)).filter((item) => item.file);
+    const dropItems: DropItem[] = await mapToDropItems(dndItems);
+
+    const fileContents = await loadFilesAndCreateFileContents(dropItems);
+    const root = {
+        rpath: findShortestPathInFnames(fileContents.map((item) => item.fpath)),
+        handle: getSingleFolderHandle(dropItems),
+        fromMain: false,
+    };
+
+    return { fileContents, root, };
+
+    async function mapToDropItems(dndItems: WebFsItem[]): Promise<DropItem[]> {
+        let rv: DropItem[] = [];
+        try {
+            rv = dndItems.map(
+                (dndItem) => {
+                    const rv: DropItem = {
+                        fname: dndItem.file!.name,
+                        fpath: dndItem.path,
+                        fileWeb: dndItem.file!,
+                        webFsItem: dndItem,
+                        notOur: false,
+                    };
+                    return rv;
+                }
+            ).filter((dropItem) => !!dropItem);
+        } catch (error) {
+            console.error('cannot read from DataTransferItemList', fileDataTransferItems);
+        }
+        return rv;
+    }
+}
+
 async function loadFilesAndCreateFileContents(dropItems: DropItem[]): Promise<FileContent[]> {
     const rv: FileContent[] = [];
 
@@ -66,45 +168,6 @@ async function loadFilesAndCreateFileContents(dropItems: DropItem[]): Promise<Fi
 }
 
 /**
- * Create FileContent items from web drag and drop operation
- */
-export async function createFileContents_WebAfterDnd(fileDataTransferItems: DataTransferItem[]): Promise<{ fileContents: FileContent[], root: PmatFolder; }> {
-
-    const dndItems = (await collectWebDndItems(fileDataTransferItems)).filter((item) => item.file);
-    const dropItems: DropItem[] = await mapToDropItems(dndItems);
-    
-    const fileContents = await loadFilesAndCreateFileContents(dropItems);
-    const root = {
-        rpath: findShortestPathInFnames(fileContents.map((item) => item.fpath)),
-        handle: getSingleFolderHandle(dropItems),
-        fromMain: false,
-    };
-
-    return { fileContents, root, };
-
-    async function mapToDropItems(dndItems: WebFsItem[]): Promise<DropItem[]> {
-        let rv: DropItem[] = [];
-        try {
-            rv = dndItems.map(
-                (dndItem) => {
-                    const rv: DropItem = {
-                        fname: dndItem.file!.name,
-                        fpath: dndItem.path,
-                        fileWeb: dndItem.file!,
-                        webFsItem: dndItem,
-                        notOur: false,
-                    };
-                    return rv;
-                }
-            ).filter((dropItem) => !!dropItem);
-        } catch (error) {
-            console.error('cannot read from DataTransferItemList', fileDataTransferItems);
-        }
-        return rv;
-    }
-}
-
-/**
  * Check if we drop a single folder and can get its handle.
  * fpath with slash is a subfolder item, so we skip any subfolder items to find the root folder.
  */
@@ -112,69 +175,6 @@ function getSingleFolderHandle(items: DropItem[]) {
     const parents = new Set(items.map((item) => item.fpath?.indexOf('/') === -1 ? item.webFsItem?.parent : undefined).filter(Boolean));
     const parentHandle = parents.size === 1 ? parents.values().next().value || undefined : undefined;
     return parentHandle;
-}
-
-/**
- * Create FileContent items from open file/directory web dialog
- */
-export async function createFileContents_WebAfterDlgOpen(files: File[]): Promise<FileContent[]> {
-
-    let dropItems: DropItem[] = await mapToDropItems(files);
-    const rv = loadFilesAndCreateFileContents(dropItems);
-    return rv;
-
-    async function mapToDropItems(files: File[]): Promise<DropItem[]> {
-        let rv: DropItem[] = [];
-        try {
-            rv = await Promise.all(files.map(
-                async (file) => {
-                    const webFsItem = new WebFsItem({
-                        file,
-                        handle: isFileWithFileHandle(file) ? file.handle : null,
-                        parent: isFileWithDirectoryAndFileHandle(file) ? file.directoryHandle : null,
-                        path: pathWithoutFilename(file.webkitRelativePath), // webkitRelativePath is "C/D/E/{10250eb8-d616-4370-b3ab-39aedb8c6950}.dpm"
-                    });
-
-                    const rv: DropItem = {
-                        fname: file.name,
-                        fpath: webFsItem.path,
-                        fileWeb: file,
-                        webFsItem: webFsItem,
-                        notOur: false,
-                    };
-                    return rv;
-                }
-            ));
-        } catch (error) {
-            console.error('cannot read from File[]', files);
-        }
-        return rv;
-    }
-}
-
-/**
- * Create FileContent items from open file/directory legacy web dialog or legacy drag and drop operation
- * Modern drag and drop and dialog operations are not supported due to electronGetPaths() limitations.
- * It should be File object not modified by JS.
- */
-export async function createFileContents_From_Main(files: File[]): Promise<FileContent[] | undefined> {
-    const fileAndNames = electronGetPaths(files);
-    const fnames = fileAndNames.map((item) => item[1]);
-    //printFnameFiles(fnames, files);
-
-    if (fileAndNames.length) {
-        const rv: FileContent[] = await invokeLoadFiles(fnames, pmAllowedToOpenExt);
-        setRootFromMainFileContents(rv);
-        return rv;
-    }
-}
-
-export async function createFileContents_FromMru_Main(folder: PmatFolder): Promise<FileContent[] | undefined> {
-    if (folder.rpath) {
-        const rv: FileContent[] = await invokeLoadFiles([folder.rpath], pmAllowedToOpenExt);
-        setRootFromMainFileContents(rv);
-        return rv;
-    }
 }
 
 //
