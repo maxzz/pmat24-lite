95b99a5c02a26579237413f7a12c865ebf132ef5
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-29 14:39:30 -0700

    WIP. welcome. MRU grid layout. 1

--- src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
@@ -5,12 +5,11 @@ import { appSettings, filesAtom, rootDir } from "@/store";
 import { WelcomeHeroTitle } from "./2-welcome-hero-title";
 import { OpenButtons } from "./3-open-buttons";
 import { HIDLogoAnimation } from "./6-hid-logo-animation";
-import { DontShowNext } from "./7-nun-dont-show-next";
+// import { DontShowNext } from "./7-nun-dont-show-next";
 
 export function WelcomePage() {
     const files = useAtomValue(filesAtom);
     const showWelcome = useSnapshot(appSettings.appUi.uiGeneral).showWelcome;
-    const { allowHandleFiles } = useSnapshot(appSettings.appUi.uiAdvanced);
 
     const { fpath } = useSnapshot(rootDir);
 
@@ -19,27 +18,37 @@ export function WelcomePage() {
 
     return (
         <MotionConfig transition={{ type: "spring", duration: .7 }}>
-            <AnimatePresence>
 
+            <AnimatePresence>
                 {showWelcomePage && (
                     <motion.div
-                        className="absolute inset-0 bg-muted z-[21]"
+                        className="absolute inset-0 bg-muted grid grid-rows-[1fr,auto,1fr] z-[21]"
                         initial={{ opacity: 0, scale: 0.9 }}
                         animate={{ opacity: 1, scale: 1 }}
                         exit={{ opacity: 0, scale: 0.9 }}
                     >
-                        <div className="w-full h-full flex flex-col items-center justify-center gap-y-4">
-
-                            <WelcomeHeroTitle allowHandleFiles={allowHandleFiles} />
-                            <OpenButtons />
-                            <HIDLogoAnimation />
-                            {/* <DontShowNext className="absolute left-0 bottom-0 p-2" /> */}
-
+                        <div className="row-start-2 relative size-full">
+                            <CenterPart />
                         </div>
+
+                        <div className="row-start-3 absolute inset-0">
+                            <HIDLogoAnimation />
+                        </div>
+
                     </motion.div>
                 )}
-
             </AnimatePresence>
         </MotionConfig >
     );
+}
+
+function CenterPart() {
+    const { allowHandleFiles } = useSnapshot(appSettings.appUi.uiAdvanced);
+    return (<>
+        <div className="w-full h-full flex flex-col items-center justify-center gap-y-4">
+            <WelcomeHeroTitle allowHandleFiles={allowHandleFiles} />
+            <OpenButtons />
+            {/* <DontShowNext className="absolute left-0 bottom-0 p-2" /> */}
+        </div>
+    </>);
 }
--- src/renderer/src/components/2-main/0-all/2-welcome-page/6-hid-logo-animation.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/6-hid-logo-animation.tsx
@@ -1,14 +1,17 @@
 import { AnimatePresence, motion, MotionConfig } from "motion/react";
 import { IconHIDWoFrame } from "@/ui/icons";
+import { ComponentPropsWithoutRef } from "react";
+import { classNames } from "@/utils";
 
-export function HIDLogoAnimation() {
+export function HIDLogoAnimation({className, ...rest}: ComponentPropsWithoutRef<typeof motion.div>) {
     return (
         <motion.div
-            className="absolute right-0.5 bottom-0 p-1.5 w-12 text-foreground/10 rounded"
+            className={classNames("absolute right-0.5 bottom-0 p-1.5 w-12 text-foreground/10 rounded", className)}
             initial={{ opacity: 0, scale: 0.2 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
             transition={{ type: "spring", delay: 3, duration: 0.7 }}
+            {...rest}
         >
             <IconHIDWoFrame />
         </motion.div>
