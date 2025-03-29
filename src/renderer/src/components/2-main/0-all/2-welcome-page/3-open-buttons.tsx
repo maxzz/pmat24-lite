ef2002c963e7fe307efecde60924d0606927134a
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-29 15:15:25 -0700

    WIP. welcome. MRU grid layout. 5. transitions

--- src/renderer/src/components/2-main/0-all/2-welcome-page/3-open-buttons.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/3-open-buttons.tsx
@@ -3,11 +3,16 @@ import { useSnapshot } from "valtio";
 import { ButtonFilesPicker } from "./4-button-files-picker";
 // import { ListViewDemo } from "@/ui/local-ui/nun/ai-listview/0-list-view-demo4";
 
+const buttonClasses = "bg-background active:scale-[0.95] transition-transform";
+
 export function OpenButtons() {
     const { allowHandleFiles } = useSnapshot(appSettings.appUi.uiAdvanced);
     return (<>
-        {allowHandleFiles && <ButtonFilesPicker className="bg-background" />}
-        <ButtonFilesPicker openAsFolder className="bg-background" />
+        {allowHandleFiles && (
+            <ButtonFilesPicker className={buttonClasses} />
+        )}
+
+        <ButtonFilesPicker openAsFolder className={buttonClasses} />
         {/* <ListViewDemo /> */}
     </>);
 }
