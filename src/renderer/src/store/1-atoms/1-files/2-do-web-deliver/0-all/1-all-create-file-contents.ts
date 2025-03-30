be3fb888decac6cc3386630cc52b339cbfbfa2f3
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-29 19:28:19 -0700

    WIP. DND. fix open empty folder for electron. done

--- src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/0-all/1-all-create-file-contents.ts
+++ src/renderer/src/store/1-atoms/1-files/2-do-web-deliver/0-all/1-all-create-file-contents.ts
@@ -24,26 +24,41 @@ type DropItem = {
 export async function createFileContents_From_Main(files: File[]): Promise<SetDeliveredFiles | undefined> {
     const filePathAndDirs = electronGetPaths(files);
 
-    if (filePathAndDirs.length === 1 && filePathAndDirs[0][2]) { // filePathAndDirs[0][2] is true file is a directory
-        return {
-            deliveredFileContents: [],
-            root: {
-                fpath: filePathAndDirs[0][1],
-                handle: undefined,
-                fromMain: true,
-            },
-            noItemsJustDir: true,
-        };
-    }
+    // if (filePathAndDirs.length === 1 && filePathAndDirs[0][2]) { // filePathAndDirs[0][2] is true file is a directory
+    //     return {
+    //         deliveredFileContents: [],
+    //         root: {
+    //             fpath: filePathAndDirs[0][1],
+    //             handle: undefined,
+    //             fromMain: true,
+    //         },
+    //         noItemsJustDir: true,
+    //     };
+    // }
 
     if (filePathAndDirs.length) {
         const fnames = filePathAndDirs.map((item) => item[1]);
         printElectronFnameFiles(fnames, files);
 
         const deliveredFileContents: FileContent[] = await invokeLoadFiles(fnames, pmAllowedToOpenExt);
+        const root = getRootFromFpath({ files: deliveredFileContents, fromMain: true });
+
+        const isSingleFolder = filePathAndDirs.length === 1 && filePathAndDirs[0][2]; // filePathAndDirs[0][2] is true file is a directory
+        if (!deliveredFileContents.length && isSingleFolder) { // case empty folder
+            return {
+                deliveredFileContents: [],
+                root: {
+                    fpath: filePathAndDirs[0][1],
+                    handle: undefined,
+                    fromMain: true,
+                },
+                noItemsJustDir: true,
+            };
+        }
+
         return {
             deliveredFileContents,
-            root: getRootFromFpath({ files: deliveredFileContents, fromMain: true }),
+            root,
         };
     }
 }
