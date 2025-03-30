3483b603a172f54ccd14a44fbcbf9d9752fcbb84
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-30 14:37:15 -0700

    WIP. welcome. update footer stats

--- src/renderer/src/components/3-footer/1-loaded-counter.tsx
+++ src/renderer/src/components/3-footer/1-loaded-counter.tsx
@@ -1,26 +1,34 @@
 import { useAtomValue } from "jotai";
 import { useSnapshot } from "valtio";
-import { totalManis, treeFilesAtom } from "@/store";
+import { rootDir, totalManis, treeFilesAtom } from "@/store";
 
 export function LoadedCounter() {
     const treeFiles = useAtomValue(treeFilesAtom);
+    const dir = useSnapshot(rootDir);
     const { normal, manual, empty, fc } = useSnapshot(totalManis);
 
     if (!treeFiles?.length) {
-        return <div className="text-[.65rem]">No files loaded</div>;
+        return (
+            <div className="text-[.65rem]">
+                {dir.fpath
+                    ? 'There are no files in the folder'
+                    : 'No open folder'
+                }
+            </div>
+        );
     }
 
-    const total = cunter('Loaded:', treeFiles.length, true);
-    const subs = ` ( ${cunter('normal:', normal)}, ${cunter('manual:', manual)}, ${cunter('empty:', empty)}, ${cunter('field catalog:', fc)})`;
-    const all = `${total}${subs}`;
+    const total = plural('Loaded:', treeFiles.length, true);
+    const subs = ` ( ${plural('normal:', normal)}, ${plural('manual:', manual)}, ${plural('empty:', empty)}, ${plural('field catalog:', fc)})`;
+    const final = `${total}${subs}`;
 
     return (
         <div className="text-[.65rem] leading-5 text-muted-foreground flex items-center gap-1 select-none">
-            {all}
+            {final}
         </div>
     );
 }
 
-function cunter(label: string, counter: number, doSuffix: boolean = false): string {
+function plural(label: string, counter: number, doSuffix: boolean = false): string {
     return `${label} ${counter} ${doSuffix ? `file${counter > 1 ? 's' : ''}` : ''}`;
 }
