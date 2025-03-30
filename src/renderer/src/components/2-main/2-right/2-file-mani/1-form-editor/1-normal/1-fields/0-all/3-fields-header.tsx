24a69e1bf93c50ab9e29e38eed7c5f83d8d6cbe1
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-30 15:13:03 -0700

    WIP. fields editor. header update

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/0-all/3-fields-header.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/0-all/3-fields-header.tsx
@@ -2,30 +2,20 @@ import { useSnapshot } from "valtio";
 import { appSettings } from "@/store";
 
 export function getFieldsGridClasses(showFieldCatalog: boolean) {
-    return (
-        showFieldCatalog ?
-            "\
-p-2 \
-grid grid-cols-[auto_auto_minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_minmax(0,max-content)] items-center gap-[3px] \
-text-foreground \
-rounded-sm"
-            :
-            "\
-p-2 \
-grid grid-cols-[auto_auto_minmax(0,1fr)_minmax(0,1fr)_minmax(0,max-content)] items-center gap-[3px] \
-text-foreground \
-rounded-sm"
-    );
+    const colsClasses = showFieldCatalog
+        ? 'grid-cols-[auto_auto_minmax(0,1fr)_minmax(0,1fr)_minmax(0,1fr)_minmax(0,max-content)]'
+        : 'grid-cols-[auto_auto_minmax(0,1fr)_minmax(0,1fr)_minmax(0,max-content)]';
+    return `p-2 grid ${colsClasses} items-center gap-[3px] text-foreground rounded-sm`;
 };
 
 const headerClasses = "\
-mb-2 px-1 text-[.65rem] truncate \
+mb-1 px-1 text-[.65rem] truncate \
 text-muted-foreground border-mani-muted-foreground \
 border-b \
 select-none";
 
 const rowColumns = [
-    ['Type',                /**/ 'Type of field'],
+    ['Type',                /**/ 'The type of the field'],
     ['Use it',              /**/ 'Use this field or not'],
     ['Label',               /**/ 'The label is shown to the user next to\nthe field for entering a value'],
     ['Value',               /**/ 'Specifies the value to fill out the field\nand how it is stored'],
