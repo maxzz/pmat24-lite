e18849267eb89d9a763c05dd281dc8fbd9f016dc
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-31 15:01:33 -0700

    WIP. welcome. fix blocking exit animation. 2. code review, tweaks

--- src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/1-body.tsx
@@ -1,7 +1,6 @@
-import { useAtomValue } from "jotai";
 import { useSnapshot } from "valtio";
 import { motion, MotionConfig } from "motion/react";
-import { appSettings, filesAtom, rootDir } from "@/store";
+import { appSettings, rootDir } from "@/store";
 import { WelcomeHeroTitle } from "./2-welcome-hero-title";
 import { OpenButtons } from "./3-open-buttons";
 import { RecentFilesList } from "./5-recent-files-list";
@@ -9,47 +8,41 @@ import { HIDLogoAnimation } from "./6-hid-logo-animation";
 import { DontShowNext } from "./7-nun-dont-show-next";
 // import { ListViewDemo } from "@/ui/local-ui/nun/ai-listview/0-list-view-demo4";
 
+/**
+ * Don't use AnimatePresence and exit animation.
+ * This page should go away immediately otherwise tree item selection will be blocked for some time.
+ * @returns 
+ */
 export function WelcomePage() {
-    const files = useAtomValue(filesAtom);
+
     const { showWelcome, showWelcomeCheck } = useSnapshot(appSettings.appUi.uiGeneral);
-
     const { fpath } = useSnapshot(rootDir);
-
-    // const showWelcomePage = showWelcome && !files.length;
     const showWelcomePage = showWelcome && !fpath.length;
 
     return (
         <MotionConfig transition={{ type: "spring", duration: .7 }}>
+            {showWelcomePage && (
+                <motion.div
+                    className="absolute inset-0 bg-muted grid grid-rows-[1fr,auto,1fr] z-[21]"
+                    initial={{ opacity: 0, scale: 0.7 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                >
+                    <HIDLogoAnimation className="row-start-1 absolute right-0.5 top-1 p-1.5 w-12" />
 
-            {/* <AnimatePresence> */} {/* This page should go away immediately otherwise tree item selection will be blocked for some time */}
-                {showWelcomePage && (
-                    <motion.div
-                        className="absolute inset-0 bg-muted grid grid-rows-[1fr,auto,1fr] z-[21]"
-                        initial={{ opacity: 0, scale: 0.9 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        // exit={{ opacity: 0, scale: 0.9 }}
-                    >
-                        <HIDLogoAnimation className="row-start-1 absolute right-0.5 top-1 p-1.5 w-12" />
+                    <div className="row-start-2 relative size-full">
+                        <CenterPart />
+                    </div>
 
-                        <div className="row-start-2 relative size-full">
-                            <CenterPart />
-                        </div>
+                    <div className="row-start-3 absolute inset-0">
+                        <Recent />
 
-                        <div className="row-start-3 absolute inset-0">
-                            <div className="p-2 size-full grid grid-cols-[1fr,auto,1fr]">
-                                <div className="col-start-2 place-self-end">
-                                    <RecentFilesList />
-                                </div>
-                            </div>
+                        {showWelcomeCheck && (
+                            <DontShowNext className="absolute left-0 bottom-0 p-2" />
+                        )}
+                    </div>
 
-                            {showWelcomeCheck && (
-                                <DontShowNext className="absolute left-0 bottom-0 p-2" />
-                            )}
-                        </div>
-
-                    </motion.div>
-                )}
-            {/* </AnimatePresence> */}
+                </motion.div>
+            )}
         </MotionConfig >
     );
 }
@@ -61,6 +54,16 @@ function CenterPart() {
             <WelcomeHeroTitle allowHandleFiles={allowHandleFiles} />
             <OpenButtons />
             {/* <ListViewDemo /> */}
+        </div>
+    </>);
+}
+
+function Recent() {
+    return (<>
+        <div className="p-2 size-full grid grid-cols-[1fr,auto,1fr]">
+            <div className="col-start-2 place-self-end">
+                <RecentFilesList />
+            </div>
         </div>
     </>);
 }
