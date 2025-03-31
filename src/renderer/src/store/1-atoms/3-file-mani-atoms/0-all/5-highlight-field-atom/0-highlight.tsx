265a3fead42d603bf9f0f97841f14725ff64dd0d
Max Zakharzhevskiy <maxzz@msn.com>
2025-03-30 18:39:25 -0700

    WIP. fields editor. highlightFieldAtom. 7. update to specific params

--- src/renderer/src/store/1-atoms/3-file-mani-atoms/0-all/5-highlight-field-atom/0-highlight.tsx
+++ src/renderer/src/store/1-atoms/3-file-mani-atoms/0-all/5-highlight-field-atom/0-highlight.tsx
@@ -1,13 +1,13 @@
 import { atom } from "jotai";
 import { type Meta } from "@/store/manifest";
-import { type MFormContextProps, type NFormContextProps } from "../../9-types";
+// import { type MFormContextProps, type NFormContextProps } from "../../9-types";
 import { type NormalField } from "../../1-normal-fields";
 import { type ManualFieldState } from "../../2-manual-fields";
 import { sawHandleAtom } from "@/store/7-napi-atoms";
 
 export const highlightFieldAtom = atom(
     null,
-    (get, set, { nCtx, mCtx, fieldIdx }: { nCtx: NFormContextProps, mCtx: MFormContextProps; fieldIdx: number; }) => {
+    (get, set, { nFieldCtx, mFieldCtx }: { nFieldCtx: NormalField.RowCtx, mFieldCtx?: ManualFieldState.Ctx; }) => {
 
         const hwnd = get(sawHandleAtom)?.hwnd;
         if (!hwnd) {
@@ -15,29 +15,30 @@ export const highlightFieldAtom = atom(
             return;
         }
 
-        if (nCtx) {
-            const formIdx = nCtx.formIdx;
+        if (nFieldCtx) {
+            //nCtx: NFormContextProps
+            // const formIdx = nCtx.formIdx;
+            // const fileUs = nCtx.nAllAtoms.fileUsCtx.fileUs;
+            // const fieldCtx: NormalField.RowCtx = nCtx.nAllAtoms.normal.rowCtxs[fieldIdx];
 
-            const fieldCtx: NormalField.RowCtx = nCtx.nAllAtoms.normal.rowCtxs[fieldIdx];
-            if (fieldCtx) {
-                const metaField: Meta.Field = fieldCtx.metaField;
+            if (nFieldCtx) {
+                const metaField: Meta.Field = nFieldCtx.metaField;
                 const path: Meta.Path = metaField.path;
                 const rectStr = path.loc; // "x y w h"
-                console.log(`highlightFieldAtom normal field location "${rectStr}"`);
-                
+                const prIndex = metaField.pidx;
+
+                console.log(`highlightFieldAtom normal field location "${rectStr}", prIndex: ${prIndex}`);
+
                 //TODO: highlight: it can be web or win32
             }
-        } else if (mCtx) {
-            const formIdx = mCtx.formIdx;
+        } else if (mFieldCtx) {
+            // const fieldCtx: ManualFieldState.Ctx = get(mCtx.mAllAtoms.manual.chunksAtom)[fieldIdx];
 
-            const fileUs = mCtx.mAllAtoms.fileUsCtx.fileUs;
-            
-            const fieldCtx: ManualFieldState.Ctx = get(mCtx.mAllAtoms.manual.chunksAtom)[fieldIdx];
-            if (fieldCtx?.type === 'pos') {
-                const x = get(fieldCtx.xAtom);
-                const y = get(fieldCtx.yAtom);
+            if (mFieldCtx.type === 'pos') {
+                const x = get(mFieldCtx.xAtom);
+                const y = get(mFieldCtx.yAtom);
                 console.log(`highlightFieldAtom manual field location "${x} x ${y}"`);
-                
+
                 //TODO: highlight
             }
         }
