c6837a5ee2a0131962c805139161c532a9d63303
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-01 16:36:03 -0700

    WIP. new-mani. add new fileUsMasterAtom

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/0-ctx-new-mani/0-ctx-content.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/0-ctx-new-mani/0-ctx-content.tsx
@@ -5,10 +5,12 @@ import { type NewManiContentData } from "./9-types";
 class NewManiContent implements NewManiContentData {
     maniXmlAtom: PA<string | undefined> = atom<string | undefined>(undefined);
     fileUsAtom: PA<FileUs | undefined> = atom<FileUs | undefined>(undefined);
+    fileUsMasterAtom: PA<FileUs | undefined> = atom<FileUs | undefined>(undefined);
 
     clear(set: Setter) {
         set(this.maniXmlAtom, undefined);
         set(this.fileUsAtom, undefined);
+        set(this.fileUsMasterAtom, undefined);
     }
 };
 
--- src/renderer/src/components/4-dialogs/2-dlg-create-login/0-ctx-new-mani/9-types.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/0-ctx-new-mani/9-types.tsx
@@ -2,6 +2,7 @@ import type { PrimitiveAtom as PA } from "jotai";
 import type { FileUs } from "@/store";
 
 export type NewManiContentData = {
-    maniXmlAtom: PA<string | undefined>;    // xml of the selected application
-    fileUsAtom: PA<FileUs | undefined>;     // fileUs of the selected application
+    maniXmlAtom: PA<string | undefined>;    // new xml string of the selected application
+    fileUsAtom: PA<FileUs | undefined>;     // new fileUs of the selected application
+    fileUsMasterAtom: PA<FileUs | undefined>; // fileUs to create password change form
 };
