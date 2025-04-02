2973d81201efb14644586ffefe3f8b4d5bdc7259
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-01 19:46:02 -0700

    WIP. new-mani. add createManualModeFormFrom()

---
+++ src/renderer/src/store/manifest/1-move-to-pm-manifest/3-create-manual-mode.ts
@@ -0,0 +1,20 @@
+import { type Mani } from "pm-manifest";
+
+/**
+ * Clean up names_ext. We don't use any sub-class names (at least for now) for manual mode forms.
+ * The rule is: don't look inside the window content.
+ * @param form - Form to duplicate from
+ * @returns New duplicated form
+ */
+export function createManualModeFormFrom(form: Mani.Form): Mani.Form {
+
+    const rv: Mani.Form = {
+        detection: { ...form.detection },
+        options: form.options,
+        fields: [],
+    };
+    
+    rv.detection.names_ext = "";
+
+    return rv;
+}
--- src/renderer/src/store/manifest/1-move-to-pm-manifest/index.ts
+++ src/renderer/src/store/manifest/1-move-to-pm-manifest/index.ts
@@ -1,2 +1,3 @@
 export * from "./1-create-fce";
 export * from "./2-value-life";
+export * from "./3-create-manual-mode";
