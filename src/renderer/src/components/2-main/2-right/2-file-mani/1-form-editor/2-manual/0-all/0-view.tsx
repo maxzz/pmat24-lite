ae8ed91c9735ba550023579bfb7333a23f4c3f05
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-02 20:00:19 -0700

    WIP. new-mani. add isNewManual() to customize sub-components

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/0-view.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/0-view.tsx
@@ -3,6 +3,7 @@ import { classNames } from "@/utils";
 import { type MFormContextProps } from "@/store/1-atoms/3-file-mani-atoms";
 import { ManualPanelActions } from "../1-panel-actions";
 import { ManualPanelProps } from "../2-panel-props";
+import { isNewManual } from "./1-is-new-manual";
 
 export function ManualModeView({ ctx, className, ...rest }: { ctx: MFormContextProps; } & ComponentPropsWithoutRef<'div'>) {
     return (
@@ -11,19 +12,6 @@ export function ManualModeView({ ctx, className, ...rest }: { ctx: MFormContextP
             <ManualPanelProps className="@container/props min-h-[180px] text-xs" ctx={ctx} />
         </div>
     );
-}
-
-/**
- * Limit height for new manifests to show manifest options otherwise
- * options are hidden and user need to quess that something is below.
- */
-function isNewManual(ctx: MFormContextProps) {
-    const fileUs = ctx.maniAtoms[ctx.formIdx]?.fileUsCtx?.fileUs;
-    if (!fileUs) {
-        return null;
-    }
-    const newManual = fileUs.fileCnt.newAsManual;
-    return newManual;
 }
 
 const manualModeViewClasses = "\
---
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/1-is-new-manual.tsx
@@ -0,0 +1,14 @@
+import { type MFormContextProps } from "@/store";
+
+/**
+ * Limit height for new manifests to show manifest options otherwise
+ * options are hidden and user need to quess that something is below.
+ */
+export function isNewManual(ctx: MFormContextProps) {
+    const fileUs = ctx.maniAtoms[ctx.formIdx]?.fileUsCtx?.fileUs;
+    if (!fileUs) {
+        return null;
+    }
+    const newManual = fileUs.fileCnt.newAsManual;
+    return newManual;
+}
