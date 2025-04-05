bc387327e2620fa42c80af47ed694a854c95d003
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-04 19:44:37 -0700

    WIP. new-mani. update advanced options page

--- src/renderer/src/components/4-dialogs/8-dlg-options/5-settings-advanced.tsx
+++ src/renderer/src/components/4-dialogs/8-dlg-options/5-settings-advanced.tsx
@@ -26,40 +26,41 @@ export function AdvancedSettings() {
         <SectionTitle title="Hidden from the end user debug only">
             <div className="py-1 grid grid-cols-[auto,auto] gap-2 grid-flow-dense">
 
-                <Label className={classNames("col-start-1", rowClasses)}>
-                    <Checkbox checked={allowWelcome} onCheckedChange={(v) => liveGeneral.allowWelcome = !!v} />
-                    Allow Welcome screen to be turn off
-                </Label>
 
                 <Label className={classNames("col-start-1", rowClasses)}>
-                    <Checkbox checked={allowHandleFiles} onCheckedChange={(v) => liveUiAdvanced.allowHandleFiles = !!v} />
-                    Allow opening of individual files
-                </Label>
-
-                <Label className={classNames("col-start-1", rowClasses)}>
-                    <Checkbox checked={showFieldCatalog} onCheckedChange={(v) => appSettings.appUi.uiAdvanced.showFieldCatalog = !!v} />
-                    Show Field Catalog access
-                </Label>
-
-                <Label className={classNames("col-start-1 has-[:disabled]:opacity-50", rowClasses)}>
-                    <Checkbox checked={showFldCat && showFieldCatalog} onCheckedChange={(v) => liveFiles.shownManis.showFldCat = !!v} disabled={!showFieldCatalog} />
-                    Show field catalog files
-                </Label>
-
-                <Label className={classNames("col-start-2", rowClasses)}>
                     <Checkbox checked={showUiHeader} onCheckedChange={(v) => liveUiAdvanced.showUiHeader = !!v} />
                     Show application main header
                 </Label>
 
-                <Label className={classNames("col-start-2", rowClasses)}>
+                <Label className={classNames("col-start-1", rowClasses)}>
                     <Checkbox checked={snapDebugOnly.showCreateSrcCodeBtn} onCheckedChange={(v) => liveDebugOnly.showCreateSrcCodeBtn = !!v} />
                     Show source code button in the new manifest editor
                 </Label>
 
-                <div className="col-start-1 text-[.67rem] text-foreground/50">
-                    <div className="">zoom Ctrl+0 to 100%</div>
-                    <div className="">zoom Ctrl+Shift+plus to zoom in</div>
-                    <div className="">zoom Ctrl+minus to zoom out</div>
+                <Label className={classNames("col-start-2", rowClasses)}>
+                    <Checkbox checked={allowWelcome} onCheckedChange={(v) => liveGeneral.allowWelcome = !!v} />
+                    Allow Welcome screen to be turn off
+                </Label>
+
+                <Label className={classNames("col-start-2", rowClasses)}>
+                    <Checkbox checked={allowHandleFiles} onCheckedChange={(v) => liveUiAdvanced.allowHandleFiles = !!v} />
+                    Allow opening of individual files
+                </Label>
+
+                <Label className={classNames("col-start-2", rowClasses)}>
+                    <Checkbox checked={showFieldCatalog} onCheckedChange={(v) => appSettings.appUi.uiAdvanced.showFieldCatalog = !!v} />
+                    Show Field Catalog access
+                </Label>
+
+                <Label className={classNames("col-start-2 has-[:disabled]:opacity-50", rowClasses)}>
+                    <Checkbox checked={showFldCat && showFieldCatalog} onCheckedChange={(v) => liveFiles.shownManis.showFldCat = !!v} disabled={!showFieldCatalog} />
+                    Show field catalog files
+                </Label>
+
+                <div className="col-start-1 col-span-2 text-[.65rem] leading-3 text-foreground/50">
+                    <div>zoom Ctrl+0 to 100%</div>
+                    <div>zoom Ctrl+Shift+plus to zoom in</div>
+                    <div>zoom Ctrl+minus to zoom out</div>
                 </div>
 
             </div>
