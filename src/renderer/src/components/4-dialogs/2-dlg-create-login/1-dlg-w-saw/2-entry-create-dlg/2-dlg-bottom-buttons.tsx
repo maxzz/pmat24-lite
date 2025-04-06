f005a15a9f4b416967a7496119a35a90b19d39e1
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-06 16:08:12 -0700

    WIP. new-mani. doSaveOneAtom. 3. add doSaveTriggerAtom

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/2-entry-create-dlg/2-dlg-bottom-buttons.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/2-entry-create-dlg/2-dlg-bottom-buttons.tsx
@@ -1,5 +1,5 @@
 import { type ComponentPropsWithoutRef } from "react";
-import { useSetAtom } from "jotai";
+import { atom, useSetAtom } from "jotai";
 import { classNames } from "@/utils";
 import { Button } from "@/ui";
 import { doClearSawHandleAtom, doOpenDlgNewManiSawAtom, doSaveNewManiAtom } from "@/store";
@@ -8,26 +8,25 @@ import { doClearSawHandleAtom, doOpenDlgNewManiSawAtom, doSaveNewManiAtom } from
  * Button Save button is always enabled to show toast as hint in case of failure.
  */
 export function DialogBottemButtons({ className, ...rest }: ComponentPropsWithoutRef<"div">) {
-    const doOpenDlg = useSetAtom(doOpenDlgNewManiSawAtom);
-    const doClearSawHandle = useSetAtom(doClearSawHandleAtom);
-    const doSaveNewMani = useSetAtom(doSaveNewManiAtom);
+    const doSaveTrigger = useSetAtom(doSaveTriggerAtom);
     return (
         <div className={classNames("relative px-4 flex items-center justify-end gap-1", className)} {...rest}>
-            <Button
-                variant="default"
-                size="xs"
-                onClick={async () => {
-                    const saved = await doSaveNewMani();
-                    if (saved) {
-                        doOpenDlg(false);
-                        doClearSawHandle(); // Turn off fields highlight
-
-                        //TODO: for password change form it wiil be different
-                    }
-                }}
-            >
+            <Button variant="default" size="xs" onClick={doSaveTrigger}>
                 Save
             </Button>
         </div>
     );
 }
+
+const doSaveTriggerAtom = atom(
+    null,
+    async (get, set) => {
+        const saved = await set(doSaveNewManiAtom);
+        if (saved) {
+            set(doOpenDlgNewManiSawAtom, false);
+            set(doClearSawHandleAtom); // Turn off fields highlight
+
+            //TODO: for password change form it wiil be different
+        }
+    }
+);
