6ce5db08b490aad2f9babb781090c5a4d6a59313
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-08 16:54:00 -0700

    WIP. new-mani. fix TestCreateWithSaw disabled state. 2. in menu

--- src/renderer/src/components/2-main/1-left/1-header/1-menu-main/11-files/11-create-mani.tsx
+++ src/renderer/src/components/2-main/1-left/1-header/1-menu-main/11-files/11-create-mani.tsx
@@ -1,16 +1,19 @@
 import { useSetAtom } from "jotai";
+import { useSnapshot } from "valtio";
+import { DropdownMenuItem, DropdownMenuShortcut, menuShortcutClasses } from "@/ui";
 import { doOpenSawOverlayForLoginAtom } from "@/store/1-atoms/7-dialogs";
 import { shortcutNameCreate } from "@/components/4-dialogs/0-global";
-import { DropdownMenuItem, DropdownMenuShortcut, menuShortcutClasses } from "@/ui";
+import { rootDir } from "@/store";
 
 export function MenuItem_CreateMani() {
-
     const doOpenCreateDialog = useSetAtom(doOpenSawOverlayForLoginAtom);
-    
+    const disabled = !useSnapshot(rootDir).fpath;
     return (<>
-        <DropdownMenuItem onClick={() => doOpenCreateDialog(true)}>
+        <DropdownMenuItem onClick={() => doOpenCreateDialog(true)} disabled={disabled}>
             Create manifest...
             <DropdownMenuShortcut className={menuShortcutClasses}>{shortcutNameCreate}</DropdownMenuShortcut>
         </DropdownMenuItem>
     </>);
 }
+
+// TODO: DropdownMenuItem.title is not shown in the menu when item is disabled, but it needs to be shown as explanation why the item is disabled
