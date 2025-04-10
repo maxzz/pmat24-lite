14287a03454dcf46678b43465546d008c6b627e8
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-09 18:52:59 -0700

    WIP. new-mani. dataWithStateAtom. add FilesFilterItem

---
+++ src/renderer/src/components/4-dialogs/1-dlg-filter-files/1-files-list/1-filtered-files-list.tsx
@@ -0,0 +1,36 @@
+import { useAtomValue } from "jotai";
+import { type TreenIconComponent } from "@/ui/shadcn/tree";
+import { type TreeFileItem, getTreeItemDisplayText, treeFilesAtom, appSettings } from "@/store";
+import { ScrollArea } from "@/ui";
+import { castTreeItemToFileUs } from "@/components/2-main/1-left/2-files-list";
+
+/**
+ * This is used by filter dialog files list.
+ */
+export function FilteredFilesList() {
+    const files = useAtomValue(treeFilesAtom);
+    return (
+        <ScrollArea className="h-[50vh] max-h-[75vh]">
+            <div>
+                {files.map(
+                    (file) => <FilesFilterItem key={file.id} file={file} IconToRender={file.icon} />
+                )}
+            </div>
+        </ScrollArea>
+    );
+}
+
+function FilesFilterItem({ file, IconToRender }: { file: TreeFileItem; IconToRender: TreenIconComponent | undefined; }) {
+    const fileUs = useAtomValue(castTreeItemToFileUs(file).fileUsAtom);
+    const chooseName = useAtomValue(fileUs.parsedSrc.stats.loginFormChooseNameAtom);
+    const displayText = getTreeItemDisplayText(fileUs, appSettings.files.itemsState, chooseName);
+
+    return (<>
+        {IconToRender && (
+            <div key={file.id} className="flex items-center gap-1 leading-5">
+                <IconToRender className="flex-none p-px size-4" />
+                <div className="truncate">{displayText}</div>
+            </div>
+        )}
+    </>);
+}
