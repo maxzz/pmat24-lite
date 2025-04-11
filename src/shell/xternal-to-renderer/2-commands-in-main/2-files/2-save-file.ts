df8fe7b5b8662775d88ba658abdf04894d4081fe
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-10 19:52:21 -0700

    WIP. new-mani. main. update saveFileFromMain() return type. 2

--- src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
@@ -1,25 +1,31 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * Save rendered file from main process.
+ * @param fileName - file name with path
+ * @param content - file content
+ * @returns - error message or empty string
+ */
 export async function saveFileFromMain(fileName: string, content: string): Promise<string> {
 
     // 1. Create dir if needed
-    
+
     const fileDir = path.dirname(fileName || '');
     if (!fileDir) {
         console.error(`saveFile: no dirname "${fileName}"`);
         return `Cannot save file "${fileName}"`;
     }
 
-    if (!fs.existsSync(fileDir)) {
-        await fs.promises.mkdir(fileDir, { recursive: true });
-    }
-
     // 2. Write file
-    
-    console.log('saveFile', fileName, content);
 
     try {
+        if (!fs.existsSync(fileDir)) {
+            await fs.promises.mkdir(fileDir, { recursive: true });
+        }
+
+        console.log('saveFile', fileName, content);
+
         await fs.promises.writeFile(fileName, content);
         return '';
     } catch (error) {
