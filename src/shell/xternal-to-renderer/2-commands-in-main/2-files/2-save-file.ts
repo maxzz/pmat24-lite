6656459a04914f5efc5b394498611af5c91672f1
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-10 19:50:36 -0700

    WIP. new-mani. main. update saveFileFromMain() return type

--- src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
 
-export async function saveFileFromMain(fileName: string, content: string): Promise<boolean> {
+export async function saveFileFromMain(fileName: string, content: string): Promise<string> {
 
     // 1. Create dir if needed
     
     const fileDir = path.dirname(fileName || '');
     if (!fileDir) {
         console.error(`saveFile: no dirname "${fileName}"`);
-        return false;
+        return `Cannot save file "${fileName}"`;
     }
 
     if (!fs.existsSync(fileDir)) {
@@ -21,9 +21,9 @@ export async function saveFileFromMain(fileName: string, content: string): Promi
 
     try {
         await fs.promises.writeFile(fileName, content);
-        return true;
+        return '';
     } catch (error) {
         console.error('saveFile', error);
-        return false;
+        return `Cannot save file "${fileName}", error: ${error}`;
     }
 }
