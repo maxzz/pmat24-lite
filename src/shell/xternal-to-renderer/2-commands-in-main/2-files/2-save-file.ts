d50e621efc29013432df864a33ea5d8c3a9b7c81
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-11 14:03:34 -0700

    WIP. new-mani. main. saveFileInMain() code review

--- src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/2-save-file.ts
@@ -2,17 +2,17 @@ import fs from 'fs';
 import path from 'path';
 
 /**
- * Save rendered file from main process.
+ * Save file from renderer in the main process.
  * @param fileName - file name with path
  * @param content - file content
  * @returns - error message or empty string
  */
-export async function saveFileFromMain(fileName: string, content: string): Promise<string> {
+export async function saveFileInMain(fileName: string, content: string): Promise<string> {
 
     // 1. Create dir if needed
 
     const fileDir = path.dirname(fileName || '');
-    if (!fileDir) {
+    if (!fileDir || fileDir === '.') {
         console.error(`saveFile: no dirname "${fileName}"`);
         return `Cannot save file "${fileName}"`;
     }
@@ -26,7 +26,7 @@ export async function saveFileFromMain(fileName: string, content: string): Promi
 
         console.log('saveFile', fileName, content);
 
-        await fs.promises.writeFile(fileName, content);
+        // await fs.promises.writeFile(fileName, content);
         return '';
     } catch (error) {
         console.error('saveFile', error);
