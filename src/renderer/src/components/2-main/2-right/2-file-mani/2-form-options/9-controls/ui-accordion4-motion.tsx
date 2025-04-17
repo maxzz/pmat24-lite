7cf1df39844060e9ed095ebfa655b82f14c18d11
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-16 18:42:53 -0700

    WIP. new-mani. Block4_ScreenDetection. UiAccordion4. code review. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/ui-accordion4-motion.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/ui-accordion4-motion.tsx
@@ -2,112 +2,122 @@ import { Children, createContext, useContext, useState, type ReactNode } from "r
 import { AnimatePresence, motion } from "motion/react";
 //https://codesandbox.io/p/sandbox/framer-motion-accordion-yhixfe?file=%2Fsrc%2FApp.jsx%3A5%2C1-113%2C2
 
-const AccordionContext = createContext({});
+type AccordionContextType = {
+    isActive: boolean;
+    index: number;
+    onChangeIndex: (index: number) => void;
+};
+
+const AccordionContext = createContext<AccordionContextType>({} as AccordionContextType);
 const useAccordion = () => useContext(AccordionContext);
 
-function Accordion({ children, multiple, defaultIndex }) {
-  const [activeIndex, setActiveIndex] = useState(
-    multiple ? [defaultIndex] : defaultIndex
-  );
+function Accordion({ children, multiple, defaultIndex }: { children: ReactNode; multiple?: boolean; defaultIndex?: number | number[]; }) {
+    const [activeIndex, setActiveIndex] = useState<number | undefined | (number | undefined | number[] | undefined[])[]>(multiple ? [defaultIndex] : defaultIndex);
 
-  function onChangeIndex(index) {
-    setActiveIndex((currentActiveIndex) => {
-      if (!multiple) {
-        return index === activeIndex ? -1 : index;
-      }
+    function onChangeIndex(index) {
+        setActiveIndex((currentActiveIndex) => {
+            if (!multiple) {
+                return index === activeIndex ? -1 : index;
+            }
 
-      if (currentActiveIndex.includes(index)) {
-        return currentActiveIndex.filter((i) => i !== index);
-      }
+            if (!currentActiveIndex || !Array.isArray(currentActiveIndex)) {
+                return;
+            }
 
-      return currentActiveIndex.concat(index);
+            if (currentActiveIndex.includes(index)) {
+                return currentActiveIndex.filter((i) => i !== index);
+            }
+
+            return currentActiveIndex.concat(index);
+        });
+    }
+
+    return Children.map(children, (child, index) => {
+        const isActive = multiple && Array.isArray(activeIndex)
+            ? activeIndex.includes(index)
+            : activeIndex === index;
+
+        return (
+            <AccordionContext.Provider value={{ isActive, index, onChangeIndex }}>
+                {child}
+            </AccordionContext.Provider>
+        );
     });
-  }
-
-  return Children.map(children, (child, index) => {
-    const isActive =
-      multiple && Array.isArray(activeIndex)
-        ? activeIndex.includes(index)
-        : activeIndex === index;
-
-    return (
-      <AccordionContext.Provider value={{ isActive, index, onChangeIndex }}>
-        {child}
-      </AccordionContext.Provider>
-    );
-  });
 }
 
 function AccordionItem({ children }) {
-  return <div className="AccordionItem">{children}</div>;
+    return <div className="AccordionItem">{children}</div>;
 }
 
 function AccordionHeader({ children }) {
-  const { isActive, index, onChangeIndex } = useAccordion();
-
-  return (
-    <motion.div
-      className={`AccordionHeader ${isActive ? "active" : ""}`}
-      onClick={() => onChangeIndex(index)}
-    >
-      {children}
-    </motion.div>
-  );
+    const { isActive, index, onChangeIndex } = useAccordion();
+    return (
+        <motion.div
+            className={`AccordionHeader ${isActive ? "active" : ""}`}
+            onClick={() => onChangeIndex(index)}
+        >
+            {children}
+        </motion.div>
+    );
 }
 
 function AccordionPanel({ children }) {
-  const { isActive } = useAccordion();
+    const { isActive } = useAccordion();
 
-  return (
-    <AnimatePresence initial={false}>
-      {isActive && (
-        <motion.div
-          initial={{ height: 0 }}
-          animate={{ height: "auto" }}
-          exit={{ height: 0 }}
-          transition={{ type: "spring", duration: 0.4, bounce: 0 }}
-        >
-          <div className="AccordionPanel">{children}</div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
+    return (
+        <AnimatePresence initial={false}>
+            {isActive && (
+                <motion.div
+                    initial={{ height: 0 }}
+                    animate={{ height: "auto" }}
+                    exit={{ height: 0 }}
+                    transition={{ type: "spring", duration: 0.4, bounce: 0 }}
+                >
+                    <div className="AccordionPanel">{children}</div>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
 }
 
 export function UiAccordion4Example() {
-  return (
-    <section className="App">
-      <h2>collapsible</h2>
-      <Accordion>
-        {[...Array(2)].map((_, i) => (
-          <AccordionItem key={i}>
-            <AccordionHeader>Accordion</AccordionHeader>
-            <AccordionPanel>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quod
-              explicabo, nam sapiente id nostrum ex, ab numquam, doloremque
-              aspernatur quisquam illo! Officiis explicabo laborum incidunt
-              corrupti provident esse eligendi.
-            </AccordionPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
+    return (
+        <section className="App">
+            <h2>collapsible</h2>
+            <Accordion>
+                {[...Array(2)].map(
+                    (_, i) => (
+                        <AccordionItem key={i}>
+                            <AccordionHeader>Accordion</AccordionHeader>
+                            <AccordionPanel>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quod
+                                explicabo, nam sapiente id nostrum ex, ab numquam, doloremque
+                                aspernatur quisquam illo! Officiis explicabo laborum incidunt
+                                corrupti provident esse eligendi.
+                            </AccordionPanel>
+                        </AccordionItem>
+                    )
+                )}
+            </Accordion>
 
-      <br />
+            <br />
 
-      <h2>multiple</h2>
-      <Accordion multiple>
-        {[...Array(2)].map((_, i) => (
-          <AccordionItem key={i}>
-            <AccordionHeader>Accordion</AccordionHeader>
-            <AccordionPanel>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quod
-              explicabo, nam sapiente id nostrum ex, ab numquam, doloremque
-              aspernatur quisquam illo! Officiis explicabo laborum incidunt
-              corrupti provident esse eligendi.
-            </AccordionPanel>
-          </AccordionItem>
-        ))}
-      </Accordion>
-    </section>
-  );
+            <h2>multiple</h2>
+            <Accordion multiple>
+                {[...Array(2)].map(
+                    (_, i) => (
+                        <AccordionItem key={i}>
+                            <AccordionHeader>Accordion</AccordionHeader>
+                            <AccordionPanel>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos quod
+                                explicabo, nam sapiente id nostrum ex, ab numquam, doloremque
+                                aspernatur quisquam illo! Officiis explicabo laborum incidunt
+                                corrupti provident esse eligendi.
+                            </AccordionPanel>
+                        </AccordionItem>
+                    )
+                )}
+            </Accordion>
+        </section>
+    );
 }
