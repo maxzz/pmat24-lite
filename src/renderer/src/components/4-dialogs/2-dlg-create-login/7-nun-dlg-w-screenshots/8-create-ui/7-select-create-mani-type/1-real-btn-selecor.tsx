98630779de2789cd4ea5e0d1ecf1460aae6676b5
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-17 19:35:36 -0700

    WIP. new-mani. cpass. 5. add ButtonCreateCpassForm

---
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/7-nun-dlg-w-screenshots/8-create-ui/7-select-create-mani-type/1-real-btn-selecor.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/ui";
+
+export function ButtonCreateCpassForm() {
+    return (
+        <Button variant="outline" size="xs" className="px-2 justify-start">
+            Create password change form
+        </Button>
+    );
+}
