3208dad8ac4ec980da3a9b80103be0df11b26a0e
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-20 16:15:45 -0700

    WIP. new-mani. add deleteFileUsAtom

--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/index.ts
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/index.ts
@@ -2,4 +2,4 @@ export * from "./0-create-mani-ctx-atoms";
 export * from "./1-create-new-mani-ctx";
 export * from "./2-do-save-mani-atom";
 export * from "./3-do-reset-atom";
-export * from "./4-highlight-field-atom";
+export * from "./5-highlight-field-atom";
