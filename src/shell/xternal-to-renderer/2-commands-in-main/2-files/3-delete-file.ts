c2558212edef23abf034027afa6e4be62dc89604
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-21 14:31:07 -0700

    WIP. new-mani. add deleteFileInMain()

---
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/3-delete-file.ts
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import { errorToString } from '@shell/3-utils-main';
+
+/**
+ * Delete file (not directory) from renderer in the main process.
+ * @param fileName - file name with path
+ * @returns - error message or empty string
+ */
+export async function deleteFileInMain(fileName: string): Promise<string> {
+    try {
+        if (!fs.existsSync(fileName)) {
+            return '';
+        }
+
+        const stats = await fs.promises.stat(fileName);
+        if (stats.isDirectory()) {
+            return 'Cannot delete directory';
+        }
+
+        await fs.promises.unlink(fileName);
+        return '';
+    } catch (error) {
+        return errorToString(error);
+    }
+}
+
+// /**
+//  * Probably OK but not tested abd not needed.
+//  * Delete directory from renderer in the main process.
+//  * @param fileName - file name with path
+//  * @returns - error message or empty string
+//  */
+// async function deleteDirInMain(fileName: string): Promise<string> {
+//     try {
+//         if (!fs.existsSync(fileName)) {
+//             return '';
+//         }
+
+//         const stats = await fs.promises.stat(fileName);
+//         if (!stats.isDirectory()) {
+//             return 'Cannot delete file';
+//         }
+
+//         await fs.promises.rmdir(fileName, { recursive: true });
+//         return '';
+//     } catch (error) {
+//         return errorToString(error);
+//     }
+// }
