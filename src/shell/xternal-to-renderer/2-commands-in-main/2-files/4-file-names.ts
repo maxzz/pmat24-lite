544ee692cd017358ccb14f3b4cb0571a63bf6ae1
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-22 18:07:38 -0700

    WIP. new-mani. file-reveal. add showFileInExplorer() in main

--- src/shell/xternal-to-renderer/2-commands-in-main/2-files/4-file-names.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/4-file-names.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
 import path from 'path';
+import fs from 'fs';
 import { errorToString } from '@shell/3-utils-main';
 
 /**
--- src/shell/xternal-to-renderer/2-commands-in-main/2-files/5-reveal-in-explorer.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/2-files/5-reveal-in-explorer.ts
@@ -1,4 +1,27 @@
+import path from 'path';
+import fs from 'fs';
+import { exec } from 'child_process';
+
 export function revealInExplorer(fpath: string): string {
-    // TODO: implement
-    return 'Not implemented yet';
+    if (!fs.existsSync(fpath)) {
+        return 'File not found';
+    }
+
+    showFileInExplorer(fpath);
+    return '';
+}
+
+async function showFileInExplorer(filePath: string) {
+    const absolutePath = path.resolve(filePath);
+    exec(
+        `explorer /select,"${absolutePath}"`,
+        (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error opening file: ${error}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        }
+    );
 }
