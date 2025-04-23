21a0d66bacf227783f849e94dda3da1157566005
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-22 18:59:13 -0700

    WIP. new-mani. file-delete. ConfirmDeleteFileDialog. 2. add doOpenConfirmDeleteDialogAtom

--- src/renderer/src/store/1-atoms/7-dialogs/index.ts
+++ src/renderer/src/store/1-atoms/7-dialogs/index.ts
@@ -1,4 +1,5 @@
 export * from "./1-filter-dialog-atoms";
 export * from "./2-create-dialog-atoms";
+export * from "./5-confirm-atoms";
 export * from "./7-policy-dialog-atoms";
 export * from "./8-options-dialog-atoms";
