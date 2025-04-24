24bb1e6ef1c71d71b357a7392b63f9c1e5670d4c
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-23 17:43:33 -0700

    WIP. new-mani. file-delete. Confirmation . 2

--- src/renderer/src/components/4-dialogs/5-confirm/1-confirm-delete-file-dlg.tsx
+++ src/renderer/src/components/4-dialogs/5-confirm/1-confirm-delete-file-dlg.tsx
@@ -1,5 +1,5 @@
 import { useAtom, useAtomValue, useSetAtom } from 'jotai';
-import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogClose, DialogFooter, DialogHeader } from '@/ui/shadcn/dialog';
+import { Dialog, DialogContent, DialogTitle, DialogDescription, DialogClose, DialogFooter, DialogHeader, DialogCloseButton } from '@/ui/shadcn/dialog';
 import { Button } from '@/ui/shadcn/button';
 import { doDeleteFileUsAtom, doOpenConfirmDeleteDialogAtom, fileUsOfRightPanelAtom, rightPanelAtom } from '@/store';
 
@@ -13,14 +13,15 @@ export function ConfirmDeleteFileDialog() {
         <Dialog open={confirmDialogOpen} onOpenChange={() => doOpenConfirmDeleteDialog(false)}>
             <DialogContent
                 className={contentClasses}
-                hiddenTitle='Delete file?'
+                hiddenTitle="Delete file?"
+                noClose
             >
-                <DialogHeader className="relative text-base font-bold border-border border-b flex items-start">
+                <DialogHeader className="relative px-4 text-base font-bold border-border border-b flex items-start">
                     <div className="py-2">Delete file?</div>
-                    <Button className="right-1 -top-0.5 p-2 hover:bg-muted hover:rounded-md" tabIndex={-1} onClick={() => doOpenConfirmDeleteDialog(false)} />
+                    <DialogCloseButton className="right-1 -top-0.5 p-2 hover:text-white hover:bg-red-500" tabIndex={-1} onClick={() => doOpenConfirmDeleteDialog(false)} />
                 </DialogHeader>
 
-                <DialogTitle>Delete file?</DialogTitle>
+                {/* <DialogTitle>Delete file?</DialogTitle> */}
                 <DialogBody />
 
             </DialogContent>
@@ -28,27 +29,27 @@ export function ConfirmDeleteFileDialog() {
     );
 }
 
-const contentClasses = "px-4 data-[state=open]:[animation-duration:200ms]";
+const contentClasses = "p-0 pb-4 data-[state=open]:[animation-duration:200ms]";
 
 function DialogBody() {
-    const rightPanel = useAtomValue(rightPanelAtom);
 
-    const fileUs = useAtomValue(fileUsOfRightPanelAtom);
-    if (!fileUs || !rightPanel) {
-        return null;
-    }
-
-    const doDeleteFileUs = useSetAtom(doDeleteFileUsAtom);
+    // const doDeleteFileUs = useSetAtom(doDeleteFileUsAtom);
+    // const rightPanel = useAtomValue(rightPanelAtom);
+    // const fileUs = useAtomValue(fileUsOfRightPanelAtom);
+    // if (!fileUs || !rightPanel) {
+    //     return null;
+    // }
 
     return (
-        <div className="">
+        <div className="px-4">
             <DialogDescription>
                 Are you sure you want to delete the manifest file?
             </DialogDescription>
 
-            <DialogFooter>
+            <DialogFooter className="">
                 <DialogClose asChild>
-                    <Button variant="outline" onClick={() => doDeleteFileUs(rightPanel)}>
+                    <Button variant="outline">
+                    {/* <Button variant="outline" onClick={() => doDeleteFileUs(rightPanel)}> */}
                         Delete
                     </Button>
                 </DialogClose>
