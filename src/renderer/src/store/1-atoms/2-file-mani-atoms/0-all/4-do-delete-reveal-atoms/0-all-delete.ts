39273e7021e9e1dc10326423f6d2a454f8609d35
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-23 18:31:20 -0700

    WIP. new-mani. file-delete. doDeleteFileUsAtom. 1

--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/4-do-delete-reveal-atoms/0-all-delete.ts
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/4-do-delete-reveal-atoms/0-all-delete.ts
@@ -1,12 +1,28 @@
 import { atom } from "jotai";
 import { type FileUsAtom } from "@/store/store-types";
+import { filesAtom } from "@/store";
+import { doDisposeFileUsAtomAtom } from "@/store/store-utils";
 
 export const doDeleteFileUsAtom = atom(null,
     (get, set, fileUsAtom: FileUsAtom) => {
-        // const fileUs = get(fileUsAtom);
-        // if (!fileUs) {
+        const fileUs = get(fileUsAtom);
+        if (!fileUs) {
+            return;
+        }
+
+        const files = get(filesAtom);
+        const newFiles = files.filter((fileUsAtom) => fileUsAtom !== fileUsAtom);
+        set(filesAtom, newFiles);
+
+        // const idx = files.indexOf(fileUsAtom);
+        // if (idx === -1) {
+        //     console.error('not in filesAtom', fileUs);
         //     return;
         // }
+        // files.splice(files.indexOf(fileUsAtom), 1);
+        // set(filesAtom, files);
+
+        set(doDisposeFileUsAtomAtom, fileUsAtom);
 
         //TODO: files tree
         //TODO: right panel
