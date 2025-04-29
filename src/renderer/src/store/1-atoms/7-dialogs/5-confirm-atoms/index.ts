c98b798176dc858c5a8e8e4ecde651602b8769dd
Max Zakharzhevskiy <maxzz@msn.com>
2025-04-28 19:28:48 -0700

    WIP. new-mani. ManiNameDialog. 7. ManiNameDialog. add ManiNameData to params

--- src/renderer/src/store/1-atoms/7-dialogs/5-confirm-atoms/index.ts
+++ src/renderer/src/store/1-atoms/7-dialogs/5-confirm-atoms/index.ts
@@ -1,13 +1,16 @@
-import { atom } from "jotai";
-import { FileUsAtom } from "@/store";
+import { atom, type PrimitiveAtom } from "jotai";
+import { type FileUsAtom } from "@/store/store-types";
+import { type RowInputState } from "@/ui";
+import { getManiDispNameAtomAtom } from "../../2-file-mani-atoms";
 
 export const doOpenConfirmDeleteDialogAtom = atom(false);
 
 // New manifest name
 
 export type ManiNameData = {
-    fileUsAtom: FileUsAtom;         // fileUs to rename
-    resolve: (ok: boolean) => void; // ok or cancel
+    fileUsAtom: FileUsAtom;                     // fileUs to rename
+    nameAtom: PrimitiveAtom<RowInputState>;     // new name
+    resolve: (ok: boolean) => void;             // ok or cancel
 };
 
 export const doOpenManiNameDialogAtom = atom<ManiNameData | undefined>(undefined); // TODO: show only if name is invalid
@@ -22,9 +25,15 @@ export const doVerifyManiNameAtom = atom(
             return false;
         }
 
+        const nameAtom = set(getManiDispNameAtomAtom, fileUsAtom);
+        if (!nameAtom) {
+            return false;
+        }
+
         const resolveName = new Promise<boolean>((resolve) => {
             set(doOpenManiNameDialogAtom, {
                 fileUsAtom: fileUsAtom,
+                nameAtom,
                 resolve,
             });
         });
