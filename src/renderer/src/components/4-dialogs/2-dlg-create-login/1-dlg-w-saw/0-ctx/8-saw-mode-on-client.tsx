25f51043f77a359e18fcc9f9d2e3f9eb5a9cb0f5
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-03 14:52:47 -0700

    WIP. new-mani. sawModeStateAtom code review

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/0-ctx/8-saw-mode-on-client.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/0-ctx/8-saw-mode-on-client.tsx
@@ -2,44 +2,24 @@ import { atom, PrimitiveAtom } from "jotai";
 import { hasMain, R2MCalls } from "@/xternal-to-main";
 import { doClose_SawMonitorAtom } from "@/store";
 
-export const doTurnOnSawModeOnClientAtom = atom(
-    null,
-    (get, set) => {
-        set(doSawModeOnClientAtom, { turnOn: true, canceledByMain: false });
-    }
-);
+// doCancelSawModeByMainAtom is called from main (gate-react-listener-atom.ts) to cancel the Saw mode.
 
-export const doTurnOffSawModeOnClientAtom = atom(
-    null,
-    (get, set) => {
-        set(doSawModeOnClientAtom, { turnOn: false, canceledByMain: hasMain(), cancelByMainAtom: doClose_SawMonitorAtom });
-    }
-);
-
-/**
- * This is called from main (gate-react-listener-atom.ts) to cancel the Saw mode.
- */
-export const doCancelSawModeByMainAtom = atom(
-    null,
-    (get, set) => {
-        set(doSawModeOnClientAtom, { turnOn: false, canceledByMain: true, cancelByMainAtom: doClose_SawMonitorAtom });
-    }
-);
+export const doCancelSawModeByMainAtom = atom(null, (get, set) => set(setSawMonitorWindowSizeAtom, { turnOn: false, canceledByMain: true, cancelByMainAtom: doClose_SawMonitorAtom }));
+export const doTurnOnSawModeOnClientAtom = atom(null, (get, set) => set(setSawMonitorWindowSizeAtom, { turnOn: true, canceledByMain: false }));
+export const doTurnOffSawModeOnClientAtom = atom(null, (get, set) => set(setSawMonitorWindowSizeAtom, { turnOn: false, canceledByMain: hasMain(), cancelByMainAtom: doClose_SawMonitorAtom }));
 
 // Implementation
 
-const _sawModeAtom = atom<boolean>(false);
-
 type DoSawModeOnClientAtomParams = {
-    turnOn: boolean;                        // is to set the mode on or off
-    canceledByMain: boolean;                // is set if app close button was pressed from main process
-    cancelByMainAtom?: PrimitiveAtom<void>; // cancel will be set to false when mode is turnned off by main process
+    turnOn: boolean;                        // Is to set the mode on or off
+    canceledByMain: boolean;                // Is set if app close button was pressed from main process
+    cancelByMainAtom?: PrimitiveAtom<void>; // Cancel will be set to false when mode is turnned off by main process
 };
 
-const doSawModeOnClientAtom = atom(
+const setSawMonitorWindowSizeAtom = atom(
     null,
     (get, set, { turnOn, canceledByMain, cancelByMainAtom }: DoSawModeOnClientAtomParams) => {
-        const isOn = get(_sawModeAtom);
+        const isOn = get(_sawModeStateAtom);
 
         if (turnOn) {
             if (isOn) {
@@ -47,7 +27,7 @@ const doSawModeOnClientAtom = atom(
             }
 
             R2MCalls.setSawModeOnMain({ setOn: true });
-            set(_sawModeAtom, true);
+            set(_sawModeStateAtom, true);
         } else {
             if (!isOn) {
                 return;
@@ -56,9 +36,11 @@ const doSawModeOnClientAtom = atom(
             canceledByMain && cancelByMainAtom && set(cancelByMainAtom);
 
             R2MCalls.setSawModeOnMain({ setOn: false });
-            set(_sawModeAtom, false);
+            set(_sawModeStateAtom, false);
         }
     }
 );
+
+const _sawModeStateAtom = atom<boolean>(false); // Is saw mode on or off
 
 //TODO: check what we do on cancel from main.
