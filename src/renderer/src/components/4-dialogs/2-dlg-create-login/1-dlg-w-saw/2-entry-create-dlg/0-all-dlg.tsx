3662674f39dc871fab99b49e7de9c977cd238bbd
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-05 18:29:38 -0700

    WIP. new-mani. noNewManiDlg. 3. doSaveNewManiTriggerAtom. 6 DialogCreateManiV3Saw will not close yourself

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/2-entry-create-dlg/0-all-dlg.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/2-entry-create-dlg/0-all-dlg.tsx
@@ -1,34 +1,43 @@
-import { useAtomValue, useSetAtom } from "jotai";
-import { useDissmissNextToasts } from "@/utils";
+import { useAtomValue } from "jotai";
+import { classNames, useDissmissNextToasts } from "@/utils";
 import * as D from "@/ui/shadcn/dialog";
 import { Button } from "@/ui";
 import { Cross2Icon } from "@radix-ui/react-icons";
-import { close_NewManiDlgAtom, isOpen_NewManiDlgAtom } from "@/store/1-atoms/7-dialogs";
-import { DialogBottomButtons } from "./4-dlg-bottom-buttons";
+import { type NewManiDlgData, dataToOpen_NewManiDlgAtom } from "@/store/1-atoms/7-dialogs";
 import { ContentEditorSelector } from "../../2-mani-content-editor";
-import { ManiLoginNameGuarded } from "./3-mani-login-name";
 import { SawPageHeader } from "./2-saw-page-header";
+import { ManiLoginNameGuarded } from "./3-mani-login-name";
+import { ComponentPropsWithoutRef } from "react";
 
 export function DialogCreateManiV3Saw() {
-    const isDlgOpen = useAtomValue(isOpen_NewManiDlgAtom);
-    const closeDlg = useSetAtom(close_NewManiDlgAtom);
+    const dataToOpen: NewManiDlgData | undefined = useAtomValue(dataToOpen_NewManiDlgAtom);
     
-    // if (!isDlgOpen) { //TOD: It should not be mounted all the time and animated with framer-motion, but we need to disconnect radix-ui animations.
+    // if (!isDlgOpen) { //TODO: It should not be mounted all the time and animated with framer-motion, but we need to disconnect radix-ui animations.
     //     return null;
     // }
 
+    if (!dataToOpen) {
+        return null;
+    }
+
+    function onCloseDlg(ok: boolean) {
+        if (!dataToOpen) {
+            throw new Error('no.in.data');
+        }
+        dataToOpen.resolve(ok);
+    }
+
     return (
-        <D.Dialog open={isDlgOpen} onOpenChange={closeDlg}>
+        <D.Dialog open={!!dataToOpen} onOpenChange={() => onCloseDlg(false)}>
             <D.DialogContent className={dialogClasses} overlayClasses="bg-background/10 backdrop-blur-[1px]" hiddenTitle="Create manifest" noClose>
-                <DialogSawBody />
+                <DialogSawBody onCloseDlg={onCloseDlg} />
             </D.DialogContent>
         </D.Dialog>
     );
 }
 
-function DialogSawBody() {
+function DialogSawBody({ onCloseDlg }: { onCloseDlg: (ok: boolean) => void; }) {
 
-    const closeDlg = useSetAtom(close_NewManiDlgAtom);
     useDissmissNextToasts();
 
     return (
@@ -40,7 +49,7 @@ function DialogSawBody() {
                     </div>
                 </D.DialogTitle>
 
-                <Button className={closeButtonClasses} variant="ghost" size="xs" tabIndex={-1} onClick={closeDlg}>
+                <Button className={closeButtonClasses} variant="ghost" size="xs" tabIndex={-1} onClick={() => onCloseDlg(false)}>
                     <Cross2Icon className="size-4" />
                 </Button>
             </D.DialogHeader>
@@ -51,7 +60,20 @@ function DialogSawBody() {
                 <ContentEditorSelector />
             </div>
 
-            <DialogBottomButtons className="py-3 border-t border-foreground/20" />
+            <DialogBottomButtons className="py-3 border-t border-foreground/20" onCloseDlg={onCloseDlg} />
+        </div>
+    );
+}
+
+/**
+ * Button Save button is always enabled to show toast as hint in case of failure.
+ */
+function DialogBottomButtons({ onCloseDlg, className, ...rest }: ComponentPropsWithoutRef<"div"> & { onCloseDlg: (ok: boolean) => void; }) {
+    return (
+        <div className={classNames("relative px-4 flex items-center justify-end gap-1", className)} {...rest}>
+            <Button variant="default" size="xs" onClick={() => onCloseDlg(true)}>
+                Save
+            </Button>
         </div>
     );
 }
