ef7ae6ed45d2a717fc8837060b3f9f89869f63a1
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-05 18:24:35 -0700

    WIP. new-mani. noNewManiDlg. 3. doSaveNewManiTriggerAtom. 6 remove side effects from doOpenDlgNewManiSawAtom

--- src/renderer/src/store/1-atoms/7-dialogs/2-create-dialog-atoms/2-open-new-mani-dlg.ts
+++ src/renderer/src/store/1-atoms/7-dialogs/2-create-dialog-atoms/2-open-new-mani-dlg.ts
@@ -4,7 +4,7 @@ import { newManiContent } from "../../2-file-mani-atoms";
 import { appSettings } from "../../9-ui-state";
 
 export const dataToOpen_NewManiDlgAtom = atom((get) => get(_isDlgOpenAtom));
-export const open_NewManiDlgAtom = atom(() => null, (get, set, openManiDlgData: NewManiDlgData) => set(doOpenDlgNewManiSawAtom, openManiDlgData));
+const open_NewManiDlgAtom = atom(() => null, (get, set, openManiDlgData: NewManiDlgData) => set(doOpenDlgNewManiSawAtom, openManiDlgData));
 export const close_NewManiDlgAtom = atom(() => null, (get, set) => set(doOpenDlgNewManiSawAtom, undefined));
 
 export type NewManiDlgData = {
@@ -15,11 +15,10 @@ const doOpenDlgNewManiSawAtom = atom(
     (get) => null,
     (get, set, open: NewManiDlgData | undefined) => {
         if (open) {
-            if (newManiContent.maniForCpassAtom) { // cpass dialog is embedded, so don't open dialog
-                return;
-            }
+            // if (newManiContent.maniForCpassAtom) { // cpass dialog is embedded, so don't open dialog
+            //     return;
+            // }
 
-            const { noNewManiDlg } = appSettings.appUi.uiAdvanced;
         } else {
             // const currentFileUsAtomAtom = get(newManiContent.newFileUsAtomAtom);
             // set(newManiContent.newFileUsAtomAtom, undefined);
@@ -34,7 +33,6 @@ const doOpenDlgNewManiSawAtom = atom(
 const _isDlgOpenAtom = atom<NewManiDlgData | undefined>(undefined);
 
 export async function asyncExecuteNewManiDlg(set: Setter): Promise<boolean> {
-    const resolveDlg = new Promise<boolean>((resolve) => { set(open_NewManiDlgAtom, { resolve, }); });
-    const ok = await resolveDlg;
+    const ok = await (new Promise<boolean>((resolve) => { set(open_NewManiDlgAtom, { resolve, }); }));
     return ok;
 }
