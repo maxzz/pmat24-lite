b8d183d4e710e80955eb4dc24064538145885261
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-10 14:06:59 -0700

    WIP. new-mani. splash. animation restart. 2. useMotionTimeline ode review. 2 types

--- src/renderer/src/components/2-main/0-all/2-welcome-page/8-timeline.tsx
+++ src/renderer/src/components/2-main/0-all/2-welcome-page/8-timeline.tsx
@@ -7,18 +7,21 @@ type Animation = AnimateParams | Animation[];
 
 export const useMotionTimeline = (keyframes: Animation[], count: number = 1, deps?: DependencyList) => {
     const mounted = useRef(true);
-
     const [scope, animate] = useAnimate();
 
-    useEffect(() => {
-        mounted.current = true;
+    useEffect(
+        () => {
+            mounted.current = true;
 
-        handleAnimate();
+            console.log("--------------------------");
+            
+            handleAnimate();
 
-        return () => {
-            mounted.current = false;
-        };
-    }, deps || []);
+            return () => {
+                mounted.current = false;
+            };
+        }, deps || []
+    );
 
     async function handleAnimate() {
         for (let i = 0; i < count; i++) {
@@ -34,13 +37,17 @@ export const useMotionTimeline = (keyframes: Animation[], count: number = 1, dep
     async function processAnimation(animation: Animation) {
         // If list of animations, run all concurrently
         if (Array.isArray(animation[0])) {
-            await Promise.all(
-                animation.map(async (a) => {
+            await Promise.all((animation as Animation[]).map(
+                async (a: Animation): Promise<void> => {
+                    console.log("    sub", JSON.stringify(a));
+                    
                     await processAnimation(a as Animation);
-                })
-            );
+                }
+            ));
         } else {
             // Else run the single animation
+            console.log("start animation", JSON.stringify(animation));
+            
             await animate(...(animation as AnimateParams));
         }
     }
