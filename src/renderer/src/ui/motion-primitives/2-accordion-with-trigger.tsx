3df10e6afa46216a460cf02ba892d7739cb39b16
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-11 17:50:23 -0700

    WIP. form-detection. 4. Accordion major code clean up. 3

--- src/renderer/src/ui/motion-primitives/2-accordion-with-trigger.tsx
+++ src/renderer/src/ui/motion-primitives/2-accordion-with-trigger.tsx
@@ -1,17 +1,17 @@
-import { type AriaAttributes, type ReactNode, useCallback } from 'react';
-import { useSnapshot } from 'valtio';
-import { classNames } from '@/utils';
-import { Accordion, AccordionItem, AccordionContent, useAccordion, AccordionItemAugmentedProps } from './1-accordion'; //https://motion-primitives.com/docs/accordion
-import { Button } from '../shadcn';
-import { SymbolChevronDown } from '../icons';
-import { appSettings } from '@/store';
+import { type AriaAttributes, type ReactNode, useCallback } from "react";
+import { useSnapshot } from "valtio";
+import { classNames } from "@/utils";
+import { Accordion, AccordionItem, AccordionContent, useAccordion, AccordionItemAugmentedProps } from "./1-accordion"; //https://motion-primitives.com/docs/accordion
+import { Button } from "../shadcn";
+import { SymbolChevronDown } from "../icons";
+import { appSettings } from "@/store";
 
 export function AccordionWithTrigger({ truggerText, formIdx, name, children }: { truggerText: ReactNode; formIdx: number; name: string; children: ReactNode; }) {
     const [open, toggleOpen] = useAccordionState({ formIdx, name });
     return (
         <Accordion
-            className='flex w-full flex-col'
-            transition={{ type: 'spring', stiffness: 120, damping: 20 }}
+            className="flex w-full flex-col"
+            transition={{ type: "spring", stiffness: 120, damping: 20 }}
             variants={{
                 expanded: { opacity: 1, scale: 1, },
                 collapsed: { opacity: 0, scale: 0.7, },
@@ -38,12 +38,7 @@ function AccordionTrigger({ children, ...rest }: { children: ReactNode; }) {
     const value = (rest as AccordionItemAugmentedProps).value;
     const isExpanded = value === expandedValue;
     return (
-        <Button
-            className={sectionTriggerClasses} 
-            {...ariaExpandedAttrs(isExpanded)}
-            // aria-expanded={isExpanded} {...(isExpanded ? { 'data-expanded': '' } : { 'data-closed': '' })}
-            onClick={() => value !== undefined && toggleItem(value)}
-        >
+        <Button className={sectionTriggerClasses} {...ariaExpandedAttrs(isExpanded)} onClick={() => value !== undefined && toggleItem(value)}>
             <div className="w-full text-start">
                 {children}
             </div>
@@ -67,10 +62,10 @@ function useAccordionState({ formIdx, name }: { formIdx: number; name: string; }
 
 function ariaExpandedAttrs(isExpanded: boolean): AriaAttributes {
     return {
-        'aria-expanded': isExpanded,
-        ...(isExpanded ? { 'data-expanded': '' } : { 'data-closed': '' }),
+        "aria-expanded": isExpanded,
+        ...(isExpanded ? { "data-expanded": '' } : { "data-closed": '' }),
     };
 }
 
 const sectionTriggerClasses = "mt-1 w-full font-normal border-border flex items-center gap-1";
-const contentClasses = 'origin-left';
+const contentClasses = "origin-left";
