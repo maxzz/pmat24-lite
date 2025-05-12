0c1e3da5458c586f304adb669a8557eae5c19330
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-11 19:18:08 -0700

    WIP. form-detection. 7. proper layout for TabContent_ManualForm w/ InFormOptions

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/0-all/3-tab-content-manual.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/0-all/3-tab-content-manual.tsx
@@ -1,11 +1,9 @@
 import { type ComponentPropsWithoutRef } from "react";
 import { type MFormContextProps } from "@/store/1-atoms/2-file-mani-atoms";
 import { ManualModeView } from "../2-manual";
-import { InFormOptions } from "../../2-form-options";
 
 export function TabContent_ManualForm({ ctx, ...rest }: { ctx: MFormContextProps; } & ComponentPropsWithoutRef<'div'>) {
-    return (<>
+    return (
         <ManualModeView ctx={ctx} {...rest} />
-        <InFormOptions ctx={ctx} />
-    </>);
+    );
 }
