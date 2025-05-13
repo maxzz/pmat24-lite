7eaf656f793a07a17650b9a22ef96232c9ac0dea
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-12 19:43:27 -0700

    WIP. form-detection. define real context menu

---
+++ src/renderer/src/components/2-main/1-left/0-all/0-all.tsx
@@ -0,0 +1,44 @@
+import { L_PanelHeader } from "../1-header/0-all-header";
+import { FilesTreeView } from "../2-files-list";
+import { panel1Classes, panel2Classes, panel3Classes } from "../../0-all/1-working-area/3-middle/shared-classes";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/ui/shadcn";
+
+export function PanelA() {
+    return (
+        <div className={`${panel1Classes} pr-0`}>
+            <div className={`${panel2Classes} rounded-l`}>
+                <div className={panel3Classes}>
+                    <L_PanelHeader />
+
+                    <ContextMenu>
+                        <ContextMenuTrigger asChild>
+                            <div className="">
+                                <FilesTreeView />
+                            </div>
+                        </ContextMenuTrigger>
+                        <ContextMenuContent>
+                            <ContextMenuItem>Reveal in File Explorer</ContextMenuItem>
+                            <ContextMenuItem>Delete</ContextMenuItem>
+                            <ContextMenuItem>Rename</ContextMenuItem>
+                        </ContextMenuContent>
+
+                    </ContextMenu>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// export function PanelA() {
+//     return (
+//         <div className={`${panel1Classes} pr-0`}>
+//             <div className={`${panel2Classes} rounded-l`}>
+//                 <div className={panel3Classes}>
+//                     <L_PanelHeader />
+
+//                     <FilesTreeView />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
