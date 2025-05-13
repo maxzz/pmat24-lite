a751def308746154a418cb02a19cda3555796806
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-12 19:40:16 -0700

    WIP. form-detection. integrate test for shadcn/context-menu

--- src/renderer/src/components/2-main/1-left/0-all/index.tsx
+++ src/renderer/src/components/2-main/1-left/0-all/index.tsx
@@ -1,6 +1,8 @@
 import { L_PanelHeader } from "../1-header/0-all-header";
 import { FilesTreeView } from "../2-files-list";
 import { panel1Classes, panel2Classes, panel3Classes } from "../../0-all/1-working-area/3-middle/shared-classes";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/ui/shadcn";
+
 
 export function PanelA() {
     return (
@@ -9,9 +11,35 @@ export function PanelA() {
                 <div className={panel3Classes}>
                     <L_PanelHeader />
 
-                    <FilesTreeView />
+                    <ContextMenu>
+                        <ContextMenuTrigger asChild>
+                            <div className="">
+                                <FilesTreeView />
+                            </div>
+                        </ContextMenuTrigger>
+                        <ContextMenuContent>
+                            <ContextMenuItem>Item 1</ContextMenuItem>
+                            <ContextMenuItem>Item 2</ContextMenuItem>
+                            <ContextMenuItem>Item 3</ContextMenuItem>
+                        </ContextMenuContent>
+
+                    </ContextMenu>
                 </div>
             </div>
         </div>
     );
 }
+
+// export function PanelA() {
+//     return (
+//         <div className={`${panel1Classes} pr-0`}>
+//             <div className={`${panel2Classes} rounded-l`}>
+//                 <div className={panel3Classes}>
+//                     <L_PanelHeader />
+
+//                     <FilesTreeView />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
