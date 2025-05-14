4e36132dcf689fefb66fa6a060def23274407c95
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-14 15:03:57 -0700

    WIP. form-detection. context-menu. fix quick right menu sequenced click wrong position

--- src/renderer/src/components/2-main/1-left/2-files-list/0-all/3-context-menu.tsx
+++ src/renderer/src/components/2-main/1-left/2-files-list/0-all/3-context-menu.tsx
@@ -44,7 +44,9 @@ export function FilesTreeViewcontextMenu({ children }: { children: ReactNode; })
                 </div>
             </ContextMenuTrigger>
 
-            <ContextMenuContent>
+            {/* <ContextMenuContent className="abc data-[state=closed]:duration-0 data-[state=closed]:[animation-name=none]"> */}
+            <ContextMenuContent className="abc data-[state=closed]:![animation:none]">
+            {/* <ContextMenuContent className="abc data-[state=open]:![animation:none]"> */}
                 <ContextMenuLabel className="text-xs">File name</ContextMenuLabel>
                 <ContextMenuItem className="text-xs">Reveal in File Explorer</ContextMenuItem>
                 <ContextMenuItem className="text-xs" onClick={onDelete}>Delete</ContextMenuItem>
