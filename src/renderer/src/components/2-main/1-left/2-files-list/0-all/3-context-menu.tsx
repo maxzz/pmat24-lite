36983d5a28d99f6d297f780830004fcf3ff3d037
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-13 20:14:17 -0700

    WIP. form-detection. FilesTreeViewcontextMenu. 1. add onRename()

--- src/renderer/src/components/2-main/1-left/2-files-list/0-all/3-context-menu.tsx
+++ src/renderer/src/components/2-main/1-left/2-files-list/0-all/3-context-menu.tsx
@@ -1,7 +1,16 @@
 import { type ReactNode } from "react";
+import { useAtomValue } from "jotai";
 import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuLabel, ContextMenuTrigger } from "@/ui/shadcn";
+import { doVerifyManiNameAtom } from "@/store";
 
 export function FilesTreeViewcontextMenu({children}: { children: ReactNode; }) {
+    const renameFileUsAtom = useAtomValue(doVerifyManiNameAtom);
+    function onRename() {
+        //TODO: get atom from tree item and
+        //TODO: click may happen on different tree item, i.e. not only on selected one
+        //currentAtom && doVerifyNameBeforeSave(currentAtom)
+        console.log('onRename');
+    }
     return (
         <ContextMenu>
             <ContextMenuTrigger asChild>
@@ -13,7 +22,7 @@ export function FilesTreeViewcontextMenu({children}: { children: ReactNode; }) {
                 <ContextMenuLabel className="text-xs">File</ContextMenuLabel>
                 <ContextMenuItem className="text-xs">Reveal in File Explorer</ContextMenuItem>
                 <ContextMenuItem className="text-xs">Delete</ContextMenuItem>
-                <ContextMenuItem className="text-xs">Rename</ContextMenuItem>
+                <ContextMenuItem className="text-xs" onClick={onRename}>Rename</ContextMenuItem>
             </ContextMenuContent>
         </ContextMenu>
     );
