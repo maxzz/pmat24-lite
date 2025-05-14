957c379b59fa4544f45eafeaf0446d6f37c84bf2
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-13 20:09:16 -0700

    WIP. form-detection. extract FilesTreeViewcontextMenu. 4

--- src/renderer/src/components/2-main/1-left/0-all/0-all.tsx
+++ src/renderer/src/components/2-main/1-left/0-all/0-all.tsx
@@ -1,7 +1,6 @@
 import { L_PanelHeader } from "../1-header/0-all-header";
-import { FilesTreeView } from "../2-files-list";
+import { FilesTreeView, FilesTreeViewcontextMenu } from "../2-files-list";
 import { panel1Classes, panel2Classes, panel3Classes } from "../../0-all/1-working-area/3-middle/shared-classes";
-import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuLabel, ContextMenuTrigger } from "@/ui/shadcn";
 
 export function PanelA() {
     return (
@@ -32,24 +31,3 @@ export function PanelA() {
 //         </div>
 //     );
 // }
-
-//05.12.25
-//TODO: get tree item under cursor and show context menu
-
-function FilesTreeViewcontextMenu({children}: { children: React.ReactNode; }) {
-    return (
-        <ContextMenu>
-            <ContextMenuTrigger asChild>
-                <div className="">
-                    {children}
-                </div>
-            </ContextMenuTrigger>
-            <ContextMenuContent>
-                <ContextMenuLabel className="text-xs">File</ContextMenuLabel>
-                <ContextMenuItem className="text-xs">Reveal in File Explorer</ContextMenuItem>
-                <ContextMenuItem className="text-xs">Delete</ContextMenuItem>
-                <ContextMenuItem className="text-xs">Rename</ContextMenuItem>
-            </ContextMenuContent>
-        </ContextMenu>
-    );
-}
