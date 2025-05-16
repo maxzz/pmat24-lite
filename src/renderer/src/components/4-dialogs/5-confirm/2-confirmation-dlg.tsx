6ef7cbf7e20575bfdba1a791331b4541cbdb2832
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-16 13:06:00 -0700

    WIP. form-detection. 4. add DialogContentWrapper

--- src/renderer/src/components/4-dialogs/5-confirm/2-confirmation-dlg.tsx
+++ src/renderer/src/components/4-dialogs/5-confirm/2-confirmation-dlg.tsx
@@ -62,3 +62,24 @@ function DialogBody({ confirmDialogOpen, onDlgClose }: { confirmDialogOpen: Conf
         </div>
     );
 }
+
+function DialogContentWrapper({ title, className, children, onDlgClose }: { title: string; className: string; children: React.ReactNode; onDlgClose: (ok: boolean) => void; }) {
+    return (
+        <DialogContent
+            className={className}
+            overlayClasses="bg-background/10 backdrop-blur-[1px]"
+            hiddenTitle={title}
+            noClose
+        >
+            <DialogHeader className="relative pl-4 pr-2 py-2 text-sm font-bold border-border border-b flex flex-row items-center justify-between space-y-0">
+                <div>
+                    {title}
+                </div>
+                <DialogCloseButton className="!relative !right-0 !top-0 p-2 hover:text-white hover:bg-red-500 hover:opacity-100" tabIndex={-1} onClick={() => onDlgClose(false)} />
+            </DialogHeader>
+
+            {children}
+
+        </DialogContent>
+    );
+}
