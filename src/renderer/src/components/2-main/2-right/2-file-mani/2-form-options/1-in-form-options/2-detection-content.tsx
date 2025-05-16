e5fe6b0ab476e64aa14ee542058ccc98a5f39d6c
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-15 18:33:32 -0700

    WIP. form-detection. 3. add real FormDetection content

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/2-detection-content.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/2-detection-content.tsx
@@ -1,25 +1,26 @@
 import { type ComponentPropsWithoutRef } from "react";
 import { useAtomValue } from "jotai";
 import { classNames } from "@/utils";
-import { FormOptionsAndFileUsCtxAtoms, OFormContextProps, type NFormContextProps } from "@/store/1-atoms/2-file-mani-atoms";
+import { FormOptionsAndFileUsCtxAtoms, type OFormContextProps, type NFormContextProps } from "@/store/1-atoms/2-file-mani-atoms";
 import { AccordionWithTrigger } from "@/ui/motion-primitives";
+import { Block4_ScreenDetection } from "../0-all/2-4-screen-detection";
 
 export function WebDetectionContenty({ ctx }: { ctx: OFormContextProps; }) {
     const formIdx = ctx.oAllAtoms.options.formIdx;
     return (
-        <p className={textClasses}>
-            Kick off your experience by setting up Motion-Primitives. This
-            section covers the basics of installation and how to add animations
-            to your projects. You’ll get familiar with the initial setup and the
-            core features quickly.
-        </p>
+        <div className={textClasses}>
+            <Block4_ScreenDetection ctx={ctx} />
+        </div>
     );
 }
 
 export function W32DetectionContent({ ctx }: { ctx: OFormContextProps; }) {
-    return (<>
-        win32
-    </>);
+    return (
+        <div>
+            win32
+            <Block4_ScreenDetection ctx={ctx} />
+        </div>
+    );
 }
 
 const textClasses = 'pl-6 pr-2 font-normal';
