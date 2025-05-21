8af594ec67ad6d652d6083f7e4789ceb9822c364
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-21 16:00:36 -0700

    WIP. policy-dlg. PolicyEditorDlg. code review. 9. done

--- src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/2-dlg-all.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/2-dlg-all.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useMemo } from "react";
 import { type PrimitiveAtom, useAtomValue, useSetAtom } from "jotai";
-import { type Mani } from "@/store/manifest";
 import { Dialog, DialogCloseButton, DialogContent } from "@/ui";
+import { type Mani } from "@/store/manifest";
 import { createUiAtoms, onChangeWithScopeDebounced } from "./0-create-ui-atoms";
-import { PolicyEditorBody } from "./3-dlg-body";
 import { doClosePolicyDlgAtom } from "./1-do-close-dlg-atom";
 import { doUpdateExplanationAtom } from "./1-util-atoms";
+import { PolicyEditorBody } from "./3-dlg-body";
+import { DialogTitleHeader } from "../../5-confirm/3-dialog-title-header";
 
 export function PolicyEditorDlg({ openAtom, toastIdAtom, policiesAtom }: PolicyEditorNewDlgProps) {
     const isOpen = useAtomValue(openAtom);
@@ -53,15 +54,18 @@ function PolicyEditorDlgIsOpen({ openAtom, toastIdAtom, policiesAtom }: PolicyEd
     return (
         <Dialog open={isOpen} onOpenChange={() => closeDlg(false)} modal>
 
-            <DialogContent className={contentClasses} modal withScroll noClose>
+            {/* <DialogContent className={contentClasses} modal withScroll noClose>
                 <PolicyEditorBody dlgUiCtx={dlgUiCtx} closeDlg={closeDlg} />
                 
                 <DialogCloseButton className="p-2 top-3 hover:text-white hover:bg-red-500 hover:opacity-100 active:scale-[.97] focus:ring-0 transition-colors" tabIndex={-1} />
-            </DialogContent>
+            </DialogContent> */}
+
+            <DialogTitleHeader title="Password Policy Editor" className={contentClasses} onDlgClose={closeDlg}>
+                <PolicyEditorBody dlgUiCtx={dlgUiCtx} closeDlg={closeDlg} />
+            </DialogTitleHeader>
 
         </Dialog>
     );
 }
 
-// const contentClasses = "p-0 !w-72 rounded-lg max-w-sm gap-0 data-[state=open]:[animation-duration:200ms]";
-const contentClasses = "px-4 py-4 w-fit min-w-[400px] max-w-[500px] text-xs select-none";
+const contentClasses = "p-0 pb-2 w-fit min-w-[420px] max-w-[500px] text-xs gap-1 select-none";
--- src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/3-dlg-body.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/3-dlg-body.tsx
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai";
 import { type PolicyDlgTypes } from "./0-conv";
-import { Button } from "@/ui";
+import { Button, DialogDescription } from "@/ui";
 import { Check } from "../9-constrols";
 import { DialogHeader } from "../1-header";
 import { SectionRuleTypes } from "../2-complexity";
@@ -15,49 +15,55 @@ type PolicyEditorBodyProps = {
 
 export function PolicyEditorBody({ dlgUiCtx, closeDlg }: PolicyEditorBodyProps) {
     const [enabled, setEnabled] = useAtom(dlgUiCtx.enabledAtom);
-    return (<>
-        <DialogHeader
+    return (
+        <div className="px-4 pb-2 grid gap-4">
+            {/* <DialogHeader
             header="Password Policy Editor"
             subHeader="Specify password complexity, history and generation requirements."
-        />
+        /> */}
 
-        <Check className="mb-2" checked={enabled} onChange={() => setEnabled((v) => !v)}>
-            Enable password policy
-        </Check>
-
-        <div className={classNames("flex flex-col space-y-4", !enabled && "opacity-10 pointer-events-none")}>
-
-            {/* Predefined or Custom rule */}
-            <div>
-                <h2 className={sectionClasses}>
-                    Password complexity
-                </h2>
-                <SectionRuleTypes dlgUiCtx={dlgUiCtx} />
+            <div className="text-xs text-foreground">
+                Specify password complexity, history and generation requirements.
             </div>
 
-            {/* History */}
-            <div>
-                <h2 className={sectionClasses}>
-                    Password history restrictions
-                </h2>
-                <SectionHistory dlgUiCtx={dlgUiCtx} />
+            <Check className="1mb-2" checked={enabled} onChange={() => setEnabled((v) => !v)}>
+                Enable password policy
+            </Check>
+
+            <div className={classNames("flex flex-col space-y-4", !enabled && "opacity-10 pointer-events-none")}>
+
+                {/* Predefined or Custom rule */}
+                <div>
+                    <h2 className={sectionClasses}>
+                        Password complexity
+                    </h2>
+                    <SectionRuleTypes dlgUiCtx={dlgUiCtx} />
+                </div>
+
+                {/* History */}
+                <div>
+                    <h2 className={sectionClasses}>
+                        Password history restrictions
+                    </h2>
+                    <SectionHistory dlgUiCtx={dlgUiCtx} />
+                </div>
+
+                {/* Generation */}
+                <div>
+                    <h2 className={sectionClasses}>
+                        Password generation
+                    </h2>
+                    <SectionGenerationBy dlgUiCtx={dlgUiCtx} />
+                </div>
             </div>
 
-            {/* Generation */}
-            <div>
-                <h2 className={sectionClasses}>
-                    Password generation
-                </h2>
-                <SectionGenerationBy dlgUiCtx={dlgUiCtx} />
+            {/* Buttons */}
+            <div className="flex items-center justify-center gap-x-2">
+                <Button size="xs" className="min-w-16" onClick={() => closeDlg(true)}>OK</Button>
+                <Button size="xs" className="min-w-16" onClick={() => closeDlg(false)}>Cancel</Button>
             </div>
         </div>
-
-        {/* Buttons */}
-        <div className="flex items-center justify-center gap-x-2">
-            <Button size="xs" className="min-w-16" onClick={() => closeDlg(true)}>OK</Button>
-            <Button size="xs" className="min-w-16" onClick={() => closeDlg(false)}>Cancel</Button>
-        </div>
-    </>);
+    );
 }
 
 const sectionClasses = "text-xs font-semibold border-gray-500/50 border-b";
