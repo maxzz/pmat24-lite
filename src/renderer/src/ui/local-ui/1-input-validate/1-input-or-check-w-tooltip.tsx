b3a95484794c5a28f60ba997b4f39c3018c5aa91
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-20 17:59:55 -0700

    WIP. form-detection. old InputOrCheckWithTooltip. 1. combined

--- src/renderer/src/ui/local-ui/1-input-validate/1-input-or-check-w-tooltip.tsx
+++ src/renderer/src/ui/local-ui/1-input-validate/1-input-or-check-w-tooltip.tsx
@@ -1,9 +1,12 @@
+import { type FC, type ReactNode, useState } from "react";
+import { useAtomValue } from "jotai";
+import { classNames } from "@/utils";
 import { type OptionInputWTypeProps } from "./9-types";
 import { SymbolWarning } from "@/ui/icons";
-import { TooltipShellWithErrorIcon } from "./2-option-input-tooltip-shell";
 import { OptionAsString } from "./4-option-string";
 import { OptionAsTextarea } from "./5-option-textarea";
 import { OptionAsCheckbox } from "./6-option-checkbox";
+import { type RowInputStateAtom, Tooltip, TooltipContent, TooltipPortal, TooltipProvider } from "@/ui";
 
 export function InputOrCheckWithTooltip({ stateAtom, asCheckbox, asTextarea, containerClasses, ...rest }: OptionInputWTypeProps) {
     return (
@@ -24,4 +27,43 @@ function TooltipTrigger({ error }: { error: string | undefined; }) {
             <SymbolWarning className="absolute mt-px mr-px right-3 top-1/2 transform -translate-y-1/2 size-4 text-red-500/90" />
         )}
     </>);
+}
+
+type TooltipShellWithErrorIconProps = {
+    stateAtom: RowInputStateAtom;
+    children: ReactNode;
+    Trigger: FC<{ error: string | undefined; }>;
+    containerClasses?: string;
+};
+
+function TooltipShellWithErrorIcon({ stateAtom, children, containerClasses, Trigger }: TooltipShellWithErrorIconProps) {
+    const [openTooltip, setOpenTooltip] = useState(false);
+    const state = useAtomValue(stateAtom);
+    return (
+        <TooltipProvider>
+            <Tooltip open={openTooltip} onOpenChange={setOpenTooltip}>
+
+                <div className={classNames("relative w-full", containerClasses)}>
+                    {children}
+
+                    {/* <TooltipTrigger asChild>
+                        <div>
+                            {/* <Trigger error={state.error} /> * /}
+                            {/* {Trigger ? <Trigger error={state.error} /> : <div className="">no trigger</div> } * /}
+                            <div className="">no trigger</div>
+                        </div>
+                    </TooltipTrigger> */}
+                </div>
+
+                {state.error && state.touched && (
+                    <TooltipPortal> {/* container={document.getElementById('portal')} // dialog from select portal will throw warning */}
+                        <TooltipContent align="end" sideOffset={-2}>
+                            {state.error}
+                        </TooltipContent>
+                    </TooltipPortal>
+                )}
+
+            </Tooltip>
+        </TooltipProvider>
+    );
 }
