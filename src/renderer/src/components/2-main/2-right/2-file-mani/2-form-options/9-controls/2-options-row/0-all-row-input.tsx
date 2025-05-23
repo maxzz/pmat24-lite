6074e600b677a44ca665ebc59407628fd4217406
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-23 15:08:09 -0700

    WIP. new-options-page. fields new set. 4. remove need of RowInputAndButtonWTitle2Cols. 3

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-options-row/0-all-row-input.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-options-row/0-all-row-input.tsx
@@ -2,12 +2,13 @@ import { type ReactNode } from "react";
 import { useAtomValue } from "jotai";
 import { classNames } from "@/utils";
 import { formRow2ColsChildrenClasses, formRow2ColsChildrenLabelClasses, FormRowChildren, InputOrCheckWithErrorMsg, type OptionInputWTypeProps } from "@/ui/local-ui";
+import { c } from "vite/dist/node/moduleRunnerTransport.d-DJ_mE5sf";
 
 // Row with input
 
-export function RowInputWTitle2Cols({ label, ...rest }: { label: string; } & OptionInputWTypeProps) {
+export function RowInputWTitle2Cols({ label, containerClasses, ...rest }: { label: string; } & OptionInputWTypeProps) {
     return (
-        <ChildrenWithLabel2Cols label={label}>
+        <ChildrenWithLabel2Cols label={label} containerClasses={containerClasses}>
             <InputOrCheckWithErrorMsg {...rest} />
         </ChildrenWithLabel2Cols>
     );
@@ -15,10 +16,10 @@ export function RowInputWTitle2Cols({ label, ...rest }: { label: string; } & Opt
 
 // Row with children
 
-export function ChildrenWithLabel2Cols({ label, children }: { label: string; children: ReactNode; }) {
+export function ChildrenWithLabel2Cols({ label, children, containerClasses }: { label: string; children: ReactNode; containerClasses?: string; }) {
     return (
         // <div className="">
-            <FormRowChildren label={label} className={formRow2ColsChildrenClasses} labelClasses={formRow2ColsChildrenLabelClasses}>
+            <FormRowChildren label={label} className={classNames(formRow2ColsChildrenClasses, containerClasses)} labelClasses={formRow2ColsChildrenLabelClasses}>
                 {children}
             </FormRowChildren>
             // {/* <div className="col-span-2 py-1 pr-0.5 text-xs grid grid-cols-subgrid items-center">
