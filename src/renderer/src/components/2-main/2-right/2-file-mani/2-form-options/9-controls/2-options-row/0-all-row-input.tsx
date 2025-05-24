e2deb66591388d3ca1b3c7c0d2f7c79f7e836941
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-23 18:14:35 -0700

    WIP. new-options-page. fields new set. 5. ChildrenWithLabel2Cols code clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-options-row/0-all-row-input.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/2-options-row/0-all-row-input.tsx
@@ -1,7 +1,6 @@
 import { type ReactNode } from "react";
-import { useAtomValue } from "jotai";
 import { classNames } from "@/utils";
-import { formRow2ColsChildrenClasses, formRow2ColsChildrenLabelClasses, FormRowChildren, InputOrCheckWithErrorMsg, type OptionInputWTypeProps } from "@/ui/local-ui";
+import { type OptionInputWTypeProps, formRow2ColsChildrenClasses, formRow2ColsChildrenLabelClasses, FormRowChildren, InputOrCheckWithErrorMsg } from "@/ui/local-ui";
 
 // Row with input
 
@@ -17,17 +16,9 @@ export function RowInputWTitle2Cols({ label, containerClasses, ...rest }: { labe
 
 export function ChildrenWithLabel2Cols({ label, children, containerClasses }: { label: string; children: ReactNode; containerClasses?: string; }) {
     return (
-        // <div className="">
-            <FormRowChildren label={label} className={classNames(formRow2ColsChildrenClasses, containerClasses)} labelClasses={formRow2ColsChildrenLabelClasses}>
-                {children}
-            </FormRowChildren>
-            // {/* <div className="col-span-2 py-1 pr-0.5 text-xs grid grid-cols-subgrid items-center">
-            //     <div className="font-light text-end">
-            //         {label}
-            //     </div>
-            //     {children}
-            // </div> */}
-        // </div>
+        <FormRowChildren label={label} className={classNames(formRow2ColsChildrenClasses, containerClasses)} labelClasses={formRow2ColsChildrenLabelClasses}>
+            {children}
+        </FormRowChildren>
     );
 }
 
