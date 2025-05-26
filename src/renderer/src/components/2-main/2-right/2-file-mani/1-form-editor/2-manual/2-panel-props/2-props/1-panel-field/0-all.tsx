c1392e7ec4419c5355b6874cf011207e0db52a9b
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-25 20:06:34 -0700

    WIP. password-change-links. LinkToLoginForm. 2 add check for FieldTyp.psw

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/0-all.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/0-all.tsx
@@ -1,13 +1,16 @@
 import { useSnapshot } from "valtio";
-import { FormIdx } from "@/store/manifest";
+import { FieldTyp, FormIdx } from "@/store/manifest";
 import { InputLabel, ManualFieldPolicy, ManualFieldType } from "../8-props-ui";
 import { type FceItem, appSettings } from "@/store";
 import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
 import { Column3_Label, Column4_Value, Column5_Catalog } from "../../../../1-normal/1-fields";
+import { useAtomValue } from "jotai";
 
 export function PropsEditorFld({ item, fileUsCtx }: { item: ManualFieldState.CtxFld; fileUsCtx: FileUsCtx; }) {
-    const { useItAtom, labelAtom, valueLifeAtom } = item.rowCtx;
+    const { useItAtom, labelAtom, valueLifeAtom, typeAtom } = item.rowCtx;
+    
     const isCpass = fileUsCtx.formIdx === FormIdx.cpass;
+    const isPsw  = useAtomValue(typeAtom) === FieldTyp.psw;
 
     const { fcAllowed } = useSnapshot(appSettings.files.shownManis);
     const valueRowClasses = fcAllowed ? "grid grid-cols-[1fr,1fr,auto]" : "grid grid-cols-[1fr,auto]";
@@ -50,7 +53,11 @@ export function PropsEditorFld({ item, fileUsCtx }: { item: ManualFieldState.Ctx
                 </InputLabel>
             </>)}
 
-            {isCpass ? (<LinkToLoginForm item={item} />) : <ManualFieldPolicy item={item} />}
+            {isCpass ? (
+                <LinkToLoginForm item={item} />
+            ) : (
+                <ManualFieldPolicy item={item} />
+            )}
 
         </div>
     </>);
