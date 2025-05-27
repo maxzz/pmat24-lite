2c5cf4405bbe63effb268228a6032ac41cc5a7ff
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-27 14:38:20 -0700

    WIP. text-fields-match. add to store showFormTextFields option

--- src/renderer/src/store/9-ui-state/8-app-ui/2-ui-general.ts
+++ src/renderer/src/store/9-ui-state/8-app-ui/2-ui-general.ts
@@ -1,17 +1,19 @@
-export type UiGeneralState = {  // General UI settings
-    showStatusbar: boolean;     // Show status bar of the main window
-    showOptOnRight: boolean;    // Show option labels on the right side of the manifest editor
-    showQuickXml: boolean;      // Show quick XML button
-    showWelcome: boolean;       // Show welcome screen
-    showWelcomeCheck: boolean;  // Show welcome screen checkbox on welcome screen
-    allowWelcome: boolean;      // Allow show welcome screen to be shown as option and checkbox showWelcomeCheck; set from debug options only
-    notifyNewFile: boolean;     // Show popup notification on new file creation
+export type UiGeneralState = {      // General UI settings
+    showStatusbar: boolean;         // Show status bar of the main window
+    showOptOnRight: boolean;        // Show option labels on the right side of the manifest editor
+    showQuickXml: boolean;          // Show quick XML button in the right panel
+    showFormTextFields: boolean;    // Show text fields in form editor. They are used for win32 extended match. In old PMAT default was false.
+    showWelcome: boolean;           // Show welcome screen
+    showWelcomeCheck: boolean;      // Show welcome screen checkbox on welcome screen
+    allowWelcome: boolean;          // Allow show welcome screen to be shown as option and checkbox showWelcomeCheck; set from debug options only
+    notifyNewFile: boolean;         // Show popup notification on new file creation
 };
 
 export const defaultUiGeneralState: UiGeneralState = {
     showStatusbar: true,
     showOptOnRight: true,
     showQuickXml: false,
+    showFormTextFields: false,
     showWelcome: true,
     showWelcomeCheck: false,
     allowWelcome: false,
