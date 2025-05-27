cab78e726bd93ace419109701de9c40ccf322473
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-27 15:13:16 -0700

    WIP. text-fields-match. switch MenuItem_ShowTextFieldsForMatch to be hidden instead of dissabled

--- src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/1-show-text-fields-for-match.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/1-show-text-fields-for-match.tsx
@@ -7,13 +7,15 @@ export function MenuItem_ShowTextFieldsForMatch({ maniAtoms }: { maniAtoms: Mani
     const { activeTab } = useSnapshot(appSettings.right.mani);
     const formIdx = maniAtiveTabToFormIdx(activeTab);
     const isNormalForm = formIdx !== undefined && maniAtoms[formIdx]?.normal;
-    return (
-        <DropdownMenuCheckboxItem
-            disabled={!isNormalForm}
-            checked={showFormTextFields}
-            onCheckedChange={(checked) => appSettings.appUi.uiGeneral.showFormTextFields = checked}
-        >
-            Show Text Fields for Match
-        </DropdownMenuCheckboxItem>
-    );
+    return (<>
+        {isNormalForm && (
+            <DropdownMenuCheckboxItem
+                disabled={!isNormalForm}
+                checked={showFormTextFields}
+                onCheckedChange={(checked) => appSettings.appUi.uiGeneral.showFormTextFields = checked}
+            >
+                Show Text Fields for Match
+            </DropdownMenuCheckboxItem>
+        )}
+    </>);
 }
