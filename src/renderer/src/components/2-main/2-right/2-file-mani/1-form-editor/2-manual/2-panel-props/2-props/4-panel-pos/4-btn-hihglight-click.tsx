cec32b4f5239a99e80d755c171e9bd6fe502fa71
Max Zakharzhevskiy <maxzz@msn.com>
2025-05-30 17:16:05 -0700

    WIP. locations. add highlightClickAtom

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/4-btn-hihglight-click.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/4-btn-hihglight-click.tsx
@@ -1,6 +1,8 @@
-import { useAtomValue } from "jotai";
+import { atom, useAtomValue } from "jotai";
 import { Button } from "@/ui/shadcn";
-import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
+import { FormIdx } from "@/store/manifest";
+import { type FileUsCtx, type ManualFieldState, doFindHwndAtom } from "@/store/1-atoms/2-file-mani-atoms";
+import { type GetTargetWindowResult } from "@shared/ipc-types";
 
 export function ButtonHighlightClick({ item, fileUsCtx }: { item: ManualFieldState.CtxPos; fileUsCtx: FileUsCtx; }) {
     const posValueX = useAtomValue(item.xAtom);
@@ -8,9 +10,38 @@ export function ButtonHighlightClick({ item, fileUsCtx }: { item: ManualFieldSta
 
     const highlightCtx = { mFieldCtx: item, fileUs: fileUsCtx.fileUs, formIdx: fileUsCtx.formIdx };
 
+    function onClick() {
+        console.log(`ButtonHighlightClick.onClick`, highlightCtx);
+    }
+
     return (
-        <Button variant="outline" size="xs">
+        <Button variant="outline" size="xs" onClick={onClick}>
             Highlight
         </Button>
     );
 }
+
+const highlightClickAtom = atom(
+    null,
+    async (get, set, { mFieldCtx, fileUs, formIdx }: { mFieldCtx: ManualFieldState.CtxPos; fileUs: FileUsCtx; formIdx: number; }) => {
+        const posX = get(mFieldCtx.xAtom);
+        const posY = get(mFieldCtx.yAtom);
+
+        let hwndHandle = fileUs && get(formIdx === FormIdx.login ? fileUs.fileUs.hwndLoginAtom : fileUs.fileUs.hwndCpassAtom);
+        if (!hwndHandle) {
+            const twInfo = await set(doFindHwndAtom, { fileUs: fileUs.fileUs, formIdx }) as GetTargetWindowResult;
+            if (twInfo) {
+                twInfo.isBrowser = false;
+                twInfo.process = ''; //TODO: we need process name
+                set(formIdx === FormIdx.login ? fileUs.fileUs.hwndLoginAtom : fileUs.fileUs.hwndCpassAtom, twInfo); //TODO: when to clean up?
+            }
+
+        }
+
+        if (!hwndHandle) {
+            console.log('no hwndHandle');
+            return;
+        }
+
+    }
+);
--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/5-highlight-field-atom/index.tsx
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/5-highlight-field-atom/index.tsx
@@ -1,1 +1,2 @@
 export * from "./1-do-highlight-rect";
+export * from "./2-find-hwnd";
