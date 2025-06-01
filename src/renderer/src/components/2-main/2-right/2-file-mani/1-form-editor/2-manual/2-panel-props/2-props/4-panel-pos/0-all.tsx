5dd6c2d670bb724f127a279c329b23e73911005e
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-01 14:16:06 -0700

    WIP. PropsEditorPos. new xy layout. 4. good enough

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/0-all.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/0-all.tsx
@@ -23,13 +23,13 @@ export function PropsEditorPos({ item, fileUsCtx }: { item: ManualFieldState.Ctx
     useBuildStateLink(item);
 
     return (<>
-        <div className="h-full grid grid-row-[auto,1fr,auto]">
-            <div className="grid grid-cols-[auto_auto_1fr] grid-row-[34px,34px,1fr] gap-x-2" style={{ gridTemplateAreas: "'r11 r12 r13' 'r21 r22 r23' 'r33 r33 r33'" }}>
-                <div className="1pb-0.5 h-3">
+        <div className="h-full grid grid-rows-[auto,1fr,auto]">
+            <div className="grid grid-cols-[auto_auto_1fr] gap-x-2" style={{ gridTemplateAreas: "'r11 r12 r13' 'r21 r22 r23' 'r33 r33 r33'" }}>
+                <div className="pb-0.5">
                     x
                 </div>
 
-                <div className="1pb-0.5 h-3">
+                <div>
                     y
                 </div>
 
@@ -70,7 +70,7 @@ function InputPosNumbers({ item }: { item: ManualFieldState.CtxPos; }) {
     }
 
     return (<>
-        <div className="flex items-center gap-0.5" style={{ gridArea: 'r21' }}>
+        <div className="py-0.5 flex items-center gap-0.5" style={{ gridArea: 'r21' }}>
             <OptionAsString stateAtom={xAtom} className={errorClasses(hasErrorMin)} />
             px
         </div>
