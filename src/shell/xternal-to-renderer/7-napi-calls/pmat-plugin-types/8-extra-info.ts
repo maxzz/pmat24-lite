d3a74989699f0e9e0318410d140cee19f2cd7124
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-01 19:08:24 -0700

    WIP. PropsEditorPos. add GetWindowExtras definition

---
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/8-extra-info.ts
@@ -0,0 +1,24 @@
+import { type PluginDataCallback, type TargetClientRect } from "./9-types";
+
+// Extra info about windows used for fields highlighting
+
+export type WindowExtraParams = {
+    hwnds: string[];                    // List of window handles to get extra info for
+};
+
+export type WindowExtra = {
+    hwnd: string;                       // Window handle from WindowExtraParams.hwnds list
+    screenRect: TargetClientRect;       // Window screen rect; We can check if window is moved out of screen
+    clientRect: TargetClientRect;       // Window client rect; We can check if highlight is inside this rect
+    isClosed: boolean;                  // True if the window is closed, i.e. not found in the list of asked top-level windows
+    isMinimized: boolean;               // True if the window is minimized; We can ask to restore it to show highlight
+    process: string;                    // Process name; If there multiple windows with the same name (like Login) and classname (like Dialog), we can use process name to distinguish them
+};
+
+export type WindowExtraResult = {
+    extra: WindowExtra[];
+};
+
+export interface GetWindowExtras {
+    (WindowExtraParams: string, cb: PluginDataCallback): void;
+}
