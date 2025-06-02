e113a83d4bca15e7feda73c22fc11dcdad8dd4ee
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-01 19:12:00 -0700

    WIP. PropsEditorPos. add SendMessageToPm definition

---
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/8-pass-through.ts
@@ -0,0 +1,25 @@
+import { type PluginDataCallback } from "./9-types";
+
+// Pass through message to PM browser extension
+
+export type MessageToPm = {
+    message: string; // message to send to PM broser extension; this multi-purpose call can be used for training or future customization, and so on
+};
+
+export interface SendMessageToPm {
+    (MessageToPm: string, cb: PluginDataCallback): void;
+}
+
+// Pass through message from PM browser extension (i.e. ext->DpAgent->PMAT)
+
+//TBD: should it be result from be invoke or a separate call from DpAgent?
+//  * if extension is not installed, we should not use invoke
+//  * it should be a similar call to create manifest fields
+
+export type MessageFromPm = {
+    message: string; // message from PM broser extension
+};
+
+export interface SendMessageFromPm {
+    (MessageFromPm: string, cb: PluginDataCallback): void;
+}
