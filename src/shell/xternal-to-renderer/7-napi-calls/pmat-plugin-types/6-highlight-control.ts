ba5bff406fd53aee99ed42f6b1ae848d5a5e9a49
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-06 14:20:02 -0700

    WIP. highlight. Take 0010. highlighter types clean up

--- src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/6-highlight-control.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/6-highlight-control.ts
@@ -4,41 +4,37 @@ import { type PluginDataCallback, type TargetClientRect } from "./9-types";
 // Highlight desktop app window control
 
 export type WindowControlHighlighterParams = {
-    hwnd: string;                // Window handle. We get process name from this window handle to decide whether it is a browser or a desktop app.
-    rect?: TargetClientRect;     // Used to highlight controls in desktop apps
-    accId?: number;              // Used to highlight controls in browsers
+    hwnd: string;                   // Window handle. We get process name from this window handle to decide whether it is a browser or a desktop app.
+    rect?: TargetClientRect;        // Used to highlight controls in desktop apps
+    accId?: number;                 // Used to highlight controls in browsers
 };
 
-//TODO: highlight() show have result: with ok, or error like 'window not found', 'window is minimized', 'no open browser', and so on
-
-/**
- * Class for getting window icon. Instantiate once and call getIcon multiple times.
- *
- * During instantiation it internally starts GDI Plus. So, do not create/destruct this class multiple times,
- * it will be expensive.
-
- * Usage:
- * let highlighter = new WindowControlHighlighter();
- * highlighter.highlight('{"hwnd":"12345", "rect":{"left":100,"right":200,"top":100,"bottom":200}}');
- *
- * TODO: There is no hide highlighter call. Is that done by timer? How to set timer interval in that case or reset show to see rect again?
- */
-
 export type Win32Errors =
-    | 'no.hwnd'                    // No hwnd found
-    | 'internal';                  // Internal error
+    | 'no.hwnd'                     // No hwnd found
+    | 'internal';                   // Internal error
 
 export type HighlightError = {
     type: 'error';
     error: BrowserExtErrors | Win32Errors;
-    hr?: number                    // Optional HRESULT for the error
-    message?: string               // Optional error message
+    hr?: number;                    // Optional HRESULT for the error
+    message?: string;               // Optional error message
 };
 
-export type WindowControlHighlighterResult = HighlightError;
+export type WindowControlHighlighterResult = HighlightError | undefined | ''; // If empty string, then opetation is started and will be finished later
 
-
+/**
+ * Class for getting window icon. Instantiate once and call getIcon multiple times. During instantiation it
+ * internally starts GDI Plus. So, do not create/destruct this class multiple times, it will be expensive.
+ * Usage:
+ * ```
+ *      let highlighter = new WindowControlHighlighter();
+ *      highlighter.highlight('{"hwnd":"12345", "rect":{"left":100,"right":200,"top":100,"bottom":200}}');
+ * ```
+ */
 export interface WindowControlHighlighter {
     new(): WindowControlHighlighter;
     highlight(windowControlHighlighterParams: string, cb: PluginDataCallback<WindowControlHighlighterResult>): void;
 }
+
+//TODO: highlight() show have result: with ok, or error like 'window not found', 'window is minimized', 'no open browser', and so on - done
+//TODO: There is no hide highlighter call. Is that done by timer? How to set timer interval in that case or reset show to see rect again?
--- src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/8-extra-info.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/8-extra-info.ts
@@ -8,10 +8,13 @@ export type WindowExtraParams = {
 
 export type WindowExtra = {
     hwnd: string;                       // Window handle from WindowExtraParams.hwnds list
+    isClosed: true;                     // True if the window is closed, i.e. not found in the list of asked top-level windows
+} | {
+    hwnd: string;                       // Window handle from WindowExtraParams.hwnds list
+    isClosed: false;                    // True if the window is closed, i.e. not found in the list of asked top-level windows
+    isMinimized: boolean;               // True if the window is minimized; We can ask to restore it to show highlight
     screenRect: TargetClientRect;       // Window screen rect; We can check if window is moved out of screen
     clientRect: TargetClientRect;       // Window client rect; We can check if highlight is inside this rect
-    isClosed: boolean;                  // True if the window is closed, i.e. not found in the list of asked top-level windows
-    isMinimized: boolean;               // True if the window is minimized; We can ask to restore it to show highlight
     process: string;                    // Process name; If there multiple windows with the same name (like Login) and classname (like Dialog), we can use process name to distinguish them
 };
 
