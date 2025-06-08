ae9db9631d1f28b15ae124420cde8108d8b62dc2
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-08 12:49:28 -0700

    WIP. highlight. combine highlight atoms in one place. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/4-btn-hihglight-click.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/4-panel-pos/4-btn-hihglight-click.tsx
@@ -1,9 +1,7 @@
-import { atom, useSetAtom } from "jotai";
-import { toast } from "sonner";
+import { useSetAtom } from "jotai";
 import { Button } from "@/ui/shadcn";
-import { FormIdx } from "@/store/manifest";
 import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
-import { doFindHwndAtom, doHighlightRectAtom } from "@/store/7-napi-atoms";
+import { doHighlightClickAtom } from "@/store/7-napi-atoms";
 
 export function ButtonHighlightClick({ item, fileUsCtx }: { item: ManualFieldState.CtxPos; fileUsCtx: FileUsCtx; }) {
     const doHighlightClick = useSetAtom(doHighlightClickAtom);
@@ -14,23 +12,3 @@ export function ButtonHighlightClick({ item, fileUsCtx }: { item: ManualFieldSta
         </Button>
     );
 }
-
-const doHighlightClickAtom = atom(
-    null,
-    async (get, set, { mFieldCtx, fileUsCtx }: { mFieldCtx: ManualFieldState.CtxPos; fileUsCtx: FileUsCtx; }) => {
-        const fileUs = fileUsCtx.fileUs;
-        const formIdx = fileUsCtx.formIdx;
-        const hwndAtom = formIdx === FormIdx.login ? fileUs.hwndLoginAtom : fileUs.hwndCpassAtom;
-
-        // Find window every time since window can be closed and atom stores stale hwnd
-        set(hwndAtom, await set(doFindHwndAtom, { fileUs, formIdx }));
-
-        const hwndHandle = get(hwndAtom);
-        if (!hwndHandle) {
-            toast.info('Open target window first');
-            return;
-        }
-
-        set(doHighlightRectAtom, { mFieldCtx, fileUs, formIdx, focusOrBlur: true });
-    }
-);
---
+++ src/renderer/src/store/7-napi-atoms/6-do-highlight-field/4-hihglight-click-atom.tsx
@@ -0,0 +1,25 @@
+import { atom } from "jotai";
+import { toast } from "sonner";
+import { FormIdx } from "@/store/manifest";
+import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
+import { doFindHwndAtom, doHighlightRectAtom } from "@/store/7-napi-atoms";
+
+export const doHighlightClickAtom = atom(
+    null,
+    async (get, set, { mFieldCtx, fileUsCtx }: { mFieldCtx: ManualFieldState.CtxPos; fileUsCtx: FileUsCtx; }) => {
+        const fileUs = fileUsCtx.fileUs;
+        const formIdx = fileUsCtx.formIdx;
+        const hwndAtom = formIdx === FormIdx.login ? fileUs.hwndLoginAtom : fileUs.hwndCpassAtom;
+
+        // Find window every time since window can be closed and atom stores stale hwnd
+        set(hwndAtom, await set(doFindHwndAtom, { fileUs, formIdx }));
+
+        const hwndHandle = get(hwndAtom);
+        if (!hwndHandle) {
+            toast.info('Open target window first');
+            return;
+        }
+
+        set(doHighlightRectAtom, { mFieldCtx, fileUs, formIdx, focusOrBlur: true });
+    }
+);
