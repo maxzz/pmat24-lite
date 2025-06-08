3f31d921e0d6e9e9cecefd850859840aa4aa91a7
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-08 14:25:32 -0700

    WIP. saw-position. CornerSelector. 2. layout with Tile

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/1-entry-saw-monitor/3-corner-selector.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/1-entry-saw-monitor/3-corner-selector.tsx
@@ -6,21 +6,21 @@ export function CornerSelector({ className }: { className?: string; }) {
         <div className={classNames("grid grid-cols-[auto,0px,auto] grid-rows-[auto,0px,auto] 1gap-1 text-xs", className)}>
             {/* <div className={classNames("grid grid-cols-3 grid-rows-3 1gap-1 text-xs", className)}> */}
 
-            <div className="size-4 border border-border col-start-1 row-start-1 grid place-items-center">1</div>
-            <div className="size-4 border border-border col-start-3 row-start-1 grid place-items-center">2</div>
+            <Tile className="absolute -left-2 -top-2 bg-purple-500 col-start-2 row-start-2">0</Tile>
+
+            <Tile className="col-start-1 row-start-1">1</Tile>
+            <Tile className="col-start-3 row-start-1">2</Tile>
             
-            <div className="absolute -left-2 -top-2 bg-purple-500 size-4 border border-border col-start-2 row-start-2 grid place-items-center">3</div>
-            
-            <div className="bg-slate-500 size-4 border border-border col-start-1 row-start-3 grid place-items-center">4</div>
-            <div className="size-4 border border-border col-start-3 row-start-3 grid place-items-center">5</div>
+            <Tile className="bg-slate-500 col-start-1 row-start-3">3</Tile>
+            <Tile className="col-start-3 row-start-3">4</Tile>
         </div>
     );
 }
 
-function Tile({ className, ...rest }: ComponentPropsWithoutRef<'div'>) {
+function Tile({ children, className, ...rest }: ComponentPropsWithoutRef<'div'>) {
     return (
-        <div className={classNames("bg-slate-500 size-4 border border-border col-start-1 row-start-3 grid place-items-center", className)} {...rest}>
-            5
+        <div className={classNames("size-4 border border-border grid place-items-center", className)} {...rest}>
+            {children}
         </div>
     );
 }
