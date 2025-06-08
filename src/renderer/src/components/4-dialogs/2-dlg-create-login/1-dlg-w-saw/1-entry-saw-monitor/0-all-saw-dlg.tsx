7dd01f0a4e377b5b695e38934fe180a3fe9fb664
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-08 14:20:42 -0700

    WIP. saw-position. add CornerSelector

--- src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/1-entry-saw-monitor/0-all-saw-dlg.tsx
+++ src/renderer/src/components/4-dialogs/2-dlg-create-login/1-dlg-w-saw/1-entry-saw-monitor/0-all-saw-dlg.tsx
@@ -1,6 +1,6 @@
 import { useAtom, useAtomValue, useSetAtom } from "jotai";
 import { useSnapshot } from "valtio";
-import { classNames, useDissmissNextToasts } from "@/utils";
+import { useDissmissNextToasts } from "@/utils";
 import { Button, Checkbox, Label } from "@/ui";
 import { type AnimationProps, type Transition, AnimatePresence, motion } from "motion/react";
 import { napiBuildState } from "@/store/7-napi-atoms";
@@ -8,6 +8,7 @@ import { checkboxCreateManualModeAtom, doMoveToSecondDlgAtom, isOpen_SawMonitorA
 import { CurrentApp } from "./1-current-app";
 import { RuntimeCounter } from "./2-runtime-counter";
 import { DebugFrame } from "./8-debug-frame";
+import { CornerSelector } from "./3-corner-selector";
 
 export function DialogSawMonitor() {
     const isOpen = useAtomValue(isOpen_SawMonitorAtom);
@@ -49,19 +50,6 @@ function SawMonitorDlgBody() {
                 <CornerSelector className="absolute left-2 bottom-1" />
                 <RuntimeCounter className="absolute right-2 bottom-1 text-right opacity-25" />
             </DebugFrame>
-        </div>
-    );
-}
-
-function CornerSelector({ className }: { className?: string; }) {
-    return (
-        <div className={classNames("grid grid-cols-[auto,0px,auto] grid-rows-[auto,0px,auto] 1gap-1 text-xs", className)}>
-            {/* <div className={classNames("grid grid-cols-3 grid-rows-3 1gap-1 text-xs", className)}> */}
-            <div className="size-4 border border-border col-start-1 row-start-1 grid place-items-center">1</div>
-            <div className="size-4 border border-border col-start-3 row-start-1 grid place-items-center">2</div>
-            <div className="absolute -left-2 -top-2 bg-purple-500 size-4 border border-border col-start-2 row-start-2 grid place-items-center">3</div>
-            <div className="bg-slate-500 size-4 border border-border col-start-1 row-start-3 grid place-items-center">4</div>
-            <div className="size-4 border border-border col-start-3 row-start-3 grid place-items-center">5</div>
         </div>
     );
 }
