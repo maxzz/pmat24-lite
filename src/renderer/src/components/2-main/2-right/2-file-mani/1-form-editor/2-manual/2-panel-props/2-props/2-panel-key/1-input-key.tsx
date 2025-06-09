3c792af60f4ecc1f133298fe1185bac28c5fb6bc
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-09 14:18:21 -0700

    WIP. cpass. manual-mode. switch InputKey to use InputSelectUi. code clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/2-panel-key/1-input-key.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/2-panel-key/1-input-key.tsx
@@ -1,6 +1,6 @@
 import { useAtom } from "jotai";
 import { type RowInputStateAtom } from "@/ui";
-import { InputLabel, InputSelect, InputSelectUi } from "../8-props-ui";
+import { InputLabel, InputSelectUi } from "../8-props-ui";
 import { actionKeys } from "@/store/manifest";
 
 export function InputKey({ valueAtom, triggerClasses }: { valueAtom: RowInputStateAtom; triggerClasses?: string; }) {
@@ -15,43 +15,5 @@ export function InputKey({ valueAtom, triggerClasses }: { valueAtom: RowInputSta
                 placeholder="Select key"
             />
         </InputLabel>
-
-        // <InputSelect
-        //     items={actionKeys}
-        //     placeholder="Select key"
-
-        //     label="Key to press"
-        //     labelClasses="min-w-[9ch]"
-
-        //     triggerClasses={triggerClasses}
-        //     value={value.data}
-        //     onValueChange={(value) => setValue((prev) => ({ ...prev, data: value }))}
-
-        //     title="Key for this action"
-        // />
     );
 }
-
-// export function InputSelect({ items, label, labelClasses, title, horizontal, ...rest }: InputSelectProps) {
-//     return (
-//         <InputLabel label={label} labelClasses={labelClasses} horizontal={horizontal} title={title}>
-//             <InputSelectUi items={items} {...rest} />
-//         </InputLabel>
-//     );
-// }
-
-// export function InputKey({ valueAtom, triggerClasses }: { valueAtom: RowInputStateAtom; triggerClasses?: string; }) {
-//     const [value, setValue] = useAtom(valueAtom);
-//     return (
-//         <InputSelect
-//             items={actionKeys}
-//             placeholder="Select key"
-//             label="Key to press"
-//             labelClasses="min-w-[9ch]"
-//             triggerClasses={triggerClasses}
-//             value={value.data}
-//             onValueChange={(value) => setValue((prev) => ({ ...prev, data: value }))}
-//             title="Key for this action"
-//         />
-//     );
-// }
--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/8-props-ui/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/8-props-ui/index.tsx
@@ -1,4 +1,3 @@
 export * from "./1-input-label";
 export * from "./2-nun-input-field";
-export * from "./3-input-select";
 export * from "./4-input-select-ui";
