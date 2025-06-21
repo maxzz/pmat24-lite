82573bc3ec28b376c57d3ca8f85b2cc695e6a9a2
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-20 17:59:24 -0700

    WIP. binaries. dnd. new dndActionInit and dndAction

--- src/shell/xternal-to-renderer/7-napi-calls/7-get-window-pos.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/7-get-window-pos.ts
@@ -1,36 +1,104 @@
 import { addon } from "./0-addon";
-import { type DragAndDropper, type DragAndDropParams } from "./pmat-plugin-types";
+import { mainToRenderer } from "../1-gates-in-main";
+import { type DragAndDropper, type DragAndDropParams, type DragAndDropResult } from "./pmat-plugin-types";
 
 /**
  * Get position inside window by drag and drop for manual mode 'position' action.
  */
 
-export function dndAction(params: DragAndDropCallParams): Promise<string> {
+export function dndActionInit(params: DragAndDropParams): string {
     if (!gDragAndDropper) {
         gDragAndDropper = new addon.DragAndDropper();
     }
 
-    return new Promise<string>((resolve, reject) => {
-        if (!gDragAndDropper) {
-            reject('no.glb');
-            return;
-        }
+    if (!gDragAndDropper) {
+        return 'no.glb';
+    }
 
-        const actionParams = params.what === 'init' ? JSON.stringify({ hwnd: params.hwnd }) : '';
-        const actionName: keyof DragAndDropper = params.what;
+    const actionParams = JSON.stringify({ hwnd: params.hwnd });
+    let rv_error: string = '';
 
-        gDragAndDropper[actionName](actionParams,
-            (err: any, data?: string) => {
-                if (err) {
-                    reject(err);
+    gDragAndDropper.init(actionParams,
+        (err: any, data?: string) => {
+            if (err) {
+                console.error('dnd.error 1');
+                rv_error = `'dnd.error 1': ${err}`;
+                return;
+            }
+
+            try {
+                const res = JSON.parse(data || '') as DragAndDropResult;
+
+                if (res.status === 'progress') { // status: 'initialized' | 'progress' | 'done' | 'abandoned'
+                    console.log('dnd.progress', res);
+
+                    mainToRenderer({ type: "m2r:position-progress", progress: res });
+                    return;
                 } else {
-                    resolve(data || '');
+                    console.log('dnd.utility res', res);
                 }
             }
-        );
-    });
+            catch (error) {
+                console.error('dnd.error 2', error);
+                rv_error = `'dnd.error 2': ${error}`;
+            }
+        }
+    );
+
+    return rv_error;
 }
 
-export type DragAndDropCallParams = Prettify<{ what: 'init'; } & DragAndDropParams | { what: 'move' | 'stop'; }>;
+export function dndAction(actionName: DragAndDropActionParams): void {
+    if (!gDragAndDropper) {
+        gDragAndDropper = new addon.DragAndDropper();
+    }
+
+    if (!gDragAndDropper) {
+        return;
+    }
+
+    gDragAndDropper[actionName]('');
+}
+
+export type DragAndDropInitParams = Prettify<{ what: 'init'; } & DragAndDropParams>;
+export type DragAndDropActionParams = 'move' | 'stop';
 
 let gDragAndDropper: DragAndDropper | null = null;
+/*
+export function getWindowPos(hwnd: string): Promise<TargetPosition> {
+    return new Promise<TargetPosition>(
+        (resolve, reject) => {
+            const params: DragAndDropParams = { hwnd };
+            const param = JSON.stringify(params);
+
+            addon.dragAndDrop(param,
+                (err: string, data: string) => {
+                    console.log('+++++++++++++', data);
+
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    try {
+                        const res = JSON.parse(data) as DragAndDropResult;
+
+                        if (res.status === 'progress') {
+                            console.log('progress', res.point);
+
+                            mainToRenderer({ type: "m2r:position-progress", progress: res });
+                            return;
+                        }
+
+                        resolve(res);
+                    } catch (error) {
+                        console.error('error', error);
+
+                        reject('>>>Faieled to get posiotion.');
+                    }
+                }
+            );
+        }
+    );
+}
+*/
