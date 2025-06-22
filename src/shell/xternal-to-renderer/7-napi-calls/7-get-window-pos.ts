d3d885004ba8d650550531f284d02f6fdb201665
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-22 13:18:08 -0700

    WIP. napi-calls. dndActionInit. trace. 2. add isIn() check

--- src/shell/xternal-to-renderer/7-napi-calls/7-get-window-pos.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/7-get-window-pos.ts
@@ -1,7 +1,7 @@
 import { addon } from "./0-addon";
 import { mainToRenderer } from "../1-gates-in-main";
-import { type DragAndDropper, type DragAndDropParams, type DragAndDropResult, type OkIfEmptyString, type TargetPosition } from "./pmat-plugin-types";
-import { debounce } from "@shell/3-utils-main";
+import { type DragAndDropper, type DragAndDropParams, type DragAndDropResult, type OkIfEmptyString, type TargetPosition, type Rect4 } from "./pmat-plugin-types";
+import { debounce, isPointInsideRect } from "@shell/3-utils-main";
 
 /**
  * Init get position inside window operation by drag and drop for manual mode 'position' action.
@@ -31,6 +31,7 @@ export function dndActionInit(params: DragAndDropParams): OkIfEmptyString {
 
                 if (res.status === 'progress') {
                     debouncedSendToClient(res);
+                    printProgress(res);
                 } else {
                     console.log('dnd.utility res', res);
                 }
@@ -54,6 +55,8 @@ export type DragAndDropActionParams = 'move' | 'stop';
 
 let dragAndDropper: DragAndDropper | null = null;
 
+//
+
 function sendToClient(res: TargetPosition) {
     res.point.x = Math.round(res.point.x);
     res.point.y = Math.round(res.point.y);
@@ -61,3 +64,22 @@ function sendToClient(res: TargetPosition) {
 }
 
 const debouncedSendToClient = debounce(sendToClient, 100);
+
+//
+
+function Rect4ToString(rect: Rect4) {
+    const [l, t, r, b] = Object.values(rect).map((v, idx) => `${Math.round(v)}`[idx === 0 || idx === 2 ? 'padStart' : 'padEnd'](4, ' '));
+    return `lt: ${l},${t}, rb: ${r},${b}`;
+}
+
+function printProgress(res: TargetPosition) {
+    console.log(
+        `dnd.progress [IS: %s] pointXY: {%s, %s} CLIENT: {%s}, WINDOW: {%s}, MSG: %o`,
+        isPointInsideRect(res.point, res.clientRect) ? ' IN' : 'OUT',
+        `${Math.round(res.point.x)}`.padStart(4, ' '),
+        `${Math.round(res.point.y)}`.padStart(4, ' '),
+        Rect4ToString(res.clientRect),
+        Rect4ToString(res.windowRect),
+        { data: JSON.stringify(res) }
+    );
+}
