06c3cf4c6678de486cbd776af881843e3b72dcdb
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-23 19:53:58 -0700

    WIP. binaries. update WindowHighlighter.WindowHighlighterParams type

--- src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/6-highlight-window.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/6-highlight-window.ts
@@ -4,8 +4,8 @@ import { type HighlightError } from "./6-highlight-control";
 // Highlight target window
 
 export type WindowHighlighterParams = {
-    hwnd: string;                       // Window handle. Not used currently, but saved in case it may be needed in the future.
-    rect: Rect4;                        // Used to highlight target window in screen coordinates
+    hwnd: string;                       // If rect is not defined and hwnd defined then the window client area will be used and deflated by the border width.
+    rect: Rect4;                        // If rect is defined it will be used to highlight target window in screen coordinates.
     highlightColor?: string;            // Color of the highlighted border, in HTML form #RRGGBB. Default: #FF0000. Do not send #000000 or #008080.
     width?: number                      // Width of the highlighted border in pixels. Default: 5
 };
@@ -17,3 +17,22 @@ export interface WindowHighlighter {
     highlight(windowHighlighterParams: string, cb: PluginDataCallback<WindowHighlighterResult>): void;
     unHighlight(dummy: string, cb: PluginDataCallback<WindowHighlighterResult>): void;
 }
+
+/*
+	GAI: 'How to get Windows 11 window rectangle? GetWindowRect returns the wrong rectangle the is somehow inflated. Why?'
+
+		Obtaining an accurate window rectangle in Windows 11:
+
+			* You are correct that GetWindowRect can return an inflated rectangle in Windows 11 and later versions.
+			  This is because GetWindowRect may include invisible resize borders in its calculation, especially when DPI virtualization is involved.
+
+			* To get the visible window bounds (excluding these borders), you should use DwmGetWindowAttribute with the DWMWA_EXTENDED_FRAME_BOUNDS flag.
+
+			* Drop shadow: While DwmGetWindowAttribute with DWMWA_EXTENDED_FRAME_BOUNDS provides the visible window bounds, it does not include the area of the window's drop shadow.
+			  If you need the rectangle including the drop shadow, you would still use GetWindowRect.
+				#include <dwmapi.h>
+				// ... inside your code ...
+				RECT rc;
+				HRESULT hr = DwmGetWindowAttribute(hWnd, DWMWA_EXTENDED_FRAME_BOUNDS, &rc, sizeof(rc));
+				if (SUCCEEDED(hr)) { /*rc now contains the visible window bounds*/ /* ... /* }
+*/
