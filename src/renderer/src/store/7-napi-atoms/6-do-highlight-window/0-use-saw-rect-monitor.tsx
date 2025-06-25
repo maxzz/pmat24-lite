fe638535dec0cec5d1350f64ad85f3d1ba4f045c
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-24 20:31:31 -0700

    WIP. napi-calls. highlight target window. end up with doRectAtom

--- src/renderer/src/store/7-napi-atoms/6-do-highlight-window/0-use-saw-rect-monitor.tsx
+++ src/renderer/src/store/7-napi-atoms/6-do-highlight-window/0-use-saw-rect-monitor.tsx
@@ -1,17 +1,17 @@
 import { useCallback, useEffect } from "react";
+import { compareRect } from "@/utils";
 import { type Getter, type Setter, atom, useSetAtom } from "jotai";
 import { type GetTargetWindowResult, type WindowHighlighterParams } from "@shared/ipc-types";
 import { invokeMainTyped } from "@/xternal-to-main";
 import { useSawHandleListener } from "../1-do-get-hwnd";
-import { compareRect } from "@/utils";
 
 export function useSawRectMonitor() {
-    const doHideTarget = useSetAtom(doHideTargetAtom);
+    const doRect = useSetAtom(doRectAtom);
 
     useEffect(
         () => {
             return () => {
-                doHideTarget();
+                doRect({ action: 'hide' });
             };
         }, []
     );
@@ -20,13 +20,13 @@ export function useSawRectMonitor() {
         useCallback(
             (get: Getter, set: Setter, newVal: GetTargetWindowResult | null, prevVal: GetTargetWindowResult | null) => {
                 if (!newVal?.hwnd) {
-                    set(doHideTargetAtom);
+                    set(doRectAtom, { action: 'hide' });
                     return;
                 }
 
                 const unchanged = prevVal?.screenClientRect && compareRect(newVal.screenClientRect, prevVal.screenClientRect) && get(highlightIsOnAtom);
                 if (!unchanged) {
-                    set(doShowTargetAtom, { hwnd: newVal.hwnd, /*rect: newVal.screenClientRect,*/ highlightColor: '#ff8800', width: 5 }); // for now use only hwnd for show window client rect
+                    set(doRectAtom, { action: 'show', params: { hwnd: newVal.hwnd, /*rect: newVal.screenClientRect,*/ highlightColor: '#ff8800', width: 5 } }); // for now use only hwnd for show window client rect
                 }
             }, []
         )
@@ -35,46 +35,21 @@ export function useSawRectMonitor() {
 
 const highlightIsOnAtom = atom(false);
 
-const doShowTargetAtom = atom(
-    null,
-    async (get, set, params: WindowHighlighterParams) => {
-        set(highlightIsOnAtom, true);
-
-        const error = await invokeMainTyped({ type: 'r2mi:highlight-target', params });
-        error && console.log('doShowTargetAtom', error);
-    }
-);
-
-const doHideTargetAtom = atom(
-    null,
-    async (get, set) => {
-        if (!get(highlightIsOnAtom)) {
-            return;
-        }
-        set(highlightIsOnAtom, false);
-
-        const error = await invokeMainTyped({ type: 'r2mi:highlight-target' });
-        error && console.log('doHideTargetAtom', error);
-    }
-);
-
-const doTargetWindowAtom = atom(
+const doRectAtom = atom(
     null,
     async (get, set, { action, params }: { action: 'show', params: WindowHighlighterParams; } | { action: 'hide', params?: undefined; }) => {
         let error: string | undefined;
 
         if (action === 'show') {
             set(highlightIsOnAtom, true);
-            error = await invokeMainTyped({ type: 'r2mi:highlight-target', params });
-        }
-        else if (action === 'hide') {
+        } else {
             if (!get(highlightIsOnAtom)) {
                 return;
             }
             set(highlightIsOnAtom, false);
-            error = await invokeMainTyped({ type: 'r2mi:highlight-target' });
         }
 
+        error = await invokeMainTyped({ type: 'r2mi:highlight-target', params });
         error && console.log('doTargetWindowAtom', error);
     }
 );
