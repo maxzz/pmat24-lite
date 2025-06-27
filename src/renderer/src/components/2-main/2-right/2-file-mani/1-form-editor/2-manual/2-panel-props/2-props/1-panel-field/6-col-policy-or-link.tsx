941eaf03c30702a7361ccd2a943ae8d1defa99b4
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-26 19:01:42 -0700

    WIP. cpass-links. buildDropdownFieldsAtom. 4. single atom for buildDropdownFieldsAtom

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/6-col-policy-or-link.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/6-col-policy-or-link.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { atom, useAtomValue, useSetAtom } from "jotai";
-import { type OptionTextValue, FieldTyp, FormIdx } from "@/store/manifest";
-import { type FormFields, type FileUsCtx, type ManualFieldState, type NormalField } from "@/store/1-atoms/2-file-mani-atoms";
+import { useAtomValue, useSetAtom } from "jotai";
+import { FieldTyp, FormIdx } from "@/store/manifest";
+import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
 import { Column6_Policy } from "../../../../1-normal/1-fields/6-column-policy";
 import { InputLabel, InputSelectUi } from "../8-props-ui";
-import { doGetLinksAtom } from "./8-forms-fields";
+import { buildDropdownFieldsAtom } from "./8-forms-fields";
 import { classNames } from "@/utils";
 
 export function Col_PolicyOrLink({ item, fileUsCtx }: { item: ManualFieldState.CtxFld; fileUsCtx: FileUsCtx; }) {
@@ -59,40 +59,3 @@ function Case_LinkToLoginForm({ item, fileUsCtx }: { item: ManualFieldState.CtxF
 }
 
 const inputAsRefClasses = "text-[0.6rem] !text-blue-400 cursor-pointer";
-
-const getLoginFormPswFieldsAtom = atom(
-    null,
-    (get, set, fileUsCtx: FileUsCtx): NormalField.RowCtx[] | undefined => {
-        const loginFields = set(doGetLinksAtom, fileUsCtx)?.[FormIdx.login];
-        if (!loginFields) {
-            return;
-        }
-
-        const rv = loginFields.filter((field) => get(field.typeAtom) === FieldTyp.psw);
-        return rv;
-    }
-);
-
-const buildDropdownFieldsAtom = atom(
-    null,
-    (get, set, rindexUuid: number, fileUsCtx: FileUsCtx): OptionTextValue[] => {
-        const fields = set(getLoginFormPswFieldsAtom, fileUsCtx);
-
-        set(printFieldsAtom, rindexUuid, fields);
-
-        const rv = (fields || []).map<OptionTextValue>((field) => ([get(field.labelAtom), get(field.dbnameAtom)]));
-        rv.unshift(['No link', '0']);
-        return rv;
-    }
-);
-
-const printFieldsAtom = atom(
-    null,
-    (get, set, rindexUuid: number, fields: NormalField.RowCtx[] | undefined) => {
-        if (!fields) {
-            return;
-        }
-        const all = fields.map((field) => `label:${get(field.labelAtom)} dbid:${get(field.dbnameAtom)}`);
-        console.log(`for uuid:"${rindexUuid}" login fields:`, all);
-    }
-);
--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/8-forms-fields.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/8-forms-fields.tsx
@@ -1,5 +1,6 @@
 import { atom } from "jotai";
-import { cpassFieldsIdx, type FormFields, loginFieldsIdx, safeManiAtoms, type FileUsCtx } from "@/store/1-atoms/2-file-mani-atoms";
+import { type FormFields, type FileUsCtx, type NormalField, cpassFieldsIdx, loginFieldsIdx, safeManiAtoms } from "@/store/1-atoms/2-file-mani-atoms";
+import { type OptionTextValue, FieldTyp, FormIdx } from "@/store/manifest";
 
 export const doGetLinksAtom = atom(
     null,
@@ -17,5 +18,42 @@ export const doGetLinksAtom = atom(
         ] as const;
 
         return rv;
+    }
+);
+
+const getLoginFormPswFieldsAtom = atom(
+    null,
+    (get, set, fileUsCtx: FileUsCtx): NormalField.RowCtx[] | undefined => {
+        const loginFields = set(doGetLinksAtom, fileUsCtx)?.[FormIdx.login];
+        if (!loginFields) {
+            return;
+        }
+
+        const rv = loginFields.filter((field) => get(field.typeAtom) === FieldTyp.psw);
+        return rv;
+    }
+);
+
+export const buildDropdownFieldsAtom = atom(
+    null,
+    (get, set, rindexUuid: number, fileUsCtx: FileUsCtx): OptionTextValue[] => {
+        const fields = set(getLoginFormPswFieldsAtom, fileUsCtx);
+
+        set(printFieldsAtom, rindexUuid, fields);
+
+        const rv = (fields || []).map<OptionTextValue>((field) => ([get(field.labelAtom), get(field.dbnameAtom)]));
+        rv.unshift(['No link', '0']);
+        return rv;
+    }
+);
+
+const printFieldsAtom = atom(
+    null,
+    (get, set, rindexUuid: number, fields: NormalField.RowCtx[] | undefined) => {
+        if (!fields) {
+            return;
+        }
+        const all = fields.map((field) => `label:${get(field.labelAtom)} dbid:${get(field.dbnameAtom)}`);
+        console.log(`for uuid:"${rindexUuid}" login fields:`, all);
     }
 );
