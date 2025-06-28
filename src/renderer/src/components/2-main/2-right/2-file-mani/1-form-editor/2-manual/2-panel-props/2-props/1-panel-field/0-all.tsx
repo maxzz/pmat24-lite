1e2202dcb913c1ef7f91c05485d11db7d344c0f6
Max Zakharzhevskiy <maxzz@msn.com>
2025-06-27 20:02:16 -0700

    WIP. cpass-links. update rows

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/0-all.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/1-panel-field/0-all.tsx
@@ -1,6 +1,6 @@
 import { type FileUsCtx, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
-import { FirstRow } from "./1-col-field-type";
-import { SecondRow } from "./2-second-row";
+import { FirstRow } from "./1-row-1";
+import { SecondRow } from "./2-row-2";
 
 export function PropsEditorFld({ item, fileUsCtx }: { item: ManualFieldState.CtxFld; fileUsCtx: FileUsCtx; }) {
     return (<>
