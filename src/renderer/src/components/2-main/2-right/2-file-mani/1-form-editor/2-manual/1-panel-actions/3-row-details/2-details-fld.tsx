ebbc89b3461fc014f117becd226780d03bb7ecc5
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-04 16:53:21 -0700

    WIP. cpass-links. update actions list reactivity

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/2-details-fld.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/2-details-fld.tsx
@@ -1,13 +1,23 @@
-import { useAtomValue } from "jotai";
+import { useAtomValue, useSetAtom } from "jotai";
 import { classNames } from "@/utils";
 import { FieldTyp, FormIdx } from "@/store/manifest";
-import { type MFormProps, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
+import { buildLoginDropdownFieldsAtom, type MFormProps, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
 import { detailKbdClasses } from "./8-classes";
 
 export function DetailsFld({ item, mFormProps }: { item: ManualFieldState.CtxFld; mFormProps: MFormProps; }) {
     const isPsw = useAtomValue(item.rowCtx.typeAtom) === FieldTyp.psw;
+    
     const rfieldUuid = useAtomValue(item.rowCtx.rfieldUuidAtom);
-    const isLinked = mFormProps.mFormCtx.fileUsCtx?.formIdx === FormIdx.cpass && isPsw && !!rfieldUuid;
+    const doBuildDropdownFields = useSetAtom(buildLoginDropdownFieldsAtom);
+
+    const isCpassForm = mFormProps.mFormCtx.fileUsCtx?.formIdx === FormIdx.cpass;
+
+    const dropdownAllItems = doBuildDropdownFields(item.rowCtx, mFormProps.mFormCtx.fileUsCtx);
+
+    const rIndexUuidItem = dropdownAllItems.find(([label, uuid]) => uuid === `${rfieldUuid}`);
+    
+    
+    const isLinked = isCpassForm && isPsw && !!rfieldUuid && !!rIndexUuidItem;
 
     const text = isPsw
         ? isLinked
