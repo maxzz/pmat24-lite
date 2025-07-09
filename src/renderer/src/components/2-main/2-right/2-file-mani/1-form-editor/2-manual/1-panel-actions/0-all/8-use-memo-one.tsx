9b3a6b6f0a3af7bf0d17d0ae8db24e67cb7de682
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-08 18:45:01 -0700

    WIP. cpass-links. reactivity. 5. use-memo-one. clean up

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/8-use-memo-one.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/8-use-memo-one.tsx
@@ -1,80 +1,70 @@
-//https://github.com/alexreardon/use-memo-one last: React 18; no updates
-
-// @flow
 import { useRef, useState, useEffect } from 'react';
 
+/**
+ * @param getResult - getResult changes on every call
+ * @param inputs - the inputs array changes on every call
+ * @returns cached result
+ */
+export function useMemoOne<T>(getResult: () => T, inputs?: any[]): T {
+    // using useState to generate initial value as it is lazy
+    const initial: Cache<T> = useState(() => ({ inputs, result: getResult(), }))[0];
+
+    const isFirstRun = useRef<boolean>(true);
+    const committed = useRef<Cache<T>>(initial);
+
+    // persist any uncommitted changes after they have been committed
+    const useCache: boolean =
+        isFirstRun.current ||
+        Boolean(
+            inputs &&
+            committed.current.inputs &&
+            areInputsEqual(inputs, committed.current.inputs),
+        );
+
+    // create a new cache if required
+    const cache: Cache<T> = useCache
+        ? committed.current
+        : {
+            inputs,
+            result: getResult(),
+        };
+
+    // commit the cache
+    useEffect(() => {
+        isFirstRun.current = false;
+        committed.current = cache;
+    }, [cache]);
+
+    return cache.result;
+}
+
 type Cache<T> = {
-  inputs?: (any[]),
-  result: T,
+    inputs?: (any[]),
+    result: T,
 };
 
-export function useMemoOne<T>(
-  // getResult changes on every call,
-  getResult: () => T,
-  // the inputs array changes on every call
-  inputs?: any[],
-): T {
-  // using useState to generate initial value as it is lazy
-  const initial: Cache<T> = useState(() => ({
-    inputs,
-    result: getResult(),
-  }))[0];
-  const isFirstRun = useRef<boolean>(true);
-  const committed = useRef<Cache<T>>(initial);
-
-  // persist any uncommitted changes after they have been committed
-  const useCache: boolean =
-    isFirstRun.current ||
-    Boolean(
-      inputs &&
-        committed.current.inputs &&
-        areInputsEqual(inputs, committed.current.inputs),
-    );
-
-  // create a new cache if required
-  const cache: Cache<T> = useCache
-    ? committed.current
-    : {
-        inputs,
-        result: getResult(),
-      };
-
-  // commit the cache
-  useEffect(() => {
-    isFirstRun.current = false;
-    committed.current = cache;
-  }, [cache]);
-
-  return cache.result;
+/**
+ * @param callback - getResult changes on every call
+ * @param inputs - the inputs array changes on every call
+ * @returns cached result
+ */
+export function useCallbackOne<T extends Function>(callback: T, inputs?: any[]): T {
+    return useMemoOne(() => callback, inputs);
 }
 
-export function useCallbackOne<T extends Function>(
-  // getResult changes on every call,
-  callback: T,
-  // the inputs array changes on every call
-  inputs?: any[],
-): T {
-  return useMemoOne(() => callback, inputs);
-}
-
-
-
-export function areInputsEqual(
-  newInputs: any[],
-  lastInputs: any[],
-) {
-  // no checks needed if the inputs length has changed
-  if (newInputs.length !== lastInputs.length) {
-    return false;
-  }
-  // Using for loop for speed. It generally performs better than array.every
-  // https://github.com/alexreardon/memoize-one/pull/59
-
-  for (let i = 0; i < newInputs.length; i++) {
-    // using shallow equality check
-    if (newInputs[i] !== lastInputs[i]) {
-      return false;
+export function areInputsEqual(newInputs: any[], lastInputs: any[]) {
+    // no checks needed if the inputs length has changed
+    if (newInputs.length !== lastInputs.length) {
+        return false;
     }
-  }
-  return true;
+    // Using for loop for speed. It generally performs better than array.every
+    // https://github.com/alexreardon/memoize-one/pull/59
+
+    for (let i = 0; i < newInputs.length; i++) {
+        // using shallow equality check
+        if (newInputs[i] !== lastInputs[i]) {
+            return false;
+        }
+    }
+    return true;
 }
