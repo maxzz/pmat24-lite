64bcf37cd563f6337d61a8595d0689b966795664
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-09 14:51:08 -0700

    WIP. cpass-links. reactivity with atomEffect. useLoginChangesEffectFn(). update types

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/2-login-changes-effect.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/2-login-changes-effect.tsx
@@ -1,17 +1,21 @@
-import { useCallbackOne } from "@/utils";
-import { type MFormProps, getAllFormsFieldsAtoms, safeByContext } from "@/store/1-atoms/2-file-mani-atoms";
+import { type GetterWithPeek, type SetterWithRecurse, useCallbackOne } from "@/utils";
+import { type FieldRowCtx, type MFormProps, getAllFormsFieldsAtoms, safeByContext } from "@/store/1-atoms/2-file-mani-atoms";
 
 export function loginChangesEffectFn({ mFormProps }: { mFormProps: MFormProps; }) {
     const rv = useCallbackOne(
-        (get, set) => {
+        (get: GetterWithPeek, set: SetterWithRecurse) => {
             const maniAtoms = safeByContext(get(mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom));
             const { loginAtom, cpassAtom } = getAllFormsFieldsAtoms(maniAtoms);
             const loginFields = get(loginAtom);
             const cpassFields = get(cpassAtom);
 
-            console.log('cb: loginFields', loginFields);
-            console.log('cb: cpassFields', cpassFields);
+            printFields(mFormProps, loginFields, cpassFields);
         }, [mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom]
     );
     return rv;
+}
+
+function printFields(mFormProps: MFormProps, loginFields: FieldRowCtx[], cpassFields: FieldRowCtx[]) {
+    const { fileUsCtx: { formIdx} } = mFormProps.mFormCtx;
+    console.log('Effect: formIdx', formIdx, '\n    login', loginFields, '\n    cpass', cpassFields);
 }
