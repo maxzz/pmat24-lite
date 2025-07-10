53f1f781d55235e8a40fcccff4e7897368362a30
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-09 18:48:06 -0700

    WIP. cpass-links. reactivity with atomEffect. useLoginChangesEffectFn(). debugging

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/2-login-changes-effect.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/0-all/2-login-changes-effect.tsx
@@ -1,3 +1,4 @@
+import { type Getter } from "jotai";
 import { type GetterWithPeek, type SetterWithRecurse, useCallbackOne } from "@/utils";
 import { type FieldRowCtx, type MFormProps, getAllFormsFieldsAtoms, safeByContext } from "@/store/1-atoms/2-file-mani-atoms";
 
@@ -9,13 +10,19 @@ export function loginChangesEffectFn({ mFormProps }: { mFormProps: MFormProps; }
             const loginFields = get(loginAtom);
             const cpassFields = get(cpassAtom);
 
-            printFields(mFormProps, loginFields, cpassFields);
+            printFields(mFormProps, loginFields, cpassFields, get);
         }, [mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom]
     );
     return rv;
 }
 
-function printFields(mFormProps: MFormProps, loginFields: FieldRowCtx[], cpassFields: FieldRowCtx[]) {
+function printFields(mFormProps: MFormProps, loginFields: FieldRowCtx[], cpassFields: FieldRowCtx[], get: Getter) {
     const { fileUsCtx: { formIdx} } = mFormProps.mFormCtx;
-    console.log('Effect: formIdx', formIdx, '\n    login', loginFields, '\n    cpass', cpassFields);
+    console.log('Effect: formIdx', formIdx);
+    printField(loginFields, get);
+    printField(cpassFields, get);
+}
+
+function printField(fields: FieldRowCtx[], get: Getter) {
+    console.log(fields.map((field) => `  uuid:${field.rfieldUuidAtom} ${get(field.labelAtom)}\n`).join(''));
 }
