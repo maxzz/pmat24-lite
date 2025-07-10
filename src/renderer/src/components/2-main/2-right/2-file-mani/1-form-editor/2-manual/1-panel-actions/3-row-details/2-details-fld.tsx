ef38dc689eccad22f7fe6c5aa4003feae24feec3
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-09 20:21:47 -0700

    WIP. cpass-links. switch DetailsFld to use isLinkedToLoginAtom

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/2-details-fld.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/2-details-fld.tsx
@@ -1,19 +1,23 @@
 import { useAtomValue, useSetAtom } from "jotai";
 import { classNames } from "@/utils";
 import { FieldTyp, FormIdx } from "@/store/manifest";
-import { buildLoginDropdownFieldsAtom, guardedFormIdx, type MFormProps, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
+import { buildLoginDropdownFieldsAtom, guardedFormIdx, isLinkedToLoginAtom, type MFormProps, type ManualFieldState } from "@/store/1-atoms/2-file-mani-atoms";
 import { detailKbdClasses } from "./8-classes";
 
 export function DetailsFld({ item, mFormProps }: { item: ManualFieldState.CtxFld; mFormProps: MFormProps; }) {
-    const rfieldUuid = useAtomValue(item.rowCtx.rfieldUuidAtom);
+    // const rfieldUuid = useAtomValue(item.rowCtx.rfieldUuidAtom);
     
-    const doBuildLoginDropdownFields = useSetAtom(buildLoginDropdownFieldsAtom);
-    const dropdownAllItems = doBuildLoginDropdownFields(item.rowCtx, mFormProps.mFormCtx.fileUsCtx);
-    const rIndexUuidItem = dropdownAllItems.find(([label, uuid]) => uuid === `${rfieldUuid}`);
+    // const doBuildLoginDropdownFields = useSetAtom(buildLoginDropdownFieldsAtom);
+    // const dropdownAllItems = doBuildLoginDropdownFields(item.rowCtx, mFormProps.mFormCtx.fileUsCtx);
+    // const rIndexUuidItem = dropdownAllItems.find(([label, uuid]) => uuid === `${rfieldUuid}`);
     
-    const isCpassForm = guardedFormIdx(mFormProps) === FormIdx.cpass;
+    // const isCpassForm = guardedFormIdx(mFormProps) === FormIdx.cpass;
+    // const isPsw = useAtomValue(item.rowCtx.typeAtom) === FieldTyp.psw;
+    // const isLinked = isCpassForm && isPsw && !!rfieldUuid && !!rIndexUuidItem;
+
     const isPsw = useAtomValue(item.rowCtx.typeAtom) === FieldTyp.psw;
-    const isLinked = isCpassForm && isPsw && !!rfieldUuid && !!rIndexUuidItem;
+    const isLinkedFn = useSetAtom(isLinkedToLoginAtom);
+    const isLinked = isLinkedFn(item.rowCtx, mFormProps.mFormCtx.fileUsCtx);
 
     const text = isPsw
         ? isLinked
