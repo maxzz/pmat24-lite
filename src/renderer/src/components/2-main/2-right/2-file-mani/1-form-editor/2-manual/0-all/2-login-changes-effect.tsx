b61f2a417aa8dfe2fc3b01e1b2edaad1bd478db5
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-10 20:14:43 -0700

    WIP. cpass-links. loginChangesEffectFn update print. 4

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/2-login-changes-effect.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/0-all/2-login-changes-effect.tsx
@@ -10,7 +10,7 @@ export function loginChangesEffectFn({ mFormProps }: { mFormProps: MFormProps; }
 
             const maniAtoms = safeByContext(get(mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom));
             const { loginAtom, cpassAtom } = getAllFormsFieldsAtoms(maniAtoms);
-            //printFields('Effect before linking', mFormProps, get(loginAtom), get(cpassAtom), get);
+            //printFields(loginChangesEffectFnbefore linking', mFormProps, get(loginAtom), get(cpassAtom), get);
 
             const loginPsws = new Set(get(loginAtom).filter((field) => get(field.typeAtom) === FieldTyp.psw).map((field) => field.metaField.uuid));
             const cpassPsws = get(cpassAtom).filter((field) => get(field.typeAtom) === FieldTyp.psw);
@@ -21,54 +21,37 @@ export function loginChangesEffectFn({ mFormProps }: { mFormProps: MFormProps; }
                 }
             });
 
-            printForms('Effect after linking', mFormProps, get(loginAtom), get(cpassAtom), get);
+            printForms('loginChangesEffectFn after linking', mFormProps, get(loginAtom), get(cpassAtom), get);
         }, [mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom]
     );
     return rv;
+}
+
+function printMFormProps(mFormProps: MFormProps) {
+    console.log(`loginChangesEffectFn formIdx:${mFormProps.mFormCtx.fileUsCtx.formIdx} ${mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom.toString()}`);
 }
 
 function printForms(label: string, mFormProps: MFormProps, loginFields: FieldRowCtx[], cpassFields: FieldRowCtx[], get: Getter) {
     const { fileUsCtx: { formIdx } } = mFormProps.mFormCtx;
     console.log(`${label}: formIdx`, formIdx);
 
-    printFields(loginFields, get);
-    printFields(cpassFields, get);
-
-    // let colors: string[] = [];
-    // let lines: string[] = [];
-    // printField(loginFields, get);
-    // console.log(lines.join('\n'), ...colors);
-
-    // colors = [];
-    // lines = [];
-    // printField(cpassFields, get);
-    // console.log(lines.join('\n'), ...colors);
-
-    // function printField(fields: FieldRowCtx[], get: Getter) {
-    //     fields.forEach((field) => {
-    //         lines.push(`%c        this.uuid: %c${field.metaField.uuid} %cref.uuid: %c${get(field.rfieldUuidAtom)} %c'${get(field.labelAtom)}'`);
-    //         colors.push('font-size:0.5rem; color: forestgreen', 'color: forestgreen', 'font-size:0.5rem; color: forestgreen', 'color: forestgreen', 'color: black');
-    //     });
-    // }
+    loginFields.length && printFields(loginFields, get);
+    cpassFields.length && printFields(cpassFields, get);
 }
 
 function printFields(fields: FieldRowCtx[], get: Getter) {
     const colors: string[] = [];
     const lines: string[] = [];
 
-    function printField(fields: FieldRowCtx[], get: Getter) {
+    function collect(fields: FieldRowCtx[], get: Getter) {
         fields.forEach((field) => {
             lines.push(`%c        this.uuid: %c${field.metaField.uuid} %cref.uuid: %c${get(field.rfieldUuidAtom)} %c'${get(field.labelAtom)}'`);
             colors.push('font-size:0.5rem; color: forestgreen', 'color: forestgreen', 'font-size:0.5rem; color: forestgreen', 'color: forestgreen', 'color: black');
         });
     }
 
-    printField(fields, get);
+    collect(fields, get);
     console.log(lines.join('\n'), ...colors);
-}
-
-function printMFormProps(mFormProps: MFormProps) {
-    console.log(`loginChangesEffectFn formIdx:${mFormProps.mFormCtx.fileUsCtx.formIdx} ${mFormProps.mFormCtx.fileUsCtx.fileUs.maniAtomsAtom.toString()}`);
 }
 
 //TODO: drill down context reactive atoms to avoid useSetAtom
