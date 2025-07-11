2afef658e0a54e9a76ee1efd202b06ac5d82f865
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-11 15:10:34 -0700

    WIP. update package dependencies. update pmat_plugin_nodejs

--- src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/10-general-info.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/10-general-info.ts
@@ -3,8 +3,16 @@ import { type PluginDataCallback } from "./9-types";
 export type GeneralInfoParams = {};             // There is no parameters for this call
 
 export type GeneralInfoResult = {               // General information about the plugin
-    curver: string;                             // Current version of the DPAgent
+    curver: string;                             // Current version of the DPAgent (must match what BrowserExtension expects)
+    apiVer: string;                             // API version of the DPAgent (for New PMAT)
     templatePath: string;                       // Path to the network share with template files (Manifests)
+    products: [                                 // Installed products
+        {
+            product: string;
+            version: string;
+        }
+    ],
+    copy: string;                               // Copyright information
 };
 
 export interface GetGeneralInfo {
--- src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/11-perform-command.ts
+++ src/shell/xternal-to-renderer/7-napi-calls/pmat-plugin-types/11-perform-command.ts
@@ -1,14 +1,24 @@
 import { type PluginDataCallback } from "./9-types";
 
 export type PerformCommandParams = {
-    command: "reloadCache";                     // Command to perform. Currently defined commands: "reloadCache" (no parameters)
-    params: any;                                // Parameters for the command, content depends on the command.
+    command: "reloadCache";                     // Command to perform. "reloadCache" is to tell DPAgent to reload cached manifests
+    params: undefined;                          // Parameters is not used "reloadCache" 
+} | {
+    command: "queryExtension";                  // Command to perform. "queryExtension" sends arbitrary string to browser extension
+    params: {
+        hwnd: string;                           // HWND of the browser window (not used currently, the active browser window is used)
+        query: string;                          // Query string to send. THe string must be passable through stringified JSON
+    };                          
 };
 
 export type ReloadCacheResult = {
 };
 
-export type PerformCommandResult = ReloadCacheResult; // add more with | anotherResult 
+export type ExtensionExchangeResult = {
+    extension_exchange: string;          // The extension exchange string is returned to the caller
+};
+
+export type PerformCommandResult = ReloadCacheResult | ExtensionExchangeResult; // add more with | anotherResult 
 
 export interface PerformCommand {
     (performCommandParams: string, cb: PluginDataCallback<PerformCommandResult>): void;
