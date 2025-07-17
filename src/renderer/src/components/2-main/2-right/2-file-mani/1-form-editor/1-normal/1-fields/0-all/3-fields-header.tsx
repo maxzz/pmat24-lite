32a8b44d7cfedcd1c82b545335e32af1ddde5d5e
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-16 19:08:07 -0700

    WIP. cpass-links-normal. TableHeader re-done

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/0-all/3-fields-header.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/0-all/3-fields-header.tsx
@@ -2,6 +2,28 @@ import { useSnapshot } from "valtio";
 import { appSettings } from "@/store";
 import { classNames } from "@/utils";
 
+export function TableHeader({ isPasswordForm }: { isPasswordForm: boolean; }) {
+    const { fcAllowed } = useSnapshot(appSettings.files.shownManis);
+    const skipIdx = fcAllowed ? -1 : 4;
+    return (<>
+        {rowColumns.map(
+            ([title, hint], idx) => {
+                if (idx === skipIdx) {
+                    return undefined;
+                }
+                if (isPasswordForm && idx === 5) {
+                    idx = 6;
+                }
+                return (
+                    <div className={headerClasses} title={hint} key={idx}>
+                        {title}
+                    </div>
+                );
+            }).filter(Boolean)
+        }
+    </>);
+}
+
 export function getFieldsGridClasses(showFieldCatalog: boolean) {
     const colsClasses =
         showFieldCatalog
@@ -23,24 +45,5 @@ const rowColumns = [
     ['Value',               /*3*/ 'Specifies the value to fill out the field\nand how it is stored'],
     ['Shared ID (Catalog)', /*4*/ 'The Shared ID determines whether the value\nwill be shared through the field catalog'],
     ['Policy',              /*5*/ 'Password policy for the field'],
+    ['Link',                /*6*/ 'Link to login form'],
 ];
-
-export function TableHeader({ isPasswordForm }: { isPasswordForm: boolean; }) {
-    const { fcAllowed } = useSnapshot(appSettings.files.shownManis);
-    const skipIdx = fcAllowed ? -1 : 4;
-    return (<>
-        {rowColumns.map(
-            ([title, hint], idx) => {
-                return (
-                    idx === skipIdx
-                        ? undefined
-                        : (
-                            <div className={headerClasses} title={hint} key={idx}>
-                                {title}
-                            </div>
-                        )
-                );
-            }).filter(Boolean)
-        }
-    </>);
-}
