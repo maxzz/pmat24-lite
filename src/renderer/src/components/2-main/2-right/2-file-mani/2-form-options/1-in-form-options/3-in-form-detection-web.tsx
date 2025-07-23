16c6e2fe50111f6efb40f6ac2986debda8a1f76c
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-23 14:45:43 -0700

    WIP. web-match. MatchHow. 7. fix regex constant value

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
@@ -2,12 +2,13 @@ import { type OFormProps } from "@/store/1-atoms/2-file-mani-atoms";
 import { AccordionWithTrigger } from "@/ui/motion-primitives";
 import { InputWithTitle2Rows } from "@/ui/local-ui";
 import { MatchHow } from "./5-2-match-how";
+import { useIsShowExample } from "./5-9-set-atoms";
 
 export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }) {
     const formIdx = oFormProps.oAllAtoms.options.formIdx;
     const { ourlAtom, rurlAtom } = oFormProps.oAllAtoms.options.p2Detect;
 
-    const showExample = false;
+    const showExample = useIsShowExample(oFormProps.oAllAtoms.options);
 
     return (
         <AccordionWithTrigger name='form-detection' formIdx={formIdx} triggerText="Screen detection">
@@ -19,16 +20,18 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
 
                 <InputWithTitle2Rows stateAtom={rurlAtom} label="Match URL" asTextarea />
 
-                <div className="mt-1">
-                    Now the match URL is a regular expression and equal to the original URL.
-                    It is recommended to specify the match URL as a part of original URL. 
-                </div>
+                {showExample && (<>
+                    <div className="mt-1">
+                        Now the match URL is a regular expression and equal to the original URL.
+                        It is recommended to specify the match URL as a part of original URL.
+                    </div>
 
-                <div className="mt-1">
-                    For example:
-                    for <span className={exampleClasses}>https://login.example.com</span> the match URL as regular expression can be: <span className={exampleClasses}>login</span>.
-                    That will not be match <span className={exampleClasses}>https://admin.example.com/login</span>.
-                </div>
+                    <div className="mt-1">
+                        For example:
+                        for <span className={exampleClasses}>https://login.example.com</span> the match URL as regular expression can be: <span className={exampleClasses}>login</span>.
+                        That will not be match <span className={exampleClasses}>https://admin.example.com/login</span>.
+                    </div>
+                </>)}
 
             </div>
         </AccordionWithTrigger>
--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/5-2-match-how.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/5-2-match-how.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue, useSetAtom } from "jotai";
-import { type Matching, type OptionTextValue } from "@/store/manifest";
+import { Matching, type OptionTextValue } from "@/store/manifest";
 import { InputSelectUi } from "@/ui";
 import { type OFormProps } from "@/store/1-atoms/2-file-mani-atoms/9-types";
 import { setUrlsEditorDataAtom } from "./5-9-set-atoms";
@@ -25,19 +25,19 @@ export function MatchHow({ oFormProps }: { oFormProps: OFormProps; }) {
 }
 
 function getNamesList(how: Matching.How) {
-    return how !== 0 && how !== 3 ? allHowNames : shortHowNames;
+    return how !== Matching.How.undef && how !== Matching.How.regex ? allHowNames : shortHowNames;
 }
 
 const shortHowNames: OptionTextValue[] = [
     ["As original URL",                                         /*0*/ "0"],
-    ["Regular expression",                                      /*3*/ "3"],
+    ["Regular expression",                                      /*2*/ "2"],
 ];
 
 const allHowNames: OptionTextValue[] = [
     ["Same as original url",                                    /*0*/ "0"],
     ["Match only domain of original url",                       /*1*/ "1"],
-    ["Wildcard match",                                          /*2*/ "2"],
-    ["Regular expresssion",                                     /*3*/ "3"],
+    ["Wildcard match",                                          /*3*/ "3"],
+    ["Regular expresssion",                                     /*2*/ "2"],
     ["No domain match (Exclude this login from domain match)",  /*4*/ "4"],
 ];
 
