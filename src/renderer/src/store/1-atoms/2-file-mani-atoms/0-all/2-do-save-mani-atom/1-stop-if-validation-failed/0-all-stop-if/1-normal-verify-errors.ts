7a35fa206cdff80f7b0100b715fc9eaf4b49257b
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-29 13:06:01 -0700

    WIP. save-mani. validation. new normalFormsVerifyErrors()

--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/1-normal-verify-errors.ts
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/1-normal-verify-errors.ts
@@ -1,45 +1,32 @@
 import { type Getter, type Setter } from "jotai";
-import { type ManiAtoms, type VerifyError } from "../../../../9-types";
+import { type ManiAtoms, type FieldRowCtx, type VerifyError } from "../../../../9-types";
 
 export function normalFormsVerifyErrors(get: Getter, set: Setter, { maniAtoms }: { maniAtoms: ManiAtoms; }): VerifyError[] | undefined {
     const rv: VerifyError[] = [];
     const [login, cpass] = maniAtoms;
 
-    if (login?.normal) {
-
-        let totalUseIt = 0;
-        login.normal.rowCtxs.forEach(
-            (fieldAtoms, idx) => {
-                const useIt = get(fieldAtoms.useItAtom);
-                if (useIt) {
-                    totalUseIt++;
-                }
-            }
-        );
-
-        if (!totalUseIt) {
-            rv.push({ error: 'No login fields selected', tab: 'login' });
-        }
+    if (login?.normal && !totalFieldsInUse(login.normal.rowCtxs, get)) {
+        rv.push({ error: 'No login fields selected', tab: 'login' });
     }
 
-    if (cpass?.normal) {
-
-        let totalUseIt = 0;
-        cpass.normal.rowCtxs.forEach(
-            (fieldAtoms, idx) => {
-                const useIt = get(fieldAtoms.useItAtom);
-                if (useIt) {
-                    totalUseIt++;
-                }
-            }
-        );
-
-        if (!totalUseIt) {
-            rv.push({ error: 'No password change fields selected', tab: 'cpass' });
-        }
+    if (cpass?.normal && !totalFieldsInUse(cpass.normal.rowCtxs, get)) {
+        rv.push({ error: 'No password change fields selected', tab: 'cpass' });
     }
 
     return rv.length ? rv : undefined;
+}
+
+function totalFieldsInUse(rowCtxs: FieldRowCtx[] | undefined, get: Getter): number {
+    let totalUseIt = 0;
+    rowCtxs?.forEach(
+        (fieldRowCtx) => {
+            const useIt = get(fieldRowCtx.useItAtom);
+            if (useIt) {
+                totalUseIt++;
+            }
+        }
+    );
+    return totalUseIt;
 }
 
 //TODO: We can remove the button elements if the form is intended for websites. Buttons were added for old IE submit method.
