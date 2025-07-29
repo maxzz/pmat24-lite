3ac44196589e879a61d937060efff5e8c6ece737
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-29 12:54:44 -0700

    WIP. save-mani. validation. new stopIfInvalidAny(). get away from vwerify atoms. 4

--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/0-stop-if-any.tsx
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/0-stop-if-any.tsx
@@ -1,10 +1,11 @@
 import { type Getter, type Setter } from "jotai";
+import { toast } from "sonner";
+import { appSettings } from "@/store/9-ui-state";
 import { FormIdx } from "@/store/manifest";
 import { type ManiAtoms, type VerifyError } from "../../../../9-types";
 import { normalFormsVerifyErrors } from "./1-normal-verify-errors";
 import { manualFormsVerifyErrors } from "./2-manual-verify-errors";
 import { optionsVerifyErrors } from "./3-options-verify-errors";
-import { showValidationErrors } from "./8-show-validation-errors";
 
 export function stopIfInvalidAny(maniAtoms: ManiAtoms, get: Getter, set: Setter): boolean | undefined {
     const maniItself: VerifyError[] | undefined = maniAtoms[FormIdx.login] ? undefined : [{ error: 'Login form is missing', tab: 'options' }];
@@ -21,6 +22,20 @@ export function stopIfInvalidAny(maniAtoms: ManiAtoms, get: Getter, set: Setter)
 
     showValidationErrors({ fromTab: errors[0].tab, verifyErrors: errors }); // errors[0].tab or 'login'
     return true;
+}
+
+function showValidationErrors({ fromTab, verifyErrors }: { fromTab: string | undefined; verifyErrors: VerifyError[]; }): void {
+    if (fromTab) {
+        appSettings.right.mani.activeTab = fromTab;
+    }
+
+    const messages = verifyErrors.map(
+        (err, idx) => {
+            return <div key={idx}>{err.error}</div>;
+        }
+    );
+
+    toast.error(<div className="flex flex-col">{messages}</div>);
 }
 
 //TODO: manual validation: activate row
