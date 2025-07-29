46206045bf05ecb5b4d8c2f600ae5113f0173625
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-28 17:56:30 -0700

    WIP. save-mani. validation. showValidationErrors(). 2

--- src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/4-show-validation-errors.tsx
+++ src/renderer/src/store/1-atoms/2-file-mani-atoms/0-all/2-do-save-mani-atom/1-stop-if-validation-failed/0-all-stop-if/4-show-validation-errors.tsx
@@ -2,17 +2,19 @@ import { toast } from "sonner";
 import { appSettings } from "@/store/9-ui-state/0-local-storage-app";
 import { type VerifyError } from "../../../../9-types";
 
-export function showValidationErrors({ fromTab, verifyErrors }: { fromTab: string; verifyErrors: VerifyError[] | undefined; }): boolean | undefined {
-    if (verifyErrors) {
-        appSettings.right.mani.activeTab = verifyErrors[0].tab;
-
-        const messages = verifyErrors.map(
-            (err, idx) => {
-                return <div key={idx}>{err.error}</div>;
-            }
-        );
-
-        toast.error(<div className="flex flex-col">{messages}</div>);
-        return true;
+export function showValidationErrors({ fromTab, verifyErrors }: { fromTab: string; verifyErrors: VerifyError[] | undefined; }): void {
+    if (!verifyErrors?.length) {
+        return;
     }
+    
+    appSettings.right.mani.activeTab = fromTab;
+    // appSettings.right.mani.activeTab = verifyErrors[0].tab; // or like this
+
+    const messages = verifyErrors.map(
+        (err, idx) => {
+            return <div key={idx}>{err.error}</div>;
+        }
+    );
+
+    toast.error(<div className="flex flex-col">{messages}</div>);
 }
