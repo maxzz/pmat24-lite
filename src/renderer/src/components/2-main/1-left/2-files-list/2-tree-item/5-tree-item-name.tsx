b7ce3e0abf948c894254c4b16b187564370f1c27
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-03 13:51:34 -0700

    WIP. save-mani-tree-icon. overflow inside truncate prevents changed icon

--- src/renderer/src/components/2-main/1-left/2-files-list/2-tree-item/5-tree-item-name.tsx
+++ src/renderer/src/components/2-main/1-left/2-files-list/2-tree-item/5-tree-item-name.tsx
@@ -18,10 +18,10 @@ export function TreeItemName({ fileUs, item }: { fileUs: FileUs; item: TreeFileI
     const displayText = getTreeItemDisplayText(fileUs, appSettings.files.itemsState, chooseName);
 
     return (<>
-        <div className="relative w-full">
-            {hasChanges && (
+        <div className="relative 1truncate after:absolute after:left-[-16px] after:-top-1/2 after:w-4 after:h-full after:bg-red-500 after:-z-[20]">
+            {/* {hasChanges && (
                 <SymbolFire className="absolute -left-4 -top-1/3 flex-none mr-0.5 size-3 text-red-500" colorize />
-            )}
+            )} */}
 
             <div className={classNames("truncate", hasChanges && "text-orange-600")} title={title}>
                 {displayText}
@@ -31,6 +31,29 @@ export function TreeItemName({ fileUs, item }: { fileUs: FileUs; item: TreeFileI
         <StateIcons fileUs={fileUs} />
     </>);
 }
+
+// export function TreeItemName({ fileUs, item }: { fileUs: FileUs; item: TreeFileItemWState; }) {
+
+//     const hasChanges = !!useSnapshot(fileUs.fileCnt.changesSet).size;
+//     const title = hasChanges ? "This file has changes" : undefined;
+
+//     const chooseName = useAtomValue(fileUs.parsedSrc.stats.loginFormChooseNameAtom);
+//     const displayText = getTreeItemDisplayText(fileUs, appSettings.files.itemsState, chooseName);
+
+//     return (<>
+//         <div className="relative">
+//             {hasChanges && (
+//                 <SymbolFire className="absolute -left-4 -top-1/3 flex-none mr-0.5 size-3 text-red-500" colorize />
+//             )}
+
+//             <div className={classNames("truncate", hasChanges && "text-orange-600")} title={title}>
+//                 {displayText}
+//             </div>
+//         </div>
+
+//         <StateIcons fileUs={fileUs} />
+//     </>);
+// }
 
 function StateIcons({ fileUs }: { fileUs: FileUs; }) {
     const isInUse = useAtomValue(fileUs.maniInUseAtom);
