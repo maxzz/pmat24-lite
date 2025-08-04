76c3ed5188c763469845a5b7efc3b057ffb7e907
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-04 13:52:54 -0700

    WIP. about. format message from napi call. 6. update templates path

--- src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
+++ src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
@@ -1,10 +1,12 @@
 import { atom } from "jotai";
+import { classNames } from "@/utils";
+import { Textarea } from "@/ui/shadcn";
+import { inputRingClasses } from "@/ui/local-ui";
+import { AlertOctagon } from "lucide-react";
 import { hasMain } from "@/xternal-to-main";
 import { aboutMessages, doAsyncConfirmDialogAtom } from "@/store/1-atoms/7-dialogs";
 import { doGetGeneralInfoAtom } from "@/store/7-napi-atoms";
-import { AlertOctagon } from "lucide-react";
 import { type GeneralInfoResult } from "@shared/ipc-types";
-import { Input } from "@/ui/shadcn";
 
 export const doAboutDialogAtom = atom(null,
     async (get, set) => {
@@ -20,6 +22,7 @@ function FormattedJson({ json }: { json: string; }) {
         const rvObj = JSON.parse(json) as GeneralInfoResult;
         let { templatePath, products, copy } = rvObj;
         const copyright = copy.replaceAll('�', '©').split('/');
+        templatePath = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima ipsum rerum molestias voluptatum accusamus porro dolorem temporibus laboriosam quia ipsam ut odio omnis eligendi doloremque at, illum quos mollitia praesentium!";
         return (
             <div className="w-full text-xs grid gap-4">
                 <div>
@@ -37,7 +40,7 @@ function FormattedJson({ json }: { json: string; }) {
                     <div className="mb-1 font-semibold">
                         GPO templates path
                     </div>
-                    <Input className="h-7 rounded" value={templatePath} readOnly tabIndex={-1} />
+                    <Textarea className={classNames(optionInputClasses, inputRingClasses)} style={{ 'fieldSizing': 'content' } as React.CSSProperties} value={templatePath} readOnly tabIndex={-1} />
                 </div>
 
                 <div className="text-[0.65rem] grid">
@@ -54,6 +57,17 @@ function FormattedJson({ json }: { json: string; }) {
         );
     }
 }
+
+const optionInputClasses = "\
+px-2 py-1 min-h-7 w-full resize-none \
+\
+text-mani-foreground font-normal bg-mani-background \
+\
+border-mani-border-muted border \
+\
+rounded-sm \
+outline-none";
+
 
 // Default reply from napi to test wo/ electron:
 const defAbount = "{\"curVer\": \"2160128\",\"apiVer\":\"2250613\",\"templatePath\":\"\",\"products\":[{\"product\":\"Altus AD Workstation\",\"version\":\"4.1.0.1083\"},{\"product\":\"Password Manager\",\"version\":\"4.1.0.1083\"}],\"copy\":\"Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB\"}";
