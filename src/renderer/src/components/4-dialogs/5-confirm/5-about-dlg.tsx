a88754ee2eb9f3d4d47e4e0ceebb4f24601bc538
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-04 14:25:02 -0700

    WIP. about. format message from napi call. 7. code review. doGetGeneralInfoAtom is now function

--- src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
+++ src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
@@ -3,27 +3,23 @@ import { classNames } from "@/utils";
 import { Textarea } from "@/ui/shadcn";
 import { inputRingClasses, optionInputClasses } from "@/ui/local-ui";
 import { AlertOctagon } from "lucide-react";
-import { hasMain } from "@/xternal-to-main";
 import { aboutMessages, doAsyncConfirmDialogAtom } from "@/store/1-atoms/7-dialogs";
-import { doGetGeneralInfoAtom } from "@/store/7-napi-atoms";
+import { asyncGetAboutInfo } from "@/store/7-napi-atoms";
 import { type GeneralInfoResult } from "@shared/ipc-types";
 
 export const doAboutDialogAtom = atom(null,
     async (get, set) => {
-        const rv = hasMain() ? await set(doGetGeneralInfoAtom) : defAbount;
-        const message = FormattedJson({ json: rv });
+        const json = await asyncGetAboutInfo(); // console.log('about.info:', json);
+        const message = FormattedJson({ json });
         const ui = { ...aboutMessages, message };
         await set(doAsyncConfirmDialogAtom, ui);
     }
 );
-            // if (data) {
-            //     console.log('general.info:', data);
-            // }
 
 function FormattedJson({ json }: { json: string; }) {
     try {
-        const rvObj = JSON.parse(json) as GeneralInfoResult;
-        let { templatePath, products, copy } = rvObj;
+        const obj = JSON.parse(json) as GeneralInfoResult;
+        let { products, templatePath = '', copy = '' } = obj;
         const copyright = copy.replaceAll('�', '©').split('/');
         return (
             <div className="w-full text-xs grid gap-4">
@@ -31,18 +27,21 @@ function FormattedJson({ json }: { json: string; }) {
                     <div className="mb-1 1font-semibold">
                         Installed products:
                     </div>
-                    {products.map(({ product, version }) => (
-                        <div key={product}>
-                            {product}: version {version}
-                        </div>
-                    ))}
+                    {products
+                        ? (products.map(({ product, version }) => (
+                            <div key={product}> {product}: version {version} </div>
+                        )))
+                        : (
+                            <div className="text-center">No products installed</div>
+                        )
+                    }
                 </div>
 
-                <div className="">
+                <div>
                     <div className="mb-1 1font-semibold">
                         GPO templates path
                     </div>
-                    <TempatePath templatePath={templatePath} />
+                    <GpoTempatesPath templatePath={templatePath} />
                 </div>
 
                 <div className="text-[0.65rem] grid">
@@ -60,7 +59,7 @@ function FormattedJson({ json }: { json: string; }) {
     }
 }
 
-function TempatePath({ templatePath }: { templatePath: string; }) {
+function GpoTempatesPath({ templatePath }: { templatePath: string; }) {
     return (
         <Textarea
             className={classNames("text-xs", optionInputClasses, inputRingClasses)}
@@ -68,32 +67,6 @@ function TempatePath({ templatePath }: { templatePath: string; }) {
             value={templatePath}
             readOnly
             tabIndex={-1}
-        />);
+        />
+    );
 }
-
-// Default reply from napi to test wo/ electron:
-const defAbount = '{"curVer": "2160128","apiVer":"2250613","templatePath":"","products":[{"product":"Altus AD Workstation","version":"4.1.0.1083"},{"product":"Password Manager","version":"4.1.0.1083"}],"copy":"Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB"}';
-
-/*
-{
-    "curVer": "2160128",
-    "apiVer": "2250613",
-    "templatePath": "",
-    "products": [
-        {
-            "product": "Altus AD Workstation",
-            "version": "4.1.0.1083"
-        },
-        {
-            "product": "Password Manager",
-            "version": "4.1.0.1083"
-        }
-    ],
-    "copy": "Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB"
-}
-
-//Products
-//Path to templates
-//version: api, current
-//Copyright
-*/
--- src/renderer/src/store/7-napi-atoms/a-do-get-general-info/0-all-general-info.tsx
+++ src/renderer/src/store/7-napi-atoms/a-do-get-general-info/0-all-general-info.tsx
@@ -1,16 +1,39 @@
-import { atom } from "jotai";
 import { toast } from "sonner";
-import { invokeMainTyped } from "@/xternal-to-main";
+import { hasMain, invokeMainTyped } from "@/xternal-to-main";
 
-export const doGetGeneralInfoAtom = atom(
-    null,
-    async (get, set): Promise<string> => {
-        try {
-            const rv = await invokeMainTyped({ type: 'r2mi:get-general-info' }); // JSON.parse(rv) as GeneralInfoResult
-            return rv;
-        } catch (error) {
-            toast.error(`Cannot get general info: ${error}`);
-            return '';
-        }
+export async function asyncGetAboutInfo(): Promise<string> {
+    try {
+        const rv = hasMain() ? await invokeMainTyped({ type: 'r2mi:get-general-info' }) : defAbount; // JSON.parse(rv) as GeneralInfoResult
+        return rv;
+    } catch (error) {
+        toast.error(`Cannot get general info: ${error}`);
+        return '{}';
     }
-);
+}
+
+// Default reply from napi to test wo/ electron:
+const defAbount = '{"curVer": "2160128","apiVer":"2250613","templatePath":"","products":[{"product":"Altus AD Workstation","version":"4.1.0.1083"},{"product":"Password Manager","version":"4.1.0.1083"}],"copy":"Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB"}';
+
+/*
+{
+    "curVer": "2160128",
+    "apiVer": "2250613",
+    "templatePath": "",
+    "products": [
+        {
+            "product": "Altus AD Workstation",
+            "version": "4.1.0.1083"
+        },
+        {
+            "product": "Password Manager",
+            "version": "4.1.0.1083"
+        }
+    ],
+    "copy": "Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB"
+}
+
+//Products
+//Path to templates
+//version: api, current
+//Copyright
+*/
