7d6f65f2f5ba414f1e57a0ba3983ac0f57537b53
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-04 13:46:24 -0700

    WIP. about. format message from napi call. 5. body ready

--- src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
+++ src/renderer/src/components/4-dialogs/5-confirm/5-about-dlg.tsx
@@ -3,32 +3,48 @@ import { hasMain } from "@/xternal-to-main";
 import { aboutMessages, doAsyncConfirmDialogAtom } from "@/store/1-atoms/7-dialogs";
 import { doGetGeneralInfoAtom } from "@/store/7-napi-atoms";
 import { AlertOctagon } from "lucide-react";
+import { type GeneralInfoResult } from "@shared/ipc-types";
+import { Input } from "@/ui/shadcn";
 
 export const doAboutDialogAtom = atom(null,
     async (get, set) => {
         const rv = hasMain() ? await set(doGetGeneralInfoAtom) : defAbount;
-
         const message = FormattedJson({ json: rv });
-
         const ui = { ...aboutMessages, message };
         await set(doAsyncConfirmDialogAtom, ui);
     }
 );
 
-// Default reply from napi to test wo/ electron:
-const defAbount = "{\"curVer\": \"2160128\",\"apiVer\":\"2250613\",\"templatePath\":\"\",\"products\":[{\"product\":\"Altus AD Workstation\",\"version\":\"4.1.0.1083\"},{\"product\":\"Password Manager\",\"version\":\"4.1.0.1083\"}],\"copy\":\"Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB\"}";
-
 function FormattedJson({ json }: { json: string; }) {
     try {
-        const rvObj = JSON.parse('');
-        const rv = (
-            <div className="text-xs">
-                <div className="whitespace-pre">
-                    {JSON.stringify(rvObj, null, 4)}
+        const rvObj = JSON.parse(json) as GeneralInfoResult;
+        let { templatePath, products, copy } = rvObj;
+        const copyright = copy.replaceAll('�', '©').split('/');
+        return (
+            <div className="w-full text-xs grid gap-4">
+                <div>
+                    <div className="mb-1 font-semibold">
+                        Installed products:
+                    </div>
+                    {products.map(({ product, version }) => (
+                        <div key={product}>
+                            {product}: version {version}
+                        </div>
+                    ))}
+                </div>
+
+                <div className="">
+                    <div className="mb-1 font-semibold">
+                        GPO templates path
+                    </div>
+                    <Input className="h-7 rounded" value={templatePath} readOnly tabIndex={-1} />
+                </div>
+
+                <div className="text-[0.65rem] grid">
+                    {copyright.map((line, index) => (<div className="text-center" key={index}>{line}</div>))}
                 </div>
             </div>
         );
-        return rv;
     } catch (error) {
         return (
             <div className="text-xs flex items-center gap-x-2">
@@ -38,6 +54,10 @@ function FormattedJson({ json }: { json: string; }) {
         );
     }
 }
+
+// Default reply from napi to test wo/ electron:
+const defAbount = "{\"curVer\": \"2160128\",\"apiVer\":\"2250613\",\"templatePath\":\"\",\"products\":[{\"product\":\"Altus AD Workstation\",\"version\":\"4.1.0.1083\"},{\"product\":\"Password Manager\",\"version\":\"4.1.0.1083\"}],\"copy\":\"Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB\"}";
+
 /*
 {
     "curVer": "2160128",
@@ -55,9 +75,9 @@ function FormattedJson({ json }: { json: string; }) {
     ],
     "copy": "Copyright � 1996-2025 HID Global Corporation/ASSA ABLOY AB"
 }
-*/
 
 //Products
 //Path to templates
 //version: api, current
 //Copyright
+*/
