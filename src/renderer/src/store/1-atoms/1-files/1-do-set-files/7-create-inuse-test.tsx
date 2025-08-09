e39afebadead8126581c84d0bf424ccde0b377f8
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-09 14:03:53 -0700

    WIP. in-test and in-use. regex. 1

--- src/renderer/src/store/1-atoms/1-files/1-do-set-files/7-create-inuse-test.tsx
+++ src/renderer/src/store/1-atoms/1-files/1-do-set-files/7-create-inuse-test.tsx
@@ -1,6 +1,7 @@
 import { type Atom, atom } from "jotai";
 import { type FileContent } from "@shared/ipc-types";
-import { type FileUs } from "@/store/store-types";
+import { type FileUsAtom, type FileUs } from "@/store/store-types";
+import { rootDir, sureRootDir } from "../0-files-atom/2-root-dir";
 
 export function createTestInUseAtoms(fileCnt: FileContent): Pick<FileUs, 'maniInUseAtom' | 'maniInTestAtom'> {
     let maniInUse = true; //TODO: initialize; not in use if sub-folder is B/C
@@ -17,4 +18,49 @@ export function createTestInUseAtoms(fileCnt: FileContent): Pick<FileUs, 'maniIn
         maniInUseAtom: atom<boolean>(maniInUse),
         maniInTestAtom: atom<boolean>(maniInTest),
     };
+}
+
+export function initTestInUseAtoms(fileUsAtoms: FileUsAtom[], getset: GetSet) {
+    const rootPath = makeTestInUseRegex(rootDir.fpath);
+
+    fileUsAtoms.forEach(
+        (fileUsAtom) => {
+            const fileUs = getset.get(fileUsAtom);
+            const fpath = fileUs.fileCnt.fpath.toLowerCase();
+            const m = fpath.match(rootPath);
+
+            console.log(`initTestInUseAtoms: fpath: "${fileUs.fileCnt.fpath}" fname: "${fileUs.fileCnt.fname}"`, m);
+
+            // const maniInUse = fileUs.maniInUseAtom.get();
+            // const maniInTest = fileUs.maniInTestAtom.get();
+            // fileUs.maniInUseAtom.set(maniInUse);
+            // fileUs.maniInTestAtom.set(maniInTest);
+        }
+    );
+}
+
+export async function initLocalCacheTestInUseAtoms(fileUsAtoms: FileUsAtom[], getset: GetSet) {
+    const promises = fileUsAtoms.map(
+        async (fileUsAtom) => {
+            const fileUs = getset.get(fileUsAtom);
+            if (fileUs) {
+                // const maniInUse = await fileUs.maniInUseAtom.get();
+                // const maniInTest = await fileUs.maniInTestAtom.get();
+                // fileUs.maniInUseAtom.set(maniInUse);
+                // fileUs.maniInTestAtom.set(maniInTest);
+            }
+        }
+    );
+    await Promise.all(promises);
+}
+
+// Utilities
+
+function makeTestInUseRegex(rootPath: string): RegExp {
+    return RegExp(`^${rootPath.toLowerCase()}(?:[\//]([a-c]))?$`, 'i');
+}
+
+function matchTestInUseRegex(rootPath: string, fpath: string): 'a' | 'b' | 'c' | null {
+    const m = fpath.match(makeTestInUseRegex(rootPath));
+    return m ? m[1] as 'a' | 'b' | 'c' : null;
 }
