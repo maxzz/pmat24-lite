ee4f77db0c136759938ff3005c8664ad8d839d82
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-11 13:03:48 -0700

    WIP. in-test and in-use. remove notUs option from parse

--- src/renderer/src/store/1-atoms/1-files/0-files-atom/2-root-dir.ts
+++ src/renderer/src/store/1-atoms/1-files/0-files-atom/2-root-dir.ts
@@ -63,22 +63,22 @@ export function sureRootDir(): string {
 
 // Subfolder detection utilities
 
-export function getTestInUse(fpath: string): { inUse: boolean; inTest: boolean; notUs: boolean; } {
+export function getTestInUse(fpath: string): { inUse: boolean; inTest: boolean; } {
     fpath = normalizeFpath(fpath);
 
     if (fpath === rootDir.fpath) {
-        //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: green", { inUse: true, inTest: false, notUs: false });
-        return { inUse: true, inTest: false, notUs: false };
+        //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: green", { inUse: true, inTest: false, });
+        return { inUse: true, inTest: false, };
     }
 
     const m = fpath.match(/[\//]([a-c])$/i);
     if (m === null) {
-        //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: blue", { inUse: false, inTest: false, notUs: true });
-        return { inUse: false, inTest: false, notUs: true };
+        //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: blue", { inUse: false, inTest: false, });
+        return { inUse: false, inTest: false, }; // if not matched suffix then it's not our subfolder
     }
 
-    //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: orange", { inUse: m[1] === 'a', inTest: m[1] === 'c', notUs: false });
-    return { inUse: m[1] === 'a', inTest: m[1] === 'c', notUs: false, };
+    //console.log(`%cgetTestInUse: fpath: "${fpath}":`, "color: orange", { inUse: m[1] === 'a', inTest: m[1] === 'c', });
+    return { inUse: m[1] === 'a', inTest: m[1] === 'c', };
 }
 
 export function isPmatFileToLoad(fileCnt: FileContent): boolean {
