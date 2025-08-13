55efa04ded77ace5bcf4498505496e45f02ca06a
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-12 19:19:14 -0700

    WIP. in-test and in-use. implementation. 1. add getCacheFolder() and listFiles()

--- src/shell/xternal-to-renderer/2-commands-in-main/3-test-inuse/0-all.ts
+++ src/shell/xternal-to-renderer/2-commands-in-main/3-test-inuse/0-all.ts
@@ -1,13 +1,40 @@
 import { type TestInUseFile } from "@shared/ipc-types/9-test-inuse";
+import { getCacheFolder, listFiles } from "./8-os-utils";
 
-export function testInUseStart(files: TestInUseFile[]): Promise<string> {
+export async function testInUseStart(files: TestInUseFile[]): Promise<string> {
+
+    for (const file of files) {
+        if (file.inTest) {
+            console.log(`\nTest in use: file "${file.fullfname}" is in test mode.`);
+        } else {
+            console.log(`\nTest in use: file "${file.fullfname}" is not in test mode.`);
+        }
+    }
+
     return Promise.resolve(files.map(file => file.fullfname).join('\n'));
 }
 
-export function testInUseUpdate(files: TestInUseFile[]): Promise<string> {
+export async function testInUseUpdate(files: TestInUseFile[]): Promise<string> {
+
+    for (const file of files) {
+        if (file.inTest) {
+            console.log(`\nTest in use: file "${file.fullfname}" is in test mode.`);
+        } else {
+            console.log(`\nTest in use: file "${file.fullfname}" is not in test mode.`);
+        }
+    }
+
     return Promise.resolve(files.map(file => file.fullfname).join('\n'));
 }
 
-export function testInUseQuit(): Promise<string> {
+export async function testInUseQuit(): Promise<string> {
+
+    // get folder 'c' location enum all files in it and delete them
+
+    const cacheFolder = getCacheFolder();
+
+    const files = await listFiles(cacheFolder);
+
+
     return Promise.resolve('');
 }
---
+++ src/shell/xternal-to-renderer/2-commands-in-main/3-test-inuse/8-os-utils.ts
@@ -0,0 +1,30 @@
+import { promises as fs } from "fs";
+
+export function getCacheFolder(): string {
+    const { LOCALAPPDATA } = process.env;
+    if (!LOCALAPPDATA) {
+        throw new Error('LOCALAPPDATA is not set');
+    }
+    return `${LOCALAPPDATA}/digitalpersona/ots/dp_wkg_admin/c`;
+}
+
+export async function listFiles(cacheFolder: string): Promise<string[]> {
+    const rv: string[] = [];
+
+    try {
+        const files = await fs.readdir(cacheFolder); // Returns an array of filenames
+
+        for (const file of files) {
+            const fullname = `${cacheFolder}/${file}`;
+            const stats = await fs.stat(fullname);
+            if (stats.isFile()) {
+                rv.push(fullname);
+            }
+        }
+
+    } catch (err) {
+        console.error(`Error reading directory: "${cacheFolder}"`, err);
+    }
+
+    return rv;
+}
