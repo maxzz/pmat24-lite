41d8ca635103b7b8ef5c3141bc305b9cd9c5c630
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-16 13:32:19 -0700

    WIP. serve-atoms. update layout. 7.

--- src/renderer/src/components/2-main/1-left/2-files-list/0-all/2-tree-action-atoms.tsx
+++ src/renderer/src/components/2-main/1-left/2-files-list/0-all/2-tree-action-atoms.tsx
@@ -4,7 +4,7 @@ import { optionsFilesProxyAtom } from "@/store/9-ui-state";
 import { doTriggerRightPanelSelectedAtom, rightPanelAtomAtom } from "@/store/5-right-panel";
 import { type TreeState, type DataItemWState, type ItemState, type DataItemNavigation, type DataItemCore, duplicateTree, walkItems, doTreeItemSelect } from "@/ui/shadcn/tree";
 import { type FileUsAtom } from "@/store/store-types";
-import { type TreeFileItem, treeFilesAtom } from "@/store/5-tree-files";
+import { type TreeFileItem, treeFilesAtom } from "@/store/5-2-tree-files";
 
 export const treeStateAtom = atom<TreeState>(() => {
     return proxy<TreeState>({
--- src/renderer/src/components/4-dialogs/1-dlg-filter-files/0-all/1-filter-dialog-trigger.tsx
+++ src/renderer/src/components/4-dialogs/1-dlg-filter-files/0-all/1-filter-dialog-trigger.tsx
@@ -3,7 +3,7 @@ import { classNames } from "@/utils";
 import * as D from "@/ui/shadcn/dialog";
 import { Button } from "@/ui/shadcn";
 import { IconFilter } from "@/ui/icons";
-import { isFilterActiveAtom } from "@/store/5-tree-files";
+import { isFilterActiveAtom } from "@/store/5-2-tree-files";
 import { filterDialogOpenAtom } from "@/store/4-dialogs-atoms";
 import { appShortcuts } from "../../0-global/2-global-shortcuts";
 import { DialogFilterBody } from "./2-body";
