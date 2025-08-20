fc0a504383d899e8396baa0fca1db013444fc2de
Max Zakharzhevskiy <maxzz@msn.com>
2025-08-19 18:12:55 -0700

    WIP. react-18. just format; remove zip

--- src/renderer/src/utils/util-hooks/use-resize-observer/utils/extractSize.ts
+++ src/renderer/src/utils/util-hooks/use-resize-observer/utils/extractSize.ts
@@ -24,28 +24,28 @@
 // This also means that we get to keep the current api, being able to return a simple { width, height } pair,
 // regardless of box option.
 export default function extractSize(
-  entry: ResizeObserverEntry,
-  boxProp: "borderBoxSize" | "contentBoxSize" | "devicePixelContentBoxSize",
-  sizeType: keyof ResizeObserverSize
+    entry: ResizeObserverEntry,
+    boxProp: "borderBoxSize" | "contentBoxSize" | "devicePixelContentBoxSize",
+    sizeType: keyof ResizeObserverSize
 ): number | undefined {
-  if (!entry[boxProp]) {
-    if (boxProp === "contentBoxSize") {
-      // The dimensions in `contentBoxSize` and `contentRect` are equivalent according to the spec.
-      // See the 6th step in the description for the RO algorithm:
-      // https://drafts.csswg.org/resize-observer/#create-and-populate-resizeobserverentry-h
-      // > Set this.contentRect to logical this.contentBoxSize given target and observedBox of "content-box".
-      // In real browser implementations of course these objects differ, but the width/height values should be equivalent.
-      return entry.contentRect[sizeType === "inlineSize" ? "width" : "height"];
+    if (!entry[boxProp]) {
+        if (boxProp === "contentBoxSize") {
+            // The dimensions in `contentBoxSize` and `contentRect` are equivalent according to the spec.
+            // See the 6th step in the description for the RO algorithm:
+            // https://drafts.csswg.org/resize-observer/#create-and-populate-resizeobserverentry-h
+            // > Set this.contentRect to logical this.contentBoxSize given target and observedBox of "content-box".
+            // In real browser implementations of course these objects differ, but the width/height values should be equivalent.
+            return entry.contentRect[sizeType === "inlineSize" ? "width" : "height"];
+        }
+
+        return undefined;
     }
 
-    return undefined;
-  }
-
-  // A couple bytes smaller than calling Array.isArray() and just as effective here.
-  return entry[boxProp][0]
-    ? entry[boxProp][0][sizeType]
-    : // TS complains about this, because the RO entry type follows the spec and does not reflect Firefox's current
-      // behaviour of returning objects instead of arrays for `borderBoxSize` and `contentBoxSize`.
-      // @ts-ignore
-      entry[boxProp][sizeType];
+    // A couple bytes smaller than calling Array.isArray() and just as effective here.
+    return entry[boxProp][0]
+        ? entry[boxProp][0][sizeType]
+        : // TS complains about this, because the RO entry type follows the spec and does not reflect Firefox's current
+        // behaviour of returning objects instead of arrays for `borderBoxSize` and `contentBoxSize`.
+        // @ts-ignore
+        entry[boxProp][sizeType];
 }
