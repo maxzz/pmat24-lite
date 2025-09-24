b75d88038f94358f7dcc204a3fdf5f545d956978
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-23 19:23:00 -0700

    WIP. update jotai base types

--- src/renderer/src/vite-typescript/jotai-types.d.ts
+++ src/renderer/src/vite-typescript/jotai-types.d.ts
@@ -6,3 +6,5 @@ type Setter = import("jotai").Setter;
 type GetSet = { get: Getter; set: Setter; };
 type GetOnly = { get: Getter; set?: Setter; };
 type SetOnly = { get?: Getter; set: Setter; };
+
+type SetStateAction<Value> = import("jotai").SetStateAction<Value>;
