c0fe85e3d23d63bb8d0a3a66e3e8776ade014f0d
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-02 20:03:22 -0700

    WIP. save. ourl-readonly. show murl only when needed

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
@@ -14,6 +14,7 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
     const formIdx = oFormProps.oAllAtoms.options.formIdx;
     const { p2Detect: { ourlAtom, rurlAtom }, howAtom } = oFormProps.oAllAtoms.options;
     const how = useAtomValue(howAtom);
+    const showRegex = how === Matching.How.regex;
 
     const showExample = useIsShowExample(oFormProps.oAllAtoms.options);
     const disabled = how === Matching.How.undef;
@@ -47,26 +48,28 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
                     onBlur={() => setIsLocked(true)}
                 />
 
-                <div className="mt-4">How to match URL:</div>
+                <div className="mt-2">How to match URL:</div>
                 <MatchHow oFormProps={oFormProps} />
 
-                <InputWithTitle2Rows
-                    label={how === Matching.How.regex ? "Regular expression" : "Original URL"}
-                    className={classNames(disabled && 'opacity-50 cursor-default')}
-                    stateAtom={rurlAtom}
-                    readOnly={disabled}
-                    asTextarea
-                    onClick={() => disabled && toast.info('This input is disabled because "How to match URL" is set "As original URL".')}
-                />
+                {showRegex && (<>
+                    <InputWithTitle2Rows
+                        label={how === Matching.How.regex ? "Regular expression" : "Original URL"}
+                        className={classNames(disabled && 'opacity-50 cursor-default')}
+                        stateAtom={rurlAtom}
+                        readOnly={disabled}
+                        asTextarea
+                        onClick={() => disabled && toast.info('This input is disabled because "How to match URL" is set "As original URL".')}
+                    />
+                </>)}
 
                 {showExample && (<>
                     <div className="mt-1">
-                        The regular expression and the original URL are an exact match, so the regular expression is useless. 
+                        The regular expression and the original URL are an exact match, so the regular expression is useless.
                         You can define the regular expression as any part of the original URL, but the website domain will be taken from the original URL.
                     </div>
 
                     <div className="mt-2">
-                        For example, if the original URL is <span className={exampleClasses}>https://login.example.com</span> and the regular expression is <span className={exampleClasses}>login</span>, 
+                        For example, if the original URL is <span className={exampleClasses}>https://login.example.com</span> and the regular expression is <span className={exampleClasses}>login</span>,
                         the domain in this case would be <span className={exampleClasses}>example.com</span>, and the login form would match <span className={exampleClasses}>login.example.com</span>, but not <span className={exampleClasses}>admin.example.com</span>.
                         This allows you to determine where the form will be used.
                     </div>
@@ -77,7 +80,7 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
     );
 }
 
-const textClasses = "pl-6 pr-0.5";
+const textClasses = "pl-6 pr-0.5 py-1";
 const exampleClasses = "text-blue-500";
 
 //TODO: Since we allow to modify original URL in Match URL field, we should should rename "As original URL" to "Match exact string" that includes the original URL domain and protocol, or dissable "Match URL" input in this case.
