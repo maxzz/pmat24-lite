ba5d77414b09b8de6092974febbd3fb8ffb3abff
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-03 16:03:49 -0700

    WIP. save. ourl-readonly. DetectionContent_Web() code reivew

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-in-form-detection-web.tsx
@@ -12,20 +12,20 @@ import { ShowExampleText, useIsShowExample } from "./5-9-use-is-show-example";
 import { MatchHow } from "./5-2-match-how";
 
 export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }) {
-    const formIdx = oFormProps.oAllAtoms.options.formIdx;
-    const { p2Detect: { ourlAtom, rurlAtom }, howAtom } = oFormProps.oAllAtoms.options;
-    const how = useAtomValue(howAtom);
-    const showRegex = how === Matching.How.regex;
-
-    const showExample = useIsShowExample(oFormProps.oAllAtoms.options);
-    const disabled = how === Matching.How.undef;
-
+    const { p2Detect: { ourlAtom, rurlAtom }, howAtom, formIdx } = oFormProps.oAllAtoms.options;
     const [isLocked, setIsLocked] = useState(true);
+    const showExample = useIsShowExample(oFormProps.oAllAtoms.options);
+    
+    const how = useAtomValue(howAtom);
+    const disabled = how === Matching.How.undef;
+    const showRegex = how === Matching.How.regex;
 
     return (
         <AccordionWithTrigger name='form-detection' formIdx={formIdx} triggerText="Screen detection">
             <div className={textClasses}>
                 <InputWithTitle2Rows
+                    asTextarea
+                    stateAtom={ourlAtom}
                     label={(
                         <div className="flex items-center gap-0.5">
                             Original URL of the website
@@ -40,9 +40,8 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
                     )}
                     labelClasses="font-normal"
                     className={classNames(isLocked ? 'opacity-75 cursor-default' : '')}
-                    stateAtom={ourlAtom}
-                    asTextarea
                     readOnly={isLocked}
+                    onClick={() => isLocked && toast.info("This input is locked by default. Only change it if you understand what you're doing.")}
                     onBlur={() => setIsLocked(true)}
                 />
 
@@ -60,13 +59,11 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
                             transition={{ duration: .4 }}
                         >
                             <InputWithTitle2Rows
-                                label={how === Matching.How.regex ? "Regular expression" : "Original URL"}
+                                asTextarea
+                                stateAtom={rurlAtom}
+                                label={showRegex ? "Regular expression" : "Original URL"}
                                 labelClasses="font-normal"
                                 className={classNames(disabled && 'opacity-50 cursor-default')}
-                                stateAtom={rurlAtom}
-                                readOnly={disabled}
-                                asTextarea
-                                onClick={() => disabled && toast.info('This input is disabled because "How to match URL" is set "As original URL".')}
                             />
 
                             {showExample && <ShowExampleText />}
@@ -80,6 +77,5 @@ export function DetectionContent_Web({ oFormProps }: { oFormProps: OFormProps; }
 }
 
 const textClasses = "pl-6 pr-0.5 py-1";
-const exampleClasses = "text-blue-500";
 
 //TODO: Since we allow to modify original URL in Match URL field, we should should rename "As original URL" to "Match exact string" that includes the original URL domain and protocol, or dissable "Match URL" input in this case.
