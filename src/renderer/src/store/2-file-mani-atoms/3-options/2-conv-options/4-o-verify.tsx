bbe727179869065da3abb15ecdaebf2c0e4504e5
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-04 14:05:16 -0700

    WIP. save. ourl-readonly. murl_regexAtom is out of form detection. 2

--- src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/4-o-verify.tsx
+++ src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/4-o-verify.tsx
@@ -1,5 +1,5 @@
 import { type RowInputStateAtoms, validateRowInputStateAtoms } from "@/ui";
-import { FormIdx } from "@/store/manifest";
+import { FormIdx, Matching } from "@/store/manifest";
 import { type VerifyError } from "../../9-types";
 import { type FormOptionsState } from "./9-types";
 
@@ -22,18 +22,20 @@ export function getOptionsVerifyErrors_OfForm(atoms: FormOptionsState.AllAtoms, 
 }
 
 function getFormAtomsToValidate(atoms: FormOptionsState.AllAtoms, formIdx: FormIdx, { get }: GetOnly): RowInputStateAtoms {
-    const { p2Detect, p5Icon, isWebAtom } = atoms;
+    const { p2Detect, p5Icon, isWebAtom, murl_howAtom, murl_regexAtom } = atoms;
+
+    const murl = get(murl_howAtom) === Matching.How.regex ? { murl_regexAtom } : undefined;
 
     const toValidate: RowInputStateAtoms =
         formIdx === FormIdx.login
-            ? { ...p2Detect, ...p5Icon, }
-            : { ...p2Detect, ...p5Icon, };
+            ? { ...p2Detect, ...murl, ...p5Icon, }
+            : { ...p2Detect, ...murl, ...p5Icon, };
 
     const isWeb = get(isWebAtom); // Exclude atom with validation that are not appropriate depending on the form
     if (isWeb) {
         delete toValidate.captionAtom;
     } else {
-        delete toValidate.rurlAtom;
+        // delete toValidate.rurlAtom; // now it's regex_url and not part of the p2Detect
     }
 
     return toValidate;
