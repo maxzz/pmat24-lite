b56d8e7825717cc0b079c8050354eae64f368498
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-09 13:12:42 -0700

    WIP. localstore-options. 2. review

--- src/renderer/src/store/9-ui-state/2-right-panel/2-mani-view/0-all.ts
+++ src/renderer/src/store/9-ui-state/2-right-panel/2-mani-view/0-all.ts
@@ -1,19 +1,42 @@
-import { FormOpenSections, defaultFormOpenSections } from "./2-mani-open-sections";
-import { FormOpenOptions, defaultFormOpenOptions } from "./3-mani-open-options";
+import { FormIdx } from "@/store/manifest";
 import { type ManiTabValue } from "@/store/2-file-mani-atoms/9-types";
 
 //
 
-export type ManiEditorState = {
+type ManiEditorState = {
     activeTab: ManiTabValue;                // 'options' | 'login' | 'cpass'
     openTestArea: boolean;                  // Is the test area open
     nToGenerate: number;                    // Number of passwords to generate
 };
 
-export const defaultManiEditorState: ManiEditorState = {
+const defaultManiEditorState: ManiEditorState = {
     activeTab: 'options',
     openTestArea: false,
     nToGenerate: 50,
+};
+
+//
+
+type FormOpenSections = {
+    [FormIdx.login]: string[],
+    [FormIdx.cpass]: string[],
+};
+
+const defaultFormOpenSections: FormOpenSections = {
+    [FormIdx.login]: [],
+    [FormIdx.cpass]: [],
+};
+
+//
+
+type FormOpenOptions = {
+    [FormIdx.login]: Record<string, boolean>,
+    [FormIdx.cpass]: Record<string, boolean>,
+};
+
+const defaultFormOpenOptions: FormOpenOptions = {
+    [FormIdx.login]: {},
+    [FormIdx.cpass]: {},
 };
 
 // all settings for the main view
--- src/renderer/src/store/9-ui-state/index.ts
+++ src/renderer/src/store/9-ui-state/index.ts
@@ -2,7 +2,6 @@ export * from "./1-files-list/1-shown-files";
 export * from "./1-files-list/2-files-sort-order";
 export * from "./1-files-list/3-ui-file-items";
 export * from "./2-right-panel/1-right-panel-view";
-export * from "./2-right-panel/2-mani-view/2-mani-open-sections";
 export * from "./01-busy-indicator";
 export * from "./02-total-files";
 export * from "./04-filters-search";
