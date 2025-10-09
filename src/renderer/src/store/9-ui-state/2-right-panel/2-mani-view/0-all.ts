9aa1b9c11dd18f3bcb1575e3b637009e4dc0f351
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-09 13:09:37 -0700

    WIP. localstore-options. 1. review

--- src/renderer/src/store/9-ui-state/2-right-panel/2-mani-view/0-all.ts
+++ src/renderer/src/store/9-ui-state/2-right-panel/2-mani-view/0-all.ts
@@ -1,9 +1,27 @@
 import { FormOpenSections, defaultFormOpenSections } from "./2-mani-open-sections";
 import { FormOpenOptions, defaultFormOpenOptions } from "./3-mani-open-options";
-import { ManiEditorState, defaultManiEditorState } from "./4-editor-tab";
+import { type ManiTabValue } from "@/store/2-file-mani-atoms/9-types";
+
+//
+
+export type ManiEditorState = {
+    activeTab: ManiTabValue;                // 'options' | 'login' | 'cpass'
+    openTestArea: boolean;                  // Is the test area open
+    nToGenerate: number;                    // Number of passwords to generate
+};
+
+export const defaultManiEditorState: ManiEditorState = {
+    activeTab: 'options',
+    openTestArea: false,
+    nToGenerate: 50,
+};
+
+// all settings for the main view
 
 export type MainViewSettings = {
-    mani: ManiEditorState & {
+    mani:
+    ManiEditorState
+    & {
         openGeneral: boolean;
         openInOptions: FormOpenOptions;
         openInForms: FormOpenSections;
