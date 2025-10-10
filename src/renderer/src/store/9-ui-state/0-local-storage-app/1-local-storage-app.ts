6ba3e604a26758352177c744fd8867fb8462d58d
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-09 18:32:30 -0700

    WIP. localstore-options. 7. reduce options nesting. 2

--- src/renderer/src/store/9-ui-state/0-local-storage-app/1-local-storage-app.ts
+++ src/renderer/src/store/9-ui-state/0-local-storage-app/1-local-storage-app.ts
@@ -7,7 +7,7 @@ import { type FileListSettings, defaultFileListSettings } from "../1-files-list"
 import { type RightPanelSettings, defaultRightPanelSettings } from "../2-right-panel";
 import { type AppUISettings, defaultAppUISettings } from "../8-app-ui";
 
-export { opensName as toggleName } from "../2-right-panel/2-mani-view";
+export { opensName } from "../2-right-panel/2-mani-view";
 
 const STORE_KEY = "pmat25-ui";
 const STORE_VER = 'v3'; // fix for network share MRU list
