e02ad94d17740291c1ff1583fe97d68d2bef5aac
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-11 19:06:33 -0700

    WIP. fields-verification. 3. add getTotalCountErrorMessage(). 2

--- src/renderer/src/store/0-serve-atoms/3-do-save-mani-atom/1-stop-if-validation-failed/2-form-verify-errors.ts
+++ src/renderer/src/store/0-serve-atoms/3-do-save-mani-atom/1-stop-if-validation-failed/2-form-verify-errors.ts
@@ -23,42 +23,9 @@ export function getVerifyErrors_NormalForm(maniAtoms: ManiAtoms, formIdx: FormId
         return;
     }
 
-    const { useItAny, useItPsw, linkedCur, linkedNew } = totalFieldsInUse(formCtx.normal.rowCtxs, getset);
-
-    const isLogin = formIdx === FormIdx.login;
-    let error: string | undefined;
-
-    // 1. Checks for login and cpass forms
-
-    let tab: ManiTabValue = isLogin ? 'login' : 'cpass';
-
-    if (!useItAny) {
-        error = 'There are no fields selected';
-    }
-
-    if (!useItPsw) {
-        error = 'There are no password fields selected';
-    }
-
-    // 2. Checks for cpass form only
-
-    if (!error && !isLogin) {
-        tab = 'cpass';
-
-        if (linkedCur > 1) {
-            error = 'Only one field can be linked to the password entry in the login form.';
-        }
-        else if (!linkedCur) {
-            error = 'The password change form does not contain a link to the password entry in the login form. To create a link, you must select a field, link it to the password field on the login form, and specify it as the current confirm password.';
-        }
-        else if (!linkedNew) {
-            error = 'The password change form does not contain links to the login form indicating where to save the new password. To create a link, you must select a field, link it to the password field on the login form, and specify it as the new password or confirm password.';
-        }
-    }
-
-    if (error) {
-        return [{ error, tab, }];
-    }
+    const totalCount = totalFieldsInUse(formCtx.normal.rowCtxs, getset);
+    const rv = getTotalCountErrorMessage(totalCount, formIdx);
+    return rv;
 }
 
 function getTotalCountErrorMessage(totalCount: TotalCount, formIdx: FormIdx): VerifyError[] | undefined {
