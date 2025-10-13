6fe0933614d67c0bc8e8b8c180de36b78f2cb278
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-12 19:16:48 -0700

    WIP. fileds-layout-responsiveness. 5. fix delay details

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/3-details-dly.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/1-panel-actions/3-row-details/3-details-dly.tsx
@@ -1,11 +1,12 @@
 import { useAtomValue } from "jotai";
+import { classNames } from "@/utils";
 import type { ManualFieldState } from "@/store/2-file-mani-atoms";
 import { hideBreakpointClasses } from "./8-classes";
 
 export function DetailsDly({ item }: { item: ManualFieldState.CtxDly; }) {
     const n = useAtomValue(item.nAtom).data;
     return (
-        <div className={hideBreakpointClasses}>
+        <div className={classNames(hideBreakpointClasses, "!justify-end")}>
             {n} ms
         </div>
     );
