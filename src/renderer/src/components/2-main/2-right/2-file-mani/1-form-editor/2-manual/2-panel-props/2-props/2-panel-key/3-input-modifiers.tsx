cebb434a9d65ab7c97feda3b6d07c1d439cd8d29
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-13 13:47:52 -0700

    WIP. props-layouts. InputModifiers. 3. remove all junk. Now only needed minimum

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/2-panel-key/3-input-modifiers.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/2-manual/2-panel-props/2-props/2-panel-key/3-input-modifiers.tsx
@@ -7,14 +7,10 @@ import { type ManualFieldState } from "@/store/2-file-mani-atoms";
 
 export function InputModifiers({ item }: { item: ManualFieldState.CtxKbd; }) {
     return (
-        <div className="@container/modifier max-w-[220px]">
-            <div className="1my-1.5 flex flex-col justify-between @[190px]/modifier:flex-row gap-1">
-
-                <Modifier label="Shift" valueAtom={item.shiftAtom} />
-                <Modifier label="Control" valueAtom={item.ctrlAtom} />
-                <Modifier label="Alt" valueAtom={item.altAtom} />
-
-            </div>
+        <div className="inline-flex max-w-min justify-items-start gap-2">
+            <Modifier label="Shift" valueAtom={item.shiftAtom} />
+            <Modifier label="Control" valueAtom={item.ctrlAtom} />
+            <Modifier label="Alt" valueAtom={item.altAtom} />
         </div>
     );
 }
@@ -22,18 +18,16 @@ export function InputModifiers({ item }: { item: ManualFieldState.CtxKbd; }) {
 function Modifier({ label, valueAtom }: { label: string; valueAtom: RowInputStateAtom; }) {
     const [modifier, setModifier] = useAtom(valueAtom);
     return (
-        <div className={classNames("flex-1 max-w-36 flex items-center @[190px]/modifier:flex-col @[190px]/modifier:gap-y-px gap-x-2")}>
-            <div className="self-start @[190px]/modifier:pl-2 min-w-12 text-xs @[190px]/modifier:text-[0.65rem] font-light">
+        <div className={classNames("flex-1 max-w-36 flex flex-col items-center gap-y-px gap-x-2")}>
+            <div className="self-start text-xs">
                 {label}
             </div>
 
-            <div className="self-start">
-                <InputSelectUi
-                    items={modifierKeys}
-                    value={modifier.data}
-                    onValueChange={(value) => setModifier((prev) => ({ ...prev, data: value }))}
-                />
-            </div>
+            <InputSelectUi
+                items={modifierKeys}
+                value={modifier.data}
+                onValueChange={(value) => setModifier((prev) => ({ ...prev, data: value }))}
+            />
         </div>
     );
 }
