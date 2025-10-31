e42f094cae1a25bd7949eacc498c1bd4170f8e28
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-31 15:48:24 -0700

    WIP. localization. output. 1

--- scripts/i18n-ast/3-output-reports.ts
+++ scripts/i18n-ast/3-output-reports.ts
@@ -14,18 +14,33 @@ import { type ResultOfScan } from "./9-types";
 export function createReport(scanResult: ResultOfScan, config: Partial<Config>): void {
     // 1. Write report file
     const outputPath = path.resolve(config?.outputFile || defaultConfig.outputFile);
-    writeReportFile(scanResult.strings, outputPath);
 
-    // 2. Display summary with all statistics
-    const totalStrings = Object.values(scanResult.strings).reduce((sum, obj) => sum + Object.keys(obj).length, 0);
+    if (config.mode === 'scan') {
+        // 2. Display summary with all statistics
+        const totalStrings = Object.values(scanResult.strings).reduce((sum, obj) => sum + Object.keys(obj).length, 0);
 
-    console.log(`✅ Extracted ${pc.cyan(pc.bold(totalStrings))} strings from ${pc.cyan(pc.bold(scanResult.totalOfFilesWithStrings))} files`);
-    
-    if (config.verbose) {
-        console.log(pc.gray(`   Total files scanned ${pc.cyan(scanResult.totalOfAllFiles)}: with extractable strings ${pc.cyan(scanResult.totalOfFilesWithStrings)} and without ${pc.cyan(scanResult.totalOfAllFiles - scanResult.totalOfFilesWithStrings)}`));
+        console.log(`✅ Extracted ${pc.cyan(pc.bold(totalStrings))} strings from ${pc.cyan(pc.bold(scanResult.totalOfFilesWithStrings))} files`);
+        
+        if (config.verbose) {
+            console.log(pc.gray(`   Total files scanned ${pc.cyan(scanResult.totalOfAllFiles)}: with extractable strings ${pc.cyan(scanResult.totalOfFilesWithStrings)} and without ${pc.cyan(scanResult.totalOfAllFiles - scanResult.totalOfFilesWithStrings)}`));
+        }
+
+        writeReportFile(scanResult.strings, outputPath);
+        console.log(pc.gray(`   Saved to: ${outputPath}`));
     }
+    else if (config.mode === 'translated') {
+        // 2. Display summary with all statistics
+        const totalStrings = Object.values(scanResult.translatedStrings).reduce((sum, obj) => sum + Object.keys(obj).length, 0);
 
-    console.log(pc.gray(`   Saved to: ${outputPath}`));
+        console.log(`✅ Collected ${pc.cyan(pc.bold(totalStrings))} strings from ${pc.cyan(pc.bold(scanResult.totalOfFilesWithStrings))} files`);
+        
+        if (config.verbose) {
+            console.log(pc.gray(`   Total files scanned ${pc.cyan(scanResult.totalOfAllFiles)}: with extractable strings ${pc.cyan(scanResult.totalOfFilesWithStrings)} and without ${pc.cyan(scanResult.totalOfAllFiles - scanResult.totalOfFilesWithStrings)}`));
+        }
+
+        writeReportFile(scanResult.translatedStrings, outputPath);
+        console.log(pc.gray(`   Saved to: ${outputPath}`));
+    }
 }
 
 function writeReportFile(results: ResultOfScan['strings'], outputPath: string): void {
