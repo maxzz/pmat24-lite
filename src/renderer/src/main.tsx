4a1dc774f91dea74488adaddd1b3e1b1f154caa9
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-04 18:03:12 -0800

    WIP. web-simplified-i18n. 5. fix, but limit is reached.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,8 +4,28 @@ import ReactDOM from "react-dom/client";
 import { App } from "./components/0-all-app";
 import "./assets/css/index.css";
 
+import esES from "./y-i18n/translations/es-ES";
+import enUS from "./y-i18n/translations/en-US";
+import en from "./y-i18n/translations/en";
+import { TranslationProvider } from "./y-i18n/react";
+import type translations from "./y-i18n/translations/en";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        {/* <App /> */}
+
+        <TranslationProvider
+            fallbackLocale={["en"]}
+            translations={{ en, "es-es": esES, "en-us": enUS, }}
+        >
+            <App />
+        </TranslationProvider>
+
     </React.StrictMode>
 );
+
+declare module "./y-i18n/lib/my-translations" {
+    interface Register {
+        translations: typeof translations;
+    }
+}
