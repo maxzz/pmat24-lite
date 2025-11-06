a4bd194a1d32e41a2073e9d079f92238ace8c899
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-05 19:09:37 -0800

    WIP. bundle-size. add react radix-react-icons

---
+++ src/renderer/src/ui/icons/normal/radix-icons/08-chevron-left.tsx
@@ -0,0 +1,11 @@
+import { type HTMLAttributes } from "react";
+import { classNames } from "@/utils";
+
+export function IconRadixChevronLeft({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-current", className)} viewBox="0 0 15 15" {...rest}>
+            {title && <title>{title}</title>}
+            <path d="M8.21192 3.09155C8.40164 2.95736 8.66555 2.96958 8.8418 3.13452C9.01806 3.29976 9.04853 3.56338 8.92676 3.76148L8.86524 3.84155L5.43555 7.49976L8.86524 11.158L8.92676 11.238C9.04853 11.4361 9.01806 11.6998 8.8418 11.865C8.66555 12.0299 8.40164 12.0422 8.21192 11.908L8.13477 11.8416L4.38477 7.84155C4.20487 7.64932 4.20487 7.35019 4.38477 7.15796L8.13477 3.15796L8.21192 3.09155Z" />
+        </svg>
+    );
+}
---
+++ src/renderer/src/ui/icons/normal/radix-icons/24-drag-handle-dots-1.tsx
@@ -0,0 +1,34 @@
+import { type HTMLAttributes } from "react";
+import { classNames } from "@/utils";
+
+export function IconRadixDragHandleDots1({ className, title, ...rest }: HTMLAttributes<SVGSVGElement>) {
+    return (
+        <svg className={classNames("fill-current", className)} viewBox="0 0 15 15" {...rest}>
+            {title && <title>{title}</title>}
+            <circle cx="4.5" cy="2.5" r=".6" />
+            <circle cx="4.5" cy="4.5" r=".6" />
+            <circle cx="4.5" cy="6.499" r=".6" />
+            <circle cx="4.5" cy="8.499" r=".6" />
+            <circle cx="4.5" cy="10.498" r=".6" />
+            <circle cx="4.5" cy="12.498" r=".6" />
+            <circle cx="6.5" cy="2.5" r=".6" />
+            <circle cx="6.5" cy="4.5" r=".6" />
+            <circle cx="6.5" cy="6.499" r=".6" />
+            <circle cx="6.5" cy="8.499" r=".6" />
+            <circle cx="6.5" cy="10.498" r=".6" />
+            <circle cx="6.5" cy="12.498" r=".6" />
+            <circle cx="8.499" cy="2.5" r=".6" />
+            <circle cx="8.499" cy="4.5" r=".6" />
+            <circle cx="8.499" cy="6.499" r=".6" />
+            <circle cx="8.499" cy="8.499" r=".6" />
+            <circle cx="8.499" cy="10.498" r=".6" />
+            <circle cx="8.499" cy="12.498" r=".6" />
+            <circle cx="10.499" cy="2.5" r=".6" />
+            <circle cx="10.499" cy="4.5" r=".6" />
+            <circle cx="10.499" cy="6.499" r=".6" />
+            <circle cx="10.499" cy="8.499" r=".6" />
+            <circle cx="10.499" cy="10.498" r=".6" />
+            <circle cx="10.499" cy="12.498" r=".6" />
+        </svg>
+    );
+}
