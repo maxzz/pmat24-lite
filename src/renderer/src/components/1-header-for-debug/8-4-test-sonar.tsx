ca667ca8325567fcb0e565efc4168f0c123b24b7
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-18 14:38:08 -0800

    WIP. toast. add test env. 5. customization

--- src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
+++ src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
@@ -7,27 +7,7 @@ type ToastTypes = NonNullable<ToastT['type']>;
 export function TestSonner() {
 
     function onClick() {
-        toast.custom(
-            () => (
-                <div className="text-xs text-background bg-foreground border-foreground/10 border rounded-md shadow-md overflow-hidden">
-
-                    <div className="grid grid-cols-[4rem_1fr] gap-2">
-                        <div className="shrink-0 flex items-center justify-center bg-orange-500 text-slate-800">
-                            <SymbolWarning className="size-5 " />
-                        </div>
-
-                        <div className="p-4">
-                            <p>Hello from a custom component!</p>
-                            <button onClick={() => toast.dismiss()}>Dismiss</button>
-                        </div>
-                    </div>
-
-                </div>
-            ),
-            {
-                duration: 33000,
-            }
-        );
+        myToast();
     }
 
     return (<>
@@ -39,4 +19,29 @@ export function TestSonner() {
             Sonnar2
         </Button>
     </>);
+}
+
+function myToast() {
+    const id = toast.custom(
+        () => (
+            <div className="text-xs text-background bg-foreground border-foreground/10 border rounded-md shadow-md overflow-hidden">
+
+                <div className="grid grid-cols-[4rem_1fr] gap-2">
+                    <div className="shrink-0 flex items-center justify-center bg-orange-500 text-slate-800">
+                        <SymbolWarning className="size-5 " />
+                    </div>
+
+                    <div className="px-2 py-3 flex items-center gap-2">
+                        <p>Hello from a custom component!</p>
+
+                        <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
+                    </div>
+                </div>
+
+            </div>
+        ),
+        {
+            duration: 3000,
+        }
+    );
 }
