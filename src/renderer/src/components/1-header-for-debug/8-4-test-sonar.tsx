62ddcf4b1ae4acfe07e7006d776bfc993002a427
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-18 19:49:16 -0800

    WIP. toast. customization. 5

--- src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
+++ src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
@@ -10,7 +10,7 @@ export function TestSonner() {
             success
         </Button>
 
-        <Button className="text-[.65rem]" onClick={() => myToast("info", "Hello from a custom component!")}>
+        <Button className="text-[.65rem]" onClick={() => myToast("info", "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")}>
             info
         </Button>
 
@@ -28,14 +28,14 @@ function myToast(type: ToastTypes, message: React.ReactNode) {
     const iconClasses =
         type === 'success' ? 'bg-green-600 text-slate-800'
             : type === 'info' ? 'bg-blue-600 text-slate-100'
-                : type === 'warning' ? 'bg-orange-600 text-slate-800'
+                : type === 'warning' ? 'bg-orange-600 text-slate-100'
                     : type === 'error' ? 'bg-red-500 text-slate-800'
                         : '';
     const id = toast.custom(
         () => (
             <div className="text-xs text-background bg-foreground border-foreground/10 border rounded-md shadow-md overflow-hidden">
 
-                <div className="grid grid-cols-[4rem_1fr] gap-2">
+                <div className="grid grid-cols-[4rem_1fr_auto] 1gap-2">
                     <div className={`shrink-0 flex items-center justify-center ${iconClasses}`}>
                         {type === 'success' && <SymbolInfo className="size-5 " />}
                         {type === 'info' && <SymbolInfo className="size-5 " />}
@@ -44,9 +44,15 @@ function myToast(type: ToastTypes, message: React.ReactNode) {
                     </div>
 
                     <div className="px-2 py-3 flex items-center gap-2">
-                        {message}
+                        <div className="hyphens-auto text-pretty">
+                            {message}
+                        </div>
 
-                        <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
+
+                    </div>
+
+                    <div className="place-self-center px-2">
+                        <button className="srink-0 px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
                     </div>
                 </div>
 
