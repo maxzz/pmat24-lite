2809a2c7682b4b81a4c3a348aef59dd294003784
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-19 14:30:03 -0800

    WIP. toast. customization. 7. good enough for now

--- src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
+++ src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/ui";
-import { IconStopCircle, SymbolInfo, SymbolWarning } from "@/ui/icons";
+import { IconStopCircle, SymbolCross, SymbolInfo, SymbolWarning } from "@/ui/icons";
 import { toast, type ToastT } from "sonner";
 
 type ToastTypes = Exclude<NonNullable<ToastT['type']>, 'normal' | 'action' | 'default' | 'loading'>; // 'success' | 'info' | 'warning' | 'error';
@@ -33,29 +33,33 @@ export function TestSonner() {
 
 function myToast(type: ToastTypes, message: React.ReactNode) {
     const iconClasses =
-        type === 'success' ? 'bg-green-600 text-green-900'
-            : type === 'info' ? 'bg-blue-600 text-slate-100'
-                : type === 'warning' ? 'bg-orange-600 text-slate-100'
-                    : type === 'error' ? 'bg-red-500 text-slate-800'
+        type === 'success' ? 'bg-green-600 stroke-background'
+            : type === 'info' ? 'bg-blue-600 stroke-background'
+                : type === 'warning' ? 'bg-orange-600 stroke-background'
+                    : type === 'error' ? 'bg-red-500 stroke-background'
                         : '';
     const id = toast.custom(
         () => (
-            <div className="text-xs text-background bg-foreground border-foreground/10 border rounded-md shadow-md overflow-hidden">
+            <div className="text-xs text-background bg-foreground border-foreground/10 dark:border-background border rounded-md shadow-md overflow-hidden">
 
                 <div className="min-h-12 grid grid-cols-[4rem_1fr_auto]">
                     <div className={`shrink-0 relative flex flex-col items-center justify-center ${iconClasses}`}>
-                        {type === 'success' && <SymbolInfo className="size-5 " />}
-                        {type === 'info' && <SymbolInfo className="size-5 " />}
-                        {type === 'warning' && <SymbolWarning className="size-5 " />}
-                        {type === 'error' && <IconStopCircle className="size-5 " />}
-                        <button className="absolute bottom-2 px-2 py-1 bg-foreground text-background border-background/50 border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
+                        {type === 'success' && <SymbolInfo className="size-5" />}
+                        {type === 'info' && <SymbolInfo className="size-5" />}
+                        {type === 'warning' && <SymbolWarning className="size-5" />}
+                        {type === 'error' && <IconStopCircle className="size-5 stroke-background!" />}
+                        {/* <button className="absolute bottom-2 px-2 py-1 bg-foreground text-background border-background/50 border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button> */}
                     </div>
 
-                    <div className="px-2 py-3 hyphens-auto text-pretty" data-content>
+                    <div className="place-self-center pl-2 py-3 hyphens-auto text-pretty" data-content>
                         {message}
                     </div>
 
-                    <div className="place-self-center px-2">
+                    <div className="place-self-center">
+                        <Button className="m-1 1ml-0 p-1 aspect-square active:scale-[.97]" variant="ghost" onClick={() => toast.dismiss(id)}>
+                            <SymbolCross className="size-4 p-1" onClick={() => toast.dismiss(id)} />
+                        </Button>
+
                         {/* <button className="srink-0 px-2 py-1 bg-foreground text-background border-background/50 border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button> */}
                         {/* <button className="srink-0 px-2 py-1 text-foreground bg-background border-foreground 1border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button> */}
                     </div>
