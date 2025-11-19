aecf0a6a663231475853d37e0a61cc61de938b3d
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-18 19:21:17 -0800

    WIP. toast. customization. 2. define all four toasts. not good

--- src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
+++ src/renderer/src/components/1-header-for-debug/8-4-test-sonar.tsx
@@ -5,40 +5,29 @@ import { toast, type ToastT } from "sonner";
 type ToastTypes = Exclude<NonNullable<ToastT['type']>, 'normal' | 'action' | 'default' | 'loading'>; // 'success' | 'info' | 'warning' | 'error';
 
 export function TestSonner() {
-
-    function onClick() {
-        myToast();
-    }
-
     return (<>
-        <Button className="text-[.65rem]" onClick={onClick}>
-            Sonnar1
+        <Button className="text-[.65rem]" onClick={() => myToast("success", "Hello from a custom component!")}>
+            success
         </Button>
 
-        <Button className="text-[.65rem]" onClick={onClick}>
-            Sonnar2
+        <Button className="text-[.65rem]" onClick={() => myToast("info", "Hello from a custom component!")}>
+            info
+        </Button>
+
+        <Button className="text-[.65rem]" onClick={() => myToast("warning", "Hello from a custom component!")}>
+            warning
+        </Button>
+
+        <Button className="text-[.65rem]" onClick={() => myToast("error", "Hello from a custom component!")}>
+            error
         </Button>
     </>);
 }
 
-function myToast(type: ToastTypes) {
+function myToast(type: ToastTypes, message: React.ReactNode) {
     const id = toast.custom(
         () => (
-            <div className="text-xs text-background bg-foreground border-foreground/10 border rounded-md shadow-md overflow-hidden">
-
-                <div className="grid grid-cols-[4rem_1fr] gap-2">
-                    <div className="shrink-0 flex items-center justify-center bg-orange-500 text-slate-800">
-                        <SymbolWarning className="size-5 " />
-                    </div>
-
-                    <div className="px-2 py-3 flex items-center gap-2">
-                        <p>Hello from a custom component!</p>
-
-                        <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
-                    </div>
-                </div>
-
-            </div>
+            type === 'success' ? SuccessToast(message, id) : type === 'info' ? InfoToast(message, id) : type === 'warning' ? WarningToast(message, id) : ErrorToast(message, id)
         ),
         {
             duration: 3000,
@@ -56,7 +45,7 @@ function ErrorToast(message: React.ReactNode, id: string | number) {
                 </div>
 
                 <div className="px-2 py-3 flex items-center gap-2">
-                    <p>Hello from a custom component!</p>
+                    {message}
 
                     <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
                 </div>
@@ -76,7 +65,7 @@ function WarningToast(message: React.ReactNode, id: string | number) {
                 </div>
 
                 <div className="px-2 py-3 flex items-center gap-2">
-                    <p>Hello from a custom component!</p>
+                    {message}
 
                     <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
                 </div>
@@ -96,7 +85,7 @@ function SuccessToast(message: React.ReactNode, id: string | number) {
                 </div>
 
                 <div className="px-2 py-3 flex items-center gap-2">
-                    <p>Hello from a custom component!</p>
+                    {message}
 
                     <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
                 </div>
@@ -116,7 +105,7 @@ function InfoToast(message: React.ReactNode, id: string | number) {
                 </div>
 
                 <div className="px-2 py-3 flex items-center gap-2">
-                    <p>Hello from a custom component!</p>
+                    {message}
 
                     <button className="px-2 py-1 text-foreground bg-background border-foreground border rounded shadow" onClick={() => toast.dismiss(id)}>Dismiss</button>
                 </div>
