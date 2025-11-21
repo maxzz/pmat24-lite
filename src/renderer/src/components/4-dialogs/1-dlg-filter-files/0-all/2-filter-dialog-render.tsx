36f404311f849d878bea2e43b5f9f5593ae8fc2a
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-20 20:39:47 -0800

    WIP. filters. split filter dialog. 7. split trigger and renderer

---
+++ src/renderer/src/components/4-dialogs/1-dlg-filter-files/0-all/2-filter-dialog-render.tsx
@@ -0,0 +1,59 @@
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { AnimatePresence, motion } from "motion/react";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { classNames } from "@/utils";
+import * as D from "@/ui/shadcn/dialog";
+import { Button } from "@/ui/shadcn";
+import { IconFilter } from "@/ui/icons";
+import { isFilterActiveAtom } from "@/store/5-2-tree-files";
+import { filterDialogOpenAtom } from "@/store/4-dialogs-atoms";
+import { appShortcuts } from "../../0-global/2-global-shortcuts";
+import { DialogFilterBody } from "./3-body";
+
+const overlayClasses2 = "fixed inset-0 z-50 backdrop-blur-[0.5px] bg-background/80";
+const contentClasses = "fixed left-[50%] top-[50%] z-50 grid w-4/5! max-w-3xl gap-4 border bg-background px-3 py-3 shadow-lg sm:rounded-lg";
+
+//TODO: multiple prefixes; now only one effective
+//TODO: add (15 last) prefixes history dropdown
+
+//TODO: icons const instances
+//TODO: filename for win apps
+//TODO: tree custom leafs
+//TODO: options page
+
+export function FilterFilesBody() {
+    const [isOpen, setIsOpen] = useAtom(filterDialogOpenAtom);
+    return (
+        <D.Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <AnimatePresence>
+                {isOpen && (
+                    <D.DialogPortal forceMount>
+                        <DialogPrimitive.Overlay asChild>
+                            <motion.div
+                                className={overlayClasses2}
+                                initial={{ opacity: 0 }}
+                                animate={{ opacity: 1 }}
+                                exit={{ opacity: 0 }}
+                                transition={{ duration: 0.2 }}
+                            />
+                        </DialogPrimitive.Overlay>
+
+                        <DialogPrimitive.Content asChild>
+                            <motion.div
+                                className={contentClasses}
+                                initial={{ opacity: 0, scale: 0.95, x: "-50%", y: "-48%" }}
+                                animate={{ opacity: 1, scale: 1, x: "-50%", y: "-50%" }}
+                                exit={{ opacity: 0, scale: 0.95, x: "-50%", y: "-48%" }}
+                                transition={{ duration: 0.2 }}
+                            >
+                                <DialogPrimitive.Title className="sr-only">Files filter</DialogPrimitive.Title>
+                                
+                                <DialogFilterBody setIsOpen={setIsOpen} />
+                            </motion.div>
+                        </DialogPrimitive.Content>
+                    </D.DialogPortal>
+                )}
+            </AnimatePresence>
+        </D.Dialog>
+    );
+}
