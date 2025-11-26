607bcc0aeda05223ebb94fbb5f904c9ad5087e0e
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-26 14:50:39 -0800

    WIP. add test button to reload open folder

--- src/renderer/src/components/1-header-for-debug/7-4-close-folder.tsx
+++ src/renderer/src/components/1-header-for-debug/7-4-close-folder.tsx
@@ -2,6 +2,9 @@ import { useSetAtom } from "jotai";
 import { useSnapshot } from "valtio";
 import { Button } from "@/ui";
 import { doCloseRootDirAtom, rootDir } from "@/store/5-1-open-files";
+import { doSetFilesFrom_MruFolder_Atom } from "@/store/0-serve-atoms/2-do-load-files";
+import { appSettings } from "@/store/9-ui-state";
+import { IconRefresh } from "@/ui/icons/normal";
 
 export function TestCloseFolder() {
     const doCloseRootDir = useSetAtom(doCloseRootDirAtom);
@@ -9,6 +12,18 @@ export function TestCloseFolder() {
     return (
         <Button variant="outline" className="text-[.65rem]" onClick={doCloseRootDir} disabled={!hasRootDir}>
             Close Folder
+        </Button>
+    );
+}
+
+export function TestReloadLastFolder() {
+    const doSetFilesFrom_MruFolder = useSetAtom(doSetFilesFrom_MruFolder_Atom);
+    const { folders } = useSnapshot(appSettings.appUi.mru);
+    const lastOpened = folders[0];
+    const isDisabled = !lastOpened;
+    return (
+        <Button variant="outline" className="text-[.65rem]" title="Reload open folder" onClick={() => doSetFilesFrom_MruFolder({ folder: lastOpened })} disabled={isDisabled}>
+            <IconRefresh className="size-3" />
         </Button>
     );
 }
