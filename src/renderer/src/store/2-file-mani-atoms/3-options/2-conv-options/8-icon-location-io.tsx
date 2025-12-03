bcde96265841234797e0c41cac61611f8a071059
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-02 18:42:37 -0800

    WIP. icon-location. 5. optimization

--- src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/8-icon-location-io.tsx
+++ src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/8-icon-location-io.tsx
@@ -8,14 +8,14 @@ export function iconLocationToStr({ quadrant, x, y }: IconLocation): string {
     return quadrant < 1 || quadrant > 4 ? '' : `Q:${quadrant - 1}:${x}:${y}`;
 }
 
-export function iconLocationFromStr(iconLoc?: string): IconLocation | undefined {
+export function iconLocationFromStr(iconLoc: string | undefined): IconLocation | undefined {
     if (!iconLoc) {
-        return undefined;
+        return;
     }
 
     const parts = iconLoc.split(':');
     if (parts.length !== 4 || parts[0] !== 'Q') { // Check if input location format is valid. Expected format: Q:<quad>:x:y.
-        return undefined;
+        return;
     }
 
     // Note: Add 1 to the input location (quadrant) to match with our internal list (None,quad 0,quad 1,quad 2,quad 3) etc.
@@ -24,8 +24,17 @@ export function iconLocationFromStr(iconLoc?: string): IconLocation | undefined 
     const y = +parts[3];
 
     if (Number.isNaN(quadrant) || Number.isNaN(x) || Number.isNaN(y)) {
-        return undefined;
+        return;
     }
 
     return { quadrant, x, y };
+}
+
+export function setQuadrant(iconLocation: string | undefined, quadrant: number): string | undefined {
+    const s = iconLocationFromStr(iconLocation);
+    if (!s) {
+        return iconLocation;
+    }
+    s.quadrant = quadrant;
+    return iconLocationToStr(s);
 }
