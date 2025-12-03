d1901e3023a72b73756192bcbaef207e93ceae13
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-02 18:28:46 -0800

    WIP. icon-location. 3. code review

--- src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/8-icon-location-io.tsx
+++ src/renderer/src/store/2-file-mani-atoms/3-options/2-conv-options/8-icon-location-io.tsx
@@ -1,42 +1,31 @@
-export function setIconLocation(quadrant: number, x: number, y: number): string {
-    // Check if we have valid quadrant values (Quad 0 , quad 1, quad 2, quad 3 i.e. 1 to 4).
-    // Note: We ignore "None" i.e. 0.
-    if (quadrant < 1 || quadrant > 4) {
-        return "";
-    }
+export type IconLocation = {
+    quadrant: number; // 0 - default (or as none), 1 - top left, 2 - top right, 3 - bottom left, 4 - bottom right
+    x: number;
+    y: number;
+};
 
-    // Note: We need to subtract 1 to match with external quadrant values (Quad 1 to 4 equivalent 0 to 3).
-    return `Q:${quadrant - 1}:${x}:${y}`;
+export function iconLocationToString({ quadrant, x, y }: IconLocation): string {
+    return quadrant < 1 || quadrant > 4 ? '' : `Q:${quadrant - 1}:${x}:${y}`;
 }
 
-export function getIconLocation(iconLoc: string): { quadrant: number; x: number; y: number } | null {
-    // Check if input parameter is valid.
+export function iconLocationFromString(iconLoc?: string): IconLocation | undefined {
     if (!iconLoc) {
-        return null;
+        return undefined;
     }
 
-    // Extract icon location details
     const parts = iconLoc.split(':');
-
-    // Check if input location format is valid.
-    // Expected format: Q:<quad>:x:y.
-    if (parts.length !== 4) {
-        return null;
+    if (parts.length !== 4 || parts[0] !== 'Q') { // Check if input location format is valid. Expected format: Q:<quad>:x:y.
+        return undefined;
     }
 
-    // Update return values and return succeeded status.
     // Note: Add 1 to the input location (quadrant) to match with our internal list (None,quad 0,quad 1,quad 2,quad 3) etc.
     const quadrant = parseInt(parts[1], 10) + 1;
     const x = parseInt(parts[2], 10);
     const y = parseInt(parts[3], 10);
 
     if (isNaN(quadrant) || isNaN(x) || isNaN(y)) {
-        return null;
+        return undefined;
     }
 
-    return {
-        quadrant,
-        x,
-        y
-    };
+    return { quadrant, x, y };
 }
