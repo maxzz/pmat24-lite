45d9c2b5b88e4551934289952cd2024944344fb1
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-03 17:04:47 -0800

    WIP. policy-dlg. add explanations animation. 4. proper place

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/4-rule-explanation.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/4-rule-explanation.tsx
@@ -1,5 +1,6 @@
 import { type HTMLAttributes, Fragment } from "react";
 import { useAtomValue } from "jotai";
+import { AnimatePresence, motion } from "motion/react";
 import { classNames } from "@/utils";
 import { SymbolDot } from "@/ui/icons";
 import { type PolicyDlgTypes } from "../../0-all";
@@ -9,23 +10,32 @@ export function RuleExplanation({ dlgUiCtx }: { dlgUiCtx: PolicyDlgTypes.PolicyU
     const errorText = useAtomValue(dlgUiCtx.errorTextAtom);
     return (
         <div className="mt-2">
-            {explanation && (<>
-                <div className="pb-1">
-                    Explanation of test password complexity. Password should consist of
-                </div>
+            <AnimatePresence initial={false}>
+                {explanation && (
+                    <motion.div
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: "auto", opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        className="overflow-hidden"
+                    >
+                        <div className="pb-1">
+                            Explanation of test password complexity. Password should consist of
+                        </div>
 
-                <div className="pl-4 text-xs grid grid-cols-[auto_1fr] gap-x-2">
+                        <div className="pl-4 text-xs grid grid-cols-[auto_1fr] gap-x-2">
 
-                    {explanation.split('\n').filter(Boolean).map(
-                        (line, idx) => (
-                            <Fragment key={idx}>
-                                <SymbolDot className="size-4" />
-                                {line}
-                            </Fragment>
-                        ))
-                    }
-                </div>
-            </>)}
+                            {explanation.split('\n').filter(Boolean).map(
+                                (line, idx) => (
+                                    <Fragment key={idx}>
+                                        <SymbolDot className="size-4" />
+                                        {line}
+                                    </Fragment>
+                                ))
+                            }
+                        </div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
             <ErrorInfo className="font-semibold" errorText={errorText} />
         </div>
