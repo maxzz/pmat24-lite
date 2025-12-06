4b6ff845d62d9baf18dda071344b52c5525d21be
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-06 14:50:05 -0800

    WIP. policy-dlg. gemini3: fix scroll inside popup generated list dialog

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/7-generate-list-popup-dlg/1-all.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/7-generate-list-popup-dlg/1-all.tsx
@@ -1,5 +1,5 @@
 import { type PrimitiveAtom, useAtom, useAtomValue } from "jotai";
-import { Button, Popover, PopoverArrorWoBottom, PopoverContent, PopoverPortal, PopoverTrigger, ScrollArea } from "@/ui";
+import { Button, Popover, PopoverArrorWoBottom, PopoverContent, PopoverTrigger, ScrollArea } from "@/ui";
 import { IconCopy } from "@/ui/icons";
 import { getGenNPasswordsListAtom } from "../../../0-all";
 import { GeneratedListBody, copyToClipboard } from "./3-generated-list-body";
@@ -7,19 +7,17 @@ import { GeneratedListBody, copyToClipboard } from "./3-generated-list-body";
 export function ButtonGeneratedList({ openAtom }: { openAtom: PrimitiveAtom<boolean>; }) {
     const [open, setOpen] = useAtom(openAtom);
     return (
-        <Popover open={open} onOpenChange={setOpen}>
+        <Popover open={open} onOpenChange={setOpen} modal>
             <PopoverTrigger asChild>
                 <div />
             </PopoverTrigger>
 
-            <PopoverPortal>
-                <PopoverContent className="relative mx-4 p-0 w-auto min-w-52 text-foreground bg-background border-border border shadow-sm" sideOffset={5} align="center">
+            <PopoverContent className="relative mx-4 p-0 w-auto min-w-52 text-foreground bg-background border-border border shadow-sm" sideOffset={5} align="center">
 
-                    <ListBody open={open} />
-                    <PopoverArrorWoBottom className="fill-muted stroke-border" />
+                <ListBody open={open} />
+                <PopoverArrorWoBottom className="fill-muted stroke-border" />
 
-                </PopoverContent>
-            </PopoverPortal>
+            </PopoverContent>
         </Popover>
     );
 }
@@ -32,7 +30,7 @@ function ListBody({ open }: { open: boolean; }) {
                 {getGenNPasswordsList.length} generated passwords
             </div>
 
-            <ScrollArea className="my-2 h-64" fullHeight horizontal>
+            <ScrollArea className="my-2 h-64" horizontal>
                 {open && (
                     <GeneratedListBody generatedList={getGenNPasswordsList} />
                 )}
