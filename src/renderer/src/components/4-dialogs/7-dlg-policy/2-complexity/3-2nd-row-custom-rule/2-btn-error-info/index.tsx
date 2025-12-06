3fd38c79f7d904fc6e924ef88c0a10f1efd9cc88
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-06 14:50:28 -0800

    WIP. policy-dlg. gemini3: fix scroll inside popup error info dialog

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-2nd-row-custom-rule/2-btn-error-info/index.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-2nd-row-custom-rule/2-btn-error-info/index.tsx
@@ -1,5 +1,5 @@
 import { useAtomValue } from "jotai";
-import { Button, Popover, PopoverArrorWoBottom, PopoverContent, PopoverPortal, PopoverTrigger } from "@/ui";
+import { Button, Popover, PopoverArrorWoBottom, PopoverContent, PopoverTrigger } from "@/ui";
 import { type PolicyDlgTypes } from "../../../0-all";
 import { SymbolWarning } from "@/ui/icons";
 import { classNames } from "@/utils";
@@ -9,21 +9,19 @@ import { ErrorInfo } from "../../3-3rd-row-test-area/1-all/5-error-info";
 export function ButtonErrorInfo({ dlgUiCtx }: { dlgUiCtx: PolicyDlgTypes.PolicyUiCtx; }) {
     const errorText = useAtomValue(dlgUiCtx.errorTextAtom);
     return (
-        <Popover>
+        <Popover modal>
             {errorText && (
                 <ErrorInfoTrigger />
             )}
 
-            <PopoverPortal>
-                <PopoverContent className="relative mx-4 py-0 w-auto text-red-500 border-red-500 border shadow-sm" sideOffset={5} align="center">
+            <PopoverContent className="relative mx-4 py-0 w-auto text-red-500 border-red-500 border shadow-sm" sideOffset={5} align="center">
 
-                    <div className="my-3 text-xs">
-                        <ErrorInfo errorTextAtom={dlgUiCtx.errorTextAtom} />
-                    </div>
+                <div className="my-3 text-xs">
+                    <ErrorInfo errorTextAtom={dlgUiCtx.errorTextAtom} />
+                </div>
 
-                    <PopoverArrorWoBottom className="fill-background stroke-red-500" />
-                </PopoverContent>
-            </PopoverPortal>
+                <PopoverArrorWoBottom className="fill-background stroke-red-500" />
+            </PopoverContent>
 
         </Popover>
     );
