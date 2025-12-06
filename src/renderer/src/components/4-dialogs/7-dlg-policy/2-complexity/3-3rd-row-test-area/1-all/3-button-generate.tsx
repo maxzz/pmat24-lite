136a5cad982bf8adb4e54e909f7cbf2bbc33af8c
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-06 15:02:58 -0800

    WIP. policy-dlg. gemini3: add dropdown menu to generate list of passwords

--- src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/1-all/3-button-generate.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/2-complexity/3-3rd-row-test-area/1-all/3-button-generate.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { atom, useSetAtom } from "jotai";
 import { useSnapshot } from "valtio";
-import { Button } from "@/ui";
+import { Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/ui";
+import { IconRadix_ChevronDown } from "@/ui/icons";
 import { appSettings } from "@/store/9-ui-state";
 import { type PolicyDlgTypes, doGeneratePswAtom, doGenNPasswordsAtom } from "../../../0-all";
 import { ButtonGeneratedList } from "../7-generate-list-popup-dlg/1-all";
@@ -15,20 +16,34 @@ export function ButtonGenerate({ dlgUiCtx }: { dlgUiCtx: PolicyDlgTypes.PolicyUi
     const doGenNPasswords = useSetAtom(doGenNPasswordsAtom);
     const doGeneratePsw = useSetAtom(doGeneratePswAtom);
 
-    function onClick(e: React.MouseEvent<HTMLButtonElement>) {
-        if (e.ctrlKey) {
-            doGenNPasswords({ dlgUiCtx: dlgUiCtx });
-            doOpenPopupGenNPasswords(true);
-        } else {
-            doGeneratePsw({ dlgUiCtx: dlgUiCtx });
-        }
+    function onGenerateOne() {
+        doGeneratePsw({ dlgUiCtx: dlgUiCtx });
+    }
+
+    function onGenerateMany() {
+        doGenNPasswords({ dlgUiCtx: dlgUiCtx });
+        doOpenPopupGenNPasswords(true);
     }
 
     return (
-        <div>
-            <Button className={localButtonClasses} variant="outline" size="xs" title={`Generate test password. Ctrl+Click to generate ${nToGenerate} passwords.`} onClick={onClick}>
+        <div className="flex items-center">
+            <Button className={localButtonClasses + " rounded-r-none border-r-0"} variant="outline" size="xs" title="Generate test password." onClick={onGenerateOne}>
                 Generate
             </Button>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button className={localButtonClasses + " rounded-l-none px-0.5 w-5"} variant="outline" size="xs" title="More options">
+                        <IconRadix_ChevronDown className="size-3.5" />
+                    </Button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={onGenerateMany}>
+                        generate {nToGenerate} passwords
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
 
             <ButtonGeneratedList openAtom={openPopupGenNPasswordsAtom} />
         </div>
