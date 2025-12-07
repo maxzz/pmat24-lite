9d69d800737a9be3a1f179a8aff0f9e3c8cdceab
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-06 19:53:24 -0800

    WIP. launch. gemini3: add call to launch application

--- src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/4-menu-items-launch.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/4-menu-items-launch.tsx
@@ -1,7 +1,9 @@
 import { useSetAtom } from "jotai";
+import { notice } from "@/ui/local-ui/7-toaster";
 import { DropdownMenuItem } from "@/ui/shadcn";
 import { type ManiAtoms } from "@/store/2-file-mani-atoms";
 import { launchDataAtom, type LaunchData } from "@/store/0-serve-atoms/8-launch-data";
+import { execFile } from "@/store/0-serve-atoms/7-file-system-manipulation/4-exec-file";
 
 export function MenuItems_Launch({ maniAtoms }: { maniAtoms: ManiAtoms; }) {
     const getLaunchData = useSetAtom(launchDataAtom);
@@ -25,12 +27,21 @@ function MenuItem_LaunchForm({ launchDataForm, isLogin }: { launchDataForm: Laun
                 >
                     {isLogin ? 'Open Login URL' : 'Open password change URL'}
                 </DropdownMenuItem>
-            ) : (
+            )
+            : (
                 <DropdownMenuItem
                     className="pl-8"
                     disabled={!launchDataForm.exe}
                     onClick={() => {
                         console.log('launch exe', launchDataForm.exe);
+                        if (!launchDataForm.exe) {
+                            notice.error('No executable file to launch');
+                            return;
+                        }
+                        const rv = execFile(launchDataForm.exe);
+                        if (rv) {
+                            notice.error(rv);
+                        }
                     }}
                 >
                     {isLogin ? 'Launch Login app' : 'Launch password change app'}
