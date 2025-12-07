6d6dc313eb94289d59781d45cd17eedfae8aec0c
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-06 19:41:33 -0800

    WIP. launch. gemini3: update MenuItem_LaunchForm

--- src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/4-menu-items-launch.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/2-menu/1-menu-mani/4-menu-items-launch.tsx
@@ -1,39 +1,41 @@
 import { useSetAtom } from "jotai";
 import { DropdownMenuItem } from "@/ui/shadcn";
 import { type ManiAtoms } from "@/store/2-file-mani-atoms";
-import { launchDataAtom, type LaunchDataForm } from "@/store/0-serve-atoms/8-launch-data";
+import { launchDataAtom, type LaunchData } from "@/store/0-serve-atoms/8-launch-data";
 
 export function MenuItems_Launch({ maniAtoms }: { maniAtoms: ManiAtoms; }) {
     const getLaunchData = useSetAtom(launchDataAtom);
     const launchData = getLaunchData({ maniAtoms });
     return (<>
-        <MenuItem_LaunchForm launchDataForm={launchData.login} />
-        <MenuItem_LaunchForm launchDataForm={launchData.cpass} />
+        <MenuItem_LaunchForm launchDataForm={launchData.login} isLogin={true} />
+        <MenuItem_LaunchForm launchDataForm={launchData.cpass} isLogin={false} />
     </>);
 }
 
-function MenuItem_LaunchForm({ launchDataForm }: { launchDataForm: LaunchDataForm; }) {
+function MenuItem_LaunchForm({ launchDataForm, isLogin }: { launchDataForm: LaunchData; isLogin: boolean; }) {
     return (<>
-        {launchDataForm.isWeb ? (
-            <DropdownMenuItem
-                className="pl-8"
-                disabled={!launchDataForm.url}
-                onClick={(event) => {
-                    console.log('launch URL', launchDataForm.url);
-                }}
-            >
-                Launch URL
-            </DropdownMenuItem>
-        ) : (
-            <DropdownMenuItem
-                className="pl-8"
-                disabled={!launchDataForm.exe}
-                onClick={(event) => {
-                    console.log('launch exe', launchDataForm.exe);
-                }}
-            >
-                Launch Process
-            </DropdownMenuItem>
-        )}
+        {launchDataForm.isWeb
+            ? (
+                <DropdownMenuItem
+                    className="pl-8"
+                    disabled={!launchDataForm.url}
+                    onClick={() => {
+                        console.log('Open URL', launchDataForm.url);
+                    }}
+                >
+                    {isLogin ? 'Open Login URL' : 'Open password change URL'}
+                </DropdownMenuItem>
+            ) : (
+                <DropdownMenuItem
+                    className="pl-8"
+                    disabled={!launchDataForm.exe}
+                    onClick={() => {
+                        console.log('launch exe', launchDataForm.exe);
+                    }}
+                >
+                    {isLogin ? 'Launch Login app' : 'Launch password change app'}
+                </DropdownMenuItem>
+            )
+        }
     </>);
 }
--- src/renderer/src/store/0-serve-atoms/8-launch-data/index.ts
+++ src/renderer/src/store/0-serve-atoms/8-launch-data/index.ts
@@ -2,51 +2,35 @@ import { atom } from "jotai";
 import { type ManiAtoms } from "@/store/2-file-mani-atoms/9-types";
 import { FormIdx } from "@/store/8-manifest";
 
-export type LaunchDataForm = {
-    url?: string;
-    exe?: string;
+export type LaunchData = {
+    url: string | undefined;
+    exe: string | undefined;
     isWeb: boolean;
 };
 
-export type LaunchData = {
-    login: LaunchDataForm;
-    cpass: LaunchDataForm;
+export type LaunchDataMain = {
+    login: LaunchData;
+    cpass: LaunchData;
 };
 
 export const launchDataAtom = atom(
     null,
-    (get, set, { maniAtoms }: { maniAtoms: ManiAtoms }): LaunchData => {
+    (get, set, { maniAtoms }: { maniAtoms: ManiAtoms }): LaunchDataMain => {
         const loginFormCtx = maniAtoms[FormIdx.login];
         const cpassFormCtx = maniAtoms[FormIdx.cpass];
 
-        let login: {
-            url?: string;
-            exe?: string;
-            isWeb: boolean;
-        } = {
-            url: undefined,
-            exe: undefined,
-            isWeb: false,
-        };
-        let cpass: {
-            url?: string;
-            exe?: string;
-            isWeb: boolean;
-        } = {
-            url: undefined,
-            exe: undefined,
-            isWeb: false,
-        };
+        let login: LaunchData = { url: undefined, exe: undefined, isWeb: false };
+        let cpass: LaunchData = { url: undefined, exe: undefined, isWeb: false };
 
         if (loginFormCtx) {
-            login.url = get(loginFormCtx.options.p2Detect.murlAtom).data;
+            login.url = get(loginFormCtx.options.p2Detect.ourlAtom).data;
             login.exe = get(loginFormCtx.options.p2Detect.processnameAtom).data;
             login.isWeb = get(loginFormCtx.options.isWebAtom);
             
         }
 
         if (cpassFormCtx) {
-            cpass.url = get(cpassFormCtx.options.p2Detect.murlAtom).data;
+            cpass.url = get(cpassFormCtx.options.p2Detect.ourlAtom).data;
             cpass.exe = get(cpassFormCtx.options.p2Detect.processnameAtom).data;
             cpass.isWeb = get(cpassFormCtx.options.isWebAtom);
         }
