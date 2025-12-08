ab61ad1908f84a880887d2cce2fe709dd39077ff
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-08 15:49:47 -0800

    WIP. electron. gemini3: 3. add block on second instance run. 2

--- src/shell/app/0-app-ready/0-all-start.ts
+++ src/shell/app/0-app-ready/0-all-start.ts
@@ -5,9 +5,15 @@ import { iniFileOptions } from "@shell/1-start-main-window/8-ini-file-options";
 
 const gotTheLock = app.requestSingleInstanceLock();
 
-if (!gotTheLock) {
-    app.quit();
-} else {
+RunApp();
+
+function RunApp() {
+    if (!gotTheLock) {
+        console.log('no.lock.second-instance.running');
+        app.quit();
+        return;
+    }
+    
     app.on('second-instance', (_event, _commandLine, _workingDirectory) => {
         // Someone tried to run a second instance, we should focus our window.
         if (appWindow.wnd) {
@@ -17,22 +23,22 @@ if (!gotTheLock) {
             appWindow.wnd.focus();
         }
     });
-
+    
     // This method will be called when Electron has finished initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
     app.whenReady().then(
         () => {
             // Set app user model id for windows
             electronApp.setAppUserModelId('com.electron');
-
+    
             connect_ListenersForCallFromRenderer();
-
+    
             // IPC test
             // ipcMain.on('ping', () => console.log('pong'));
-
+    
             iniFileOptions.load();
             createAppWindow();
-
+    
             setAppListeners();
         }
     );
