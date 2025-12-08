3d22a0b15c35a93deaa0db7f0175ac2f1429e9a8
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-08 15:36:37 -0800

    WIP. electron. gemini3: 3. add block on second instance run

--- src/shell/app/0-app-ready/0-all-start.ts
+++ src/shell/app/0-app-ready/0-all-start.ts
@@ -1,26 +1,42 @@
 import { app } from "electron";
 import { electronApp } from "@electron-toolkit/utils";
-import { connect_ListenersForCallFromRenderer, createAppWindow, setAppListeners } from "../1-start-main-window";
+import { connect_ListenersForCallFromRenderer, createAppWindow, setAppListeners, appWindow } from "../1-start-main-window";
 import { iniFileOptions } from "@shell/1-start-main-window/8-ini-file-options";
 
-// This method will be called when Electron has finished initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(
-    () => {
-        // Set app user model id for windows
-        electronApp.setAppUserModelId('com.electron');
+const gotTheLock = app.requestSingleInstanceLock();
 
-        connect_ListenersForCallFromRenderer();
+if (!gotTheLock) {
+    app.quit();
+} else {
+    app.on('second-instance', (_event, _commandLine, _workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (appWindow.wnd) {
+            if (appWindow.wnd.isMinimized()) {
+                appWindow.wnd.restore();
+            }
+            appWindow.wnd.focus();
+        }
+    });
 
-        // IPC test
-        // ipcMain.on('ping', () => console.log('pong'));
+    // This method will be called when Electron has finished initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.whenReady().then(
+        () => {
+            // Set app user model id for windows
+            electronApp.setAppUserModelId('com.electron');
 
-        iniFileOptions.load();
-        createAppWindow();
+            connect_ListenersForCallFromRenderer();
 
-        setAppListeners();
-    }
-);
+            // IPC test
+            // ipcMain.on('ping', () => console.log('pong'));
+
+            iniFileOptions.load();
+            createAppWindow();
+
+            setAppListeners();
+        }
+    );
+}
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
