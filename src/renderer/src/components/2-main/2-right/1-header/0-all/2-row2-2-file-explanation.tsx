533da5432ae2e7df3ccda32142e565bd06a1f83a
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-07 18:23:41 -0800

    WIP. launch. gemini3: switch to launchDataAtom. 8. add LaunchOrOpenIcons

--- src/renderer/src/components/2-main/2-right/1-header/0-all/2-row2-2-file-explanation.tsx
+++ src/renderer/src/components/2-main/2-right/1-header/0-all/2-row2-2-file-explanation.tsx
@@ -3,28 +3,14 @@ import { type FileUs } from "@/store/store-types";
 import { launchDataIdx, safeManiAtoms } from "@/store/2-file-mani-atoms";
 import { isAnyManual } from "@/store/8-manifest";
 import { SymbolOpenLink } from "@/ui/icons";
+import { LaunchDataAll } from "@/store/0-serve-atoms/8-launch-data/9-launch-types";
 
 export function Row2_Explanation({ fileUs }: { fileUs: FileUs; }) {
     const maniAtoms = safeManiAtoms(useAtomValue(fileUs.maniAtomsAtom));
     const launchData = useAtomValue(maniAtoms[launchDataIdx]);
 
     const { meta } = fileUs.parsedSrc;
-
-    const loginUrl = launchData.login.url;
-    const cpassUrl = launchData.cpass.url;
-
-    const loginDomain = launchData.loginDomain;
-
     const prefix = launchData.login.isWeb ? 'The login is defined for' : isAnyManual(meta) ? 'Manually defined login for a Windows application' : 'Login for a Windows application';
-
-    if (launchData.login.isWeb) {
-    } else {
-    }
-
-    if (launchData.cpass.isWeb) {
-    } else {
-
-    }
 
     // const loginUrl = meta?.[0]?.mani?.detection?.web_ourl || domainName; // open domain in browser if url is not defined
     // const cpassUrl = meta?.[1]?.mani?.detection?.web_ourl;
@@ -36,18 +22,37 @@ export function Row2_Explanation({ fileUs }: { fileUs: FileUs; }) {
                 {prefix}
             </span>
 
-            {loginUrl && (
-                <DomainAndOpenIcon url={loginUrl} anchorText={loginDomain} title="Open the login site" />
-            )}
-
-            {cpassUrl && cpassUrl !== loginUrl && (
-                <DomainAndOpenIcon url={cpassUrl} title="Open the password change site" />
-            )}
+            <LaunchOrOpenIcons launchData={launchData} />
         </div>
     );
 }
 
+function LaunchOrOpenIcons({ launchData }: { launchData: LaunchDataAll; }) {
+    const loginUrl = launchData.login.url;
+    const cpassUrl = launchData.cpass.url;
+
+    const loginDomain = launchData.loginDomain;
+
+    if (launchData.login.isWeb) {
+    } else {
+    }
+
+    if (launchData.cpass.isWeb) {
+    } else {
+
+    }
+
+    return (<>
+        <DomainAndOpenIcon url={loginUrl} anchorText={loginDomain} title="Open the login site" />
+
+        <DomainAndOpenIcon url={cpassUrl} title="Open the password change site" />
+    </>);
+}
+
 function DomainAndOpenIcon({ anchorText, url, title }: { anchorText?: string; url: string | undefined; title: string; }) {
+    if (!url) {
+        return null;
+    }
     return (
         <a href={url} className={DomainAndOpenIconClasses} target="_blank" rel="noreferrer noopener" title={title}>
             {anchorText}
