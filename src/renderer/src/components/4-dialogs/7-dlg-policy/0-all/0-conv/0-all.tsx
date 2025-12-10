f3b512cbf10defb2c64cc80c645219305e9c027d
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-09 18:58:55 -0800

    WIP. to-from-atoms. gemini3: 3. update layout

--- src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/0-conv/0-all.tsx
+++ src/renderer/src/components/4-dialogs/7-dlg-policy/0-all/0-conv/0-all.tsx
@@ -1,5 +1,5 @@
-export * from "./1-for-atoms";
-export * from "./2-create-atoms";
+export * from "./2-to-atoms-from-meta";
+export * from "./1-create-atoms";
 export * from "./3-from-atoms";
 export * from "./4-comparison";
 export * from "./5-values-to-atoms";
