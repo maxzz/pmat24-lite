f226a9aeab278777b00480e662e23fa5eb58991b
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-05 19:41:15 -0800

    WIP. cpass. reactivity with login fields for dropdown as atomFamily

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/6-column-policy-selector/7-col-link-to-cpass.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/6-column-policy-selector/7-col-link-to-cpass.tsx
@@ -1,4 +1,5 @@
 import { atom, useAtomValue, useSetAtom } from "jotai";
+import { atomFamily } from "jotai-family";
 import { classNames } from "@/utils";
 import { FieldTyp } from "@/store/8-manifest";
 import { type FieldRowCtx, type FileUsCtx, buildLoginFieldsDropdownAtom, getAllFormsFields_byFileUsCtx } from "@/store/2-file-mani-atoms";
@@ -9,6 +10,9 @@ export function Case_LinkToLoginForm({ rowCtx, fileUsCtx }: { rowCtx: FieldRowCt
     const rindexUuid = useAtomValue(rfieldUuidAtom);
     const setRefUuid = useSetAtom(onSetRefUuidAtom);
     const dropdownAllItems = useSetAtom(buildLoginFieldsDropdownAtom)(rowCtx, fileUsCtx);
+
+    const dropdownNames = useAtomValue(getDropdownNamesAtom(fileUsCtx));
+    console.log('dropdownNames', dropdownNames);
 
     function onValueChange(value: string) {
         setRefUuid(rowCtx, value, fileUsCtx);
@@ -49,3 +53,11 @@ function getInPswFormFieldIdx(uuid: number, fileUsCtx: FileUsCtx, getOnly: GetOn
     const thisIdx = cpassPasswords.findIndex((field) => field.metaField.uuid === uuid);
     return thisIdx;
 }
+
+export const getDropdownNamesAtom = atomFamily((fileUsCtx: FileUsCtx) =>
+    atom((get) => {
+        const loginFields = getAllFormsFields_byFileUsCtx(fileUsCtx, { get }).login;
+        const loginPasswords = loginFields.filter((field) => get(field.typeAtom) === FieldTyp.psw);
+        return loginPasswords.map((field) => get(field.labelAtom) || "Without a label");
+    })
+);
