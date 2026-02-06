e2977165fe699cee15e69ee2324f16b8e560298c
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-05 18:57:32 -0800

    WIP. cpass. validity when not enough checked. 2

--- src/renderer/src/store/0-serve-atoms/3-do-save-mani-atom/1-stop-if-validation-failed/7-verify-total-count.ts
+++ src/renderer/src/store/0-serve-atoms/3-do-save-mani-atom/1-stop-if-validation-failed/7-verify-total-count.ts
@@ -2,7 +2,17 @@ import { FormIdx, FieldTyp } from "@/store/8-manifest";
 import { type VerifyError, type ManiTabValue, type FieldRowCtx } from "@/store/2-file-mani-atoms";
 
 export function getTotalCountError(fieldRowCtxs: FieldRowCtx[], formIdx: FormIdx, get: Getter): VerifyError[] | undefined {
-    const totalCount = totalFieldsInUse(fieldRowCtxs, get);
+    const totalCount: TotalCount = {
+        nUseItAny: 0,
+        nUseItPsw: 0,
+        nLinkedCurPsw: 0,
+        nLinkedNewPsw: 0,
+    };
+
+    fieldRowCtxs?.forEach(
+        (fieldRowCtx) => processFieldRowCtx(fieldRowCtx, totalCount, get)
+    );
+
     const rv = getTotalCountErrorMessage(totalCount, formIdx);
     return rv;
 }
@@ -15,21 +25,6 @@ type TotalCount = {
 };
 
 // 1. Counts the total number of fields in use and the number of linked fields
-
-function totalFieldsInUse(rowCtxs: FieldRowCtx[] | undefined, get: Getter): TotalCount {
-    const rv: TotalCount = {
-        nUseItAny: 0,
-        nUseItPsw: 0,
-        nLinkedCurPsw: 0,
-        nLinkedNewPsw: 0,
-    };
-
-    rowCtxs?.forEach(
-        (fieldRowCtx) => processFieldRowCtx(fieldRowCtx, rv, get)
-    );
-
-    return rv;
-}
 
 function processFieldRowCtx(fieldRowCtx: FieldRowCtx, rv: TotalCount, get: Getter): void {
     const useIt = get(fieldRowCtx.useItAtom);
@@ -79,27 +74,24 @@ function getTotalCountErrorMessage(totalCount: TotalCount, formIdx: FormIdx): Ve
 
     let tab: ManiTabValue = isLogin ? 'login' : 'cpass';
 
-    if (!nUseItAny) {
-        error = 'There are no fields selected';
-    }
-
-    if (!nUseItPsw) {
-        error = 'There are no password fields selected';
-    }
+    !nUseItAny && (error = 'There are no fields selected');
+    !nUseItPsw && (error = 'There are no password fields selected');
 
     // 2. Checks for cpass form only
 
-    if (!error && !isLogin) {
-        tab = 'cpass';
+    if (!error) {
+        if (!isLogin) {
+            tab = 'cpass';
 
-        if (nLinkedCurPsw > 1) {
-            error = 'Only one field can be linked to the password entry in the login form.';
-        }
-        else if (!nLinkedCurPsw) {
-            error = 'The password change form does not contain a link to the password entry in the login form. To create a link, you must select a field, link it to the password field on the login form, and specify it as the current confirm password.';
-        }
-        else if (!nLinkedNewPsw) {
-            error = 'The password change form does not contain links to the login form indicating where to save the new password. To create a link, you must select a field, link it to the password field on the login form, and specify it as the new password or confirm password.';
+            if (nLinkedCurPsw > 1) {
+                error = 'Only one field can be linked to the password entry in the login form.';
+            }
+            else if (!nLinkedCurPsw) {
+                error = 'The password change form does not contain a link to the password entry in the login form. To create a link, you must select a field, link it to the password field on the login form, and specify it as the current confirm password.';
+            }
+            else if (!nLinkedNewPsw) {
+                error = 'The password change form does not contain links to the login form indicating where to save the new password. To create a link, you must select a field, link it to the password field on the login form, and specify it as the new password or confirm password.';
+            }
         }
     }
 
