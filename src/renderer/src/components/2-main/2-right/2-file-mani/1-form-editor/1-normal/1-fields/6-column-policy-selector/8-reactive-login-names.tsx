74e1a804fd3a11c233c8afc83fa72760ee093764
Max Zakharzhevskiy <maxzz@msn.com>
2026-02-05 20:18:34 -0800

    WIP. cpass. reactivity with login fields for dropdown. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/6-column-policy-selector/8-reactive-login-names.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/6-column-policy-selector/8-reactive-login-names.tsx
@@ -4,9 +4,21 @@ import { atomFamily } from "jotai-family";
 import { FieldTyp } from "pm-manifest";
 
 export const getDropdownNamesAtom = atomFamily(
-    (fileUsCtx: FileUsCtx) => atom((get) => {
-        const loginFields = getAllFormsFields_byFileUsCtx(fileUsCtx, { get }).login;
-        const loginPasswords = loginFields.filter((field) => get(field.typeAtom) === FieldTyp.psw);
-        return loginPasswords.map((field) => get(field.labelAtom) || "Without a label");
-    })
+    (fileUsCtx: FileUsCtx) => atom(
+        (get) => {
+            const loginFields = getAllFormsFields_byFileUsCtx(fileUsCtx, { get }).login;
+            
+            const loginPasswords = loginFields.filter(
+                (field) => get(field.typeAtom) === FieldTyp.psw
+            );
+
+            const rv = loginPasswords.map(
+                (field) => [get(field.labelAtom) || "Without a label", `${field.metaField.uuid}`]
+            );
+
+            console.log('getDropdownNamesAtom', JSON.stringify(rv));
+
+            return rv;
+        }
+    )
 );
