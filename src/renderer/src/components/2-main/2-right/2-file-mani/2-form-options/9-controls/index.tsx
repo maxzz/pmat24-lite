77a93208413ecff1c6744303320053f2ef8f8822
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-02 15:16:31 -0700

    WIP. lock-fileds. 5. ---------- big clean up of nun controls

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/index.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/index.tsx
@@ -1,6 +1,3 @@
 export * from "./4-ext-policies";
-export * from "./nun-controls/1-nun-section-header";
-export * from "./nun-controls/3-nun-button-sliders";
-export * from "./nun-controls/ui-accordion";
 
 //TODO: add RowString, RowBoolean, RowNumber, SectionBody: ml-0
--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/9-controls/nun-controls/1-nun-section-header/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from "./1-nun-section-title";
-export * from "./2-nun-sub-section-title";
