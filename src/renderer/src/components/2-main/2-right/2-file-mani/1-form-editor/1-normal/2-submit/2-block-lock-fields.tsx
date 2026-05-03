954599561e967116064446070c8db540fac1d0a1
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-03 14:40:59 -0700

    WIP. lock-fileds. 5. code review

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/2-submit/2-block-lock-fields.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/2-submit/2-block-lock-fields.tsx
@@ -5,6 +5,7 @@ import { ChildrenWithLabel2Cols, InputWithTitle2Cols, OptionAsCheckbox } from "@
 import { AuthImmSelect } from "../../../2-form-options/9-controls/5-select-controls";
 import { notice } from "@/ui/local-ui/7-toaster";
 import { SymbolLockClosed } from "@/ui/icons";
+import { classNames } from "@/utils";
 
 export function LoginLockFieldsFlag({ nFormProps }: { nFormProps: NFormProps; }) {
     const formIdx = nFormProps.nFormCtx.fileUsCtx.formIdx; // Only login form has lock fields, but old PMAT allows to lock fields for all forms
@@ -19,7 +20,7 @@ export function LoginLockFieldsFlag({ nFormProps }: { nFormProps: NFormProps; })
     const oFormProps: OFormProps = { maniAtoms: nFormProps.maniAtoms, oAllAtoms };
 
     return (
-        <div className="ml-1 mr-3 mb-4 grid grid-cols-[auto_minmax(0,1fr)] gap-x-2 gap-y-0.5 select-none">
+        <div className="ml-2 mr-3 mb-4 grid grid-cols-[auto_minmax(0,1fr)] gap-x-2 gap-y-0.5 select-none">
             <LoginLock_Guarded oFormProps={oFormProps} />
         </div>
     );
@@ -29,19 +30,27 @@ function LoginLock_Guarded({ oFormProps }: { oFormProps: OFormProps; }) {
     const { lockEnabledAtom, p3Auth } = oFormProps.oAllAtoms.options;
     const lockEnabled = useAtomValue(lockEnabledAtom);
 
+    function onLockFieldsClick() {
+        if (!lockEnabled) {
+            notice.info('The "Lock form input fields" option cannot be enabled unless the "Submit Form Data" option is selected.');
+        }
+    }
+
     return (
-        <div className="flex items-center gap-2">
+        <label className="flex items-center gap-2" onClick={onLockFieldsClick}>
             {lockEnabled
                 ? <OptionAsCheckbox stateAtom={p3Auth.lockAtom} />
-                : (<>
-                    <div className="size-4 dark-checkbox"></div>
-                    {/* (allowed only if form submission data has been selected) */}
-                </>)
+                : (
+                    <div className="flex items-center gap-1">
+                        <div className="size-4 dark-checkbox"></div>
+                        {/* unavailable */}
+                        {/* (allowed only if form submission data has been selected) */}
+                    </div>
+                )
             }
 
-            Lock out login fields
-
-        </div>
+            <span className={classNames("cursor-pointer", lockEnabled ? "" : "opacity-30 cursor-default")}>Lock form input fields</span>
+        </label>
     );
 }
 
