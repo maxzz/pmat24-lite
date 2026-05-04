a70b222289efe0329fb2d8d80d9bc00da8f0cb0b
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-03 19:25:07 -0700

    WIP. quick-link. basic UI. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-quick-link.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/1-in-form-options/3-quick-link.tsx
@@ -1,9 +1,7 @@
-import { useAtom, useAtomValue } from "jotai";
-import { classNames } from "@/utils";
 import { AccordionWithTrigger } from "@/ui/motion-primitives";
-import { FormRowChildren, InputWithTitle2Rows, SelectTm } from "@/ui/local-ui";
+import { InputWithTitle2Rows } from "@/ui/local-ui";
 import { type OFormProps } from "@/store/2-file-mani-atoms";
-import { FormIdx, type OptionTextValue } from "@/store/8-manifest";
+import { FormIdx } from "@/store/8-manifest";
 import { InFormAccordionValue } from "@/store/2-file-mani-atoms/9-types";
 
 export function BlockQuickLink({ oFormProps }: { oFormProps: OFormProps; }) {
@@ -18,22 +16,16 @@ export function BlockQuickLink({ oFormProps }: { oFormProps: OFormProps; }) {
                     </div>
                 )
                 : (
-                    <div className={textClasses}>
-                        <BlockQuickLinkContent_Guarded oFormProps={oFormProps} />
-                    </div>
+                    <BlockQuickLinkContent_Guarded oFormProps={oFormProps} />
                 )
             }
         </AccordionWithTrigger>
     );
 }
 
-// display on mini-dashboard boolean
-// quick link name string
-// quick link URL string
-//TODO: original url with % is that OK?
-
 function BlockQuickLinkContent_Guarded({ oFormProps }: { oFormProps: OFormProps; }) {
     const { qUseAtom, qNameAtom, qUrlAtom } = oFormProps.oAllAtoms.options.p4QL;
+
     return (
         <div className={textClasses}>
             <InputWithTitle2Rows
@@ -41,53 +33,25 @@ function BlockQuickLinkContent_Guarded({ oFormProps }: { oFormProps: OFormProps;
                 stateAtom={qUseAtom}
                 label="Show on mini-dashboard"
                 labelClasses="font-normal"
+                containerClasses="py-0!"
             />
             <InputWithTitle2Rows
                 stateAtom={qNameAtom}
                 label="Quick link name"
                 labelClasses="font-normal"
+                containerClasses="py-0!"
             />
             <InputWithTitle2Rows
                 asTextarea
                 stateAtom={qUrlAtom}
                 label="Quick link URL"
                 labelClasses="font-normal"
+                containerClasses="py-0!"
             />
         </div>
     );
 }
 
-const textClasses = "pl-3 pr-0.5 py-1 text-balance";
+const textClasses = "pl-6 pr-0.5 py-3 text-balance grid gap-1.5";
 
-
-/*
-export function BlockWrap_Quicklink({ oFormProps }: { oFormProps: OFormProps; }) {
-    const name = "ql";
-    const { formIdx } = oFormProps.oAllAtoms.options;
-    const open = useSnapshot(appSettings).right.mani.opened[openedName(formIdx, name)];
-
-    return (<>
-        <OptionsSubSectionTitle label="Quick link" formIdx={formIdx} name={name} />
-
-        <UiAccordion open={open}>
-            <Block3_QL atoms={oFormProps.oAllAtoms.options} />
-        </UiAccordion>
-    </>);
-}
-
-import { type OptionsState } from "@/store/2-file-mani-atoms";
-import { InputWithTitle2Cols } from "@/ui/local-ui";
-
-export function Block3_QL({ atoms }: { atoms: OptionsState.Atoms; }) {
-
-    const { qNameAtom, qUrlAtom } = atoms.p4QL;
-
-    return (<>
-        <InputWithTitle2Cols stateAtom={qNameAtom} label="Name on the mini-dashboard" /> {/* "Name displayed on the mini-dashboard" * /}
-        <InputWithTitle2Cols stateAtom={qUrlAtom} label="Quick link URL" />
-
-        {/* No need to show checkbox. We can update checkbox by content of mini-dashboard name * /}
-        {/* <InputWithTitle2Cols stateAtom={qUseAtom} label="Show on mini-dashboard" asCheckbox /> * /}
-    </>);
-}
-*/
+//TODO: original url with % is that OK?
