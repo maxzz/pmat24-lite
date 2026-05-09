6d201a33cd06f7126d2c445777e3b7298c3645de
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-08 20:46:39 -0700

    WIP. shadcn. update button imports

--- src/renderer/src/ui/shadcn/button.tsx
+++ src/renderer/src/ui/shadcn/button.tsx
@@ -1,7 +1,7 @@
-import { ButtonHTMLAttributes, forwardRef } from "react";
-import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
+import { type ButtonHTMLAttributes, forwardRef } from "react";
+import { type VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/utils";
+import { Slot } from "@radix-ui/react-slot";
 
 const defaultVariantClasses = "\
 text-sm font-medium \
