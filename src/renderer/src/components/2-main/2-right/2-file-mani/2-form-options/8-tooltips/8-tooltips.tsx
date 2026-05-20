605efbb7eab7cb9ed9097c0ade431a747ffa9674
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-19 20:22:20 -0700

    WIP. tooltips. 7. reorganize tooltips by sections

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/8-tooltips/8-tooltips.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/8-tooltips/8-tooltips.tsx
@@ -1,16 +1,34 @@
+// Options page tooltips
+
+export const manifestName      /**/ = "Specifies a managed logon name for a logon screen.";
+export const extendedAuthPolicy/**/ = "Select an additional authentication credential for this application. This additional credential will be used in conjunction with existing session policy credential(s) when accessing this application.";
 export const baloonCount       /**/ = "Specifies the number of times the balloon help is shown when logon screen displays.";
 export const autoPrompt        /**/ = "Prompts to verify your identity immediately after logon screen displays.";
 // export const autoPromptReload  /**/ = "Prompts to verify your identity upon browser reload.";
+
+// Form fields submit options tooltips
+
 export const lockoutFields     /**/ = "Prevents user from typing data in logon fields.";
-export const templateName      /**/ = "Specifies a managed logon name for a logon screen.";
-// export const userHint          /**/ = "Text shown to the user when a screen is being automatically specified.";
-// export const description       /**/ = "Text shown to the user to describe screen.";
+
+// Form detection tooltips
+
+export const windowCaption     /**/ = "Specifies the caption of the logon screen window.";
+export const monitor           /**/ = "Monitor for any changes to the screen, when the caption and/or URL may be insufficient to uniquely identify the screen.";
+
+export const matchUrl          /**/ = "Specifies rules for detecting matching URLs.";
+
+// Quick link tooltips
+
+export const optionsNeedQl     /**/ = "Specifies whether this logon screen appears in the Quick Links list.";
 export const quicklinkName     /**/ = "Specifies the name to be displayed in One Touch Menu.";
 export const quicklink         /**/ = "Specifies the URL of the page loaded by Quick Link";
-export const windowCaption     /**/ = "Specifies the caption of the logon screen window.";
+
+
+// Not used anymore
+
+// export const userHint          /**/ = "Text shown to the user when a screen is being automatically specified.";
+// export const description       /**/ = "Text shown to the user to describe screen.";
+
 // export const extMatch          /**/ = "Extended Match Fileds\n(not for a web applications)";
 // export const emuMatch          /**/ = "Terminal Emulator Screen Match\n(for terminal emulator applications only)";
 // export const matchOnOff        /**/ = "Enable URL matching\r\nIf window caption is the same for multiple Web sites, URL matching can be used to resolve any ambiguity.";
-export const matchUrl          /**/ = "Specifies rules for detecting matching URLs.";
-export const monitor           /**/ = "Monitor for any changes to the screen, when the caption and/or URL may be insufficient to uniquely identify the screen.";
-export const optionsNeedQl     /**/ = "Specifies whether this logon screen appears in the Quick Links list.";
