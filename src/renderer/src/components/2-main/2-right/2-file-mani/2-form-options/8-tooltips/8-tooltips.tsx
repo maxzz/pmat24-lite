95ef2c23c0d29a6c8202efcabf7073386704bae0
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-21 20:36:29 -0700

    WIP. tooltips. udpate more descriptions

--- src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/8-tooltips/8-tooltips.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/2-form-options/8-tooltips/8-tooltips.tsx
@@ -1,10 +1,10 @@
 // Options page tooltips
 
-export const manifestName      /**/ = "Specifies a managed logon name for a logon screen.";
+export const manifestName      /**/ = "Specifies the name of the managed login template for the login screen—exactly as it will appear in the Password Manager Admin Tool.";
 export const extendedAuthPolicy/**/ = "Select an additional authentication credential for this application. This additional credential will be used in conjunction with existing session policy credential(s) when accessing this application.";
 export const baloonCount       /**/ = "Determines how many times the tooltip is displayed when the login screen appears.";
 export const autoPrompt        /**/ = "Prompts the user to verify their identity immediately after the login screen appears.";
-export const testMode          /**/ = "Specifies whether the logon screen is in test mode.";
+export const testMode          /**/ = "Determines whether the login screen is in test or production mode (and, accordingly, whether it is accessible to users).";
 // export const autoPromptReload  /**/ = "Prompts to verify your identity upon browser reload.";
 
 // Form fields submit options tooltips
@@ -17,6 +17,10 @@ export const windowCaption     /**/ = "Specifies the caption of the logon screen
 export const monitor           /**/ = "Monitor for any changes to the screen, when the caption and/or URL may be insufficient to uniquely identify the screen.";
 
 export const matchUrl          /**/ = "Defines the URL matching method. This can be either the source URL or a regular expression. If you use a regular expression, you can remove parameters from the URL or restrict the match to a specific part of the URL. However, the domain corresponding to the original URL will always be used. For additional examples, see the tooltip for the regular expression input field.";
+
+// Icon position
+
+export const iconPosition      /**/ = "Specifies the corner of the login screen where the Password Manager icon is positioned (relative to that screen). This is useful in cases where the Password Manager icon obscures an important part of the login screen.";
 
 // Quick link tooltips
 
