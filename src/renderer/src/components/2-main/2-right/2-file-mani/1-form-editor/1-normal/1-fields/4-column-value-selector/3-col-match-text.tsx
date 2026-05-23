18e42cca9523f83d3096ac626f4a2afe35e6fdc6
Max Zakharzhevskiy <maxzz@msn.com>
2026-05-23 14:11:48 -0700

    WIP. text-match. 4, code review. 2

--- src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/4-column-value-selector/3-col-match-text.tsx
+++ src/renderer/src/components/2-main/2-right/2-file-mani/1-form-editor/1-normal/1-fields/4-column-value-selector/3-col-match-text.tsx
@@ -43,7 +43,7 @@ export function Case_ValueMatchedText({ rowCtx }: { rowCtx: FieldRowCtx; }) {
     };
 
     return (
-        <div className={classNames(inputParentClasses, inputRingClasses, !useIt && "opacity-30 cursor-pointer")}>
+        <div className={classNames(containerClasses, inputRingClasses, !useIt && "opacity-30 cursor-pointer")}>
             <div className="pl-1 pr-1.5 text-muted-foreground bg-muted border-r select-none flex items-center justify-center" title={isRegex ? "Matching as regex" : "Matching as string"}>
                 {isRegex ? <SymbolMatchRegex className="size-4" /> : <SymbolMatchString className="size-4" />}
             </div>
@@ -60,30 +60,15 @@ export function Case_ValueMatchedText({ rowCtx }: { rowCtx: FieldRowCtx; }) {
 
             <M.Root>
                 <M.Trigger asChild>
-                    <button
-                        onClick={enableRow}
-                        className="px-1.5 border-mani-border-separator border-l focus:rounded focus:outline-1 focus:-outline-offset-3 focus:outline-dashed outline-muted-foreground cursor-pointer"
-                        title={isRegex ? "Matching as regex" : "Matching as string"}
-                    >
+                    <button onClick={enableRow} className={buttonClasses} title={isRegex ? "Matching as regex" : "Matching as string"}>
                         <SymbolChevronDown className="size-4 border-muted-foreground rounded" />
                     </button>
                 </M.Trigger>
 
                 <M.Portal>
                     <M.Content className={menuContentClasses} sideOffset={4} align="end" >
-                        <M.Item className={classNames(menuItemClasses, !isRegex && "bg-accent")} onSelect={() => handleModeSelect("string")}>
-                            {!isRegex && <SymbolDot className="absolute left-1.5 size-5 fill-foreground" />}
-                            <span className="grow">
-                                Match as string
-                            </span>
-                        </M.Item>
-
-                        <M.Item className={classNames(menuItemClasses, isRegex && "bg-accent")} onSelect={() => handleModeSelect("regex")}>
-                            {isRegex && <SymbolDot className="absolute left-1.5 size-5 fill-foreground" />}
-                            <span className="grow">
-                                Match as regex
-                            </span>
-                        </M.Item>
+                        <MenuItemMode label="Match as string" selected={!isRegex} onSelect={() => handleModeSelect("string")} />
+                        <MenuItemMode label="Match as regex" selected={isRegex} onSelect={() => handleModeSelect("regex")} />
                     </M.Content>
                 </M.Portal>
             </M.Root>
@@ -91,12 +76,20 @@ export function Case_ValueMatchedText({ rowCtx }: { rowCtx: FieldRowCtx; }) {
     );
 }
 
-const inputParentClasses = "\
+function MenuItemMode({ label, selected, onSelect }: { label: string; selected: boolean; onSelect: () => void; }) {
+    return (
+        <M.Item className={classNames(menuItemClasses, selected && "bg-accent")} onSelect={onSelect}>
+            {selected && <SymbolDot className="absolute left-1.5 size-5 fill-foreground" />}
+            <span className="grow">
+                {label}
+            </span>
+        </M.Item>
+    );
+}
+
+const containerClasses = "\
 h-7 grid grid-cols-[auto_minmax(0,1fr)_auto] \
-\
-bg-mani-background \
-border-mani-border-muted border \
-\
+bg-mani-background border-mani-border-muted border \
 rounded overflow-hidden";
 
 const inputClasses = "\
@@ -104,25 +97,27 @@ pl-1 pr-2 py-3 h-7 \
 bg-mani-background text-mani-foreground \
 truncate outline-hidden";
 
+const buttonClasses = "\
+px-1.5 \
+border-mani-border-separator border-l \
+focus:rounded focus:outline-1 focus:-outline-offset-3 focus:outline-dashed \
+outline-muted-foreground cursor-pointer";
+
 const menuContentClasses = "\
 py-1 max-h-[50vh] \
 \
-text-popover-foreground bg-popover \
-\
-border-mani-border border \
+text-popover-foreground bg-popover border-mani-border border \
 \
 radix-side-top:animate-slide-up \
 radix-side-bottom:animate-slide-down \
 \
 rounded-lg shadow-md \
-\
 overflow-auto smallscroll smallscroll-light \
 \
-grid grid-cols-1 \
-z-51"; // dialog has z-index 50, so we need to be higher
+grid grid-cols-1 z-51"; // dialog has z-index 50, so we need to be higher
 
 const menuItemClasses = "\
-relative mx-1 pl-7 pr-2 py-1.5 text-xs \
+relative mx-1 px-7 py-1.5 text-xs \
 \
 text-accent-foreground \
 \
